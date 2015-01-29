3d1fc0b36a342213e2acd1533e57dc0fac6ed3e1
cgatti2co <christophe.gatti@thalesgroup.com>
2015-01-29 19:18:11 +0100

    [173] Take into account IPZilla review remarks.
    Reverted copyright headers to conform to original files and changed a
    method comment for more readability.
    
    Bug: 173
    Change-Id: I2cd7911ee754f8d1ed1b03d63a7812a9845d3c38
    Signed-off-by: cgatti2co <christophe.gatti@thalesgroup.com>

--- core/plugins/org.polarsys.capella.core.platform.sirius.ui.perspective/src/org/polarsys/capella/core/platform/sirius/ui/perspective/CapellaSplashHandler.java
+++ core/plugins/org.polarsys.capella.core.platform.sirius.ui.perspective/src/org/polarsys/capella/core/platform/sirius/ui/perspective/CapellaSplashHandler.java
@@ -1,12 +1,14 @@
 /*******************************************************************************
- * Copyright (c) 2006, 2014 THALES GLOBAL SERVICES.
+ * Copyright (c) 2007, 2014 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v10.html
- *  
+ *
  * Contributors:
- *    Thales - initial API and implementation
+ *     IBM Corporation - initial API and implementation
+ *     Lars Vogel <Lars.Vogel@vogella.com> - Bug 430848
+ *     Thales - Contributor 
  *******************************************************************************/
 package org.polarsys.capella.core.platform.sirius.ui.perspective;
 
--- core/plugins/org.polarsys.capella.core.platform.sirius.ui.project/src/org/polarsys/capella/core/platform/sirius/ui/project/internal/WizardNewProjectCreationPage.java
+++ core/plugins/org.polarsys.capella.core.platform.sirius.ui.project/src/org/polarsys/capella/core/platform/sirius/ui/project/internal/WizardNewProjectCreationPage.java
@@ -1,12 +1,15 @@
 /*******************************************************************************
- * Copyright (c) 2006, 2014 THALES GLOBAL SERVICES.
+ * Copyright (c) 2002, 2015 Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v10.html
  *  
  * Contributors:
- *    Thales - initial API and implementation
+ *    IBM - Initial API and implementation 
+ *    Jakub Jurkiewicz - Fix for Bug 174737
+ *    Oakland Software Incorporated - Bug 224997 [Workbench]
+ *    Thales - Contributor
  *******************************************************************************/
 package org.polarsys.capella.core.platform.sirius.ui.project.internal;
 
