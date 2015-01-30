ab8f201eb703685ae17863da08ac56269be167b5
Aurelien Didier <aurelien.didier51@gmail.com>
2015-01-30 13:59:38 +0100

    [158] BQ correction : DoActivity/Entry/Exit, Effect/Triggers, FE to CE allocation
    Change-Id: I061306afd90ce4c5587941e78e1cbdca85228bd0
    Signed-off-by: Aurelien Didier <aurelien.didier51@gmail.com>

--- core/plugins/org.polarsys.capella.core.data.business.queries/src/org/polarsys/capella/core/business/queries/queries/capellacommon/GetAvailable_AbstractStateProperties.java
+++ core/plugins/org.polarsys.capella.core.data.business.queries/src/org/polarsys/capella/core/business/queries/queries/capellacommon/GetAvailable_AbstractStateProperties.java
@@ -15,21 +15,19 @@ import java.util.Collection;
 import java.util.List;
 
 import org.eclipse.emf.common.util.TreeIterator;
+import org.eclipse.emf.ecore.EObject;
 import org.eclipse.emf.ecore.util.EcoreUtil;
-import org.polarsys.capella.common.data.behavior.AbstractEvent;
 import org.polarsys.capella.common.data.modellingcore.IState;
 import org.polarsys.capella.common.queries.AbstractQuery;
 import org.polarsys.capella.common.queries.interpretor.QueryInterpretor;
 import org.polarsys.capella.common.queries.queryContext.IQueryContext;
-import org.polarsys.capella.core.business.queries.QueryConstants;
 import org.polarsys.capella.core.data.capellacommon.State;
 import org.polarsys.capella.core.data.capellacore.CapellaElement;
 import org.polarsys.capella.core.data.cs.BlockArchitecture;
 import org.polarsys.capella.core.data.cs.Component;
-import org.polarsys.capella.core.data.fa.AbstractFunction;
+import org.polarsys.capella.core.data.information.Class;
 import org.polarsys.capella.core.data.information.ExchangeItem;
 import org.polarsys.capella.core.data.information.Operation;
-import org.polarsys.capella.core.data.interaction.Event;
 import org.polarsys.capella.core.model.helpers.BlockArchitectureExt;
 import org.polarsys.capella.core.model.helpers.ComponentExt;
 import org.polarsys.capella.core.model.helpers.SystemEngineeringExt;
@@ -43,6 +41,7 @@ public class GetAvailable_AbstractStateProperties extends AbstractQuery {
     BlockArchitecture arch = SystemEngineeringExt.getRootBlockArchitecture(inputElement);
     if (arch != null) {
       for (BlockArchitecture block : BlockArchitectureExt.getAllAllocatedArchitectures(arch)) {
+    	  
         TreeIterator<Object> allContents = EcoreUtil.getAllContents(block, false);
         while (allContents.hasNext()) {
           Object object = allContents.next();
@@ -51,18 +50,18 @@ public class GetAvailable_AbstractStateProperties extends AbstractQuery {
           }
         }
       }
-          TreeIterator<Object> allContents = EcoreUtil.getAllContents(arch, false);
-          while (allContents.hasNext()) {
-            Object object = allContents.next();
-            if ((object instanceof AbstractEvent) && !(object instanceof Event) && !(object instanceof AbstractFunction)) {
-              availableElements.add((CapellaElement) object);
-            }
-          }
+      EObject eContainer = inputElement.eContainer();
+      if (eContainer != null) {
+        while (!(eContainer instanceof Component) && !(eContainer instanceof Class)) {
+          eContainer = eContainer.eContainer();
         }
+        if ((eContainer instanceof Component) && (inputElement instanceof State)){
+        availableElements.addAll(getElementsFromComponentAndSubComponents((Component) eContainer));
+        }
+      }
+    }
     
     if (inputElement instanceof IState) {
-      List<CapellaElement> availableFunctions = QueryInterpretor.executeQuery(QueryConstants.GET_CURRENT__MODE__AVAILABLE_IN_STATES, inputElement, context);//$NON-NLS-1$
-      availableElements.addAll(availableFunctions);
       List<CapellaElement> currentElements = QueryInterpretor.executeQuery("GetCurrent_AbstractStateProperties", inputElement, context);//$NON-NLS-1$
       availableElements.removeAll(currentElements);
     }
@@ -73,12 +72,12 @@ public class GetAvailable_AbstractStateProperties extends AbstractQuery {
      * @param component_p
      * @return
      */
-    private List<CapellaElement> getElementsFromComponentAndAncestors(State state_p, Component component_p) {
+    private List<CapellaElement> getElementsFromComponentAndSubComponents(Component component_p) {
       List<CapellaElement> availableElements = new ArrayList<CapellaElement>(1);
-      Collection<Component> componentAncestors = ComponentExt.getComponentAncestors(component_p);
-      componentAncestors.add(component_p);
+      Collection<Component> subComponents = ComponentExt.getSubDefinedComponents(component_p);
+      subComponents.add(component_p);
 
-      for (Component component : componentAncestors) {
+      for (Component component : subComponents) {
         availableElements.addAll(component.getAllocatedFunctions());
       }
       return availableElements;
