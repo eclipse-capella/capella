a280590f3229d3903f6a966237c2fa7ef4a83db2
Joao Barata <jean.barata@thalesgroup.com>
2015-02-10 16:21:38 +0100

    [184] Initial commit of test framework prototype
    Bug: 184
    Change-Id: I95c5ff79cb5b39d6fa68e1706f12f59c9734ce5a
    Signed-off-by: Joao Barata <jean.barata@thalesgroup.com>

--- tests/plugins/org.polarsys.capella.test.business.queries.ju/src/org/polarsys/capella/test/business/queries/ju/testSuites/BusinessQueryTestSuite.java
+++ tests/plugins/org.polarsys.capella.test.business.queries.ju/src/org/polarsys/capella/test/business/queries/ju/testSuites/BusinessQueryTestSuite.java
@@ -16,6 +16,24 @@ import java.util.List;
 import junit.framework.Test;
 
 import org.polarsys.capella.test.business.queries.ju.testcases.sysmodel.cs.Component_ImplementedInterfaces;
+import org.polarsys.capella.test.business.queries.ju.testcases.sysmodel.cs.ExchangeItemAllocation_AllocatedItem;
+import org.polarsys.capella.test.business.queries.ju.testcases.sysmodel.cs.InterfaceImpl_ImplementedInterface;
+import org.polarsys.capella.test.business.queries.ju.testcases.sysmodel.cs.InterfaceUse_UsedInterface;
+import org.polarsys.capella.test.business.queries.ju.testcases.sysmodel.cs.Interface_ExchangeItems;
+import org.polarsys.capella.test.business.queries.ju.testcases.sysmodel.cs.Interface_InheritedInterfaces;
+import org.polarsys.capella.test.business.queries.ju.testcases.sysmodel.cs.Part_DeployedElements;
+import org.polarsys.capella.test.business.queries.ju.testcases.sysmodel.cs.Part_MaxCardinality;
+import org.polarsys.capella.test.business.queries.ju.testcases.sysmodel.cs.Part_MinCardinality;
+import org.polarsys.capella.test.business.queries.ju.testcases.sysmodel.cs.Part_Type;
+import org.polarsys.capella.test.business.queries.ju.testcases.sysmodel.cs.PhysicalLinkCategory_Links;
+import org.polarsys.capella.test.business.queries.ju.testcases.sysmodel.cs.PhysicalLink_AllocatedComponentExchanges;
+import org.polarsys.capella.test.business.queries.ju.testcases.sysmodel.cs.PhysicalLink_Categories;
+import org.polarsys.capella.test.business.queries.ju.testcases.sysmodel.cs.PhysicalLink_RealizedPhysicalLinks;
+import org.polarsys.capella.test.business.queries.ju.testcases.sysmodel.cs.PhysicalPath_AllocatedComponentExchanges;
+import org.polarsys.capella.test.business.queries.ju.testcases.sysmodel.cs.PhysicalPath_RealizedPhysicalPaths;
+import org.polarsys.capella.test.business.queries.ju.testcases.sysmodel.cs.PhysicalPort_AllocatedComponentPorts;
+import org.polarsys.capella.test.business.queries.ju.testcases.sysmodel.cs.PhysicalPort_AllocatedFunctionPorts;
+import org.polarsys.capella.test.business.queries.ju.testcases.sysmodel.cs.PhysicalPort_RealizedPhysicalPorts;
 import org.polarsys.capella.test.framework.api.BasicTestSuite;
 
 /**
@@ -30,12 +48,29 @@ public class BusinessQueryTestSuite extends BasicTestSuite {
     return new BusinessQueryTestSuite();
   }
 
-	@Override
-	protected List<? extends Test> getTests() {
-		List<Test> tests = new ArrayList<Test>();
-		tests.add(new Component_ImplementedInterfaces());
-		return tests;
-	}
-	
+  @Override
+  protected List<? extends Test> getTests() {
+    List<Test> tests = new ArrayList<Test>();
+    tests.add(new Component_ImplementedInterfaces());
+    tests.add(new ExchangeItemAllocation_AllocatedItem());
+    tests.add(new InterfaceImpl_ImplementedInterface());
+    tests.add(new InterfaceUse_UsedInterface());
+    tests.add(new Interface_ExchangeItems());
+    tests.add(new Interface_InheritedInterfaces());
+    tests.add(new Part_DeployedElements());
+    tests.add(new Part_MaxCardinality());
+    tests.add(new Part_MinCardinality());
+    tests.add(new Part_Type());
+    tests.add(new PhysicalLinkCategory_Links());
+    tests.add(new PhysicalLink_AllocatedComponentExchanges());
+    tests.add(new PhysicalLink_Categories());
+    tests.add(new PhysicalLink_RealizedPhysicalLinks());
+    tests.add(new PhysicalPath_AllocatedComponentExchanges());
+    tests.add(new PhysicalPath_RealizedPhysicalPaths());
+    tests.add(new PhysicalPort_AllocatedComponentPorts());
+    tests.add(new PhysicalPort_AllocatedFunctionPorts());
+    tests.add(new PhysicalPort_RealizedPhysicalPorts());
+    return tests;
+  }
 
 }
