f65efa2952ddc994148b36d6a5180cff844a78fd
Philippe DUL <philippe.dul@thalesgroup.com>
2015-02-23 17:36:37 +0100

    [218] Update SiriusSessionFactory with latest Sirius version of SessionFactoryImpl
    Bug: 218
    Change-Id: Ia4441ec35d623160fc0b4c910278dca42308af83
    Task-Url: https://polarsys.org/bugs/show_bug.cgi?id=218
    Signed-off-by: Philippe DUL <philippe.dul@thalesgroup.com>

--- common/plugins/org.polarsys.capella.common.platform.sirius.ted/src/org/polarsys/capella/common/platform/sirius/ted/SiriusSessionFactory.java
+++ common/plugins/org.polarsys.capella.common.platform.sirius.ted/src/org/polarsys/capella/common/platform/sirius/ted/SiriusSessionFactory.java
@@ -12,7 +12,6 @@
 package org.polarsys.capella.common.platform.sirius.ted;
 
 import java.io.IOException;
-import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collection;
@@ -46,12 +45,13 @@ import org.eclipse.sirius.tools.internal.resource.ResourceSetUtil;
 import org.eclipse.sirius.viewpoint.DAnalysis;
 import org.eclipse.sirius.viewpoint.SiriusPlugin;
 import org.eclipse.sirius.viewpoint.ViewpointFactory;
+import org.eclipse.sirius.viewpoint.description.util.DescriptionResourceImpl;
 import org.polarsys.capella.common.mdsofa.common.helper.ExtensionPointHelper;
 
 /**
  * This class is a fork of {@link SessionFactoryImpl}.<br>
  * The original class is final and cannot be override.<br>
- * The fork has been done to allow overriding {@link DAnalysisSessionImpl#getSemanticResources()}. 
+ * The fork has been done to allow overriding {@link DAnalysisSessionImpl#getSemanticResources()}.
  */
 public class SiriusSessionFactory implements SessionFactory {
 
@@ -63,7 +63,8 @@ public class SiriusSessionFactory implements SessionFactory {
   }
 
   /**
-   * @see org.eclipse.sirius.business.api.session.factory.SessionFactory#createSession(org.eclipse.emf.common.util.URI, org.eclipse.core.runtime.IProgressMonitor)
+   * @see org.eclipse.sirius.business.api.session.factory.SessionFactory#createSession(org.eclipse.emf.common.util.URI,
+   *      org.eclipse.core.runtime.IProgressMonitor)
    */
   @Override
   public Session createSession(URI sessionResourceURI, IProgressMonitor monitor) throws CoreException {
@@ -74,106 +75,42 @@ public class SiriusSessionFactory implements SessionFactory {
     // editing domain creation which could provide its own resource set.
     transactionalEditingDomain.getResourceSet().eAdapters().add(new AirDCrossReferenceAdapterImpl());
     if (Movida.isEnabled()) {
-        transactionalEditingDomain.getResourceSet()
-                .setURIConverter(new ViewpointURIConverter((ViewpointRegistry) org.eclipse.sirius.business.api.componentization.ViewpointRegistry.getInstance()));
+      transactionalEditingDomain.getResourceSet().setURIConverter(
+          new ViewpointURIConverter((ViewpointRegistry) org.eclipse.sirius.business.api.componentization.ViewpointRegistry.getInstance()));
     }
 
+    if (set instanceof ResourceSetImpl) {
+      ResourceSetImpl resourceSetImpl = (ResourceSetImpl) set;
+      new ResourceSetImpl.MappedResourceLocator(resourceSetImpl);
+    }
+
+    set.getLoadOptions().put(DescriptionResourceImpl.OPTION_USE_URI_FRAGMENT_AS_ID, true);
+
     // Create or load the session.
-    boolean alreadyExistingResource = exists(sessionResourceURI, transactionalEditingDomain.getResourceSet());
+    boolean alreadyExistingResource = set.getURIConverter().exists(sessionResourceURI, null);
     Session session = null;
     if (alreadyExistingResource) {
-        session = loadSessionModelResource(sessionResourceURI, transactionalEditingDomain, monitor);
+      session = loadSessionModelResource(sessionResourceURI, transactionalEditingDomain, monitor);
     } else {
-        session = createSessionResource(sessionResourceURI, transactionalEditingDomain, monitor);
+      session = createSessionResource(sessionResourceURI, transactionalEditingDomain, monitor);
     }
     return session;
   }
 
-  /**
-   * Used to check if a Resource exists at this URI with EMF 2.3 since from
-   * EMF 2.4 URIConverter.exists(URI) can be used.
-   * 
-   * @param sessionResourceURI
-   *            the URI of the Resource to which checks existence
-   * @param resourceSet
-   * @return true if a Resource exists at this URI
-   */
-  private boolean exists(URI sessionResourceURI, ResourceSet resourceSet) {
-      boolean exists = false;
-      InputStream inputStream = null;
-      try {
-          inputStream = resourceSet.getURIConverter().createInputStream(sessionResourceURI);
-          if (inputStream != null) {
-              exists = true;
-          }
-      } catch (IOException e) {
-          // Do nothing, we consider that the resource does not exist
-      } finally {
-          if (inputStream != null) {
-              try {
-                  inputStream.close();
-              } catch (IOException e) {
-                  // Do nothing, we consider that the resource does not exist
-              }
-          }
-      }
-      return exists;
-  }
+  protected Session loadSessionModelResource(URI sessionResourceURI, TransactionalEditingDomain transactionalEditingDomain, IProgressMonitor monitor)
+      throws CoreException {
+    ResourceSet resourceSet = transactionalEditingDomain.getResourceSet();
+    // Make ResourceSet aware of resource loading with progress monitor
+    ResourceSetUtil.setProgressMonitor(resourceSet, new SubProgressMonitor(monitor, 2));
 
-  private Session loadSessionModelResource(URI sessionResourceURI, TransactionalEditingDomain transactionalEditingDomain, IProgressMonitor monitor) throws CoreException {
-      ResourceSet resourceSet = transactionalEditingDomain.getResourceSet();
-      // Make ResourceSet aware of resource loading with progress monitor
-      ResourceSetUtil.setProgressMonitor(resourceSet, new SubProgressMonitor(monitor, 2));
-
-      Session session = null;
-      try {
-          monitor.beginTask("Session loading", 4);
-          // Get resource
-          final Resource sessionModelResource = resourceSet.getResource(sessionResourceURI, true);
-          if (sessionModelResource != null) {
-              DAnalysis analysis = null;
-              if (!sessionModelResource.getContents().isEmpty() && (sessionModelResource.getContents().get(0) instanceof DAnalysis)) {
-                  analysis = (DAnalysis) sessionModelResource.getContents().get(0);
-                  session = new DAnalysisSessionImpl(analysis) {
-                    @Override
-                    public Collection<Resource> getSemanticResources() {
-                      Collection<Resource> semanticResources = new ArrayList<Resource>(super.getSemanticResources());
-                      semanticResources.addAll(getDerivedSemanticResources(transactionalEditingDomain, semanticResources));
-                      return Collections.unmodifiableCollection(semanticResources);
-                    }
-                  };
-                  monitor.worked(2);
-              } else {
-                  session = createSessionResource(sessionResourceURI, transactionalEditingDomain, new SubProgressMonitor(monitor, 2));
-              }
-          }
-      } catch (WrappedException e) {
-          throw new CoreException(new Status(IStatus.ERROR, SiriusPlugin.ID, "Error while loading representations file", e));
-      } finally {
-          monitor.done();
-          ResourceSetUtil.resetProgressMonitor(resourceSet);
-      }
-      return session;
-  }
-
-  private Session createSessionResource(final URI sessionResourceURI, final TransactionalEditingDomain transactionalEditingDomain, IProgressMonitor monitor) throws CoreException {
-      Session session = null;
-      try {
-          monitor.beginTask("Session creation", 2);
-          Resource sessionModelResource = new ResourceSetImpl().createResource(sessionResourceURI);
-          DAnalysis analysis = ViewpointFactory.eINSTANCE.createDAnalysis();
-          sessionModelResource.getContents().add(analysis);
-          try {
-              sessionModelResource.save(Collections.emptyMap());
-          } catch (IOException e) {
-              throw new CoreException(new Status(IStatus.ERROR, SiriusPlugin.ID, "session creation failed", e));
-          }
-          monitor.worked(1);
-          // Now load it from the TED
-          sessionModelResource = transactionalEditingDomain.getResourceSet().getResource(sessionResourceURI, true);
-          if (sessionModelResource.getContents().isEmpty()) {
-              throw new CoreException(new Status(IStatus.ERROR, SiriusPlugin.ID, "session creation failed: the resource content is empty."));
-          }
+    Session session = null;
+    try {
+      monitor.beginTask("Session loading", 4);
+      // Get resource
+      final Resource sessionModelResource = resourceSet.getResource(sessionResourceURI, true);
+      if (sessionModelResource != null) {
+        DAnalysis analysis = null;
+        if (!sessionModelResource.getContents().isEmpty() && (sessionModelResource.getContents().get(0) instanceof DAnalysis)) {
           analysis = (DAnalysis) sessionModelResource.getContents().get(0);
           session = new DAnalysisSessionImpl(analysis) {
             @Override
@@ -183,23 +120,64 @@ public class SiriusSessionFactory implements SessionFactory {
               return Collections.unmodifiableCollection(semanticResources);
             }
           };
-          monitor.worked(1);
-      } finally {
-          monitor.done();
+          monitor.worked(2);
+        } else {
+          session = createSessionResource(sessionResourceURI, transactionalEditingDomain, new SubProgressMonitor(monitor, 2));
+        }
       }
-      return session;
+    } catch (WrappedException e) {
+      throw new CoreException(new Status(IStatus.ERROR, SiriusPlugin.ID, "Error while loading representations file", e));
+    } finally {
+      monitor.done();
+      ResourceSetUtil.resetProgressMonitor(resourceSet);
+    }
+    return session;
+  }
+
+  protected Session createSessionResource(final URI sessionResourceURI, final TransactionalEditingDomain transactionalEditingDomain, IProgressMonitor monitor)
+      throws CoreException {
+    Session session = null;
+    try {
+      monitor.beginTask("Session creation", 2);
+      Resource sessionModelResource = new ResourceSetImpl().createResource(sessionResourceURI);
+      DAnalysis analysis = ViewpointFactory.eINSTANCE.createDAnalysis();
+      sessionModelResource.getContents().add(analysis);
+      try {
+        sessionModelResource.save(Collections.emptyMap());
+      } catch (IOException e) {
+        throw new CoreException(new Status(IStatus.ERROR, SiriusPlugin.ID, "session creation failed", e));
+      }
+      monitor.worked(1);
+      // Now load it from the TED
+      sessionModelResource = transactionalEditingDomain.getResourceSet().getResource(sessionResourceURI, true);
+      if (sessionModelResource.getContents().isEmpty()) {
+        throw new CoreException(new Status(IStatus.ERROR, SiriusPlugin.ID, "session creation failed: the resource content is empty."));
+      }
+      analysis = (DAnalysis) sessionModelResource.getContents().get(0);
+      session = new DAnalysisSessionImpl(analysis) {
+        @Override
+        public Collection<Resource> getSemanticResources() {
+          Collection<Resource> semanticResources = new ArrayList<Resource>(super.getSemanticResources());
+          semanticResources.addAll(getDerivedSemanticResources(transactionalEditingDomain, semanticResources));
+          return Collections.unmodifiableCollection(semanticResources);
+        }
+      };
+      monitor.worked(1);
+    } finally {
+      monitor.done();
+    }
+    return session;
   }
 
   /**
    * Adds new derived semantic resources that shall not be present in aird model
-   * 
    * @param editingDomain the editing domain
    * @param nonDerivedSemanticResources the resources already present in aird model
    * @return all the derived semantic resources
    */
   protected Collection<Resource> getDerivedSemanticResources(TransactionalEditingDomain editingDomain, Collection<Resource> nonDerivedSemanticResources) {
     Collection<Resource> derivedSemanticResources = new ArrayList<Resource>();
-    
+
     for (IDerivedSemanticResourceProvider provider : getAllDerivedSemanticResourceProviders()) {
       for (Resource resource : provider.getDerivedSemanticResources(editingDomain)) {
         if (!nonDerivedSemanticResources.contains(resource) && !derivedSemanticResources.contains(resource)) {
@@ -208,7 +186,7 @@ public class SiriusSessionFactory implements SessionFactory {
       }
     }
     return derivedSemanticResources;
-  }            
+  }
 
   private List<IDerivedSemanticResourceProvider> _derivedSemanticResourceProviders;
   private static final String DERIVED_SEMANTIC_RESOURCE_PROVIDER_EXTENSION_ID = "derivedSemanticResourceProvider"; //$NON-NLS-1$
@@ -220,9 +198,11 @@ public class SiriusSessionFactory implements SessionFactory {
     if (null == _derivedSemanticResourceProviders) {
       _derivedSemanticResourceProviders = new ArrayList<IDerivedSemanticResourceProvider>();
       List<IConfigurationElement> BQProvider =
-          Arrays.asList(ExtensionPointHelper.getConfigurationElements(PlatformSiriusTedActivator.getDefault().getPluginId(), DERIVED_SEMANTIC_RESOURCE_PROVIDER_EXTENSION_ID));
+          Arrays.asList(ExtensionPointHelper.getConfigurationElements(PlatformSiriusTedActivator.getDefault().getPluginId(),
+              DERIVED_SEMANTIC_RESOURCE_PROVIDER_EXTENSION_ID));
       for (IConfigurationElement configurationElement : BQProvider) {
-        IDerivedSemanticResourceProvider contrib = (IDerivedSemanticResourceProvider) ExtensionPointHelper.createInstance(configurationElement, ExtensionPointHelper.ATT_CLASS);
+        IDerivedSemanticResourceProvider contrib =
+            (IDerivedSemanticResourceProvider) ExtensionPointHelper.createInstance(configurationElement, ExtensionPointHelper.ATT_CLASS);
         if (contrib != null) {
           _derivedSemanticResourceProviders.add(contrib);
         }
