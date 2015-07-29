36897124af92d8dcd0417b8c301f6580afca6bea
Tu Ton <minhtutonthat@gmail.com>
2015-07-29 14:17:52 +0200

    [391] Required/provided interfaces and component inheritance are not taken into account when creating messages in interface scenarii. Take into account Communication Links
    Bug : 391
    
    Change-Id: Ie05ba5860bbb2e44d50f85a0a95217b2372ba3d3
    Signed-off-by: Tu Ton <minhtutonthat@gmail.com>

--- core/plugins/org.polarsys.capella.core.data.helpers/src/org/polarsys/capella/core/data/helpers/information/services/CommunicationLinkExt.java
+++ core/plugins/org.polarsys.capella.core.data.helpers/src/org/polarsys/capella/core/data/helpers/information/services/CommunicationLinkExt.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2006, 2014 THALES GLOBAL SERVICES.
+ * Copyright (c) 2006, 2015 THALES GLOBAL SERVICES.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -32,147 +32,167 @@ import org.polarsys.capella.core.data.information.communication.CommunicationLin
  */
 public class CommunicationLinkExt {
 
-  /**
-   * Change the communication link kind and protocol according to the mechanism
-   */
-  public static void changeExchangeItemMechanism(CommunicationLink link, ExchangeMechanism mechanism) {
-    boolean isSender = isSender(link);
+	/**
+	 * Change the communication link kind and protocol according to the
+	 * mechanism
+	 */
+	public static void changeExchangeItemMechanism(CommunicationLink link,
+			ExchangeMechanism mechanism) {
+		boolean isSender = isSender(link);
 
-    link.setProtocol(ExchangeMechanismExt.getProtocol(mechanism, isSender));
-    link.setKind(ExchangeMechanismExt.getKind(mechanism, isSender));
-  }
+		link.setProtocol(ExchangeMechanismExt.getProtocol(mechanism, isSender));
+		link.setKind(ExchangeMechanismExt.getKind(mechanism, isSender));
+	}
 
-  /**
-   * Change the exchange item linked by the link, and performs modifications of kind/protocol
-   */
-  public static void changeExchangeItem(CommunicationLink link, ExchangeItem new_p) {
-    link.setExchangeItem(new_p);
-    changeExchangeItemMechanism(link, new_p.getExchangeMechanism());
-  }
+	/**
+	 * Change the exchange item linked by the link, and performs modifications
+	 * of kind/protocol
+	 */
+	public static void changeExchangeItem(CommunicationLink link,
+			ExchangeItem newEI) {
+		link.setExchangeItem(newEI);
+		changeExchangeItemMechanism(link, newEI.getExchangeMechanism());
+	}
 
-  /**
-   * Retrieves all communication links for an exchanger
-   * (include generalization)
-   */
-  @SuppressWarnings({ "rawtypes", "unchecked" })
-  public static Collection<CommunicationLink> getAllCommunicationLinks(CommunicationLinkExchanger sndCpnt_p) {
-    HashSet<CommunicationLink> result = new HashSet<CommunicationLink>();
+	/**
+	 * Retrieves all communication links for an exchanger (include
+	 * generalization)
+	 */
+	@SuppressWarnings({ "rawtypes", "unchecked" })
+	public static Collection<CommunicationLink> getAllCommunicationLinks(
+			CommunicationLinkExchanger sndCpnt) {
+		HashSet<CommunicationLink> result = new HashSet<CommunicationLink>();
 
-    Collection<NamedElement> components = null;
-    if (sndCpnt_p instanceof GeneralizableElement) {
-      components = (Collection)GeneralizableElementExt.getAllSuperGeneralizableElements((GeneralizableElement)sndCpnt_p);
-      components.add((GeneralizableElement)sndCpnt_p);
-    } else {
-      components = (Collection)Collections.singleton(sndCpnt_p);
-    }
+		Collection<NamedElement> components = null;
+		if (sndCpnt instanceof GeneralizableElement) {
+			components = (Collection) GeneralizableElementExt
+					.getAllSuperGeneralizableElements((GeneralizableElement) sndCpnt);
+			components.add((GeneralizableElement) sndCpnt);
+		} else {
+			components = (Collection) Collections.singleton(sndCpnt);
+		}
 
-    for (NamedElement snd : components) {
-      if (snd!=null && snd instanceof CommunicationLinkExchanger) {
-        CommunicationLinkExchanger sndCompo = (CommunicationLinkExchanger)snd;
-        result.addAll(sndCompo.getOwnedCommunicationLinks());
-      }
-    }
-    return result;
-  }
+		for (NamedElement snd : components) {
+			if (snd != null && snd instanceof CommunicationLinkExchanger) {
+				CommunicationLinkExchanger sndCompo = (CommunicationLinkExchanger) snd;
+				result.addAll(sndCompo.getOwnedCommunicationLinks());
+			}
+		}
+		return result;
+	}
 
-  /**
-   * Filter the given list of CommunicationLink according to the given kind
-   * @return a new list
-   */
-  public static Collection<CommunicationLink> filterByKind(Collection<CommunicationLink> links, CommunicationLinkKind kind) {
-    HashSet<CommunicationLink> result = new HashSet<CommunicationLink>();
+	/**
+	 * Filter the given list of CommunicationLink according to the given kind
+	 * 
+	 * @return a new list
+	 */
+	public static Collection<CommunicationLink> filterByKind(
+			Collection<CommunicationLink> links, CommunicationLinkKind kind) {
+		HashSet<CommunicationLink> result = new HashSet<CommunicationLink>();
 
-    for (CommunicationLink snd : links) {
-      if (snd!=null && snd.getKind() == kind) {
-        result.add(snd);
-      }
-    }
-    return result;
-  }
+		for (CommunicationLink snd : links) {
+			if (snd != null && snd.getKind() == kind) {
+				result.add(snd);
+			}
+		}
+		return result;
+	}
 
-  /**
-   * Retrieve exchange items from the given list of CommuncationLink
-   */
-  public static Collection<AbstractExchangeItem> getExchangeItems(Collection<CommunicationLink> links) {
-    HashSet<AbstractExchangeItem> result = new HashSet<AbstractExchangeItem>();
+	/**
+	 * Retrieve exchange items from the given list of CommuncationLink
+	 */
+	public static Collection<AbstractExchangeItem> getExchangeItems(
+			Collection<CommunicationLink> links) {
+		HashSet<AbstractExchangeItem> result = new HashSet<AbstractExchangeItem>();
 
-    for (CommunicationLink snd : links) {
-      if (snd!=null && snd.getExchangeItem()!=null) {
-        result.add(snd.getExchangeItem());
-      }
-    }
-    return result;
-  }
+		for (CommunicationLink snd : links) {
+			if (snd != null && snd.getExchangeItem() != null) {
+				result.add(snd.getExchangeItem());
+			}
+		}
+		return result;
+	}
 
-  /**
-   * Retrieve exchange items from the given list of CommuncationLink filtered by the given kinds
-   */
-  public static Collection<AbstractExchangeItem> getExchangeItemsByKinds(Collection<CommunicationLink> links, CommunicationLinkKind[] kinds) {
-    HashSet<AbstractExchangeItem> result = new HashSet<AbstractExchangeItem>();
-    HashSet<CommunicationLinkKind> kinds_set = new HashSet<CommunicationLinkKind>();
-    for (CommunicationLinkKind kind : kinds) {
-      kinds_set.add(kind);
-    }
-    for (CommunicationLink snd : links) {
-      if (snd!=null && kinds_set.contains(snd.getKind())) {
-        result.add(snd.getExchangeItem());
-      }
-    }
-    return result;
-  }
+	/**
+	 * Retrieve exchange items from the given list of CommuncationLink filtered
+	 * by the given kinds
+	 */
+	public static Collection<AbstractExchangeItem> getExchangeItemsByKinds(
+			Collection<CommunicationLink> links, CommunicationLinkKind[] kinds) {
+		HashSet<AbstractExchangeItem> result = new HashSet<AbstractExchangeItem>();
+		HashSet<CommunicationLinkKind> kinds_set = new HashSet<CommunicationLinkKind>();
+		for (CommunicationLinkKind kind : kinds) {
+			kinds_set.add(kind);
+		}
+		for (CommunicationLink snd : links) {
+			if (snd != null && kinds_set.contains(snd.getKind())) {
+				result.add(snd.getExchangeItem());
+			}
+		}
+		return result;
+	}
 
-  /**
-   * Retrieve exchange items from the given list of CommuncationLink filtered by the given kinds
-   */
-  public static Collection<AbstractExchangeItem> getExchangeItemsByKind(Collection<CommunicationLink> links, CommunicationLinkKind kind) {
-    HashSet<AbstractExchangeItem> result = new HashSet<AbstractExchangeItem>();
+	/**
+	 * Retrieve exchange items from the given list of CommuncationLink filtered
+	 * by the given kinds
+	 */
+	public static Collection<AbstractExchangeItem> getExchangeItemsByKind(
+			Collection<CommunicationLink> links, CommunicationLinkKind kind) {
+		HashSet<AbstractExchangeItem> result = new HashSet<AbstractExchangeItem>();
 
-    for (CommunicationLink snd : links) {
-      if (snd!=null && kind.equals(snd.getKind())) {
-        result.add(snd.getExchangeItem());
-      }
-    }
-    return result;
-  }
-  
-  public static Component getSource(CommunicationLink link) {
-	  return (Component) link.eContainer();
-  }
+		for (CommunicationLink snd : links) {
+			if (snd != null && kind.equals(snd.getKind())) {
+				result.add(snd.getExchangeItem());
+			}
+		}
+		return result;
+	}
 
-  public static AbstractExchangeItem getExchangeItemsByKind(CommunicationLink link, CommunicationLinkKind kind) {
-    if (link!=null && kind.equals(link.getKind())) {
-      return link.getExchangeItem();
-    }
-    return null;
-  }
+	public static Component getSource(CommunicationLink link) {
+		return (Component) link.eContainer();
+	}
 
-  public static boolean isSender(CommunicationLink link) {
-    return link.getKind() == CommunicationLinkKind.PRODUCE
-    || link.getKind() == CommunicationLinkKind.SEND
-    || link.getKind() == CommunicationLinkKind.CALL
-    || link.getKind() == CommunicationLinkKind.WRITE
-    || link.getKind() == CommunicationLinkKind.TRANSMIT;
-  }
+	public static AbstractExchangeItem getExchangeItemsByKind(
+			CommunicationLink link, CommunicationLinkKind kind) {
+		if (link != null && kind.equals(link.getKind())) {
+			return link.getExchangeItem();
+		}
+		return null;
+	}
 
-  /**
-   * return true if communication link if of kind receiving
-   * @param link : CommunicationLink
-   * @return boolean 
-   */
-  public static boolean isReceiver(CommunicationLink link) {
-    return link.getKind() == CommunicationLinkKind.RECEIVE
-        || link.getKind() == CommunicationLinkKind.CONSUME
-        || link.getKind() == CommunicationLinkKind.EXECUTE
-        || link.getKind() == CommunicationLinkKind.ACCESS
-        || link.getKind() == CommunicationLinkKind.ACQUIRE;
-  }
+	public static boolean isSender(CommunicationLink link) {
+		return link.getKind() == CommunicationLinkKind.PRODUCE
+				|| link.getKind() == CommunicationLinkKind.SEND
+				|| link.getKind() == CommunicationLinkKind.CALL
+				|| link.getKind() == CommunicationLinkKind.WRITE
+				|| link.getKind() == CommunicationLinkKind.TRANSMIT;
+	}
 
-  /**
-   * return the list of all Communication Link contained by the given component that are senders. 
-   * @param component : Component
-   * @return List<CommunicationLink>
-   */  
-	public static List<CommunicationLink> getSenderCommunicationLink(Component component) {
+	/**
+	 * return true if communication link if of kind receiving
+	 * 
+	 * @param link
+	 *            : CommunicationLink
+	 * @return boolean
+	 */
+	public static boolean isReceiver(CommunicationLink link) {
+		return link.getKind() == CommunicationLinkKind.RECEIVE
+				|| link.getKind() == CommunicationLinkKind.CONSUME
+				|| link.getKind() == CommunicationLinkKind.EXECUTE
+				|| link.getKind() == CommunicationLinkKind.ACCESS
+				|| link.getKind() == CommunicationLinkKind.ACQUIRE;
+	}
+
+	/**
+	 * return the list of all Communication Link contained by the given
+	 * component that are senders.
+	 * 
+	 * @param component
+	 *            : Component
+	 * @return List<CommunicationLink>
+	 */
+	public static List<CommunicationLink> getSenderCommunicationLink(
+			Component component) {
 		List<CommunicationLink> senderLinks = new ArrayList<CommunicationLink>();
 		for (CommunicationLink link : component.getOwnedCommunicationLinks()) {
 			if (isSender(link)) {
@@ -182,26 +202,89 @@ public class CommunicationLinkExt {
 		return senderLinks;
 	}
 
-  /**
-   * return the list of all Communication Link contained by the given component that are receivers. 
-   * @param component : Component
-   * @return List<CommunicationLink>
-   */
-	public static List<CommunicationLink> getReceiverCommunicationLink(Component component) {
+	/**
+	 * return the list of all Communication Link contained by the given
+	 * component and its ancestors that are senders.
+	 * 
+	 * @param component
+	 *            : Component
+	 * @return List<CommunicationLink>
+	 */
+	public static List<CommunicationLink> getAllSenderCommunicationLink(
+			Component component) {
 		List<CommunicationLink> senderLinks = new ArrayList<CommunicationLink>();
-		for (CommunicationLink link : component.getOwnedCommunicationLinks()) {
-			if (isReceiver(link)) {
-				senderLinks.add(link);
-			}
+		List<GeneralizableElement> components = (List<GeneralizableElement>) GeneralizableElementExt
+				.getAllSuperGeneralizableElements(component);
+		components.add(component);
+
+		for (GeneralizableElement ge : components) {
+			if (ge instanceof Component)
+				for (CommunicationLink link : ((Component) ge)
+						.getOwnedCommunicationLinks()) {
+					if (isSender(link)) {
+						senderLinks.add(link);
+					}
+				}
 		}
 		return senderLinks;
 	}
 
-	/** True if link1 and link2 target the same Exchange Item, have the same kind and the same protocol. */
-	public static boolean isSameCommunication(CommunicationLink link1, CommunicationLink link2) {
+	/**
+	 * return the list of all Communication Link contained by the given
+	 * component that are receivers.
+	 * 
+	 * @param component
+	 *            : Component
+	 * @return List<CommunicationLink>
+	 */
+	public static List<CommunicationLink> getReceiverCommunicationLink(
+			Component component) {
+		List<CommunicationLink> receiverLinks = new ArrayList<CommunicationLink>();
+		for (CommunicationLink link : component.getOwnedCommunicationLinks()) {
+			if (isReceiver(link)) {
+				receiverLinks.add(link);
+			}
+		}
+		return receiverLinks;
+	}
+
+	/**
+	 * return the list of all Communication Link contained by the given
+	 * component and its ancestor that are receivers.
+	 * 
+	 * @param component
+	 *            : Component
+	 * @return List<CommunicationLink>
+	 */
+	public static List<CommunicationLink> getAllReceiverCommunicationLink(
+			Component component) {
+		List<CommunicationLink> receiverLinks = new ArrayList<CommunicationLink>();
+		List<GeneralizableElement> components = (List<GeneralizableElement>) GeneralizableElementExt
+				.getAllSuperGeneralizableElements(component);
+		components.add(component);
+
+		for (GeneralizableElement ge : components) {
+			if (ge instanceof Component)
+				for (CommunicationLink link : ((Component) ge)
+						.getOwnedCommunicationLinks()) {
+					if (isReceiver(link)) {
+						receiverLinks.add(link);
+					}
+				}
+		}
+
+		return receiverLinks;
+	}
+
+	/**
+	 * True if link1 and link2 target the same Exchange Item, have the same kind
+	 * and the same protocol.
+	 */
+	public static boolean isSameCommunication(CommunicationLink link1,
+			CommunicationLink link2) {
 		return link1.getExchangeItem() == link2.getExchangeItem()
 				&& link1.getProtocol() == link2.getProtocol()
 				&& link1.getKind() == link2.getKind();
 	}
-		
+
 }
--- core/plugins/org.polarsys.capella.core.data.interaction.properties/src/org/polarsys/capella/core/data/interaction/properties/dialogs/sequenceMessage/model/algorithms/SelectionAlgorithms.java
+++ core/plugins/org.polarsys.capella.core.data.interaction.properties/src/org/polarsys/capella/core/data/interaction/properties/dialogs/sequenceMessage/model/algorithms/SelectionAlgorithms.java
@@ -32,113 +32,129 @@ import org.polarsys.capella.core.model.helpers.ExchangeItemExt;
 
 public class SelectionAlgorithms {
 
-  public static List<InterfaceCommunication> getInterfaceCommunications(Component c1, Component c2) {
-    List<InterfaceCommunication> res = new ArrayList<InterfaceCommunication>();
-    List<Interface> usedInterfaces = new ArrayList<Interface>();
-    List<Interface> implementedInterfaces = new ArrayList<Interface>();
+	public static List<InterfaceCommunication> getInterfaceCommunications(
+			Component c1, Component c2) {
+		List<InterfaceCommunication> res = new ArrayList<InterfaceCommunication>();
+		List<Interface> usedInterfaces = new ArrayList<Interface>();
+		List<Interface> implementedInterfaces = new ArrayList<Interface>();
 
-    usedInterfaces.addAll(ComponentExt.getAllUsedAndRequiredInterfaces(c1));
+		usedInterfaces.addAll(ComponentExt.getAllUsedAndRequiredInterfaces(c1));
 
-    implementedInterfaces.addAll(ComponentExt.getAllImplementedAndProvidedInterfaces(c2));
+		implementedInterfaces.addAll(ComponentExt
+				.getAllImplementedAndProvidedInterfaces(c2));
 
-    for (Interface interface_ : usedInterfaces) {
-      boolean partial = !implementedInterfaces.contains(interface_);
-      for (ExchangeItemAllocation allocation : interface_.getOwnedExchangeItemAllocations()) {
-        res.add(new InterfaceCommunication(allocation, interface_, partial));
-      }
-    }
-    implementedInterfaces.removeAll(usedInterfaces);
-    for (Interface interface_ : implementedInterfaces) {
-      for (ExchangeItemAllocation allocation : interface_.getOwnedExchangeItemAllocations()) {
-        res.add(new InterfaceCommunication(allocation, interface_, true));
-      }
-    }
-    return res;
-  }
+		for (Interface interface_ : usedInterfaces) {
+			boolean partial = !implementedInterfaces.contains(interface_);
+			for (ExchangeItemAllocation allocation : interface_
+					.getOwnedExchangeItemAllocations()) {
+				res.add(new InterfaceCommunication(allocation, interface_,
+						partial));
+			}
+		}
+		implementedInterfaces.removeAll(usedInterfaces);
+		for (Interface interface_ : implementedInterfaces) {
+			for (ExchangeItemAllocation allocation : interface_
+					.getOwnedExchangeItemAllocations()) {
+				res.add(new InterfaceCommunication(allocation, interface_, true));
+			}
+		}
+		return res;
+	}
 
-  public static List<LinkCommunication> getLinkCommunications(Component c1, Component c2) {
-    // calculate function table that give for each items the targeting receiver links
-    List<ExchangeItem> itemsToProcess = new ArrayList<ExchangeItem>();
-    Hashtable<ExchangeItem, List<CommunicationLink>> item2receivers = new Hashtable<ExchangeItem, List<CommunicationLink>>();
-    for (CommunicationLink link : CommunicationLinkExt.getReceiverCommunicationLink(c2)) {
-      ExchangeItem item = link.getExchangeItem();
-      if (!itemsToProcess.contains(item)) {
-        itemsToProcess.add(item);
-      }
-      List<CommunicationLink> targetingLinks = item2receivers.get(item);
-      if (targetingLinks == null) {
-        targetingLinks = new ArrayList<CommunicationLink>();
-        item2receivers.put(item, targetingLinks);
-      }
-      targetingLinks.add(link);
-    }
-    // calculate communications based on senders
-    List<LinkCommunication> res = new ArrayList<LinkCommunication>();
-    for (CommunicationLink senderLink : CommunicationLinkExt.getSenderCommunicationLink(c1)) {
-      ExchangeItem item = senderLink.getExchangeItem();
-      itemsToProcess.remove(item);
-      if (item2receivers.containsKey(item)) {
-        for (CommunicationLink receiverLink : item2receivers.get(item)) {
-          res.add(new LinkCommunication(senderLink, item, receiverLink));
-        }
-      } else {
-        res.add(new LinkCommunication(senderLink, item, null));
-      }
-    }
-    // calculate partial communications with no sender links
-    for (ExchangeItem item : itemsToProcess) {
-      for (CommunicationLink receiverLink : item2receivers.get(item)) {
-        res.add(new LinkCommunication(null, item, receiverLink));
-      }
-    }
-    return res;
-  }
+	public static List<LinkCommunication> getLinkCommunications(Component c1,
+			Component c2) {
+		// calculate function table that give for each items the targeting
+		// receiver links
+		List<ExchangeItem> itemsToProcess = new ArrayList<ExchangeItem>();
+		Hashtable<ExchangeItem, List<CommunicationLink>> item2receivers = new Hashtable<ExchangeItem, List<CommunicationLink>>();
+		for (CommunicationLink link : CommunicationLinkExt
+				.getAllReceiverCommunicationLink(c2)) {
+			ExchangeItem item = link.getExchangeItem();
+			if (!itemsToProcess.contains(item)) {
+				itemsToProcess.add(item);
+			}
+			List<CommunicationLink> targetingLinks = item2receivers.get(item);
+			if (targetingLinks == null) {
+				targetingLinks = new ArrayList<CommunicationLink>();
+				item2receivers.put(item, targetingLinks);
+			}
+			targetingLinks.add(link);
+		}
+		// calculate communications based on senders
+		List<LinkCommunication> res = new ArrayList<LinkCommunication>();
+		for (CommunicationLink senderLink : CommunicationLinkExt
+				.getAllSenderCommunicationLink(c1)) {
+			ExchangeItem item = senderLink.getExchangeItem();
+			itemsToProcess.remove(item);
+			if (item2receivers.containsKey(item)) {
+				for (CommunicationLink receiverLink : item2receivers.get(item)) {
+					res.add(new LinkCommunication(senderLink, item,
+							receiverLink));
+				}
+			} else {
+				res.add(new LinkCommunication(senderLink, item, null));
+			}
+		}
+		// calculate partial communications with no sender links
+		for (ExchangeItem item : itemsToProcess) {
+			for (CommunicationLink receiverLink : item2receivers.get(item)) {
+				res.add(new LinkCommunication(null, item, receiverLink));
+			}
+		}
+		return res;
+	}
 
-  public static List<UndefinedCommunication> getUndefinedCommunications(Component c1, Component c2, List<AbstractCommunication> existingCommunications) {
-    List<ExchangeItem> items = new ArrayList<ExchangeItem>();
-    items.addAll(getExchangeItemsVisibleIn(c1));
-    for (ExchangeItem item : getExchangeItemsVisibleIn(c2)) {
-      if (!items.contains(item)) {
-        items.add(item);
-      }
-    }
-    for (AbstractCommunication communication : existingCommunications) {
-      items.remove(communication.exchangeItem);
-    }
-    List<UndefinedCommunication> res = new ArrayList<UndefinedCommunication>();
-    for (ExchangeItem item : items) {
-      res.add(new UndefinedCommunication(item));
-    }
-    return res;
-  }
+	public static List<UndefinedCommunication> getUndefinedCommunications(
+			Component c1, Component c2,
+			List<AbstractCommunication> existingCommunications) {
+		List<ExchangeItem> items = new ArrayList<ExchangeItem>();
+		items.addAll(getExchangeItemsVisibleIn(c1));
+		for (ExchangeItem item : getExchangeItemsVisibleIn(c2)) {
+			if (!items.contains(item)) {
+				items.add(item);
+			}
+		}
+		for (AbstractCommunication communication : existingCommunications) {
+			items.remove(communication.exchangeItem);
+		}
+		List<UndefinedCommunication> res = new ArrayList<UndefinedCommunication>();
+		for (ExchangeItem item : items) {
+			res.add(new UndefinedCommunication(item));
+		}
+		return res;
+	}
 
-  public static Collection<ExchangeItem> getExchangeItemsVisibleIn(Component component) {
-    List<ExchangeItem> items = new ArrayList<ExchangeItem>();
-    for (AbstractExchangeItem item : ExchangeItemExt.getAllExchangeItems(component)) {
-      if (item instanceof ExchangeItem) {
-        items.add((ExchangeItem) item);
-      }
-    }
-    return items;
-  }
+	public static Collection<ExchangeItem> getExchangeItemsVisibleIn(
+			Component component) {
+		List<ExchangeItem> items = new ArrayList<ExchangeItem>();
+		for (AbstractExchangeItem item : ExchangeItemExt
+				.getAllExchangeItems(component)) {
+			if (item instanceof ExchangeItem) {
+				items.add((ExchangeItem) item);
+			}
+		}
+		return items;
+	}
 
-  public static MessageKind getDefaultMessageKind(boolean withReturn) {
-    if (withReturn) {
-      return MessageKind.SYNCHRONOUS_CALL;
-    } else {
-      return MessageKind.ASYNCHRONOUS_CALL;
-    }
-  }
+	public static MessageKind getDefaultMessageKind(boolean withReturn) {
+		if (withReturn) {
+			return MessageKind.SYNCHRONOUS_CALL;
+		} else {
+			return MessageKind.ASYNCHRONOUS_CALL;
+		}
+	}
 
-  public static Interface getTechnicalInterface(Component source, Component target) {
-    List<Interface> interfacesByCom = new ArrayList<Interface>(source.getUsedInterfaces());
-    interfacesByCom.retainAll(target.getImplementedInterfaces());
-    for (Interface interface_ : interfacesByCom) {
-      if (!interface_.isStructural()) {
-        return interface_;
-      }
-    }
-    return null;
-  }
+	public static Interface getTechnicalInterface(Component source,
+			Component target) {
+		List<Interface> interfacesByCom = new ArrayList<Interface>(
+				source.getUsedInterfaces());
+		interfacesByCom.retainAll(target.getImplementedInterfaces());
+		for (Interface interface_ : interfacesByCom) {
+			if (!interface_.isStructural()) {
+				return interface_;
+			}
+		}
+		return null;
+	}
 
 }
