18d3e70ecf3f782d4385bcc659f08030943004f5
Aurelien Didier <aurelien.didier51@gmail.com>
2015-08-31 19:05:29 +0200

    [469] Go To action for Instance Role with Part and label of Part in Representing Instance
    Change-Id: Ia0a2da57b9c187649e161026862449ff3339f8ca
    Signed-off-by: Aurelien Didier <aurelien.didier51@gmail.com>

---
+++ core/plugins/org.polarsys.capella.core.data.helpers/src/org/polarsys/capella/core/data/helpers/cs/delegates/PartNamingHelper.java
@@ -0,0 +1,30 @@
+/*******************************************************************************
+ * Copyright (c) 2006, 2015 THALES GLOBAL SERVICES.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *  
+ * Contributors:
+ *    Thales - initial API and implementation
+ *******************************************************************************/
+package org.polarsys.capella.core.data.helpers.cs.delegates;
+
+import org.polarsys.capella.core.data.cs.Part;
+import org.polarsys.capella.core.data.helpers.cs.services.Messages;
+
+public class PartNamingHelper {
+  /**
+   * @param element
+   *          element whose value is requested
+   */
+  public static String getValue(Part element) {
+    if (element != null) {
+      if (element.getName() != null && element.getName() != "") {
+        return element.getName() + " : " + element.getAbstractType().getName();
+      }
+      return Messages.getString("Part") + element.getAbstractType().getName();
+    }
+    return Messages.getString("UndefinedValue"); //$NON-NLS-1$
+  }
+}
