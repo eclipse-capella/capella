35cc41eeefeb2ef2d3dec73aae065d7189cebccd
Jeremy Aubry <jaubry@obeo.fr>
2015-09-14 15:27:13 +0200

    [403] Consolidate model validation on Component and Data No error if implicit typing in DWF_D_52 rule
    Bug: 403
    Change-Id: I198118b564d4d956ff371a7405321fc6419146d6
    Signed-off-by: Jeremy Aubry <jaubry@obeo.fr>

--- tests/plugins/org.polarsys.capella.test.validation.rules.ju/src/org/polarsys/capella/test/validation/rules/ju/testcases/dwf_d/Rule_DWF_D_52.java
+++ tests/plugins/org.polarsys.capella.test.validation.rules.ju/src/org/polarsys/capella/test/validation/rules/ju/testcases/dwf_d/Rule_DWF_D_52.java
@@ -47,19 +47,19 @@ public class Rule_DWF_D_52 extends AbstractRulesOnDesignTest {
    */
   protected List<String> getScopeDefinition() {
     return Arrays.asList(new String[] {
-        "373c7118-a957-4499-9485-6e89d30b1d22",
-        "1d057a80-1f52-4092-8db4-b8e42898f567",
-        "9dff8332-f6b6-4516-9168-529604764831",
-        "b5a86f51-1c94-47c0-8d62-262d235f682d",
-        "ff102fef-4c22-4726-89f3-18ad63ec8e1b",
-        "52090b7a-3926-457c-bf0c-2495c1e87f0a",
-        "c4f72f6c-e5db-4cfa-8179-61d0901e6656",
-        "ba53cb67-dcae-415f-81ba-23a8a4886e3b",
-        "f8ee8b34-641f-4aad-907f-1e0f68ca34df",
-        "e01bc138-bed6-41f2-ad19-d7c460358d09",
-        "9b17f3c1-164b-46e2-a344-e02c0c17b7cf",
-        "a083a96e-8b8b-4b70-98bc-47d0948a052e",
-        "4828fe6e-edfb-4901-906f-949e7c370e0a"});
+        "373c7118-a957-4499-9485-6e89d30b1d22",   // 1  - no default/null value
+        "1d057a80-1f52-4092-8db4-b8e42898f567",   // 2  - default value inconsistent with its property type (fail expected)
+        "9dff8332-f6b6-4516-9168-529604764831",   // 3  - default value with same type (String)
+        "b5a86f51-1c94-47c0-8d62-262d235f682d",   // 4  - null value inconsistent with its property type (fail expected)
+        "ff102fef-4c22-4726-89f3-18ad63ec8e1b",   // 5  - default value with same type (Integer)
+        "52090b7a-3926-457c-bf0c-2495c1e87f0a",   // 6  - default value type undefined (implicit typing)  
+        "c4f72f6c-e5db-4cfa-8179-61d0901e6656",   // 7  - null value with same type but no name (Enumeration)
+        "ba53cb67-dcae-415f-81ba-23a8a4886e3b",   // 8  - null value inconsistent with its property type (fail expected)
+        "f8ee8b34-641f-4aad-907f-1e0f68ca34df",   // 9  - property type is a super type of default value type (BooleanTest -> Boolean)
+        "e01bc138-bed6-41f2-ad19-d7c460358d09",   // 10 - not same boolean type (fail expected)
+        "9b17f3c1-164b-46e2-a344-e02c0c17b7cf",   // 11 - property type is a super type of default value type (Class 3 -> Class 4)
+        "a083a96e-8b8b-4b70-98bc-47d0948a052e",   // 12 - property type is a sub type of default value type (fail expected)
+        "4828fe6e-edfb-4901-906f-949e7c370e0a"}); // 13 - default and null value inconsistent with their property type (2 error messages expected)
   }
 
   /**
@@ -70,7 +70,6 @@ public class Rule_DWF_D_52 extends AbstractRulesOnDesignTest {
     return Arrays.asList(new OracleDefinition[] {
         new OracleDefinition("1d057a80-1f52-4092-8db4-b8e42898f567", 1),
         new OracleDefinition("b5a86f51-1c94-47c0-8d62-262d235f682d", 1),
-        new OracleDefinition("52090b7a-3926-457c-bf0c-2495c1e87f0a", 1),
         new OracleDefinition("ba53cb67-dcae-415f-81ba-23a8a4886e3b", 1),
         new OracleDefinition("e01bc138-bed6-41f2-ad19-d7c460358d09", 1),
         new OracleDefinition("a083a96e-8b8b-4b70-98bc-47d0948a052e", 1),
