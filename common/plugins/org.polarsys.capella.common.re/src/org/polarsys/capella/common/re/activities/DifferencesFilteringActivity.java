9b06f84c334e061649b43b56aca309339434667b
Jeremy Aubry <jaubry@obeo.fr>
2015-09-15 10:56:26 +0200

    [461][REC] Update RPL elements name with REC elements name + RPL suffix Fix "Update REC from selected RPL" and re-enable AvoidUnsynchronizedFeatureItem filter
    Bug: 461
    Change-Id: I53a3bd1c91021ce9605b4a276fe84009fc2f75f9
    Signed-off-by: Jeremy Aubry <jaubry@obeo.fr>

--- common/plugins/org.polarsys.capella.common.re/src/org/polarsys/capella/common/re/activities/DifferencesFilteringActivity.java
+++ common/plugins/org.polarsys.capella.common.re/src/org/polarsys/capella/common/re/activities/DifferencesFilteringActivity.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2006, 2014 THALES GLOBAL SERVICES.
+ * Copyright (c) 2006, 2015 THALES GLOBAL SERVICES.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -14,6 +14,7 @@ import org.eclipse.core.runtime.IStatus;
 import org.eclipse.core.runtime.Status;
 import org.polarsys.capella.common.re.handlers.filter.DefaultFilterItem;
 import org.polarsys.capella.common.re.handlers.filter.FilterFromReItem;
+import org.polarsys.capella.common.re.re2rpl.filters.AvoidUnsynchronizedFeatureItem;
 import org.polarsys.capella.core.transition.common.handlers.filter.CompoundFilteringItems;
 import org.polarsys.kitalpha.cadence.core.api.parameter.ActivityParameters;
 import org.polarsys.kitalpha.transposer.api.ITransposerWorkflow;
@@ -33,7 +34,7 @@ public class DifferencesFilteringActivity extends org.polarsys.capella.core.tran
 
     handler.addFilterItem(new FilterFromReItem(), context);
 
-//    handler.addFilterItem(new AvoidUnsynchronizedFeatureItem(), context);
+    handler.addFilterItem(new AvoidUnsynchronizedFeatureItem(), context);
     return Status.OK_STATUS;
   }
 
