4a97e9a1082f0c4ff6b9ede22f8f13e901122ba0
Jeremy Aubry <jaubry@obeo.fr>
2015-09-17 11:21:22 +0200

    [461][REC] Update RPL elements name with REC elements name + RPL suffix Improve REC from RPL/RPL from REC updates, create ReScope (parent class of ReSourceScope and ReTargetScope)
    Bug: 461
    Change-Id: Ie445288ae939f48ab304bbc43ab60d65007d9f1c
    Signed-off-by: Jeremy Aubry <jaubry@obeo.fr>

--- common/plugins/org.polarsys.capella.common.re/src/org/polarsys/capella/common/re/merge/scope/ReSourceScope.java
+++ common/plugins/org.polarsys.capella.common.re/src/org/polarsys/capella/common/re/merge/scope/ReSourceScope.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2006, 2014 THALES GLOBAL SERVICES.
+ * Copyright (c) 2006, 2015 THALES GLOBAL SERVICES.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -10,248 +10,128 @@
  *******************************************************************************/
 package org.polarsys.capella.common.re.merge.scope;
 
+import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
 
-import org.eclipse.emf.diffmerge.api.IMatch;
-import org.eclipse.emf.diffmerge.api.Role;
 import org.eclipse.emf.ecore.EAttribute;
 import org.eclipse.emf.ecore.EObject;
-import org.eclipse.emf.ecore.EReference;
-import org.eclipse.emf.ecore.EStructuralFeature;
-import org.eclipse.osgi.util.NLS;
 import org.polarsys.capella.common.re.CatalogElement;
 import org.polarsys.capella.common.re.CatalogElementKind;
 import org.polarsys.capella.common.re.CatalogElementLink;
 import org.polarsys.capella.common.re.constants.IReConstants;
 import org.polarsys.capella.common.re.handlers.replicable.ReplicableElementHandlerHelper;
-import org.polarsys.capella.common.ui.services.helper.EObjectLabelProviderHelper;
-import org.polarsys.capella.core.transition.common.constants.ITransitionConstants;
-import org.polarsys.capella.core.transition.common.handlers.contextscope.ContextScopeHandlerHelper;
-import org.polarsys.capella.core.transition.common.handlers.notify.INotifyChangeEvent;
-import org.polarsys.capella.core.transition.common.handlers.notify.INotifyListener;
 import org.polarsys.capella.core.transition.common.handlers.traceability.ITraceabilityHandler;
-import org.polarsys.capella.core.transition.common.merge.ExtendedComparison;
-import org.polarsys.capella.core.transition.common.merge.scope.IModelScopeFilter;
-import org.polarsys.capella.core.transition.common.merge.scope.ReferenceModelScope;
 import org.polarsys.kitalpha.transposer.rules.handler.rules.api.IContext;
 
 /**
  */
-public class ReSourceScope extends ReferenceModelScope implements INotifyListener {
+public class ReSourceScope extends ReScope {
 
-  CatalogElement element;
-  ITraceabilityHandler handler;
-
-  Collection<? extends EObject> _initialElements;
-
+  @Override
   protected boolean isSource() {
     return true;
   }
 
   /**
-   * @param elements_p
-   * @param context_p
+   * @param elements
+   * @param context
    */
-  public ReSourceScope(CatalogElement element_p, ITraceabilityHandler handler_p, Collection<? extends EObject> elements_p, IContext context_p) {
-    super(elements_p, context_p);
-    element = element_p;
-    handler = handler_p;
-    _initialElements = elements_p;
+  public ReSourceScope(CatalogElement element, ITraceabilityHandler handler, Collection<? extends EObject> elements,
+      IContext context) {
+    super(element, handler, elements, context);
   }
 
-  public Collection<? extends EObject> getInitialElements() {
-    return _initialElements;
-  }
-
-  /**
-   * {@inheritDoc}
-   */
   @Override
-  public boolean add(EObject element_p) {
-    System.out.println(NLS.bind("ADD {0}", new String[] { EObjectLabelProviderHelper.getText(element_p) }));
+  public List<Object> get(EObject source, EAttribute attribute) {
 
-    if (element_p instanceof CatalogElement) {
+    List<Object> values = super.get(source, attribute);
 
-      if (!ContextScopeHandlerHelper.getInstance(context).contains(IReConstants.UNMERGEABLE_ELEMENTS, element_p, context)) {
-        ((CatalogElement) element_p).setKind(CatalogElementKind.RPL);
+    // if values is not empty
+    if (!values.isEmpty()) {
+
+      // if this command is update RPL from REC:
+      String commandValue = (String) context.get(IReConstants.COMMAND__CURRENT_VALUE);
+      if (IReConstants.COMMAND__UPDATE_A_REPLICA_FROM_REPLICABLE.equals(commandValue)) {
+
+        // and if element is a REC
+        if ((element.getKind() == CatalogElementKind.REC) || (element.getKind() == CatalogElementKind.REC_RPL)) {
+
+          // find the link about the current source
+          for (CatalogElementLink link : element.getOwnedLinks()) {
+            if (link.getTarget() == source) {
+
+              // if the element is suffixed
+              if (link.isSuffixed()) {
+
+                // get the RPL suffix
+                CatalogElement rpl = ReplicableElementHandlerHelper.getInstance(context).getTarget(context);
+                if (rpl != null) {
+                  String suffix = rpl.getSuffix();
+                  if (suffix != null) {
+
+                    // and add it on all values
+                    List<Object> newValues = new ArrayList<Object>();
+                    for (Object value : values) {
+                      newValues.add(value + suffix);
+                    }
+
+                    return Collections.unmodifiableList(newValues);
+                  }
+                }
+              }
+            }
+          }
+        }
       }
 
-    }
-    addTraceabilityLink(element_p, context);
+      // if this command is update REC from RPL:
+      else if (IReConstants.COMMAND__UPDATE_DEFINITION_REPLICA_FROM_REPLICA.equals(commandValue)) {
 
-    return super.add(element_p);
-  }
+        // and if element is a RPL
+        if ((element.getKind() == CatalogElementKind.RPL) || (element.getKind() == CatalogElementKind.REC_RPL)) {
 
-  /**
-   * {@inheritDoc}
-   */
-  @Override
-  public void notifyChanged(INotifyChangeEvent event_p, IContext context_p) {
+          // find the link about the current source
+          for (CatalogElementLink link : element.getOwnedLinks()) {
+            if (link.getTarget() == source) {
 
-  }
+              // if the element is suffixed
+              if (link.isSuffixed()) {
 
-  /**
-   * {@inheritDoc}
-   */
-  @Override
-  public boolean add(EObject element_p, boolean includeChildren_p) {
-    System.out.println(NLS.bind("ADD-includeChildren {0}", new String[] { EObjectLabelProviderHelper.getText(element_p) }));
-    return super.add(element_p, includeChildren_p);
-  }
+                // get the RPL suffix
+                String suffix = element.getSuffix();
+                if (suffix != null) {
 
-  /**
-   * {@inheritDoc}
-   */
-  @Override
-  public boolean add(EObject source_p, EReference reference_p, EObject value_p) {
-    System.out.println(NLS.bind("ADD {0} {1} {2}", new String[] { EObjectLabelProviderHelper.getText(source_p), reference_p.getName(),
-                                                                 EObjectLabelProviderHelper.getText(value_p) }));
-    if (reference_p.isContainment()) {
-      addTraceabilityLink(value_p, context);
-    }
+                  // and remove it on all values:
+                  List<Object> newValues = new ArrayList<Object>();
+                  for (Object object : values) {
+                    if (object instanceof String) {
+                      String currentValue = (String) object;
+                      
+                      // if found
+                      if (currentValue.endsWith(suffix)) {
 
-    if (ReplicableElementHandlerHelper.getInstance(context).isUnmodifiableElement(source_p, context)) {
-      return false;
-    }
+                        // remove the suffix
+                        String newValue = currentValue.substring(0, currentValue.length() - suffix.length());
+                        newValues.add(newValue);
+                      } else {
+                        newValues.add(currentValue);
+                      }
+                    } else {
+                      newValues.add(object);
+                    }
+                  }
 
-    if (reference_p.isContainment()) {
-      if (ReplicableElementHandlerHelper.getInstance(context).isUnmodifiableElement(value_p, context)) {
-        return false;
-      }
-    }
-
-    return super.add(source_p, reference_p, value_p);
-
-  }
-
-  /**
-   * @param value_p
-   */
-  protected void addTraceabilityLink(EObject value_p, IContext context_p) {
-
-    ExtendedComparison comparison = (ExtendedComparison) context.get(ITransitionConstants.MERGE_COMPARISON);
-    Role destinationRole = isSource() ? Role.REFERENCE : Role.TARGET;
-    Role oppositeRole = !isSource() ? Role.REFERENCE : Role.TARGET;
-
-    String destinationScope = isSource() ? IReConstants.SOURCE__ADDED_ELEMENTS : IReConstants.TARGET__ADDED_ELEMENTS;
-    String oppositeScope = !isSource() ? IReConstants.SOURCE__ADDED_ELEMENTS : IReConstants.TARGET__ADDED_ELEMENTS;
-
-    if (shouldAddTraceability(value_p, destinationRole, context)) {
-
-      IMatch match = comparison.getMapping().getMatchFor(value_p, destinationRole);
-      EObject oppositeValue = match.get(oppositeRole);
-
-      CatalogElementLink link = ReplicableElementHandlerHelper.getInstance(context).addLink(context, handler, element, value_p, oppositeValue);
-      ContextScopeHandlerHelper.getInstance(context).add(destinationScope, link, context);
-
-      if (!isSource()) {
-        CatalogElement source = ReplicableElementHandlerHelper.getInstance(context).getSource(context);
-        //In case where element is not traced into source replica, we add a link into it
-        if (source != null) {
-          if (oppositeValue != null) {
-            if (shouldAddTraceability(value_p, oppositeRole, context)) {
-              link = ReplicableElementHandlerHelper.getInstance(context).addLink(context, handler, source, oppositeValue, value_p);
-              ContextScopeHandlerHelper.getInstance(context).add(oppositeScope, link, context);
+                  return Collections.unmodifiableList(newValues);
+                }
+              }
             }
           }
         }
       }
     }
+
+    return values;
   }
-
-  /**
-   * @param value_p
-   * @return
-   */
-  protected boolean shouldAddTraceability(EObject value_p, Role role_p, IContext context_p) {
-    if (ReplicableElementHandlerHelper.getInstance(context_p).isUnmodifiableElement(value_p, context_p)) {
-      return false;
-    }
-
-    return true;
-  }
-
-  /**
-   * {@inheritDoc}
-   */
-  @Override
-  public void build(IModelScopeFilter filter_p) {
-    super.build(filter_p);
-  }
-
-  /**
-   * {@inheritDoc}
-   */
-  @Override
-  public boolean covers(EObject element_p) {
-    return super.covers(element_p);
-  }
-
-  /**
-   * {@inheritDoc}
-   */
-  @Override
-  public boolean remove(EObject element_p) {
-    System.out.println(NLS.bind("REMOVE {0}", new String[] { EObjectLabelProviderHelper.getText(element_p) }));
-    ReplicableElementHandlerHelper.getInstance(context).addDeletableElement(element_p, context);
-
-    _roots.remove(element_p);
-    removeFromScope(element_p);
-    return true;
-  }
-
-  /**
-   * {@inheritDoc}
-   */
-  @Override
-  public void removeFromScope(EObject element_p) {
-    System.out.println(NLS.bind("REMOVE-fromScope {0}", new String[] { EObjectLabelProviderHelper.getText(element_p) }));
-    super.removeFromScope(element_p);
-  }
-
-  /**
-   * {@inheritDoc}
-   */
-  @Override
-  public boolean add(EObject source_p, EAttribute attribute_p, Object value_p) {
-    System.out.println(NLS.bind("ADD {0} {1} {2}", new String[] { EObjectLabelProviderHelper.getText(source_p), attribute_p.getName(),
-                                                                 EObjectLabelProviderHelper.getText(value_p) }));
-
-    return super.add(source_p, attribute_p, value_p);
-  }
-
-  /**
-   * {@inheritDoc}
-   */
-  @Override
-  public Object move(EObject source_p, EStructuralFeature feature_p, int newPosition_p, int oldPosition_p) {
-    return super.move(source_p, feature_p, newPosition_p, oldPosition_p);
-  }
-
-  /**
-   * {@inheritDoc}
-   */
-  @Override
-  public boolean remove(EObject source_p, EAttribute attribute_p, Object value_p) {
-    System.out.println(NLS.bind("REMOVE {0} {1} {2}", new String[] { EObjectLabelProviderHelper.getText(source_p), attribute_p.getName(),
-                                                                    EObjectLabelProviderHelper.getText(value_p) }));
-    return super.remove(source_p, attribute_p, value_p);
-  }
-
-  /**
-   * {@inheritDoc}
-   */
-  @Override
-  public boolean remove(EObject source_p, EReference reference_p, EObject value_p) {
-    System.out.println(NLS.bind("REMOVE {0} {1} {2}", new String[] { EObjectLabelProviderHelper.getText(source_p), reference_p.getName(),
-                                                                    EObjectLabelProviderHelper.getText(value_p) }));
-
-    if (reference_p.isContainment()) {
-      ReplicableElementHandlerHelper.getInstance(context).addDeletableElement(value_p, context);
-      return true;
-    }
-    return super.remove(source_p, reference_p, value_p);
-
-  }
-
 }
--- common/plugins/org.polarsys.capella.common.re/src/org/polarsys/capella/common/re/merge/scope/ReTargetScope.java
+++ common/plugins/org.polarsys.capella.common.re/src/org/polarsys/capella/common/re/merge/scope/ReTargetScope.java
@@ -12,20 +12,15 @@ package org.polarsys.capella.common.re.merge.scope;
 
 import java.util.Collection;
 
-import org.eclipse.emf.ecore.EAttribute;
 import org.eclipse.emf.ecore.EObject;
-
+import org.polarsys.capella.common.re.CatalogElement;
 import org.polarsys.capella.core.transition.common.handlers.traceability.ITraceabilityHandler;
 import org.polarsys.capella.core.transition.common.merge.scope.ITargetModelScope;
-import org.polarsys.capella.common.re.CatalogElement;
-import org.polarsys.capella.common.re.CatalogElementKind;
-import org.polarsys.capella.common.re.CatalogElementLink;
-import org.polarsys.capella.common.re.constants.IReConstants;
 import org.polarsys.kitalpha.transposer.rules.handler.rules.api.IContext;
 
 /**
  */
-public class ReTargetScope extends ReSourceScope implements ITargetModelScope {
+public class ReTargetScope extends ReScope implements ITargetModelScope {
 
   @Override
   protected boolean isSource() {
@@ -49,36 +44,5 @@ public class ReTargetScope extends ReSourceScope implements ITargetModelScope {
   @Override
   public Collection<EObject> retrieveTransformedElementsFromTarget(EObject targetElement) {
     return null;
-  }
-
-  @Override
-  public boolean add(EObject source, EAttribute attribute, Object value) {
-
-    // if command is create or update RPL from REC
-    String commandValue = (String) context.get(IReConstants.COMMAND__CURRENT_VALUE);
-    if (IReConstants.COMMAND__CREATE_A_REPLICA_FROM_REPLICABLE.equals(commandValue)
-        || IReConstants.COMMAND__UPDATE_A_REPLICA_FROM_REPLICABLE.equals(commandValue)) {
-
-      // and if element is a RPL
-      if ((element.getKind() == CatalogElementKind.RPL) || (element.getKind() == CatalogElementKind.REC_RPL)) {
-
-        // find the link about the current source
-        for (CatalogElementLink link : element.getOwnedLinks()) {
-          if (link.getTarget() == source) {
-
-            // if it is suffixed and the attribute is the name feature, add the RPL suffix on value
-            if (link.isSuffixed() && "name".equals(attribute.getName())) {
-              value += element.getSuffix();
-            }
-            // if not, do nothing
-            else {
-              break;
-            }
-          }
-        }
-      }
-    }
-
-    return super.add(source, attribute, value);
   }
 }
