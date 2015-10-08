5abadf974707f17e062ec9fc1153122e7ec198d8
Tu Ton <minhtutonthat@gmail.com>
2015-10-08 14:23:18 +0200

    [551] "Open Diagram / Table" on a Capabiliy shows all scenario diagrams and not only those of owned scenarios
    Bug: 551
    Change-Id: Ib2d00fa4cb67af62f8bc359a38f6b303d679bd79
    Signed-off-by: Tu Ton <minhtutonthat@gmail.com>

--- core/plugins/org.polarsys.capella.core.platform.sirius.ui.navigator/src/org/polarsys/capella/core/platform/sirius/ui/navigator/actions/providers/OpenRepresentationActionProvider.java
+++ core/plugins/org.polarsys.capella.core.platform.sirius.ui.navigator/src/org/polarsys/capella/core/platform/sirius/ui/navigator/actions/providers/OpenRepresentationActionProvider.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2006, 2014 THALES GLOBAL SERVICES.
+ * Copyright (c) 2006, 2015 THALES GLOBAL SERVICES.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -10,8 +10,10 @@
  *******************************************************************************/
 package org.polarsys.capella.core.platform.sirius.ui.navigator.actions.providers;
 
+import java.util.ArrayList;
 import java.util.Collection;
 
+import org.eclipse.emf.common.util.EList;
 import org.eclipse.emf.ecore.EObject;
 import org.eclipse.jface.action.IMenuManager;
 import org.eclipse.jface.action.MenuManager;
@@ -26,6 +28,7 @@ import org.eclipse.sirius.viewpoint.description.Viewpoint;
 import org.eclipse.ui.navigator.CommonActionProvider;
 import org.eclipse.ui.navigator.ICommonMenuConstants;
 import org.polarsys.capella.core.data.interaction.AbstractCapability;
+import org.polarsys.capella.core.data.interaction.Scenario;
 import org.polarsys.capella.core.sirius.ui.actions.OpenRepresentationsAction;
 
 /**
@@ -34,62 +37,76 @@ import org.polarsys.capella.core.sirius.ui.actions.OpenRepresentationsAction;
  */
 public class OpenRepresentationActionProvider extends CommonActionProvider {
 
-	/**
-	 * @see org.eclipse.ui.actions.ActionGroup#fillContextMenu(org.eclipse.jface.action.IMenuManager)
-	 */
-	@Override
-	public void fillContextMenu(IMenuManager menu_p) {
-		IStructuredSelection selection = (IStructuredSelection) getContext().getSelection();
-		Object firstElement = selection.getFirstElement();
-		if (firstElement instanceof EObject) {
-			EObject firstSelectedEObject = (EObject) firstElement;
-			Session currentSession = SessionManager.INSTANCE.getSession(firstSelectedEObject);
-			if (null != currentSession) {
-				Collection<Viewpoint> selectedViewpoints = currentSession.getSelectedViewpoints(false);
-				Collection<RepresentationDescription> descriptions =
-						DialectManager.INSTANCE.getAvailableRepresentationDescriptions(selectedViewpoints, firstSelectedEObject);
+  /**
+   * @see org.eclipse.ui.actions.ActionGroup#fillContextMenu(org.eclipse.jface.action.IMenuManager)
+   */
+  @Override
+  public void fillContextMenu(IMenuManager menu_p) {
+    IStructuredSelection selection = (IStructuredSelection) getContext().getSelection();
+    Object firstElement = selection.getFirstElement();
+    if (firstElement instanceof EObject) {
+      EObject firstSelectedEObject = (EObject) firstElement;
+      Session currentSession = SessionManager.INSTANCE.getSession(firstSelectedEObject);
+      if (null != currentSession) {
+        Collection<Viewpoint> selectedViewpoints = currentSession.getSelectedViewpoints(false);
+        Collection<RepresentationDescription> descriptions = DialectManager.INSTANCE
+            .getAvailableRepresentationDescriptions(selectedViewpoints, firstSelectedEObject);
 
-				if (!descriptions.isEmpty()) {
-					// Creates the "New Diagram / Table" menu.
-					MenuManager openDiagramMenu = new MenuManager(Messages.OpenRepresentationActionProvider_OpenRepresentationAction_Title, "capella.project.diagrams.menu"); //$NON-NLS-1$
+        if (!descriptions.isEmpty()) {
+          // Creates the "New Diagram / Table" menu.
+          MenuManager openDiagramMenu = new MenuManager(
+              Messages.OpenRepresentationActionProvider_OpenRepresentationAction_Title, "capella.project.diagrams.menu"); //$NON-NLS-1$
 
-					for (RepresentationDescription description : descriptions) {
-						// Computes the "Open Diagram / Table..." menu content according to the current selection.
-						if(DialectManager.INSTANCE.getRepresentations(description, currentSession)!= null){
-							Collection<DRepresentation> rep = DialectManager.INSTANCE.getRepresentations(description, currentSession);
-							Collection<DRepresentation> ownedRep = DialectManager.INSTANCE.getRepresentations(firstSelectedEObject, currentSession);
-							rep.retainAll(ownedRep);
-							for(DRepresentation drep : rep){
-								OpenRepresentationsAction ora = new OpenRepresentationsAction(description, drep);
-								openDiagramMenu.add(ora);
-							}
-						}
-					}
+          for (RepresentationDescription description : descriptions) {
+            // Computes the "Open Diagram / Table..." menu content according to the current selection.
+            if (DialectManager.INSTANCE.getRepresentations(description, currentSession) != null) {
+              Collection<DRepresentation> rep = DialectManager.INSTANCE.getRepresentations(description, currentSession);
+              Collection<DRepresentation> ownedRep = DialectManager.INSTANCE.getRepresentations(firstSelectedEObject,
+                  currentSession);
+              rep.retainAll(ownedRep);
+              for (DRepresentation drep : rep) {
+                OpenRepresentationsAction ora = new OpenRepresentationsAction(description, drep);
+                openDiagramMenu.add(ora);
+              }
+            }
+          }
 
-					//create scenarios from capabilities
-					if (firstSelectedEObject instanceof AbstractCapability) {
-						for (Viewpoint vp : selectedViewpoints) {
-							for (RepresentationDescription representationDescription : vp.getOwnedRepresentations()) {
-								if (representationDescription instanceof SequenceDiagramDescription) {						
-									SequenceDiagramDescription sdd = (SequenceDiagramDescription) representationDescription;
-									if(DialectManager.INSTANCE.getRepresentations(sdd, currentSession)!= null){										
-										Collection<DRepresentation> rep = DialectManager.INSTANCE.getRepresentations(sdd, currentSession);
-										for(DRepresentation drep : rep){
-											OpenRepresentationsAction ora = new OpenRepresentationsAction(sdd, drep);
-											openDiagramMenu.add(ora);
-										}
-									}
-								}
-							} 
-						}
+          // create scenarios from capabilities
+          if (firstSelectedEObject instanceof AbstractCapability) {
+            for (Viewpoint vp : selectedViewpoints) {
+              for (RepresentationDescription representationDescription : vp.getOwnedRepresentations()) {
+                if (representationDescription instanceof SequenceDiagramDescription) {
+                  SequenceDiagramDescription sdd = (SequenceDiagramDescription) representationDescription;
+                  if (DialectManager.INSTANCE.getRepresentations(sdd, currentSession) != null) {
+                    Collection<DRepresentation> rep = DialectManager.INSTANCE.getRepresentations(sdd, currentSession);
 
-					}
-					// Attaches the "Open Diagram / Table..." menu to group.new of the parent commonViewer contextual menu.
-					if (openDiagramMenu.getSize() > 0) {
-						menu_p.appendToGroup(ICommonMenuConstants.GROUP_NEW, openDiagramMenu);
-					}
-				}
-			}
-		}
-	}
+                    // Select only scenarios belonging to the Capability
+                    Collection<DRepresentation> ownedRep = new ArrayList<DRepresentation>();
+                    EList<Scenario> lstScenarios = ((AbstractCapability) firstSelectedEObject).getOwnedScenarios();
+                    for (Scenario scenario : lstScenarios) {
+                      Collection<DRepresentation> repScenario = DialectManager.INSTANCE.getRepresentations(scenario,
+                          currentSession);
+                      ownedRep.addAll(repScenario);
+                    }
+
+                    rep.retainAll(ownedRep);
+
+                    for (DRepresentation drep : rep) {
+                      OpenRepresentationsAction ora = new OpenRepresentationsAction(sdd, drep);
+                      openDiagramMenu.add(ora);
+                    }
+                  }
+                }
+              }
+            }
+
+          }
+          // Attaches the "Open Diagram / Table..." menu to group.new of the parent commonViewer contextual menu.
+          if (openDiagramMenu.getSize() > 0) {
+            menu_p.appendToGroup(ICommonMenuConstants.GROUP_NEW, openDiagramMenu);
+          }
+        }
+      }
+    }
+  }
 }
