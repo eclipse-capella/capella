7e82d0af8f0d5f9cfb283db83a0bfbd144d00c95
aurelien <aurelien.didier51@gmail.com>
2015-10-14 16:03:59 +0200

    [550] Migration was launching repair without closing the session and back up the aird
    Bug: 550
    Change-Id: I4b4f401b09f669556586f570c65cca60a21ad3e2
    Signed-off-by: aurelien <aurelien.didier51@gmail.com>

--- core/plugins/org.polarsys.capella.core.data.migration/src/org/polarsys/capella/core/data/migration/repair/RepairSiriusRunnable.java
+++ core/plugins/org.polarsys.capella.core.data.migration/src/org/polarsys/capella/core/data/migration/repair/RepairSiriusRunnable.java
@@ -14,6 +14,8 @@ import org.eclipse.core.resources.IFile;
 import org.eclipse.core.runtime.IStatus;
 import org.eclipse.core.runtime.Status;
 import org.eclipse.sirius.business.api.repair.SiriusRepairProcess;
+import org.eclipse.sirius.ui.tools.internal.actions.repair.RepresentationFilesRepairValidator;
+import org.eclipse.swt.widgets.Display;
 import org.polarsys.capella.common.helpers.operations.LongRunningListenersRegistry;
 import org.polarsys.capella.core.data.migration.AbstractMigrationRunnable;
 import org.polarsys.capella.core.data.migration.MigrationHelpers;
@@ -21,49 +23,76 @@ import org.polarsys.capella.core.data.migration.context.MigrationContext;
 
 public class RepairSiriusRunnable extends AbstractMigrationRunnable {
 
-  public RepairSiriusRunnable(IFile file) {
-    super(file);
-  }
+	public RepairSiriusRunnable(IFile file) {
+		super(file);
+	}
 
-  @Override
-  public IStatus run(final MigrationContext context, boolean checkVersion) {
-    // async/sync? GMF also run async... so when many
-    // repair/migrate are triggered, we will mix R/Ms and
-    // GMF commands between models..
-    // sync should lead to UI deadlock between GMF and RMs.
-    // so we should find a better way.
-    // async lead to popups..
+	@Override
+	public IStatus run(final MigrationContext context, boolean checkVersion) {
+		// async/sync? GMF also run async... so when many
+		// repair/migrate are triggered, we will mix R/Ms and
+		// GMF commands between models..
+		// sync should lead to UI deadlock between GMF and RMs.
+		// so we should find a better way.
+		// async lead to popups..
 
-    try {
-      LongRunningListenersRegistry.getInstance().operationStarting(getClass());
-      doRepairMigrate(getName(), getFile(), true, context);
+		try {
+			LongRunningListenersRegistry.getInstance().operationStarting(
+					getClass());
+			doRepairMigrate(getName(), getFile(), context.isSkipConfirmation(), context);
+		} catch (InterruptedException e) {
+			LongRunningListenersRegistry.getInstance().operationAborted(
+					getClass());
+			return Status.CANCEL_STATUS;
+		} catch (OutOfMemoryError error) {
+			MigrationHelpers.getInstance().onOutOfMemoryError(error, context);
+			throw error;
 
-    } catch (InterruptedException e) {
-      LongRunningListenersRegistry.getInstance().operationAborted(getClass());
-      return Status.CANCEL_STATUS;
+		} finally {
+			LongRunningListenersRegistry.getInstance().operationEnded(
+					getClass());
 
-    } catch (OutOfMemoryError error) {
-      MigrationHelpers.getInstance().onOutOfMemoryError(error, context);
-      throw error;
+			context.getProgressMonitor().done();
+		}
 
-    } finally {
-      LongRunningListenersRegistry.getInstance().operationEnded(getClass());
+		return Status.OK_STATUS;
+	}
 
-      context.getProgressMonitor().done();
-    }
+	@Override
+	public String getName() {
+		return Messages.MigrationAction_RepairMigration;
+	}
 
-    return Status.OK_STATUS;
-  }
+	protected void doRepairMigrate(String actionName, final IFile file,
+			final boolean backup, final MigrationContext context)
+			throws InterruptedException {
+	
+		Display.getDefault().syncExec(new Runnable() {
+			@Override
+			public void run() {
+				runRepairMigrate(file, backup, context);
+			}
+		});
 
-  @Override
-  public String getName() {
-    return Messages.MigrationAction_RepairMigration;
-  }
+	}
 
-  protected void doRepairMigrate(String actionName, final IFile file, boolean backup, final MigrationContext context) throws InterruptedException {
-    SiriusRepairProcess process = new SiriusRepairProcess(file, backup);
-    context.getProgressMonitor().setTaskName(getName());
-    process.run(context.getProgressMonitor());
-  }
+	private void runRepairMigrate(final IFile file, final boolean backup,
+			final MigrationContext context) {
+			if (file != null) {
+				@SuppressWarnings("restriction")
+				IStatus validationStatus = new RepresentationFilesRepairValidator()
+						.validate(file);
+				if (validationStatus.isOK()) {
 
+					SiriusRepairProcess process = new SiriusRepairProcess(file, backup);
+					context.getProgressMonitor().setTaskName(getName());
+					try {
+						process.run(context.getProgressMonitor());
+					} catch (InterruptedException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
+				}
+			}
+		}
 }
