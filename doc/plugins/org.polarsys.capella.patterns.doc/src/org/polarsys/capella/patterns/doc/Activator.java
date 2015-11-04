313a153aa9bf8028f7270c2426f6961c6b83076b
cgatti <christophe.gatti@thalesgroup.com>
2015-11-04 17:29:03 +0100

    [621] Update Capella Documentation
    Updated Capella documentation
    
    Bug: 621
    Change-Id: If044918c3fb32cb8a21823c18812aff1ae8a4e56
    Signed-off-by: cgatti <christophe.gatti@thalesgroup.com>

--- doc/plugins/org.polarsys.capella.patterns.doc/src/org/polarsys/capella/patterns/doc/Activator.java
+++ doc/plugins/org.polarsys.capella.patterns.doc/src/org/polarsys/capella/patterns/doc/Activator.java
@@ -10,6 +10,7 @@
  *******************************************************************************/
 package org.polarsys.capella.patterns.doc;
 
+import org.eclipse.jface.resource.ImageDescriptor;
 import org.eclipse.ui.plugin.AbstractUIPlugin;
 import org.osgi.framework.BundleContext;
 
@@ -19,7 +20,7 @@ import org.osgi.framework.BundleContext;
 public class Activator extends AbstractUIPlugin {
 
 	// The plug-in ID
-	public static final String PLUGIN_ID = "org.polarsys.capella.core.libraries.doc"; //$NON-NLS-1$
+	public static final String PLUGIN_ID = "org.polarsys.capella.patterns.doc"; //$NON-NLS-1$
 
 	// The shared instance
 	private static Activator plugin;
@@ -57,4 +58,14 @@ public class Activator extends AbstractUIPlugin {
 		return plugin;
 	}
 
+	/**
+	 * Returns an image descriptor for the image file at the given
+	 * plug-in relative path
+	 *
+	 * @param path the path
+	 * @return the image descriptor
+	 */
+	public static ImageDescriptor getImageDescriptor(String path) {
+		return imageDescriptorFromPlugin(PLUGIN_ID, path);
+	}
 }
