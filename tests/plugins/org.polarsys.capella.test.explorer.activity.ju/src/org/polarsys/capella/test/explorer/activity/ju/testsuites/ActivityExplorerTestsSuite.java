71a2b440a8f232f144c571a44f09b6de81a362bd
Damien Filliat <damien.filliat@thalesgroup.com>
2015-12-11 14:26:06 +0100

    [676] Add JUnit tests for the ActivityExplorer
    Bug: 676
    Change-Id: If74583a2de32b701b96a384f9b7bf44e3e447025
    Signed-off-by: Damien Filliat <damien.filliat@thalesgroup.com>

---
+++ tests/plugins/org.polarsys.capella.test.explorer.activity.ju/src/org/polarsys/capella/test/explorer/activity/ju/testsuites/ActivityExplorerTestsSuite.java
@@ -0,0 +1,50 @@
+/*******************************************************************************
+ * Copyright (c) 2006, 2015 THALES GLOBAL SERVICES.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *  
+ * Contributors:
+ *    Thales - initial API and implementation
+ *******************************************************************************/
+package org.polarsys.capella.test.explorer.activity.ju.testsuites;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import junit.framework.Test;
+
+import org.polarsys.capella.test.explorer.activity.ju.testcases.ManualOpen;
+import org.polarsys.capella.test.framework.api.BasicTestArtefact;
+import org.polarsys.capella.test.framework.api.BasicTestSuite;
+
+/**
+ * Test Suite - Activity Explorer (integration in Capella). 
+ */
+public class ActivityExplorerTestsSuite extends BasicTestSuite {
+
+  /**
+   * {@inheritDoc}
+   */
+  @Override
+  protected List<BasicTestArtefact> getTests() {
+    List<BasicTestArtefact> tests = new ArrayList<BasicTestArtefact>();
+    //tests.add(new AutoOpen());
+    tests.add(new ManualOpen());
+    return tests;
+  }
+
+  /**
+   * Added in order to launch this test suite without the Capella test framework.
+   * @return
+   */
+  public static Test suite() {
+    return new ActivityExplorerTestsSuite();
+  }
+
+  @Override
+  public List<String> getRequiredTestModels() {
+    return null;
+  }
+}
