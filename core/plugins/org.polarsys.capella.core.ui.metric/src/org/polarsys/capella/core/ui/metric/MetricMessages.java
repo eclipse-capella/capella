65dfead66cc58efa171c8f2be743019120fc0f19
Hakim Sellou <hakim.sellou@obeo.fr>
2016-01-06 14:37:48 +0100

    [706] Add an element filter for Set Progress Monitoring
    Bug: 706
    Change-Id: I198a8c2904fc19ff20a6ac55cb0690fa2ea7463b
    Signed-off-by: Hakim Sellou <hakim.sellou@obeo.fr>

--- core/plugins/org.polarsys.capella.core.ui.metric/src/org/polarsys/capella/core/ui/metric/MetricMessages.java
+++ core/plugins/org.polarsys.capella.core.ui.metric/src/org/polarsys/capella/core/ui/metric/MetricMessages.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2006, 2015 THALES GLOBAL SERVICES.
+ * Copyright (c) 2006, 2016 THALES GLOBAL SERVICES.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -16,63 +16,74 @@ import org.eclipse.osgi.util.NLS;
  * Messages for the metric plugin.
  */
 public class MetricMessages extends NLS {
-	private static final String BUNDLE_NAME = "messages"; //$NON-NLS-1$
-	
-	// Action
-	public static String progressMonitorMsg;
-	public static String metricAction_lbl;
+  private static final String BUNDLE_NAME = "org.polarsys.capella.core.ui.metric.messages"; //$NON-NLS-1$
 
-	// Dialog
-	public static String metricDialogShellTitle;
-	public static String metricDialogDefaultTitle;
-	public static String metricDialogTitleFromEObj;
-	public static String metricDialogTitleFromFile;
+  // Action
+  public static String progressMonitorMsg;
+  public static String metricAction_lbl;
 
-	public static String treeObjectColumnLabel;
-	public static String treeResultColumnLabel;
+  // Dialog
+  public static String metricDialogShellTitle;
+  public static String metricDialogDefaultTitle;
+  public static String metricDialogTitleFromEObj;
+  public static String metricDialogTitleFromFile;
 
-	public static String layer;
+  public static String treeObjectColumnLabel;
+  public static String treeResultColumnLabel;
 
-	//
-	// Progress Monitoring
-	//
-	public static String progressMonitoring_menu_lbl;
-	public static String progressMonitoring_setAction_lbl;
-	public static String progressMonitoring_overviewAction_lbl;
+  public static String layer;
 
-	public static String progressMonitoring_setAction_dialog_title;
-	public static String progressMonitoring_setAction_dialog_main_lbl;
-	public static String progressMonitoring_setAction_dialog_clear_lbl;
-	public static String progressMonitoring_setAction_dialog_combo_lbl;
-	public static String progressMonitoring_setAction_cmd_lbl;
-	public static String progressMonitoring_setAction_nochanges_info;
-	public static String progressMonitoring_setAction_changes_info;
+  public static String progressMonitoring_clearReview_button_lbl;
+  public static String progressMonitoring_clearReview_button_tooltip;
 
-	public static String progressMonitoring_dialog_title;
-	public static String progressMonitoring_dialog_msg;
+  //
+  // Progress Monitoring
+  //
+  public static String progressMonitoring_menu_lbl;
+  public static String progressMonitoring_setAction_lbl;
+  public static String progressMonitoring_overviewAction_lbl;
 
-	public static String progressMonitoring_dialog_combo_lbl;
-	public static String progressMonitoring_dialog_combo_allStatus;
+  public static String progressMonitoring_setAction_dialog_title;
 
-	public static String progressMonitoring_dialog_header_col0;
-	public static String progressMonitoring_dialog_header_col1;
-	public static String progressMonitoring_dialog_header_col2;
+  public static String progressMonitoring_setAction_dialog_main_lbl;
 
-	public static String progressMonitoring_dialog_propagate_button_lbl;
-	public static String progressMonitoring_dialog_propagate_button_tooltip;
+  public static String progressMonitoring_setAction_dialog_clear_lbl;
 
-	public static String progressMonitoring_dialog_propagate_to_representation_button_lbl;
-	public static String progressMonitoring_dialog_propagate_to_representation_button_tooltip;
+  public static String progressMonitoring_setAction_dialog_combo_lbl;
 
-	static {
-		// initialize resource bundle
-		NLS.initializeMessages(BUNDLE_NAME, MetricMessages.class);
-	}
+  public static String progressMonitoring_setAction_cmd_lbl;
+  public static String progressMonitoring_setAction_nochanges_info;
+  public static String progressMonitoring_setAction_changes_info;
 
-	/**
-	 * Constructor.
-	 */
-	private MetricMessages() {
-		// Do nothing.
-	}
+  public static String progressMonitoring_dialog_title;
+  public static String progressMonitoring_dialog_msg;
+
+  public static String progressMonitoring_dialog_combo_lbl;
+  public static String progressMonitoring_dialog_combo_allStatus;
+
+  public static String progressMonitoring_dialog_header_col0;
+  public static String progressMonitoring_dialog_header_col1;
+  public static String progressMonitoring_dialog_header_col2;
+
+  public static String progressMonitoring_dialog_propagate_button_lbl;
+  public static String progressMonitoring_dialog_propagate_button_tooltip;
+
+  public static String progressMonitoring_dialog_propagate_to_representation_button_lbl;
+  public static String progressMonitoring_dialog_propagate_to_representation_button_tooltip;
+
+  public static String progressMonitoring_filters_group_lbl;
+  public static String progressMonitoring_filterStatus_button_lbl;
+  public static String progressMonitoring_filterStatus_button_tooltip;
+
+  static {
+    // initialize resource bundle
+    NLS.initializeMessages(BUNDLE_NAME, MetricMessages.class);
+  }
+
+  /**
+   * Constructor.
+   */
+  private MetricMessages() {
+    // Do nothing.
+  }
 }
