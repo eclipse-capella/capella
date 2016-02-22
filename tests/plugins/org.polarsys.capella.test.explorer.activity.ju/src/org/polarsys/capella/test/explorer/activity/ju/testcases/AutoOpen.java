e6669dc2b512717e28e931e8b7b46982213f15b4
Damien Filliat <damien.filliat@thalesgroup.com>
2016-02-22 14:17:19 +0100

    [782] Auto Tests - add a test case to check Activity Explorer bug 487984
    Bug: 782
    
    Change-Id: I4ce636a55915dee1dd4e17c97520a208ee44ded6
    Signed-off-by: Damien Filliat <damien.filliat@thalesgroup.com>

--- tests/plugins/org.polarsys.capella.test.explorer.activity.ju/src/org/polarsys/capella/test/explorer/activity/ju/testcases/AutoOpen.java
+++ tests/plugins/org.polarsys.capella.test.explorer.activity.ju/src/org/polarsys/capella/test/explorer/activity/ju/testcases/AutoOpen.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2006, 2015 THALES GLOBAL SERVICES.
+ * Copyright (c) 2006, 2016 THALES GLOBAL SERVICES.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -10,12 +10,16 @@
  *******************************************************************************/
 package org.polarsys.capella.test.explorer.activity.ju.testcases;
 
+import java.util.Collections;
+import java.util.List;
+
 import org.eclipse.amalgam.explorer.activity.ui.api.editor.ActivityExplorerEditor;
 import org.eclipse.core.resources.IFile;
 import org.eclipse.sirius.business.api.session.Session;
 import org.eclipse.ui.IEditorPart;
 import org.eclipse.ui.PlatformUI;
 import org.polarsys.capella.core.sirius.ui.helper.SessionHelper;
+import org.polarsys.capella.test.framework.api.BasicTestCase;
 import org.polarsys.capella.test.framework.helpers.GuiActions;
 
 /**
@@ -25,8 +29,10 @@ import org.polarsys.capella.test.framework.helpers.GuiActions;
  * <li>ActivityExplorer is closed when a Session is closed.</li>
  * </ol> 
  */
-public class AutoOpen extends AbstractActivityExplorerTestCase {
+public class AutoOpen extends BasicTestCase {
 
+  public static final String TEST_PROJECT_NAME = "EmptyModel";
+  
   @Override
   public void test() throws Exception {
     // Hypothesis: project already imported in workspace (by setUp()), but session not open.
@@ -34,20 +40,26 @@ public class AutoOpen extends AbstractActivityExplorerTestCase {
     // Open session using Capella's OpenSessionAction.
     IFile airdFile = getAirdFileForLoadedModel(TEST_PROJECT_NAME);
     GuiActions.openSession(airdFile, true);
+    final Session session = SessionHelper.getSession(airdFile);
+    
     // Check ActivityExplorer Editor is open.
     IEditorPart activeEditor = PlatformUI.getWorkbench().getActiveWorkbenchWindow().getActivePage().getActiveEditor();
     assertTrue(activeEditor instanceof ActivityExplorerEditor);
     
     // Check ActivityExplorer content is valid.
     ActivityExplorerEditor activityExplorerEditor = (ActivityExplorerEditor) activeEditor;
-    checkActivityExploreContent(activityExplorerEditor, TEST_PROJECT_NAME);
+    ActivityExplorerTestsHelper.checkActivityExploreContent(activityExplorerEditor, session, TEST_PROJECT_NAME);
 
     // Close session.
-    Session session = SessionHelper.getSession(airdFile);
     GuiActions.closeSession(session);
     
     // No editor (i.e. ActivityExplorer is closed).
     assertNull(PlatformUI.getWorkbench().getActiveWorkbenchWindow().getActivePage().getActiveEditor());
+  }
+  
+  @Override
+  public List<String> getRequiredTestModels() {
+    return Collections.singletonList(TEST_PROJECT_NAME);
   }
   
   @Override
