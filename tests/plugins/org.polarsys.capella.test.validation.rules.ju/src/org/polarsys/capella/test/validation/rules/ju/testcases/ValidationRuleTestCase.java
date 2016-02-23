b7945c2e82c5b9d7f3964861ce01d45d4742f593
Felix Dorner <felix.dorner@gmail.com>
2016-02-23 11:38:15 +0100

    [785][Test Framework] Validation tests may now check false positives
    To check that a rule does not incorrectly flag a valid object as invalid
    you can now pass 0 as the number of expected errors on an
    OracleDefinition.
    
    Bug: 785
    Change-Id: Iad1e44491cc92b04380b3d5cba738fd1329e5557
    Signed-off-by: Felix Dorner <felix.dorner@gmail.com>

--- tests/plugins/org.polarsys.capella.test.validation.rules.ju/src/org/polarsys/capella/test/validation/rules/ju/testcases/ValidationRuleTestCase.java
+++ tests/plugins/org.polarsys.capella.test.validation.rules.ju/src/org/polarsys/capella/test/validation/rules/ju/testcases/ValidationRuleTestCase.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2006, 2015 THALES GLOBAL SERVICES.
+ * Copyright (c) 2006, 2016 THALES GLOBAL SERVICES.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -144,7 +144,7 @@ public abstract class ValidationRuleTestCase extends BasicTestCase {
         OracleDefinition oracleDef = objectID2OracleDefinition.get(objectID);
         Diagnostic diagnostic = diagnostician.validate(object);
 
-        if ((diagnostic.getSeverity() == Diagnostic.OK) && (oracleDef != null)) {
+        if ((diagnostic.getSeverity() == Diagnostic.OK) && (oracleDef != null) && oracleDef.getNbExpectedErrors() > 0) {
           fail("Validation rule " + ruleID + " has not detected an error on object " + objectID + " while it must be the case"); //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$
         } else {
           if (diagnostic.getSeverity() != Diagnostic.OK) {
