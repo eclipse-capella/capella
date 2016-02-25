5039d6cc5ec1cc90b583b7dd7af78e862981c030
Philippe DUL <philippe.dul@thalesgroup.com>
2016-02-25 10:30:28 +0100

    [781] AutoTests - Improve auto tests framework
    Bug: 781
    Change-Id: I2b9e1787887067990adcec8f16c08fb88db8bae2
    Signed-off-by: Philippe DUL <philippe.dul@thalesgroup.com>

--- tests/plugins/org.polarsys.capella.test.diagram.tools.ju/src/org/polarsys/capella/test/diagram/tools/ju/testsuites/partial/MCBDiagramToolsTestSuite.java
+++ tests/plugins/org.polarsys.capella.test.diagram.tools.ju/src/org/polarsys/capella/test/diagram/tools/ju/testsuites/partial/MCBDiagramToolsTestSuite.java
@@ -15,7 +15,14 @@ import java.util.List;
 
 import junit.framework.Test;
 
+import org.polarsys.capella.test.diagram.tools.ju.mcb.CCScenario;
+import org.polarsys.capella.test.diagram.tools.ju.mcb.CMScenario;
+import org.polarsys.capella.test.diagram.tools.ju.mcb.EPBSCRBScenario;
 import org.polarsys.capella.test.diagram.tools.ju.mcb.InsertRemoveScenario;
+import org.polarsys.capella.test.diagram.tools.ju.mcb.LACRBScenario;
+import org.polarsys.capella.test.diagram.tools.ju.mcb.MBScenario;
+import org.polarsys.capella.test.diagram.tools.ju.mcb.MCBScenario;
+import org.polarsys.capella.test.diagram.tools.ju.mcb.OCBScenario;
 import org.polarsys.capella.test.framework.api.BasicTestArtefact;
 import org.polarsys.capella.test.framework.api.BasicTestSuite;
 
@@ -36,13 +43,13 @@ public class MCBDiagramToolsTestSuite extends BasicTestSuite {
     List<BasicTestArtefact> tests = new ArrayList<BasicTestArtefact>();
 
     tests.add(new InsertRemoveScenario());
-    // tests.add(new MCBScenario());
-    // tests.add(new CCScenario());
-    // tests.add(new EPBSCRBScenario());
-    // tests.add(new LACRBScenario());
-    // tests.add(new CMScenario());
-    // tests.add(new MBScenario());
-    // tests.add(new OCBScenario());
+    tests.add(new MCBScenario());
+    tests.add(new CCScenario());
+    tests.add(new EPBSCRBScenario());
+    tests.add(new LACRBScenario());
+    tests.add(new CMScenario());
+    tests.add(new MBScenario());
+    tests.add(new OCBScenario());
 
     return tests;
   }
