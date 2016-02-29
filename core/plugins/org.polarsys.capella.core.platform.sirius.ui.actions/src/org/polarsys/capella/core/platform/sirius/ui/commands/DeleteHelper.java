7539cc690373675fde7202566d8ed3fb4be811b0
Felix Dorner <felix.dorner@gmail.com>
2016-02-29 22:48:38 +0100

    [794] Do not delete state transition when deleting its guard
    By default, Capella deletes an AbstractRelationship element when any of
    the relationships referenced elements is deleted. In this case, this is
    not the desired behavior. To avoid this, an additional business rule was
    added to the DeleteHelper class
    
    Bug: 794
    Change-Id: I64fde16afa0d9dd6a2353ac458c5d1795d835d35
    Signed-off-by: Felix Dorner <felix.dorner@gmail.com>

--- core/plugins/org.polarsys.capella.core.platform.sirius.ui.actions/src/org/polarsys/capella/core/platform/sirius/ui/commands/DeleteHelper.java
+++ core/plugins/org.polarsys.capella.core.platform.sirius.ui.actions/src/org/polarsys/capella/core/platform/sirius/ui/commands/DeleteHelper.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2006, 2014 THALES GLOBAL SERVICES.
+ * Copyright (c) 2006, 2016 THALES GLOBAL SERVICES.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -827,6 +827,11 @@ public class DeleteHelper implements IDeleteHelper {
       result = !(ModellingcorePackage.Literals.TRACEABLE_ELEMENT__INCOMING_TRACES.equals(feature_p) || ModellingcorePackage.Literals.TRACEABLE_ELEMENT__OUTGOING_TRACES
           .equals(feature_p));
     }
+
+    if (CapellacommonPackage.Literals.STATE_TRANSITION__GUARD.equals(feature_p)){
+      result = false;
+    }
+
     return result;
   }
 
