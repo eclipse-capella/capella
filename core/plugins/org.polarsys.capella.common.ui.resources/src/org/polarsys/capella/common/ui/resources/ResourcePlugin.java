cf84d393cc592ebc06f8105563a9631c2049ed6f
Damien Filliat <damien.filliat@thalesgroup.com>
2016-03-25 18:21:52 +0100

    [336] Resolve Sonar issues
    Bug: 336
    
    Change-Id: Iafebafe5a8098007217bdec3cf3ff85bce33241b
    Signed-off-by: Damien Filliat <damien.filliat@thalesgroup.com>

--- core/plugins/org.polarsys.capella.common.ui.resources/src/org/polarsys/capella/common/ui/resources/ResourcePlugin.java
+++ core/plugins/org.polarsys.capella.common.ui.resources/src/org/polarsys/capella/common/ui/resources/ResourcePlugin.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2006, 2014 THALES GLOBAL SERVICES.
+ * Copyright (c) 2006, 2016 THALES GLOBAL SERVICES.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -32,7 +32,7 @@ public class ResourcePlugin extends AbstractUIPlugin {
   @Override
   public void start(BundleContext context_p) throws Exception {
     super.start(context_p);
-    plugin = this;
+    ResourcePlugin.plugin = this;
   }
 
   /**
@@ -40,7 +40,7 @@ public class ResourcePlugin extends AbstractUIPlugin {
    */
   @Override
   public void stop(BundleContext context) throws Exception {
-    plugin = null;
+	  ResourcePlugin.plugin = null;
     super.stop(context);
   }
 
