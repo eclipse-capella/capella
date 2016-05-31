7b551535dbe1d73745c6bec95609ce92296c598d
Tu Ton <minhtutonthat@gmail.com>
2016-05-31 18:19:54 +0200

    [962] - [4.0 regression]Decomposition wizard: automatic interface creation and EI delegation on the fly does not work vs Interface delegation
    Bug: 962
    Change-Id: I6e5f4bb76aadb14b6da01ca60971d407e51a8074
    Signed-off-by: Tu Ton <minhtutonthat@gmail.com>

--- tests/plugins/org.polarsys.capella.test.model.ju/src/org/polarsys/capella/test/model/ju/testcase/LCDecomposition/LCDecompositionWithCommunicationLink.java
+++ tests/plugins/org.polarsys.capella.test.model.ju/src/org/polarsys/capella/test/model/ju/testcase/LCDecomposition/LCDecompositionWithCommunicationLink.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2006, 2015 THALES GLOBAL SERVICES.
+ * Copyright (c) 2006, 2016 THALES GLOBAL SERVICES.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -10,9 +10,6 @@
  *******************************************************************************/
 package org.polarsys.capella.test.model.ju.testcase.LCDecomposition;
 
-import java.util.Arrays;
-import java.util.List;
-
 import org.eclipse.emf.ecore.EObject;
 import org.polarsys.capella.core.common.ui.wizards.LCDecompositionController;
 import org.polarsys.capella.core.data.la.LogicalComponent;
@@ -21,26 +18,13 @@ import org.polarsys.capella.core.libraries.utils.ScopeModelWrapper;
 import org.polarsys.capella.core.ui.toolkit.decomposition.DecompositionModel;
 import org.polarsys.capella.shared.id.handler.IScope;
 import org.polarsys.capella.shared.id.handler.IdManager;
-import org.polarsys.capella.test.framework.api.BasicTestCase;
 
 /**
  * The test model has a Logical System containing a Logical Component. Both of them have ACQUIRE link towards an
  * Exchange Item. Before the bug fix, the decomposition cannot finish due to an exception in DeleteCommand. The test
  * case should check that the decomposition operation can be finished
  */
-public class LCDecomposition extends BasicTestCase {
-
-  public static String MODEL_NAME = "lcdecomposition"; //$NON-NLS-1$ 
-  public static String LCDECOMPOSITION__LA__LOGICAL_SYSTEM = "7fbff2ab-afe1-4f39-8813-c40b31d66077"; //$NON-NLS-1$ 
-
-  /**
-   * {@inheritDoc}
-   */
-  @Override
-  public List<String> getRequiredTestModels() {
-    return Arrays.asList(MODEL_NAME);
-  }
-
+public class LCDecompositionWithCommunicationLink extends LCDecompositionTestCase {
   /**
    * {@inheritDoc}
    */
