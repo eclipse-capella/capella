69dccbe9e431d8b75b1181696f0d8617e8ceaad8
Hakim Sellou <hakim.sellou@obeo.fr>
2016-07-21 17:37:35 +0200

    [1053] Capella meta-model enhancements
    * [JU][M&S] Force refresh after model modification
    * [JU][Validation] Finalize migration of aird models
    
    Bug: 1053
    Change-Id: Iba28e5cf925c4eb80c268608611c02c05dab93e5
    Signed-off-by: Hakim Sellou <hakim.sellou@obeo.fr>

--- tests/plugins/org.polarsys.capella.test.diagram.tools.ju/src/org/polarsys/capella/test/diagram/tools/ju/msm/MSMFunctionalExchangeActionLabelTest.java
+++ tests/plugins/org.polarsys.capella.test.diagram.tools.ju/src/org/polarsys/capella/test/diagram/tools/ju/msm/MSMFunctionalExchangeActionLabelTest.java
@@ -4,7 +4,7 @@
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v10.html
- *  
+ *
  * Contributors:
  *    Thales - initial API and implementation
  *******************************************************************************/
@@ -35,48 +35,56 @@ import org.polarsys.capella.test.diagram.tools.ju.model.GenericModel;
  */
 public class MSMFunctionalExchangeActionLabelTest extends EmptyProject {
 
-	public void test() throws Exception {
+  public void test() throws Exception {
 
-		Session session = getSession(getRequiredTestModel());
-		SessionContext context = new SessionContext(session);
-    
-		XDFBDiagram xdfb = XDFBDiagram.createDiagram(context, EmptyProject.SA__ROOT_SF);
+    Session session = getSession(getRequiredTestModel());
+    SessionContext context = new SessionContext(session);
+
+    XDFBDiagram xdfb = XDFBDiagram.createDiagram(context, EmptyProject.SA__ROOT_SF);
     xdfb.createFunction(GenericModel.FUNCTION_1);
     xdfb.createFunction(GenericModel.FUNCTION_2);
-		
-		XABDiagram sabDiagram = XABDiagram.createDiagram(context, EmptyProject.SA__SYSTEM);
-		sabDiagram.manageAllocatedFunction(GenericModel.FUNCTION_1, EmptyProject.SA__SYSTEM_CONTEXT__PART_SYSTEM__SYSTEM);
-		sabDiagram.manageAllocatedFunction(GenericModel.FUNCTION_2, EmptyProject.SA__SYSTEM_CONTEXT__PART_SYSTEM__SYSTEM);
-		sabDiagram.createFunctionalExchange(GenericModel.FUNCTION_1, GenericModel.FUNCTION_2, GenericModel.FUNCTIONAL_EXCHANGE_1);
-		final FunctionalExchange fe1 = (FunctionalExchange) sabDiagram.getSemanticElement(GenericModel.FUNCTIONAL_EXCHANGE_1);
-		
-		MSMDiagram msmDiagram = MSMDiagram.createDiagram(context, EmptyProject.SA__SYSTEM__SYSTEM_STATE_MACHINE__DEFAULT_REGION);
-		msmDiagram.createState(msmDiagram.getDiagramId(), GenericModel.STATE_1);
-		final State state1 = (State) msmDiagram.getSemanticElement(GenericModel.STATE_1);
 
-		ChangeCommand command = new ChangeCommand(state1.eResource()) {
+    XABDiagram sabDiagram = XABDiagram.createDiagram(context, EmptyProject.SA__SYSTEM);
+    sabDiagram.manageAllocatedFunction(GenericModel.FUNCTION_1, EmptyProject.SA__SYSTEM_CONTEXT__PART_SYSTEM__SYSTEM);
+    sabDiagram.manageAllocatedFunction(GenericModel.FUNCTION_2, EmptyProject.SA__SYSTEM_CONTEXT__PART_SYSTEM__SYSTEM);
+    sabDiagram.createFunctionalExchange(GenericModel.FUNCTION_1, GenericModel.FUNCTION_2,
+        GenericModel.FUNCTIONAL_EXCHANGE_1);
+    final FunctionalExchange fe1 = (FunctionalExchange) sabDiagram
+        .getSemanticElement(GenericModel.FUNCTIONAL_EXCHANGE_1);
+
+    MSMDiagram msmDiagram = MSMDiagram.createDiagram(context,
+        EmptyProject.SA__SYSTEM__SYSTEM_STATE_MACHINE__DEFAULT_REGION);
+    msmDiagram.createState(msmDiagram.getDiagramId(), GenericModel.STATE_1);
+    final State state1 = (State) msmDiagram.getSemanticElement(GenericModel.STATE_1);
+
+    ChangeCommand command = new ChangeCommand(state1.eResource()) {
       @Override
       protected void doExecute() {
         state1.getDoActivity().add(fe1);
         state1.getEntry().add(fe1);
         state1.getExit().add(fe1);
       }
-		};
+    };
     AdapterFactoryEditingDomain.getEditingDomainFor(state1).getCommandStack().execute(command);
-    
+
+    msmDiagram.refreshDiagram();
+
     DDiagram diagram = msmDiagram.getDiagram();
-    Iterator<DDiagramElement> doIt = getDiagramElements(diagram, IMappingNameConstants.MSM_DOACTIVITY_MAPPING_NAME, fe1).iterator();
-    Iterator<DDiagramElement> entryIt = getDiagramElements(diagram, IMappingNameConstants.MSM_ENTRY_MAPPING_NAME, fe1).iterator();
-    Iterator<DDiagramElement> exitIt = getDiagramElements(diagram, IMappingNameConstants.MSM_EXIT_MAPPING_NAME, fe1).iterator();
-    
+    Iterator<DDiagramElement> doIt = getDiagramElements(diagram, IMappingNameConstants.MSM_DOACTIVITY_MAPPING_NAME, fe1)
+        .iterator();
+    Iterator<DDiagramElement> entryIt = getDiagramElements(diagram, IMappingNameConstants.MSM_ENTRY_MAPPING_NAME, fe1)
+        .iterator();
+    Iterator<DDiagramElement> exitIt = getDiagramElements(diagram, IMappingNameConstants.MSM_EXIT_MAPPING_NAME, fe1)
+        .iterator();
+
     String label = "FunctionalExchange 1 [-> SystemFunction 2]";
     assertEquals(" do / " + label, doIt.hasNext() ? doIt.next().getName() : null);
     assertEquals(" entry / " + label, entryIt.hasNext() ? entryIt.next().getName() : null);
-		assertEquals(" exit / " + label, exitIt.hasNext() ? exitIt.next().getName() : null);
-	}
-	
-	private Iterable<DDiagramElement> getDiagramElements(DDiagram diagram, String targetMappingName, EObject target) {
-	  DiagramElementMapping mapping = DiagramServices.getDiagramServices().getNodeMapping(diagram, targetMappingName);
+    assertEquals(" exit / " + label, exitIt.hasNext() ? exitIt.next().getName() : null);
+  }
+
+  private Iterable<DDiagramElement> getDiagramElements(DDiagram diagram, String targetMappingName, EObject target) {
+    DiagramElementMapping mapping = DiagramServices.getDiagramServices().getNodeMapping(diagram, targetMappingName);
     return new DDiagramContents(diagram).getDiagramElements(target, mapping);
-	}
+  }
 }
