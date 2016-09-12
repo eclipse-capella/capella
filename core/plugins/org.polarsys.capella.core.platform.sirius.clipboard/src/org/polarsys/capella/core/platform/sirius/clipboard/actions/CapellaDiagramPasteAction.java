bd661f1acd24774fa893913ae8799e2c8c7ef066
Philippe DUL <philippe.dul@thalesgroup.com>
2016-09-12 15:05:21 +0200

    [1147] [PASTE] Capella Paste does not take account of original layout copied
    Bug: 1147
    Change-Id: I49a0fb0c989ed85b848528d02123f7c2bc432bdd
    Signed-off-by: Philippe DUL <philippe.dul@thalesgroup.com>

--- core/plugins/org.polarsys.capella.core.platform.sirius.clipboard/src/org/polarsys/capella/core/platform/sirius/clipboard/actions/CapellaDiagramPasteAction.java
+++ core/plugins/org.polarsys.capella.core.platform.sirius.clipboard/src/org/polarsys/capella/core/platform/sirius/clipboard/actions/CapellaDiagramPasteAction.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2006, 2014 THALES GLOBAL SERVICES.
+ * Copyright (c) 2006, 2016 THALES GLOBAL SERVICES.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -111,9 +111,10 @@ public class CapellaDiagramPasteAction extends AbstractCopyPasteAction {
       final List<? extends View> gmfSelection = getCopyPasteSelection();
       // Sirius + Capella layer
       monitor_p.worked(1);
-      final CapellaDiagramPasteCommand siriusCmd = new CapellaDiagramPasteCommand(gmfSelection);
+      final CapellaDiagramPasteCommand siriusCmd = new CapellaDiagramPasteCommand(gmfSelection, true);
       _success = MiscUtil.transactionallyExecute(gmfSelection, siriusCmd);
       monitor_p.worked(1);
+      
       // Set format (layout and/or style) to pasted elements and move them under the mouse cursor
       if (_success) {
         // Keep mouse location
