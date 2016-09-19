8fc97e16e411e29cb51fa23adc6c9707e2f183b6
Tu Ton <minhtutonthat@gmail.com>
2016-09-19 15:22:38 +0200

    [1170] In Collection objects, feature index allows several times the same object leads to model corruption
    Bug: 1170
    Change-Id: I8ca0a1cf7dab4c91c4133334f4dd695d4486c952
    Signed-off-by: Tu Ton <minhtutonthat@gmail.com>

---
+++ common/plugins/org.polarsys.capella.common.ui/src/org/polarsys/capella/common/ui/toolkit/dialogs/OrderedUniqueTransferTreeListDialog.java
@@ -0,0 +1,65 @@
+/*******************************************************************************
+ * Copyright (c) 2016 THALES GLOBAL SERVICES.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *  
+ * Contributors:
+ *    Thales - initial API and implementation
+ *******************************************************************************/
+
+package org.polarsys.capella.common.ui.toolkit.dialogs;
+
+import java.util.List;
+
+import org.eclipse.emf.common.notify.AdapterFactory;
+import org.eclipse.emf.ecore.EObject;
+import org.eclipse.emf.transaction.TransactionalEditingDomain;
+import org.eclipse.swt.widgets.Shell;
+import org.polarsys.capella.common.ui.toolkit.viewers.data.AbstractData;
+import org.polarsys.capella.common.ui.toolkit.viewers.data.DataLabelProvider;
+import org.polarsys.capella.common.ui.toolkit.viewers.data.UniqueValidElementsTreeData;
+import org.polarsys.capella.common.ui.toolkit.viewers.transfer.OrderedTransferTreeListViewer;
+
+/**
+ * Dialog that displays a {@link OrderedTransferTreeListViewer} and does not accept duplicated elements
+ */
+public class OrderedUniqueTransferTreeListDialog extends OrderedTransferTreeListDialog {
+  /**
+   * Constructor.
+   * @param parentShell
+   * @param editingDomain
+   * @param adapterFactory
+   * @param dialogTitle
+   * @param dialogMessage
+   */
+  public OrderedUniqueTransferTreeListDialog(Shell parentShell, TransactionalEditingDomain editingDomain, AdapterFactory adapterFactory, String dialogTitle, String dialogMessage) {
+    super(parentShell, editingDomain, adapterFactory, dialogTitle, dialogMessage);
+  }
+
+  /**
+   * Constructor.
+   * @param parentShell
+   * @param dialogTitle
+   * @param dialogMessage
+   * @param leftLabelProvider
+   * @param rightLabelProvider
+   * @param leftViewerStyle
+   * @param rightViewerStyle
+   */
+  public OrderedUniqueTransferTreeListDialog(Shell parentShell, String dialogTitle, String dialogMessage,
+    DataLabelProvider leftLabelProvider, DataLabelProvider rightLabelProvider,
+    int leftViewerStyle, int rightViewerStyle)
+  {
+    super(parentShell, dialogTitle, dialogMessage, leftLabelProvider, rightLabelProvider, leftViewerStyle, rightViewerStyle);
+  }
+
+  /**
+   * @see org.polarsys.capella.common.ui.toolkit.dialogs.TransferTreeListDialog#createRightViewerData(java.util.List, java.lang.Object)
+   */
+  @Override
+  protected AbstractData createRightViewerData(List<? extends EObject> elements, Object context) {
+    return new UniqueValidElementsTreeData(elements, context);
+  }
+}
--- core/plugins/org.polarsys.capella.core.data.information.properties/src/org/polarsys/capella/core/data/information/properties/controllers/Collection_IndexController.java
+++ core/plugins/org.polarsys.capella.core.data.information.properties/src/org/polarsys/capella/core/data/information/properties/controllers/Collection_IndexController.java
@@ -10,7 +10,11 @@
  *******************************************************************************/
 package org.polarsys.capella.core.data.information.properties.controllers;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.eclipse.emf.ecore.EObject;
+import org.eclipse.emf.ecore.EStructuralFeature;
 import org.polarsys.capella.core.business.queries.IBusinessQuery;
 import org.polarsys.capella.core.business.queries.capellacore.BusinessQueriesProvider;
 import org.polarsys.capella.core.data.information.InformationPackage;
@@ -25,5 +29,25 @@ public class Collection_IndexController extends AbstractMultipleSemanticFieldCon
   @Override
   protected IBusinessQuery getReadOpenValuesQuery(EObject semanticElement) {
     return BusinessQueriesProvider.getInstance().getContribution(semanticElement.eClass(), InformationPackage.Literals.COLLECTION__INDEX);
+  }
+  
+  /**
+   * {@inheritDoc}
+   * Override this method to modify model when the order of indexes change
+   */
+  @Override
+  public List<EObject> writeOpenValues(EObject semanticElement, EStructuralFeature semanticFeature, List<EObject> values) {
+    List<EObject> result = new ArrayList<>();
+    if (null != values) {
+      List<EObject> modelCurrentList = readOpenValues(semanticElement, semanticFeature, false);
+      for (EObject currentModelObject : modelCurrentList) {
+        doRemoveOperationInWriteOpenValues(semanticElement, semanticFeature, currentModelObject);
+      }
+      for (EObject currentObject : values) {
+        doAddOperationInWriteOpenValues(semanticElement, semanticFeature, currentObject);
+        result.add(currentObject);
+      }
+    }
+    return result;
   }
 }
