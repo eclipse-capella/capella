626a1ea2c4cd103ed84252d1c27b11ff53ac1ba9
Felix Dorner <felix.dorner@gmail.com>
2016-09-20 10:12:00 +0200

    [1176] Fix viewpoint selection in new project wizard
    Bug: 1176
    Change-Id: If28433920295f1fa8a44e95e6524a0167413a6cd
    Signed-off-by: Felix Dorner <felix.dorner@gmail.com>

--- core/plugins/org.polarsys.capella.core.platform.sirius.ui.project/src/org/polarsys/capella/core/platform/sirius/ui/project/NewProjectWizard.java
+++ core/plugins/org.polarsys.capella.core.platform.sirius.ui.project/src/org/polarsys/capella/core/platform/sirius/ui/project/NewProjectWizard.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2006, 2015 THALES GLOBAL SERVICES.
+ * Copyright (c) 2006, 2016 THALES GLOBAL SERVICES.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -248,10 +248,8 @@ public class NewProjectWizard extends BasicNewResourceWizard {
    */
   protected Set<Viewpoint> getSelectedViewpoints() {
 
-    // Check which page was used to complete the wizard.
-    IWizardPage completingPage = getContainer().getCurrentPage();
     Set<Viewpoint> viewpoints = null;
-    if (completingPage == viewpointWizardPage) {
+    if (viewpointWizardPage.isPageComplete()) {
       // Get user selected viewpoints.
       viewpoints = new HashSet<Viewpoint>(0);
       Iterator<Entry<Viewpoint, Boolean>> iterator = viewpointsMap.entrySet().iterator();
