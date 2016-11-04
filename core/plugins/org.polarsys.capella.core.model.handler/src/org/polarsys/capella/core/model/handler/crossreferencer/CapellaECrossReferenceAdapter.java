74625041b444cf73312ba785e2b668eed446bd9d
pguilet <pierre.guilet@obeo.fr>
2016-11-04 16:15:29 +0100

    [1286] Fix wrong cross references handling
    - Do not call unsetTarget on detachement notification if the object has
    already been reattached (eContainer != null). When receiving changes
    in some contexts, there is no guaranty that the notification
    order will be the same that the initial command impact.
    REMOVE and ADD notification can come unordered.
    
    Bug: 1286
    Change-Id: Icbc1aa90dc6a2f0f74153fb6348f44ebb5711789
    Signed-off-by: pguilet <pierre.guilet@obeo.fr>

--- core/plugins/org.polarsys.capella.core.model.handler/src/org/polarsys/capella/core/model/handler/crossreferencer/CapellaECrossReferenceAdapter.java
+++ core/plugins/org.polarsys.capella.core.model.handler/src/org/polarsys/capella/core/model/handler/crossreferencer/CapellaECrossReferenceAdapter.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2006, 2015 THALES GLOBAL SERVICES.
+ * Copyright (c) 2006, 2016 THALES GLOBAL SERVICES.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -147,17 +147,26 @@ public class CapellaECrossReferenceAdapter extends SemanticCrossReferencer imple
         }
         // 'unset' case equivalent to remove case...
       case Notification.REMOVE:
-        EObject oldValue = null;
-        try {
-          oldValue = EObject.class.cast(notification.getOldValue());
-        } catch (Exception exception_p) {
-          // Do not deal with this value.
-        }
-        if (null != oldValue) {
-          // Free references pointing this element. Call unsetTarget to make sure inverse cross referencer cleans its data accordingly.
-          unsetTarget(oldValue);
-        }
-      break;
+          EObject oldValue = null;
+          try {
+              oldValue = EObject.class.cast(notification.getOldValue());
+          } catch (Exception exception_p) {
+              // Do not deal with this value.
+          }
+          // The add/remove notification order is not guaranteed in all
+          // execution context. It means that we can have an Add
+          // notification followed by a Remove one whereas it should be the
+          // inverse.
+          // So we have to check before doing the unset that it has not
+          // been added into another reference by checking that it's
+          // container is null.
+          if (null != oldValue && oldValue.eContainer() == null) {
+              // Free references pointing this element. Call unsetTarget
+              // to make sure inverse cross referencer cleans its data
+              // accordingly.
+              unsetTarget(oldValue);
+          }
+          break;
       case Notification.ADD_MANY:
         for (Object value : (Collection<?>) notification.getNewValue()) {
           if (value instanceof EObject) {
