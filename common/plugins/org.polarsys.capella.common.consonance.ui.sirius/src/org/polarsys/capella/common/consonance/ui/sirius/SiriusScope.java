aa5ff0d72404d2dbc92a7f3266c899a49bfe16a5
Tu Ton <minhtutonthat@gmail.com>
2016-11-04 16:17:13 +0100

    [1285] Regression on aird diffmerge
    Bug: 1285
    Change-Id: I6f0ff1206842a8d3fa25ac5ef99fdaff870c8751
    Signed-off-by: Tu Ton <minhtutonthat@gmail.com>

--- common/plugins/org.polarsys.capella.common.consonance.ui.sirius/src/org/polarsys/capella/common/consonance/ui/sirius/SiriusScope.java
+++ common/plugins/org.polarsys.capella.common.consonance.ui.sirius/src/org/polarsys/capella/common/consonance/ui/sirius/SiriusScope.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2006, 2015 THALES GLOBAL SERVICES.
+ * Copyright (c) 2006, 2016 THALES GLOBAL SERVICES.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -22,6 +22,7 @@ import org.eclipse.emf.ecore.resource.Resource;
 import org.eclipse.emf.ecore.resource.ResourceSet;
 import org.eclipse.emf.edit.domain.EditingDomain;
 import org.eclipse.gmf.runtime.notation.NotationPackage;
+import org.eclipse.sirius.diagram.DiagramPackage;
 import org.eclipse.sirius.viewpoint.DAnalysis;
 import org.eclipse.sirius.viewpoint.DSemanticDecorator;
 import org.eclipse.sirius.viewpoint.ViewpointPackage;
@@ -38,7 +39,8 @@ public class SiriusScope extends GMFScope {
     Arrays.asList(
         ViewpointPackage.eINSTANCE,
         StylePackage.eINSTANCE,
-        NotationPackage.eINSTANCE
+        NotationPackage.eINSTANCE,
+        DiagramPackage.eINSTANCE
     );
   
   
@@ -85,7 +87,7 @@ public class SiriusScope extends GMFScope {
    */
   @Override
   protected boolean isSuitableFor(Resource resource, EObject root) {
-    boolean result = isViewpointResource(resource) == isViewpointElement(root);
+    boolean result = isViewpointResource(resource) && isViewpointElement(root);
     return result;
   }
   
