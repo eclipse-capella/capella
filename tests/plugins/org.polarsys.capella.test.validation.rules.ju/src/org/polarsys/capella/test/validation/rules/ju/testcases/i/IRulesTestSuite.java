8ec9f0a7ab60d9fcabb3af42d54b6cfa9aa005d1
Felix Dorner <felix.dorner@gmail.com>
2016-12-15 10:18:41 +0100

    [1348] New rule checks that FE with equal ports have the same name
    This rule is disabled by default and has id I_35.
    
    Bug: 1348
    Change-Id: I38b27b0d4c06ac88f39a46f9e07ec9639080e9f5
    Signed-off-by: Felix Dorner <felix.dorner@gmail.com>

--- tests/plugins/org.polarsys.capella.test.validation.rules.ju/src/org/polarsys/capella/test/validation/rules/ju/testcases/i/IRulesTestSuite.java
+++ tests/plugins/org.polarsys.capella.test.validation.rules.ju/src/org/polarsys/capella/test/validation/rules/ju/testcases/i/IRulesTestSuite.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2006, 2015 THALES GLOBAL SERVICES.
+ * Copyright (c) 2006, 2017 THALES GLOBAL SERVICES.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -63,6 +63,7 @@ public class IRulesTestSuite extends BasicTestSuite {
     tests.add(new Rule_I_33());
     tests.add(new Rule_I_34());
     tests.add(new Rule_I_34_OnFragment());
+    tests.add(new Rule_I_35());
     return tests;
   }
 
