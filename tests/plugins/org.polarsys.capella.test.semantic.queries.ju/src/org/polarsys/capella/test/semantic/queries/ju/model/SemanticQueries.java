38089ea548989246043514c3138cad0a474b9653
Joao Barata <jean.barata@thalesgroup.com>
2017-02-28 16:15:48 +0100

    [releng] remote test suite
    Change-Id: I26fdcd02ff60ca829ff448fbf2a9279bde5a5f62
    Signed-off-by: Joao Barata <jean.barata@thalesgroup.com>

--- tests/plugins/org.polarsys.capella.test.semantic.queries.ju/src/org/polarsys/capella/test/semantic/queries/ju/model/SemanticQueries.java
+++ tests/plugins/org.polarsys.capella.test.semantic.queries.ju/src/org/polarsys/capella/test/semantic/queries/ju/model/SemanticQueries.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2016 THALES GLOBAL SERVICES.
+ * Copyright (c) 2017 THALES GLOBAL SERVICES.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -200,6 +200,6 @@ public abstract class SemanticQueries extends AbstractSemanticQueryTestCase {
 
 	@Override
 	public List<String> getRequiredTestModels() {
-		return Collections.singletonList(SemanticQueries.class.getSimpleName());
+		return Collections.singletonList("semanticqueries");
 	}
 }
