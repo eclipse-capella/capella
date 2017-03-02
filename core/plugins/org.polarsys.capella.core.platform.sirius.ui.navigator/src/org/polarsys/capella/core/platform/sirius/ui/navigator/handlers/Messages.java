da615f2457645d99ed2f0f77bbb2f578bd5b3bd3
Jeremy Aubry <jaubry@obeo.fr>
2017-03-02 15:54:32 +0100

    [1483] Remove the technical command "Fix Custom Features"
    Bug: 1483
    Change-Id: Ia0f3402d56eff58d2fe1aeb8914cb1dc83b65ba8
    Signed-off-by: Jeremy Aubry <jaubry@obeo.fr>

--- core/plugins/org.polarsys.capella.core.platform.sirius.ui.navigator/src/org/polarsys/capella/core/platform/sirius/ui/navigator/handlers/Messages.java
+++ core/plugins/org.polarsys.capella.core.platform.sirius.ui.navigator/src/org/polarsys/capella/core/platform/sirius/ui/navigator/handlers/Messages.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2006, 2016 THALES GLOBAL SERVICES.
+ * Copyright (c) 2006, 2017 THALES GLOBAL SERVICES.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -40,8 +40,6 @@ public class Messages extends NLS {
 
   public static String RemoveHiddenElementsHandler_ConfirmRefreshDialog_Text;
   
-  public static String FixCustomFeaturesJobName;
-
   public static String FixDAnnotationsJobName;
 
   static {
