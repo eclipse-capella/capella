8b96b2cd97b148f02b9338bb58f00c6c64b6b3d8
Tu Ton <minhtutonthat@gmail.com>
2017-03-03 18:29:57 +0100

    [Tests] Add new tests for the following actions: Refresh All Sub Representations, Fix Filters On All Representations, Remove Hidden Elements
    Change-Id: Ia3a8f9020d9a5dc2b9a187e1c173c11e535a2247
    Signed-off-by: Tu Ton <minhtutonthat@gmail.com>

--- core/plugins/org.polarsys.capella.core.platform.sirius.ui.navigator/src/org/polarsys/capella/core/platform/sirius/ui/navigator/handlers/FixDiagramFiltersHandler.java
+++ core/plugins/org.polarsys.capella.core.platform.sirius.ui.navigator/src/org/polarsys/capella/core/platform/sirius/ui/navigator/handlers/FixDiagramFiltersHandler.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2006, 2014 THALES GLOBAL SERVICES.
+ * Copyright (c) 2006, 2017 THALES GLOBAL SERVICES.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -39,7 +39,7 @@ import org.polarsys.capella.core.sirius.ui.helper.SessionHelper;
  */
 public class FixDiagramFiltersHandler extends AbstractDiagramCommandHandler {
 
-	protected class FixDiagramsJob extends WorkspaceJob {
+	public class FixDiagramsJob extends WorkspaceJob {
 
 		private Session _session;
 		private String _resName;
