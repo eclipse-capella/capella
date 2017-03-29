45188227620c19761a6ae025837d20761c35cc1c
Damien Filliat <damien.filliat@thalesgroup.com>
2017-03-29 13:46:13 +0200

    [336] Resolve Sonar issues
    Bug: 336
    
    Change-Id: Iaa5b3a30e28ab120aec63a54e679bef698b26ddb
    Signed-off-by: Damien Filliat <damien.filliat@thalesgroup.com>

--- core/plugins/org.polarsys.capella.core.platform.eclipse.ui.trace/src/org/polarsys/capella/core/platform/eclipse/capella/ui/trace/views/components/CapellaModelTreeViewer.java
+++ core/plugins/org.polarsys.capella.core.platform.eclipse.ui.trace/src/org/polarsys/capella/core/platform/eclipse/capella/ui/trace/views/components/CapellaModelTreeViewer.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2006, 2014 THALES GLOBAL SERVICES.
+ * Copyright (c) 2006, 2017 THALES GLOBAL SERVICES.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -60,7 +60,6 @@ public class CapellaModelTreeViewer {
     compo.setLayout(layout);
 
     GridData layoutData = new GridData();
-    layoutData = new GridData();
     layoutData.grabExcessHorizontalSpace = true;
     layoutData.grabExcessVerticalSpace = true;
     layoutData.horizontalAlignment = GridData.FILL;
