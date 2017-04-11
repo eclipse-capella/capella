78d38d038a0f1aa26b13eff2386ba476a4539c0d
Tu Ton <minhtutonthat@gmail.com>
2017-04-11 14:30:17 +0200

    [1551] JUnit tests for Capability Realization BQs
    Bug: 1551
    Change-Id: I312f52a81f7a4d80325e33de686aaed3a3fa78a7
    Signed-off-by: Tu Ton <minhtutonthat@gmail.com>

--- tests/plugins/org.polarsys.capella.test.semantic.queries.ju/src/org/polarsys/capella/test/semantic/queries/ju/testsuites/SemanticQueriesTestSuite.java
+++ tests/plugins/org.polarsys.capella.test.semantic.queries.ju/src/org/polarsys/capella/test/semantic/queries/ju/testsuites/SemanticQueriesTestSuite.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2006, 2016 THALES GLOBAL SERVICES.
+ * Copyright (c) 2006, 2017 THALES GLOBAL SERVICES.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -14,20 +14,27 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
-import junit.framework.Test;
-
 import org.polarsys.capella.test.framework.api.BasicTestArtefact;
 import org.polarsys.capella.test.framework.api.BasicTestSuite;
 import org.polarsys.capella.test.semantic.queries.ju.model.SemanticQueries;
 import org.polarsys.capella.test.semantic.queries.ju.testcases.AbstractFunction_mother_activity_allocation;
 import org.polarsys.capella.test.semantic.queries.ju.testcases.AbstractFunction_mother_function_allocation;
 import org.polarsys.capella.test.semantic.queries.ju.testcases.AvailableForTypeClassExistTest;
+import org.polarsys.capella.test.semantic.queries.ju.testcases.CapabilityRealization_InvolvedActors;
+import org.polarsys.capella.test.semantic.queries.ju.testcases.CapabilityRealization_InvolvedComponents;
+import org.polarsys.capella.test.semantic.queries.ju.testcases.CapabilityRealization_InvolvedFunctionalChains;
+import org.polarsys.capella.test.semantic.queries.ju.testcases.CapabilityRealization_InvolvedFunctions;
+import org.polarsys.capella.test.semantic.queries.ju.testcases.CapabilityRealization_OwnedFunctionalChains;
+import org.polarsys.capella.test.semantic.queries.ju.testcases.CapabilityRealization_RealizedCapability;
+import org.polarsys.capella.test.semantic.queries.ju.testcases.CapabilityRealization_RealizingCapabilityRealization;
 import org.polarsys.capella.test.semantic.queries.ju.testcases.CapellaElement_applied_property_value_groups;
 import org.polarsys.capella.test.semantic.queries.ju.testcases.CapellaElement_applied_property_values;
 import org.polarsys.capella.test.semantic.queries.ju.testcases.EntryExitPoint_ParentRegionTest;
 import org.polarsys.capella.test.semantic.queries.ju.testcases.PropertyValueGroup_applying_valued_element;
 import org.polarsys.capella.test.semantic.queries.ju.testcases.PropertyValue_applying_valued_element;
 import org.polarsys.capella.test.semantic.queries.ju.testcases.State_OwnedEntryExitPointsTest;
+
+import junit.framework.Test;
 
 public class SemanticQueriesTestSuite extends BasicTestSuite {
 
@@ -44,6 +51,14 @@ public class SemanticQueriesTestSuite extends BasicTestSuite {
     tests.add(new PropertyValueGroup_applying_valued_element());
     tests.add(new EntryExitPoint_ParentRegionTest());
     tests.add(new State_OwnedEntryExitPointsTest());
+    tests.add(new CapabilityRealization_InvolvedActors());
+    tests.add(new CapabilityRealization_InvolvedComponents());
+    tests.add(new CapabilityRealization_InvolvedFunctionalChains());
+    tests.add(new CapabilityRealization_InvolvedFunctions());
+    tests.add(new CapabilityRealization_OwnedFunctionalChains());
+    tests.add(new CapabilityRealization_RealizedCapability());
+    tests.add(new CapabilityRealization_RealizingCapabilityRealization());
+    
     return tests;
   }
   
