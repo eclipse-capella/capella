e22f3168e696a6301bbb9d7cdc81d9c4077066be
Tu Ton <minhtutonthat@gmail.com>
2017-05-03 13:24:54 +0200

    [1584]Sequence messages (except for Lost messages, Delete messages and Create messages) should not be deleted directly to avoid invalid model elements (EMF Errors)
    Bug: 1584
    Change-Id: I9530d9934981319d2e6893dec1bece37f9128911
    Signed-off-by: Tu Ton <minhtutonthat@gmail.com>

--- core/plugins/org.polarsys.capella.core.platform.sirius.ui.actions/src/org/polarsys/capella/core/platform/sirius/ui/actions/CapellaDeleteAction.java
+++ core/plugins/org.polarsys.capella.core.platform.sirius.ui.actions/src/org/polarsys/capella/core/platform/sirius/ui/actions/CapellaDeleteAction.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2006, 2014 THALES GLOBAL SERVICES.
+ * Copyright (c) 2006, 2017 THALES GLOBAL SERVICES.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -186,10 +186,10 @@ public class CapellaDeleteAction extends AbstractCommandActionHandler implements
       }
     } else if (elt_p instanceof SequenceMessage) {
       SequenceMessage msg = (SequenceMessage) elt_p;
-      if ((msg.getSendingEnd() == null) || (msg.getReceivingEnd() == null)) {
+      if (msg.getReceivingEnd() == null || msg.getKind() == MessageKind.CREATE || msg.getKind() == MessageKind.DELETE) {
         return false;
-      }
-      return (msg.getKind() == MessageKind.SYNCHRONOUS_CALL) || (msg.getKind() == MessageKind.ASYNCHRONOUS_CALL) || (msg.getKind() == MessageKind.REPLY);
+      } 
+      return true;
     } else if (elt_p instanceof ModelInformation) {
       return true;
     }
