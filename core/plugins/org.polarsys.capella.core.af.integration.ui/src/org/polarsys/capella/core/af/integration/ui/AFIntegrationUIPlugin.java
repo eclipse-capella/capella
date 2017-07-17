89305781a4dd076dfb9795a265ca87a150a1c818
Joao Barata <jean.barata@thalesgroup.com>
2017-07-17 17:49:00 +0200

    [1408] Capella headless support
    Bug: 1408
    Change-Id: I22de945f190895f4ffa62f80dcf90308d9e64985
    Signed-off-by: Joao Barata <jean.barata@thalesgroup.com>

---
+++ core/plugins/org.polarsys.capella.core.af.integration.ui/src/org/polarsys/capella/core/af/integration/ui/AFIntegrationUIPlugin.java
@@ -0,0 +1,71 @@
+/*******************************************************************************
+ * Copyright (c) 2017 THALES GLOBAL SERVICES.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *  
+ * Contributors:
+ *    Thales - initial API and implementation
+ *******************************************************************************/
+package org.polarsys.capella.core.af.integration.ui;
+
+import org.eclipse.ui.plugin.AbstractUIPlugin;
+import org.osgi.framework.BundleContext;
+import org.polarsys.capella.core.af.integration.ui.listener.CapellaExplorerUpdater;
+import org.polarsys.capella.core.af.integration.ui.listener.SemanticBrowserUpdater;
+import org.polarsys.kitalpha.emde.extension.ModelExtensionHelper;
+import org.polarsys.kitalpha.emde.extension.ModelExtensionOverallListener;
+
+/**
+ * Plugin activator
+ */
+public class AFIntegrationUIPlugin extends AbstractUIPlugin {
+
+	// The shared instance
+	private static AFIntegrationUIPlugin plugin;
+
+	//
+	private final ModelExtensionOverallListener[] listeners = { new CapellaExplorerUpdater(), new SemanticBrowserUpdater() };
+
+	/**
+	 * The constructor
+	 */
+	public AFIntegrationUIPlugin() {
+	}
+
+	/**
+	 * @see org.eclipse.ui.plugin.AbstractUIPlugin#start(org.osgi.framework.BundleContext)
+	 */
+	public void start(BundleContext context) throws Exception {
+		super.start(context);
+		plugin = this;
+    for (ModelExtensionOverallListener l : listeners) {
+      ModelExtensionHelper.addOverallListener(l);
+    }
+	}
+
+	/**
+	 * @see org.eclipse.ui.plugin.AbstractUIPlugin#stop(org.osgi.framework.BundleContext)
+	 */
+	public void stop(BundleContext context) throws Exception {
+		plugin = null;
+    for (ModelExtensionOverallListener l : listeners) {
+      ModelExtensionHelper.removeOverallListener(l);
+    }
+		super.stop(context);
+	}
+
+	/**
+	 * Returns the shared instance
+	 * 
+	 * @return the shared instance
+	 */
+	public static AFIntegrationUIPlugin getDefault() {
+		return plugin;
+	}
+
+	public static String getSymbolicName() {
+		return plugin.getBundle().getSymbolicName();
+	}
+}
--- core/plugins/org.polarsys.capella.core.af.integration.ui/src/org/polarsys/capella/core/af/integration/ui/services/DisplaySelectionServiceImplementation.java
+++ core/plugins/org.polarsys.capella.core.af.integration.ui/src/org/polarsys/capella/core/af/integration/ui/services/DisplaySelectionServiceImplementation.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2006, 2014 THALES GLOBAL SERVICES.
+ * Copyright (c) 2006, 2017 THALES GLOBAL SERVICES.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -8,12 +8,13 @@
  * Contributors:
  *    Thales - initial API and implementation
  *******************************************************************************/
-package org.polarsys.capella.core.af.integration.services;
+package org.polarsys.capella.core.af.integration.ui.services;
 
 import java.util.Arrays;
 
 import org.eclipse.jface.dialogs.MessageDialog;
 import org.eclipse.ui.PlatformUI;
+import org.polarsys.capella.core.af.integration.services.CapellaServiceImplementation;
 import org.polarsys.kitalpha.ad.viewpoint.coredomain.viewpoint.model.Service;
 import org.polarsys.kitalpha.ad.viewpoint.utils.ModelAccessor;
 
