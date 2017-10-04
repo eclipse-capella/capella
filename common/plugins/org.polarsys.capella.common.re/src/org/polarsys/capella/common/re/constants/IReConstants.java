6dec7fe850a4f4b8aa2ff2b43f30c3f6b9b8fb79
Felix Dorner <felix.dorner@gmail.com>
2017-10-04 18:26:10 +0200

    [1743] Show readOnly checkbox on Create and Update RPL Wizard
    Change-Id: Ia6e220cfdfd5784b19d081a0854e13473d1125e7
    Signed-off-by: Felix Dorner <felix.dorner@gmail.com>

--- common/plugins/org.polarsys.capella.common.re/src/org/polarsys/capella/common/re/constants/IReConstants.java
+++ common/plugins/org.polarsys.capella.common.re/src/org/polarsys/capella/common/re/constants/IReConstants.java
@@ -1,10 +1,10 @@
 /*******************************************************************************
- * Copyright (c) 2006, 2016 THALES GLOBAL SERVICES.
+ * Copyright (c) 2006, 2017 THALES GLOBAL SERVICES.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v10.html
- *  
+ *
  * Contributors:
  *    Thales - initial API and implementation
  *******************************************************************************/
@@ -68,6 +68,7 @@ public class IReConstants {
   public static final String LOCATION_HANDLER = "LOCATION_HANDLER";
   public static final String REPLICABLE_ELEMENT_HANDLER = "REPLICABLE_ELEMENT_HANDLER";
   public static final String PROPERTY__TARGET_NAME = "targetName";
+  public static final String PROPERTY__READONLY = "readOnly";
 
   // Virtual links are:
   // When we create a replica from a rec, we create for all elements of the
---
+++ common/plugins/org.polarsys.capella.common.re/src/org/polarsys/capella/common/re/properties/ReadOnlyProperty.java
@@ -0,0 +1,95 @@
+/*******************************************************************************
+ * Copyright (c) 2017 THALES GLOBAL SERVICES.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *   
+ * Contributors:
+ *    Thales - initial API and implementation
+ *******************************************************************************/
+package org.polarsys.capella.common.re.properties;
+
+import org.eclipse.core.runtime.IStatus;
+import org.eclipse.core.runtime.Status;
+import org.polarsys.capella.common.flexibility.properties.property.AbstractProperty;
+import org.polarsys.capella.common.flexibility.properties.schema.ICompoundProperty;
+import org.polarsys.capella.common.flexibility.properties.schema.IEditableProperty;
+import org.polarsys.capella.common.flexibility.properties.schema.IModifiedProperty;
+import org.polarsys.capella.common.flexibility.properties.schema.IProperty;
+import org.polarsys.capella.common.flexibility.properties.schema.IPropertyContext;
+import org.polarsys.capella.common.re.CatalogElement;
+import org.polarsys.capella.common.re.constants.IReConstants;
+
+public class ReadOnlyProperty extends AbstractProperty implements ICompoundProperty, IEditableProperty, IModifiedProperty {
+
+  /**
+   * {@inheritDoc}
+   */
+  @Override
+  public String[] getRelatedProperties() {
+    return new String[] { };
+  }
+
+  /**
+   * {@inheritDoc}
+   */
+  @Override
+  public Object getValue(IPropertyContext context) {
+    CatalogElement element =
+        (CatalogElement) context.getCurrentValue(context.getProperties().getProperty(IReConstants.PROPERTY__REPLICABLE_ELEMENT__INITIAL_TARGET));
+    return element.isReadOnly();
+  }
+
+  /**
+   * {@inheritDoc}
+   */
+  @Override
+  public IStatus validate(Object newValue, IPropertyContext context) {
+    return Status.OK_STATUS;
+  }
+
+  /**
+   * {@inheritDoc}
+   */
+  @Override
+  public Object getType() {
+    return Boolean.class;
+  }
+
+  /**
+   * {@inheritDoc}
+   */
+  @Override
+  public Object toType(Object value, IPropertyContext context) {
+    return value;
+  }
+
+  /**
+   * {@inheritDoc}
+   */
+  @Override
+  public void setValue(IPropertyContext context) {
+    CatalogElement element =
+        (CatalogElement) context.getCurrentValue(context.getProperties().getProperty(IReConstants.PROPERTY__REPLICABLE_ELEMENT__INITIAL_TARGET));
+    Boolean current = (Boolean) context.getCurrentValue(this);
+    element.setReadOnly(current);
+
+  }
+
+  /**
+   * {@inheritDoc}
+   */
+  @Override
+  public void updatedValue(IProperty property, IPropertyContext context) {
+
+  }
+
+  /**
+   * {@inheritDoc}
+   */
+  @Override
+  public boolean isModified(IPropertyContext context) {
+    return true;
+  }
+}
