9f21cd4ae57cb7eb6ac94deaf5a05e5e9df72845
Roland Bary <roland.bary@obeo.fr>
2017-10-27 18:03:44 +0200

    [1812] Deactivate by default Sirius diagram Auto-scale preference
    Bug: 1812
    Change-Id: I21f1ce4d34e7c2eb5ce24eaa6fb80837526f5d11
    Signed-off-by: Roland Bary <roland.bary@obeo.fr>

--- common/plugins/org.polarsys.capella.common.platform.sirius.customization/src/org/polarsys/capella/common/platform/sirius/customisation/SiriusPlugin.java
+++ common/plugins/org.polarsys.capella.common.platform.sirius.customization/src/org/polarsys/capella/common/platform/sirius/customisation/SiriusPlugin.java
@@ -11,12 +11,13 @@
 package org.polarsys.capella.common.platform.sirius.customisation;
 
 import org.eclipse.sirius.business.api.preferences.SiriusPreferencesKeys;
+import org.eclipse.sirius.diagram.ui.provider.DiagramUIPlugin;
+import org.eclipse.sirius.diagram.ui.tools.api.preferences.SiriusDiagramUiPreferencesKeys;
 import org.eclipse.sirius.viewpoint.provider.SiriusEditPlugin;
 import org.eclipse.ui.IWorkbenchPreferenceConstants;
 import org.eclipse.ui.PlatformUI;
 import org.eclipse.ui.plugin.AbstractUIPlugin;
 import org.osgi.framework.BundleContext;
-
 import org.polarsys.capella.common.platform.sirius.customisation.uicallback.SiriusUiCallBack;
 
 /**
@@ -25,52 +26,57 @@ import org.polarsys.capella.common.platform.sirius.customisation.uicallback.Siri
  */
 public class SiriusPlugin extends AbstractUIPlugin {
 
-    /** The plug-in ID. */
-    public static final String PLUGIN_ID = "org.polarsys.capella.common.platform.sirius.customisation"; //$NON-NLS-1$
+  /** The plug-in ID. */
+  public static final String PLUGIN_ID = "org.polarsys.capella.common.platform.sirius.customisation"; //$NON-NLS-1$
 
-    // The shared instance
-    private static SiriusPlugin plugin;
+  // The shared instance
+  private static SiriusPlugin plugin;
 
-    /**
-     * The default constructor.
-     */
-    public SiriusPlugin() {
-    }
+  /**
+   * The default constructor.
+   */
+  public SiriusPlugin() {
+  }
 
-    /**
-     * {@inheritDoc}
-     * 
-     * @see org.eclipse.ui.plugin.AbstractUIPlugin#start(org.osgi.framework.BundleContext)
-     */
-    public void start(BundleContext context) throws Exception {
-        super.start(context);
-        plugin = this;
-        // Initialize the preferences for Sirius
-        PlatformUI.getPreferenceStore().setValue(IWorkbenchPreferenceConstants.PROMPT_WHEN_SAVEABLE_STILL_OPEN, false);
+  /**
+   * {@inheritDoc}
+   * 
+   * @see org.eclipse.ui.plugin.AbstractUIPlugin#start(org.osgi.framework.BundleContext)
+   */
+  public void start(BundleContext context) throws Exception {
+    super.start(context);
+    plugin = this;
+    // Initialize the preferences for Sirius
+    PlatformUI.getPreferenceStore().setValue(IWorkbenchPreferenceConstants.PROMPT_WHEN_SAVEABLE_STILL_OPEN, false);
 
-        // Change the UICallBack to have a specific Session displayed name
-        SiriusEditPlugin.getPlugin().setUiCallback(new SiriusUiCallBack(SiriusEditPlugin.getPlugin().getUiCallback()));
-        
-        // Allow by default aird fragment with no representation creation
-        SiriusEditPlugin.getPlugin().getPreferenceStore().setValue(SiriusPreferencesKeys.PREF_EMPTY_AIRD_FRAGMENT_ON_CONTROL.name(), true);
-    }
+    // Change the UICallBack to have a specific Session displayed name
+    SiriusEditPlugin.getPlugin().setUiCallback(new SiriusUiCallBack(SiriusEditPlugin.getPlugin().getUiCallback()));
 
-    /**
-     * {@inheritDoc}
-     * 
-     * @see org.eclipse.ui.plugin.AbstractUIPlugin#stop(org.osgi.framework.BundleContext)
-     */
-    public void stop(BundleContext context) throws Exception {
-        plugin = null;
-        super.stop(context);
-    }
+    // Allow by default aird fragment with no representation creation
+    SiriusEditPlugin.getPlugin().getPreferenceStore()
+        .setValue(SiriusPreferencesKeys.PREF_EMPTY_AIRD_FRAGMENT_ON_CONTROL.name(), true);
 
-    /**
-     * Returns the shared instance.
-     * 
-     * @return the shared instance
-     */
-    public static SiriusPlugin getDefault() {
-        return plugin;
-    }
+    // Deactivate by default Sirius Auto-scale preference
+    DiagramUIPlugin.getPlugin().getPreferenceStore()
+        .setValue(SiriusDiagramUiPreferencesKeys.PREF_SCALE_DIAGRAMS_ON_EXPORT.name(), false);
+  }
+
+  /**
+   * {@inheritDoc}
+   * 
+   * @see org.eclipse.ui.plugin.AbstractUIPlugin#stop(org.osgi.framework.BundleContext)
+   */
+  public void stop(BundleContext context) throws Exception {
+    plugin = null;
+    super.stop(context);
+  }
+
+  /**
+   * Returns the shared instance.
+   * 
+   * @return the shared instance
+   */
+  public static SiriusPlugin getDefault() {
+    return plugin;
+  }
 }
