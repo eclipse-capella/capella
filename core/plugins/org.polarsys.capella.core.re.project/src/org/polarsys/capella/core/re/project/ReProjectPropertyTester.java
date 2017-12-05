88df20e1018cbff133be853f30cf1a447767f9fc
Felix Dorner <felix.dorner@gmail.com>
2017-12-05 11:48:39 +0100

    [1780] Create REC of entire library
    Adds a new functionality to create REC from entire libraries. The set of
    elements in such a REC is calculated as follows:
    
    The set of considered elements consists of all non-template elements,
    excluding REC Catalogs and their content trees. The set is then searched
    for elements with external references. If such an element is found, its
    content tree (including the element itself) is removed from the set. It
    is expected that these "REC Libraries" are self-contained, but
    external references may still occur if:
    
     - A library element references an element in the template
     - A library is referencing a second library
    
    The REC context menu has been redesigned: To create/update 'standard'
    REC use REC/RPL->Create/Update REC->From selection... In library
    context, use REC/RPL->Create/Update REC->With whole library content...
    to create/update a REC which includes the whole library.
    
    Change-Id: If317da363211a139f1af75b68336a22d224c6431
    Signed-off-by: Felix Dorner <felix.dorner@gmail.com>

---
+++ core/plugins/org.polarsys.capella.core.re.project/src/org/polarsys/capella/core/re/project/ReProjectPropertyTester.java
@@ -0,0 +1,72 @@
+/*******************************************************************************
+ * Copyright (c) 2017 THALES GLOBAL SERVICES.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *   
+ * Contributors:
+ *    Thales - initial API and implementation
+ *******************************************************************************/
+package org.polarsys.capella.core.re.project;
+
+import org.eclipse.core.expressions.PropertyTester;
+import org.eclipse.emf.ecore.EObject;
+import org.eclipse.emf.ecore.util.EcoreUtil;
+import org.polarsys.capella.common.helpers.EObjectExt;
+import org.polarsys.capella.common.re.CatalogElement;
+import org.polarsys.capella.common.re.CatalogElementKind;
+import org.polarsys.capella.common.re.CatalogElementLink;
+import org.polarsys.capella.common.re.RePackage;
+import org.polarsys.capella.core.data.capellamodeller.Library;
+
+public class ReProjectPropertyTester extends PropertyTester {
+
+  /**
+   * Is the currently selected EObject in a library. TODO move somewhere else for 1.3.0
+   */
+  public final static String P_IN_LIBRARY = "inLibrary"; //$NON-NLS-1$
+
+
+  /**
+   * FIXME merge with isRecElement in RePropertyTester, see
+   * https://bugs.polarsys.org/show_bug.cgi?id=1843
+   */
+  public final static String P_IS_REC_ELEMENT = "isRecElement"; //$NON-NLS-1$
+
+  @Override
+  public boolean test(Object receiver, String property, Object[] args, Object expectedValue) {
+
+    if (P_IN_LIBRARY.equals(property)) {
+        return EcoreUtil.getRootContainer((EObject) receiver) instanceof Library;
+    }
+
+    if (P_IS_REC_ELEMENT.equals(property)) {
+      return isRecElement(receiver);
+    }
+
+    throw new IllegalArgumentException("unknown property"); //$NON-NLS-1$
+
+  }
+
+  private boolean isRecElement(Object receiver) {
+    if (receiver instanceof EObject) {
+      for (CatalogElementLink link : EObjectExt.<CatalogElementLink> getReferencers((EObject) receiver,
+          RePackage.Literals.CATALOG_ELEMENT_LINK__TARGET)) {
+        if (isRecCatalogElement(link.getSource())) {
+          return true;
+        }
+      }
+    }
+    return false;
+  }
+
+  private boolean isRecCatalogElement(Object receiver) {
+    if (receiver instanceof CatalogElement) {
+      CatalogElement ce = (CatalogElement) receiver;
+      return ce.getKind() == CatalogElementKind.REC || ce.getKind() == CatalogElementKind.REC_RPL;
+    }
+    return false;
+  }
+
+}
---
+++ core/plugins/org.polarsys.capella.core.re.project/src/org/polarsys/capella/core/re/project/handlers/ProjectRecHandler.java
@@ -0,0 +1,124 @@
+/*******************************************************************************
+ * Copyright (c) 2017 THALES GLOBAL SERVICES.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *    Thales - initial API and implementation
+ *******************************************************************************/
+package org.polarsys.capella.core.re.project.handlers;
+
+import java.lang.reflect.InvocationTargetException;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.concurrent.atomic.AtomicReference;
+
+import org.eclipse.core.commands.ExecutionException;
+import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.core.runtime.NullProgressMonitor;
+import org.eclipse.emf.ecore.EObject;
+import org.eclipse.jface.dialogs.ProgressMonitorDialog;
+import org.eclipse.jface.operation.IRunnableWithProgress;
+import org.eclipse.ui.PlatformUI;
+import org.polarsys.capella.common.ef.command.ICommand;
+import org.polarsys.capella.common.helpers.TransactionHelper;
+import org.polarsys.capella.common.helpers.operations.LongRunningListenersRegistry;
+import org.polarsys.capella.common.re.constants.IReConstants;
+import org.polarsys.capella.common.re.handlers.scope.DefaultDependenciesHandler;
+import org.polarsys.capella.common.re.ui.handlers.uihead.UIHeadHandler;
+import org.polarsys.capella.core.re.commands.CreateRecCommand;
+import org.polarsys.capella.core.re.commands.UpdateCurCommand;
+import org.polarsys.capella.core.re.project.ReProjectScope;
+import org.polarsys.capella.core.transition.common.commands.CommandHandler;
+import org.polarsys.capella.core.transition.common.commands.DefaultCommand;
+import org.polarsys.capella.core.transition.common.commands.LauncherCommand;
+import org.polarsys.capella.core.transition.common.handlers.IHandler;
+import org.polarsys.capella.core.transition.common.transposer.SharedWorkflowActivityParameter;
+import org.polarsys.kitalpha.cadence.core.api.parameter.GenericParameter;
+import org.polarsys.kitalpha.transposer.rules.handler.rules.api.IContext;
+
+public abstract class ProjectRecHandler extends CommandHandler {
+
+  @Override
+  public Object execute(Collection<?> selection, String name) throws ExecutionException {
+    try {
+      LongRunningListenersRegistry.getInstance().operationStarting(getClass());
+
+      final AtomicReference<ICommand> cmdRef = new AtomicReference<ICommand>();
+      IRunnableWithProgress progress = new IRunnableWithProgress() {
+        @Override
+        public void run(IProgressMonitor monitor) throws InvocationTargetException, InterruptedException {
+          cmdRef.set(createInterruptableCommand(selection, monitor));
+        }
+      };
+      try {
+        new ProgressMonitorDialog(PlatformUI.getWorkbench().getDisplay().getActiveShell()).run(true, true, progress);
+      } catch (InvocationTargetException e) {
+        throw new ExecutionException(e.getLocalizedMessage(), e);
+      } catch (InterruptedException e) {
+        // user canceled
+      }
+
+      ICommand cmd = cmdRef.get();
+      if (cmd instanceof LauncherCommand) {
+        ((LauncherCommand) cmd).setName(name);
+      }
+      TransactionHelper.getExecutionManager(getSemanticObjects(selection)).execute(cmd);
+    } finally {
+      LongRunningListenersRegistry.getInstance().operationEnded(getClass());
+    }
+
+    return null;
+  }
+
+  protected ICommand createInterruptableCommand(Collection<?> selection, IProgressMonitor progressMonitor) throws InterruptedException {
+    return createRecProjectCommand(ReProjectScope.getScope((EObject) selection.iterator().next(), progressMonitor));
+  }
+
+  @Override
+  protected ICommand createCommand(Collection<?> selection, IProgressMonitor progressMonitor) {
+    try {
+      return createInterruptableCommand(selection, progressMonitor);
+    } catch (InterruptedException e) {
+    }
+    return null;
+  }
+
+  protected abstract DefaultCommand createRecProjectCommand(Collection<EObject> elementsForRec);
+
+    public static class Create extends ProjectRecHandler {
+      @Override
+      protected DefaultCommand createRecProjectCommand(Collection<EObject> elementsForRec) {
+        DefaultCommand c = new CreateRecCommand(elementsForRec, new NullProgressMonitor());
+        SharedWorkflowActivityParameter param = new UIHeadHandler(false);
+        param.addSharedParameter(
+            new GenericParameter<IHandler>(IReConstants.DEPENDENCIES_HANDLER, new DefaultDependenciesHandler() {
+              @Override
+              public Collection<EObject> getScopeElements(Collection<EObject> initialScopeElements, Collection<EObject> scopeElements, IContext context) {
+                return new ArrayList<EObject>(scopeElements);
+              }
+
+              @Override
+              public Collection<EObject> getComplementaryScopeElements(Collection<EObject> initialScopeElements, Collection<EObject> scopeElements, IContext context) {
+                return new ArrayList<EObject>(scopeElements);
+              }
+
+        }, Messages.ProjectRecHandler_OptionsHandler));
+
+        c.addParameters(param);
+        return c;
+      }
+    }
+
+    public static class Update extends ProjectRecHandler {
+      @Override
+      protected DefaultCommand createRecProjectCommand(Collection<EObject> elementsForRec) {
+        DefaultCommand c = new UpdateCurCommand(elementsForRec, new NullProgressMonitor());
+        c.addParameters(new UIHeadHandler(true));
+        return c;
+      }
+    }
+
+}
