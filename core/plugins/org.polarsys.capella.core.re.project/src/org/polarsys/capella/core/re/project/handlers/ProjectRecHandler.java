14d5efc40a5f5f5a8bb8a8e552981ce8aa1967ff
Felix Dorner <felix.dorner@gmail.com>
2017-12-07 14:33:32 +0100

    [1780] Fix update REC from whole library contents
    Use same dummy-dependencies handler as for create
    
    Change-Id: Ic92848e9c7511c6e245cfc04daa92be140be1ea3
    Signed-off-by: Felix Dorner <felix.dorner@gmail.com>

--- core/plugins/org.polarsys.capella.core.re.project/src/org/polarsys/capella/core/re/project/handlers/ProjectRecHandler.java
+++ core/plugins/org.polarsys.capella.core.re.project/src/org/polarsys/capella/core/re/project/handlers/ProjectRecHandler.java
@@ -86,39 +86,44 @@ public abstract class ProjectRecHandler extends CommandHandler {
     return null;
   }
 
-  protected abstract DefaultCommand createRecProjectCommand(Collection<EObject> elementsForRec);
+  protected abstract ICommand createRecProjectCommand(Collection<EObject> elementsForRec);
 
-    public static class Create extends ProjectRecHandler {
-      @Override
-      protected DefaultCommand createRecProjectCommand(Collection<EObject> elementsForRec) {
-        DefaultCommand c = new CreateRecCommand(elementsForRec, new NullProgressMonitor());
-        SharedWorkflowActivityParameter param = new UIHeadHandler(false);
-        param.addSharedParameter(
-            new GenericParameter<IHandler>(IReConstants.DEPENDENCIES_HANDLER, new DefaultDependenciesHandler() {
-              @Override
-              public Collection<EObject> getScopeElements(Collection<EObject> initialScopeElements, Collection<EObject> scopeElements, IContext context) {
-                return new ArrayList<EObject>(scopeElements);
-              }
-
-              @Override
-              public Collection<EObject> getComplementaryScopeElements(Collection<EObject> initialScopeElements, Collection<EObject> scopeElements, IContext context) {
-                return new ArrayList<EObject>(scopeElements);
-              }
-
-        }, Messages.ProjectRecHandler_OptionsHandler));
-
-        c.addParameters(param);
-        return c;
-      }
+  public static class Create extends ProjectRecHandler {
+    @Override
+    protected ICommand createRecProjectCommand(Collection<EObject> elementsForRec) {
+      return customizeParameters(new CreateRecCommand(elementsForRec, new NullProgressMonitor()), false);
     }
+  }
 
-    public static class Update extends ProjectRecHandler {
-      @Override
-      protected DefaultCommand createRecProjectCommand(Collection<EObject> elementsForRec) {
-        DefaultCommand c = new UpdateCurCommand(elementsForRec, new NullProgressMonitor());
-        c.addParameters(new UIHeadHandler(true));
-        return c;
-      }
+  public static class Update extends ProjectRecHandler {
+    @Override
+    protected ICommand createRecProjectCommand(Collection<EObject> elementsForRec) {
+      return customizeParameters(new UpdateCurCommand(elementsForRec, new NullProgressMonitor()), true);
     }
+  }
+
+  /**
+   * - Show the wizard.
+   * - During update also show diffmerge.
+   * - Override dependencies handler to not include any other elements, since we already calculated all required elements
+   */
+  protected final ICommand customizeParameters(DefaultCommand command, boolean showDiffmerge) {
+    SharedWorkflowActivityParameter param = new UIHeadHandler(showDiffmerge);
+    param.addSharedParameter(
+        new GenericParameter<IHandler>(IReConstants.DEPENDENCIES_HANDLER, new DefaultDependenciesHandler() {
+          @Override
+          public Collection<EObject> getScopeElements(Collection<EObject> initialScopeElements, Collection<EObject> scopeElements, IContext context) {
+            return new ArrayList<EObject>(initialScopeElements);
+          }
+
+          @Override
+          public Collection<EObject> getComplementaryScopeElements(Collection<EObject> initialScopeElements, Collection<EObject> scopeElements, IContext context) {
+            return new ArrayList<EObject>(initialScopeElements);
+          }
+
+    }, Messages.ProjectRecHandler_OptionsHandler));
+    command.addParameters(param);
+    return command;
+  }
 
 }
