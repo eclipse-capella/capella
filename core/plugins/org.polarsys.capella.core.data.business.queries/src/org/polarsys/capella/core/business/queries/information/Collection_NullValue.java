b2f25dd8bce374f8d5e328b5e73d0d9f585f9414
Felix Dorner <felix.dorner@gmail.com>
2017-12-12 00:09:58 +0100

    [1868] Fix typo in Collection_NullValue business query
    Bug: 1868
    Change-Id: Ibd0b4d38997a448eca2740490811f3770629cafa
    Signed-off-by: Felix Dorner <felix.dorner@gmail.com>

--- core/plugins/org.polarsys.capella.core.data.business.queries/src/org/polarsys/capella/core/business/queries/information/Collection_NullValue.java
+++ core/plugins/org.polarsys.capella.core.data.business.queries/src/org/polarsys/capella/core/business/queries/information/Collection_NullValue.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2006, 2016 THALES GLOBAL SERVICES.
+ * Copyright (c) 2006, 2017 THALES GLOBAL SERVICES.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -24,7 +24,7 @@ import org.polarsys.capella.core.data.information.InformationPackage;
 
 /**
  * This is the query for collections null values.
- * 
+ *
  */
 public class Collection_NullValue extends AbstractMultiplicityElement_NullValue implements IBusinessQuery {
 
@@ -41,7 +41,7 @@ public class Collection_NullValue extends AbstractMultiplicityElement_NullValue 
 	 */
 	@Override
 	public EClass getEClass() {
-		return InformationPackage.Literals.PROPERTY;
+		return InformationPackage.Literals.COLLECTION;
 	}
 
 	@Override
