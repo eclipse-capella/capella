938566414cfb3f15734240e37f7d489c2324822d
Felix Dorner <felix.dorner@gmail.com>
2017-12-11 15:27:13 +0100

    [1871] Don't allow partial move of elements connected via exchanges
    Both sides of an exchange must be in the same architecture subtree.
    
    Bug: 1871
    Change-Id: I531e5e29ce4f6182403430902d042edb78e414b0
    Signed-off-by: Felix Dorner <felix.dorner@gmail.com>

---
+++ core/plugins/org.polarsys.capella.core.data.business.queries/src/org/polarsys/capella/core/business/queries/cs/PhysicalLink_LinkEnds.java
@@ -0,0 +1,66 @@
+/*******************************************************************************
+ * Copyright (c) 2017 THALES GLOBAL SERVICES.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *    Thales - initial API and implementation
+ *******************************************************************************/
+
+package org.polarsys.capella.core.business.queries.cs;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Iterator;
+import java.util.List;
+
+import org.eclipse.emf.ecore.EClass;
+import org.eclipse.emf.ecore.EObject;
+import org.eclipse.emf.ecore.EReference;
+import org.polarsys.capella.core.business.queries.IBusinessQuery;
+import org.polarsys.capella.core.data.cs.AbstractPhysicalLinkEnd;
+import org.polarsys.capella.core.data.cs.BlockArchitecture;
+import org.polarsys.capella.core.data.cs.CsPackage;
+import org.polarsys.capella.core.data.cs.PhysicalLink;
+import org.polarsys.capella.core.model.helpers.SystemEngineeringExt;
+
+/**
+ */
+public class PhysicalLink_LinkEnds implements IBusinessQuery {
+
+  /**
+   * @see org.polarsys.capella.core.business.queries.core.business.queries.IBusinessQuery#getEClass()
+   */
+  @Override
+  public EClass getEClass() {
+    return CsPackage.Literals.PHYSICAL_LINK;
+  }
+
+  /**
+   * @see org.polarsys.capella.core.business.queries.core.business.queries.IBusinessQuery#getEStructuralFeatures()
+   */
+  @Override
+  public List<EReference> getEStructuralFeatures() {
+    return Collections.singletonList(CsPackage.Literals.PHYSICAL_LINK__LINK_ENDS);
+  }
+
+  @Override
+  public List<EObject> getAvailableElements(EObject element) {
+    List<EObject> availableElements = new ArrayList<EObject>();
+    BlockArchitecture arch = SystemEngineeringExt.getRootBlockArchitecture((PhysicalLink) element);
+    for (Iterator<EObject> it = arch.eAllContents(); it.hasNext();) {
+      EObject next = it.next();
+      if (next instanceof AbstractPhysicalLinkEnd) {
+        availableElements.add(next);
+      }
+    }
+    return availableElements;
+  }
+
+  @Override
+  public List<EObject> getCurrentElements(EObject element, boolean onlyGenerated) {
+    return new ArrayList<EObject>(((PhysicalLink) element).getLinkEnds());
+  }
+}
---
+++ core/plugins/org.polarsys.capella.core.data.business.queries/src/org/polarsys/capella/core/business/queries/fa/ComponentExchange_Source.java
@@ -0,0 +1,70 @@
+/*******************************************************************************
+ * Copyright (c) 2006, 2016 THALES GLOBAL SERVICES.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *    Thales - initial API and implementation
+ *******************************************************************************/
+package org.polarsys.capella.core.business.queries.fa;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Iterator;
+import java.util.List;
+
+import org.eclipse.emf.ecore.EClass;
+import org.eclipse.emf.ecore.EObject;
+import org.eclipse.emf.ecore.EReference;
+import org.polarsys.capella.common.data.modellingcore.InformationsExchanger;
+import org.polarsys.capella.common.data.modellingcore.ModellingcorePackage;
+import org.polarsys.capella.core.business.queries.IBusinessQuery;
+import org.polarsys.capella.core.data.cs.BlockArchitecture;
+import org.polarsys.capella.core.data.fa.ComponentExchange;
+import org.polarsys.capella.core.data.fa.FaPackage;
+import org.polarsys.capella.core.model.helpers.BlockArchitectureExt;
+
+/**
+ */
+public class ComponentExchange_Source implements IBusinessQuery {
+
+  /**
+   * @see org.polarsys.capella.core.business.queries.capellacore.IBusinessQuery#getEClass()
+   */
+  @Override
+  public EClass getEClass() {
+    return FaPackage.Literals.COMPONENT_EXCHANGE;
+  }
+
+  /**
+   * @see org.polarsys.capella.core.business.queries.capellacore.IBusinessQuery#getEStructuralFeatures()
+   */
+  @Override
+  public List<EReference> getEStructuralFeatures() {
+    return Collections.singletonList(ModellingcorePackage.Literals.ABSTRACT_INFORMATION_FLOW__SOURCE);
+  }
+
+  @Override
+  public List<EObject> getAvailableElements(EObject element) {
+    List<EObject> result = new ArrayList<EObject>();
+    BlockArchitecture archi = BlockArchitectureExt.getRootBlockArchitecture(element);
+    for (Iterator<EObject> it = archi.eAllContents(); it.hasNext();) {
+      EObject next = it.next();
+      if (next instanceof InformationsExchanger) {
+        result.add(next);
+      }
+    }
+    return result;
+  }
+
+  @Override
+  public List<EObject> getCurrentElements(EObject element, boolean onlyGenerated) {
+    ComponentExchange ce = (ComponentExchange) element;
+    if (ce.getSource() != null) {
+      return Collections.<EObject> singletonList(ce.getSource());
+    }
+    return Collections.emptyList();
+  }
+}
