3277450e27d5eba9bfed33fa5f1f32a6a68da997
Jeremy Aubry <jaubry@obeo.fr>
2018-01-31 13:24:44 +0100

    [1932] NPE on transition from LC to PC
    Fix JUnit
    
    Bug: 1932
    Change-Id: I2786b5d8bbda51aac815c66da5869a142f3190f3
    Signed-off-by: Jeremy Aubry <jaubry@obeo.fr>

---
+++ tests/plugins/org.polarsys.capella.test.transition.ju/src/org/polarsys/capella/test/transition/ju/testcases/LcWithPropertyValueTestCase.java
@@ -0,0 +1,43 @@
+/*******************************************************************************
+ * Copyright (c) 2018 THALES GLOBAL SERVICES.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *  
+ * Contributors:
+ *    Thales - initial API and implementation
+ *******************************************************************************/
+package org.polarsys.capella.test.transition.ju.testcases;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.eclipse.emf.ecore.EObject;
+import org.polarsys.capella.core.data.pa.PhysicalComponent;
+import org.polarsys.capella.core.transition.system.topdown.constants.ITopDownConstants;
+import org.polarsys.capella.test.transition.ju.TopDownTransitionTestCase;
+
+public class LcWithPropertyValueTestCase extends TopDownTransitionTestCase {
+  
+  private static String LA__LC1 = "5f7e5043-541e-4f2d-81f9-361309d15433";
+  
+  @Override
+  public void performTest() throws Exception {
+	setPreferenceValue(ITopDownConstants.OPTIONS__PROPERTY_VALUE__APPLIED_PROPERTY_VALUES, Boolean.TRUE);
+	
+    // The LA__LC1 has an EnumarationPropertyValue referenced in appliedPropertyValues
+    performLCtoPCTransition(getObjects(LA__LC1));
+
+    EObject physicalComponent = mustBeMonoTransitioned(LA__LC1);
+    assertTrue(physicalComponent instanceof PhysicalComponent);
+    
+    // The transitioned PhysicalComponent should also reference this EnumerationPropertyValue in appliedPropertyValues
+    assertEquals(1, ((PhysicalComponent) physicalComponent).getAppliedPropertyValues().size());
+  }
+  
+  @Override
+  public List<String> getRequiredTestModels() {
+    return Arrays.asList("enumInPropertyPkgUnderSysEng");
+  }
+}
