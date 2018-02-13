87b523ce720c9af33d7d535799f5ad89e8012dfa
Philippe DUL <philippe.dul@thalesgroup.com>
2018-02-13 10:56:39 +0100

    [1957] Add 'Log Information' button to Error Log
    Bug: 1957
    Change-Id: I394c3acf21c54afdf667c9995a251f9a6f8560f8
    Signed-off-by: Philippe DUL <philippe.dul@thalesgroup.com>

---
+++ core/plugins/org.polarsys.capella.core.ui.reportlog/src/org/polarsys/capella/core/ui/reportlog/Messages.java
@@ -0,0 +1,27 @@
+/*******************************************************************************
+ * Copyright (c) 2018 THALES GLOBAL SERVICES.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *  
+ * Contributors:
+ *    Thales - initial API and implementation
+ *******************************************************************************/
+package org.polarsys.capella.core.ui.reportlog;
+
+import org.eclipse.osgi.util.NLS;
+
+public class Messages extends NLS {
+  private static final String BUNDLE_NAME = "org.polarsys.capella.core.ui.reportlog.messages"; //$NON-NLS-1$
+  public static String LogDataHandler_DefaultText;
+  public static String LogDataHandler_Message;
+  public static String LogDataHandler_Title;
+  static {
+    // initialize resource bundle
+    NLS.initializeMessages(BUNDLE_NAME, Messages.class);
+  }
+
+  private Messages() {
+  }
+}
