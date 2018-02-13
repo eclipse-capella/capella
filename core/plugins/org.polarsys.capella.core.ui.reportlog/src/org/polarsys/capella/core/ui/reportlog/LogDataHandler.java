ffd2fc58825482b89e861ba8b08f65fb6c47c4c1
Philippe DUL <philippe.dul@thalesgroup.com>
2018-02-13 10:56:39 +0100

    [1957] Add 'Log Information' button to Error Log
    Bug: 1957
    Change-Id: I394c3acf21c54afdf667c9995a251f9a6f8560f8
    Signed-off-by: Philippe DUL <philippe.dul@thalesgroup.com>

---
+++ core/plugins/org.polarsys.capella.core.ui.reportlog/src/org/polarsys/capella/core/ui/reportlog/LogDataHandler.java
@@ -0,0 +1,49 @@
+/*******************************************************************************
+ * Copyright (c) 2018 THALES GLOBAL SERVICES.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *  
+ * Contributors:
+ *    Thales - initial API and implementation
+ *******************************************************************************/
+package org.polarsys.capella.core.ui.reportlog;
+
+import org.eclipse.core.commands.AbstractHandler;
+import org.eclipse.core.commands.ExecutionEvent;
+import org.eclipse.core.commands.ExecutionException;
+import org.eclipse.core.runtime.IStatus;
+import org.eclipse.core.runtime.Status;
+import org.eclipse.jface.dialogs.IInputValidator;
+import org.eclipse.jface.dialogs.InputDialog;
+import org.eclipse.ui.PlatformUI;
+
+public class LogDataHandler extends AbstractHandler {
+
+  @Override
+  public Object execute(ExecutionEvent event) throws ExecutionException {
+    String defaultText = Messages.LogDataHandler_DefaultText;
+    String title = Messages.LogDataHandler_Title;
+    String message = Messages.LogDataHandler_Message;
+    InputDialog d = new InputDialog(PlatformUI.getWorkbench().getActiveWorkbenchWindow().getShell(), title, message, defaultText, new IInputValidator() {
+      
+      @Override
+      public String isValid(String newText) {
+        if (newText.isEmpty()) {
+          return Messages.LogDataHandler_Message;
+        }
+        return null;
+      }
+    });
+    
+    d.open();
+    if (d.getReturnCode() == InputDialog.OK) {
+      String value = d.getValue();
+      IStatus status = new Status(IStatus.INFO, ReportLogActivator.getDefault().getPluginId(), value);
+      ReportLogActivator.getDefault().getLog().log(status);
+    }
+    return null;
+  }
+
+}
