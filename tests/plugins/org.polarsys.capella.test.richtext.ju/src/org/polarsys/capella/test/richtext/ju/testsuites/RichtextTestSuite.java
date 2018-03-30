32c086435205687d57e7d83bdb26a8f1bce661ae
Tu Ton <minhtutonthat@gmail.com>
2018-03-30 13:43:30 +0200

    [Test] Add test for different link opening actions from Richtext editor (for model elements, diagrams and tables)
    Change-Id: I9cf16c7fe09350cddc2b48a49cb92d6026a0e686
    Signed-off-by: Tu Ton <minhtutonthat@gmail.com>

---
+++ tests/plugins/org.polarsys.capella.test.richtext.ju/src/org/polarsys/capella/test/richtext/ju/testsuites/RichtextTestSuite.java
@@ -0,0 +1,45 @@
+/*******************************************************************************
+ * Copyright (c) 2018 THALES GLOBAL SERVICES.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *  
+ * Contributors:
+ *    Thales - initial API and implementation
+ *******************************************************************************/
+package org.polarsys.capella.test.richtext.ju.testsuites;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.polarsys.capella.test.framework.api.BasicTestArtefact;
+import org.polarsys.capella.test.framework.api.BasicTestSuite;
+import org.polarsys.capella.test.richtext.ju.testcases.RichtextOpenLinkTest;
+
+import junit.framework.Test;
+
+/**
+ * Test Suite for Richtext component (integrated in Capella). 
+ */
+public class RichtextTestSuite extends BasicTestSuite {
+
+  /**
+   * {@inheritDoc}
+   */
+  @Override
+  protected List<BasicTestArtefact> getTests() {
+    List<BasicTestArtefact> tests = new ArrayList<BasicTestArtefact>();
+    tests.add(new RichtextOpenLinkTest());
+    return tests;
+  }
+
+  /**
+   * Added in order to launch this test suite without the Capella test framework.
+   * @return
+   */
+  public static Test suite() {
+    return new RichtextTestSuite();
+  }
+
+}
