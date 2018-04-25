09c7b39ddf29e496a35dabb864dde5cbdd152eaf
Ali Akar <ali.akar82@gmail.com>
2018-04-25 08:46:57 +0200

    [2045] Rule DWF_D_42 raise error on some Capella model
    Bug 2045
    
    Change-Id: If9e6f5fa09b1e647b542934dde9eed6113acf4fb
    Signed-off-by: Ali Akar <ali.akar82@gmail.com>

--- core/plugins/org.polarsys.capella.core.data.core.validation/src/org/polarsys/capella/core/data/capellamodeller/validation/MDCHKPackageVisibility.java
+++ core/plugins/org.polarsys.capella.core.data.core.validation/src/org/polarsys/capella/core/data/capellamodeller/validation/MDCHKPackageVisibility.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2006, 2016 THALES GLOBAL SERVICES.
+ * Copyright (c) 2006, 2018 THALES GLOBAL SERVICES.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -121,7 +121,7 @@ public class MDCHKPackageVisibility extends AbstractValidationRule{
 		
 		if (relation instanceof Association) {
 			Association association = (Association) relation;
-			if (association.getNavigableMembers().size()>0){
+			if (!association.getNavigableMembers().isEmpty() && !association.getOwnedMembers().isEmpty()){
 			Property navigableMember = association.getNavigableMembers().get(0);
 			Property ownedMemmber = association.getOwnedMembers().get(0);
 			
