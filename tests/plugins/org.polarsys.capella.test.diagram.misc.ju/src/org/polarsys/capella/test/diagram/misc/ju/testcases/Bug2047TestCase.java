dc7a728e20339258b0f1160a04442f6c630280f8
Ali Akar <ali.akar82@gmail.com>
2018-04-25 12:15:15 +0200

    [2047] Fix NPE on diagram refresh / migration
    Bug 2047
    
    Change-Id: I521e2b9cdae5bec2e1d0893ed7634bdf012068ec
    Signed-off-by: Ali Akar <ali.akar82@gmail.com>

---
+++ tests/plugins/org.polarsys.capella.test.diagram.misc.ju/src/org/polarsys/capella/test/diagram/misc/ju/testcases/Bug2047TestCase.java
@@ -0,0 +1,69 @@
+/*******************************************************************************
+ * Copyright (c) 2018 THALES GLOBAL SERVICES.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *  
+ * Contributors:
+ *    Thales - initial API and implementation
+ *******************************************************************************/
+package org.polarsys.capella.test.diagram.misc.ju.testcases;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.eclipse.core.runtime.ILogListener;
+import org.eclipse.core.runtime.IStatus;
+import org.eclipse.core.runtime.Platform;
+import org.eclipse.sirius.business.api.session.Session;
+import org.polarsys.capella.test.diagram.common.ju.context.DiagramContext;
+import org.polarsys.capella.test.diagram.common.ju.step.crud.OpenDiagramStep;
+import org.polarsys.capella.test.framework.api.BasicTestCase;
+import org.polarsys.capella.test.framework.context.SessionContext;
+
+public class Bug2047TestCase extends BasicTestCase {
+
+  private static final String projectTestName = "bug2047";
+  
+  private String labDiagramName =  "[LAB] Logical System"; 
+  
+  ILogListener logListener = new ILogListener() {
+    
+    @Override
+    public void logging(IStatus status, String plugin) {
+      if(status !=  null){
+        String pluginId = status.getPlugin();
+        if(pluginId != null && (pluginId.startsWith("org.eclipse.sirius") || pluginId.startsWith("org.polarsys.capella"))){
+          fail("Exception while refreshing diagram: " + status.getMessage());
+        }
+      }
+    }
+  };
+  
+  @Override
+  public List<String> getRequiredTestModels() {
+    return Arrays.asList(projectTestName);
+  }
+  
+  @Override
+  public void test() throws Exception {
+    Session session = getSession(projectTestName);
+    assertNotNull(session);
+    
+    SessionContext context = new SessionContext(session);
+    DiagramContext diagramContext = new OpenDiagramStep(context, labDiagramName).run();
+    // Add the log listener before the refresh
+    Platform.addLogListener(logListener);
+   
+    // Refresh the diagram
+    diagramContext.refreshDiagram();
+  }
+  
+  @Override
+  protected void tearDown() throws Exception {
+    // Remove the log listener on tear down
+    Platform.removeLogListener(logListener);
+    super.tearDown();    
+  }
+}
