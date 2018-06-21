a08a8821ea2b76319058a760382ee08f00563c86
Felix Dorner <felix.dorner@gmail.com>
2018-06-21 15:11:52 +0200

    [2104] Allow diagrams to reference model elements of interest
    This adds a new property 'Elements of Interest' when
    selecting the canvas of a diagram. Any model element can be
    an element of interest for any diagram. Internally this
    property is stored as a DAnnotation on the diagrams
    RepresentationDescriptor, so it is possible to find elements
    of interes without actually loading the diagram.
    
    The semantic browser also shows all diagrams for which a
    focused element is an element of interest.
    
    Change-Id: Iea7f21c5adf8b79c01354ede039a4550d094b518
    Signed-off-by: Felix Dorner <felix.dorner@gmail.com>

---
+++ core/plugins/org.polarsys.capella.core.semantic.queries.sirius/src/org/polarsys/capella/core/semantic/queries/sirius/annotation/DAnnotationReference.java
@@ -0,0 +1,41 @@
+/*******************************************************************************
+ * Copyright (c) 2006, 2018 THALES GLOBAL SERVICES.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *  
+ * Contributors:
+ *    Thales - initial API and implementation
+ *******************************************************************************/
+package org.polarsys.capella.core.semantic.queries.sirius.annotation;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+import org.eclipse.sirius.viewpoint.description.DAnnotation;
+import org.eclipse.sirius.viewpoint.description.DModelElement;
+import org.polarsys.capella.core.diagram.helpers.DAnnotationHelper;
+
+/**
+ * Calculates all elements referenced by a DModelElement via a contained DAnnotation of a given source string.
+ */
+public class DAnnotationReference extends DAnnotationQuery {
+
+  public DAnnotationReference(String source) {
+    super(source);
+  }
+
+  /**
+   * {@inheritDoc}
+   */
+  public List<Object> compute(Object object_p) {
+    DAnnotation annot = DAnnotationHelper.getAnnotation(source, (DModelElement) object_p, false);
+    if (annot != null) {
+      return new ArrayList<Object>(annot.getReferences());
+    }
+    return Collections.emptyList();
+  }
+
+}
