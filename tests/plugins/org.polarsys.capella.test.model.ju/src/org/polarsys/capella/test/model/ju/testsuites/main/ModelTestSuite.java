1d16a290682959ec86a98798c9e4dbd4d229c717
Tu Ton <minhtutonthat@gmail.com>
2018-08-03 13:38:17 +0200

    [2138] Invalid inter-model cycle detected while copying a class referencing a class in library
    Bug: 2138
    Change-Id: I1971e5a7ffd809f85a5dafd1880661a939a79abb
    Signed-off-by: Tu Ton <minhtutonthat@gmail.com>

--- tests/plugins/org.polarsys.capella.test.model.ju/src/org/polarsys/capella/test/model/ju/testsuites/main/ModelTestSuite.java
+++ tests/plugins/org.polarsys.capella.test.model.ju/src/org/polarsys/capella/test/model/ju/testsuites/main/ModelTestSuite.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2006, 2017 THALES GLOBAL SERVICES.
+ * Copyright (c) 2006, 2018 THALES GLOBAL SERVICES.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -23,6 +23,7 @@ import org.polarsys.capella.test.model.ju.sortContent.SortContentTestSuite;
 import org.polarsys.capella.test.model.ju.sortSelection.SortSelectionTestSuite;
 import org.polarsys.capella.test.model.ju.testcase.LCDecomposition.LCDecompositionWithCommunicationLink;
 import org.polarsys.capella.test.model.ju.testcase.LCDecomposition.LCDecompositionWithInternalInterface;
+import org.polarsys.capella.test.model.ju.testcase.copyPasteModel.CopyPasteModelElementReferencingLibrary;
 import org.polarsys.capella.test.model.ju.testcase.copyPasteModel.CopyPasteModelWithAppliedPVGandPV;
 import org.polarsys.capella.test.model.ju.testcase.copyPasteModel.CopyPasteModelWithFunctionalAllocation;
 import org.polarsys.capella.test.model.ju.testcase.dialoglabel.NewDiagramDialogLabel;
@@ -47,6 +48,7 @@ public class ModelTestSuite extends BasicTestSuite {
     List<BasicTestArtefact> tests = new ArrayList<BasicTestArtefact>();
     tests.add(new CopyPasteModelWithAppliedPVGandPV());
     tests.add(new CopyPasteModelWithFunctionalAllocation());
+    tests.add(new CopyPasteModelElementReferencingLibrary());
     tests.add(new LCDecompositionWithCommunicationLink());
     tests.add(new LCDecompositionWithInternalInterface());
     tests.add(new ISMessage());
