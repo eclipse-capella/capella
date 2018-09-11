1d879061cd4c2646b1bc473da2694367693c3372
Jeremy Aubry <jaubry@obeo.fr>
2018-09-11 17:16:19 +0200

    [2094] getDiagramElements() should not return duplicates
    Bug: 2094
    Change-Id: Ic493f24b1fb082d7bb0e3bd88aeb49e2d7c49b42
    Signed-off-by: Jeremy Aubry <jaubry@obeo.fr>

--- core/plugins/org.polarsys.capella.core.sirius.analysis/src/org/polarsys/capella/core/sirius/analysis/DiagramServices.java
+++ core/plugins/org.polarsys.capella.core.sirius.analysis/src/org/polarsys/capella/core/sirius/analysis/DiagramServices.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2006, 2017 THALES GLOBAL SERVICES.
+ * Copyright (c) 2006, 2018 THALES GLOBAL SERVICES.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -616,7 +616,7 @@ public class DiagramServices {
    * @return
    */
   public Collection<DSemanticDecorator> getDiagramElements(DRepresentation diagram, EObject semantic) {
-    Collection<DSemanticDecorator> semanticDecorators = new ArrayList<DSemanticDecorator>();
+    Set<DSemanticDecorator> semanticDecorators = new HashSet<>();
     Session session = SessionManager.INSTANCE.getSession(semantic);
     
     for (Setting setting : session.getSemanticCrossReferencer().getInverseReferences(semantic)) {
