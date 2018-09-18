57f4ca4b68d2456c480fe822e97326d4279bc0e7
Tu Ton <minhtutonthat@gmail.com>
2018-09-18 10:48:06 +0200

    [Releng] Fix compilation issue due to new version of DiffMerge
    Change-Id: I1ff406dcc32018a50d82d9db85d84d2b04152f22
    Signed-off-by: Tu Ton <minhtutonthat@gmail.com>

--- core/plugins/org.polarsys.capella.core.transition.common.ui/src/org/polarsys/capella/core/transition/common/ui/handlers/merge/DiffComparisonViewer.java
+++ core/plugins/org.polarsys.capella.core.transition.common.ui/src/org/polarsys/capella/core/transition/common/ui/handlers/merge/DiffComparisonViewer.java
@@ -21,7 +21,7 @@ import org.eclipse.emf.diffmerge.api.scopes.IEditableModelScope;
 import org.eclipse.emf.diffmerge.diffdata.EComparison;
 import org.eclipse.emf.diffmerge.diffdata.EMatch;
 import org.eclipse.emf.diffmerge.ui.diffuidata.ComparisonSelection;
-import org.eclipse.emf.diffmerge.ui.viewers.DirectedComparisonViewer;
+import org.eclipse.emf.diffmerge.ui.viewers.ComparisonViewer;
 import org.eclipse.emf.diffmerge.ui.viewers.EMFDiffNode;
 import org.eclipse.emf.diffmerge.ui.viewers.MergeChoiceData;
 import org.eclipse.emf.ecore.EObject;
@@ -44,7 +44,7 @@ import org.eclipse.ui.IActionBars;
 import org.eclipse.ui.plugin.AbstractUIPlugin;
 import org.polarsys.capella.core.transition.common.ui.Activator;
 
-public class DiffComparisonViewer extends DirectedComparisonViewer {
+public class DiffComparisonViewer extends ComparisonViewer {
 
   /** The name of the "merge all" image */
   private static final String CHECKIN_ACTION_ALL = "checkin_action_all.gif";
