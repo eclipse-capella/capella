0419390a1c185650e569604e6b6511b44944d70c
Georgiana Ecobici <georgiana-elena.ecobici@thalesgroup.com>
2018-11-15 16:34:09 +0100

    [2268]  Bad information about EI inheritance for Interface generalization in Semantic Browser
      The parent interface displays both its own EIs and the ones from all
    its sub-interfaces, need to display only its own EIs under the Exchange
    Item category;
      The child interface displays only its EIs and shows no details about
    the inherited EIs, need to display also the inherited EIs under the
    Inherited Exchange Item category;
    
    Bug: 2268
    
    Change-Id: Ib626786c6a36b6078db3849cdc1a616548797805
    Signed-off-by: Georgiana Ecobici <georgiana-elena.ecobici@thalesgroup.com>

--- core/plugins/org.polarsys.capella.core.semantic.queries/src/org/polarsys/capella/core/semantic/queries/basic/queries/InterfaceExchangesItems.java
+++ core/plugins/org.polarsys.capella.core.semantic.queries/src/org/polarsys/capella/core/semantic/queries/basic/queries/InterfaceExchangesItems.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2006, 2016 THALES GLOBAL SERVICES.
+ * Copyright (c) 2006, 2018 THALES GLOBAL SERVICES.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -18,9 +18,7 @@ import org.eclipse.emf.common.util.EList;
 
 import org.polarsys.capella.core.data.cs.ExchangeItemAllocation;
 import org.polarsys.capella.core.data.cs.Interface;
-import org.polarsys.capella.core.data.helpers.capellacore.services.GeneralizableElementExt;
 import org.polarsys.capella.core.data.information.ExchangeItem;
-import org.polarsys.capella.core.data.capellacore.GeneralizableElement;
 import org.polarsys.capella.common.data.modellingcore.AbstractExchangeItem;
 import org.polarsys.capella.common.helpers.query.IQuery;
 
@@ -33,35 +31,21 @@ public class InterfaceExchangesItems implements IQuery {
    * @see org.polarsys.capella.common.helpers.query.IQuery#compute(java.lang.Object)
    */
   public List<Object> compute(Object object) {
+
     List<Object> result = new ArrayList<Object>();
     if (object instanceof Interface) {
+
       Interface itf = (Interface) object;
-      // list to collect all the super interfaces + current
-      List<GeneralizableElement> interfaces = new ArrayList<GeneralizableElement>();
-      // add current interface
-      interfaces.add(itf);
-      // add all super interfaces of current interfaces 
-      List<GeneralizableElement> allSuperGenElts = GeneralizableElementExt.getAllSubGeneralizableElements(itf);
-      if (!allSuperGenElts.isEmpty()) {
-        interfaces.addAll(allSuperGenElts);
-      }
-      
-      // get related exchageItems
-      for (GeneralizableElement generalizableElement : interfaces) {
-        if (generalizableElement instanceof Interface) {
-          EList<ExchangeItemAllocation> ownedExchangeItemAllocations = ((Interface)generalizableElement).getOwnedExchangeItemAllocations();
-          for (ExchangeItemAllocation exchangeItemAllocation : ownedExchangeItemAllocations) {
-            AbstractExchangeItem allocatedItem = exchangeItemAllocation.getAllocatedItem();
-            if (null != allocatedItem && allocatedItem instanceof ExchangeItem) {
-              result.add(allocatedItem);
-            }
-          }  
+      EList<ExchangeItemAllocation> ownedExchangeItemAllocations = itf.getOwnedExchangeItemAllocations();
+      for (ExchangeItemAllocation exchangeItemAllocation : ownedExchangeItemAllocations) {
+
+        AbstractExchangeItem allocatedItem = exchangeItemAllocation.getAllocatedItem();
+        if (null != allocatedItem && allocatedItem instanceof ExchangeItem) {
+          result.add(allocatedItem);
         }
       }
-
-
     }
-    
+
     return result;
   }
 }
