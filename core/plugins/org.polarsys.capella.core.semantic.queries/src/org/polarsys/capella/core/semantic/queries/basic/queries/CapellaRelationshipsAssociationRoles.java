98b609e5e7be0d2be08b6119d22e663e2c69dfb0
Georgiana Ecobici <georgiana-elena.ecobici@thalesgroup.com>
2018-11-19 14:56:31 +0100

    [2272]  Semantic Browser not working as expected for Class Diagrams
    - When an association is selected, the Semantic Browser does not show
    any information:
    - Other kinds of semantic connections between elements of class diagrams
    are also not shown.
    
    Bug: 2272
    Change-Id: I66ed6e942d93c070fc6de68d402f0b3ee880423e
    Signed-off-by: Georgiana Ecobici <georgiana-elena.ecobici@thalesgroup.com>

---
+++ core/plugins/org.polarsys.capella.core.semantic.queries/src/org/polarsys/capella/core/semantic/queries/basic/queries/CapellaRelationshipsAssociationRoles.java
@@ -0,0 +1,41 @@
+/*******************************************************************************
+ * Copyright (c) 2018 THALES GLOBAL SERVICES.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *  
+ * Contributors:
+ *    Thales - initial API and implementation
+ *******************************************************************************/
+
+package org.polarsys.capella.core.semantic.queries.basic.queries;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+import org.polarsys.capella.common.helpers.query.IQuery;
+import org.polarsys.capella.core.data.information.Association;
+import org.polarsys.capella.core.data.information.Property;
+import org.polarsys.capella.core.model.helpers.AssociationExt;
+
+public class CapellaRelationshipsAssociationRoles implements IQuery {
+
+  /**
+   * @see org.polarsys.capella.common.helpers.query.IQuery#compute(java.lang.Object)
+   */
+  public List<Object> compute(Object object) {
+    List<Object> result = new ArrayList<Object>();
+    if (object instanceof Association) {
+
+      Association association = (Association) object;
+
+      Collection<Property> properties = AssociationExt.getProperties(association);
+      if (!properties.isEmpty()) {
+        result.addAll(properties);
+      }
+    }
+    return result;
+  }
+}
