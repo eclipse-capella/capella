7b6101baa325a14a4c391d150b3b9c8f323b9783
Matei-Alexandru Stamate <matei-alexandru.stamate@thalesgroup.com>
2018-12-04 14:01:52 +0100

    [2299] Validation rule + quickfix to detect duplicated Involvement links
    Today, an Operational Capability can be involved twice in an Operational
    Actor without any validation rule to detect.
    
    Steps to reproduce:
    
    1) Create an Operational Capabilities Blank (OCB) Diagram;
    2) Create an Operational Capability;
    3) Create an Operational Actor;
    4) Add an Involvement Link between the Capability and the Actor;
    5) From Capella Explorer, copy/paste the Involvement link of the
    Operational Capability (disable the filter for
    EntityOperationalCapabilityInvolvement if it  is active).
    6) Validate the model.
    
    Actual results:
    
    The duplicate Involvement link is not detected by the validation.
    
    Expected results:
    
    The duplicate Involvement link is detected by the validation rules and a
    quickfix is available to remove the duplicates.
    
    EDIT: Also, fixed the bug where rule I_26 which detects duplicate Trace
    elements was not working.
    
    Change-Id: I0a99e8d45e30d7ccbcdc323f6e2f49968693dbfe
    Signed-off-by: Matei-Alexandru Stamate <matei-alexandru.stamate@thalesgroup.com>

---
+++ tests/plugins/org.polarsys.capella.test.validation.rules.ju/src/org/polarsys/capella/test/validation/rules/ju/testcases/i/Rule_I_40_OnCapability.java
@@ -0,0 +1,67 @@
+/*******************************************************************************
+ * Copyright (c) 2018 THALES GLOBAL SERVICES.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *  
+ * Contributors:
+ *    Thales - initial API and implementation
+ *******************************************************************************/
+package org.polarsys.capella.test.validation.rules.ju.testcases.i;
+
+import java.util.Arrays;
+import java.util.List;
+import org.eclipse.emf.ecore.EClass;
+import org.polarsys.capella.core.data.ctx.CtxPackage;
+import org.polarsys.capella.test.framework.api.OracleDefinition;
+import org.polarsys.capella.test.validation.rules.ju.testcases.AbstractRulesOnIntegrityTest;
+
+/**
+ * test on I_26: Checks that there are no equivalent Involvement elements. Two Involvement elements are equivalent if
+ * they have identical types and identical source and target elements.
+ * 
+ * @generated
+ */
+public class Rule_I_40_OnCapability extends AbstractRulesOnIntegrityTest {
+
+  private final static String OPERATIONAL_CAPABILITY_1 = "d38df0ca-5af1-4396-8e51-e003645e7407";
+  private final static String OPERATIONAL_CAPABILITY_2 = "390d29fd-d10f-464d-bc23-598f623a5d20";
+  private final static String CAPABILITY_REALIZATION_1 = "14914d40-66e2-4729-8955-c8abb0903bea";
+  private final static String CAPABILITY_REALIZATION_2 = "25585ce0-f36e-413b-a056-fb4cd43ddf30";
+
+  /**
+   * @see org.polarsys.capella.test.validation.rules.ju.testcases.ValidationRuleTestCase#getTargetedEClass()
+   * @generated
+   */
+  protected EClass getTargetedEClass() {
+    return CtxPackage.Literals.CAPABILITY;
+  }
+
+  /**
+   * @see org.polarsys.capella.test.validation.rules.ju.testcases.ValidationRuleTestCase#getRuleID()
+   * @generated
+   */
+  protected String getRuleID() {
+    return "org.polarsys.capella.core.data.core.validation.I_40";
+  }
+
+  /**
+   * @see org.polarsys.capella.test.validation.rules.ju.testcases.ValidationRulePartialTestCase#getScopeDefinition()
+   * @generated
+   */
+  protected List<String> getScopeDefinition() {
+    return Arrays.asList(new String[] { OPERATIONAL_CAPABILITY_1, OPERATIONAL_CAPABILITY_2, CAPABILITY_REALIZATION_1,
+        CAPABILITY_REALIZATION_2 });
+  }
+
+  /**
+   * @see org.polarsys.capella.test.validation.rules.ju.testcases.ValidationRuleTestCase#getOracleDefinitions()
+   * @generated
+   */
+  protected List<OracleDefinition> getOracleDefinitions() {
+    return Arrays.asList(new OracleDefinition[] { new OracleDefinition(OPERATIONAL_CAPABILITY_1, 1),
+        new OracleDefinition(CAPABILITY_REALIZATION_1, 1), new OracleDefinition(OPERATIONAL_CAPABILITY_2, 0),
+        new OracleDefinition(CAPABILITY_REALIZATION_2, 0) });
+  }
+}
---
+++ tests/plugins/org.polarsys.capella.test.validation.rules.ju/src/org/polarsys/capella/test/validation/rules/ju/testcases/i/Rule_I_40_OnMission.java
@@ -0,0 +1,63 @@
+/*******************************************************************************
+ * Copyright (c) 2018 THALES GLOBAL SERVICES.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *  
+ * Contributors:
+ *    Thales - initial API and implementation
+ *******************************************************************************/
+package org.polarsys.capella.test.validation.rules.ju.testcases.i;
+
+import java.util.Arrays;
+import java.util.List;
+import org.eclipse.emf.ecore.EClass;
+import org.polarsys.capella.core.data.ctx.CtxPackage;
+import org.polarsys.capella.test.framework.api.OracleDefinition;
+import org.polarsys.capella.test.validation.rules.ju.testcases.AbstractRulesOnIntegrityTest;
+
+/**
+ * test on I_40: Checks that there are no equivalent Involvement elements. Two Involvement elements are equivalent if
+ * they have identical types and identical source and target elements.
+ * 
+ * @generated
+ */
+public class Rule_I_40_OnMission extends AbstractRulesOnIntegrityTest {
+
+  private final static String MISSION_1 = "75f29c7e-09bd-4475-a2cd-1adfe4f35a67";
+  private final static String MISSION_2 = "0c29dd27-b23f-4255-8abf-6603f6d81c89";
+
+  /**
+   * @see org.polarsys.capella.test.validation.rules.ju.testcases.ValidationRuleTestCase#getTargetedEClass()
+   * @generated
+   */
+  protected EClass getTargetedEClass() {
+    return CtxPackage.Literals.MISSION;
+  }
+
+  /**
+   * @see org.polarsys.capella.test.validation.rules.ju.testcases.ValidationRuleTestCase#getRuleID()
+   * @generated
+   */
+  protected String getRuleID() {
+    return "org.polarsys.capella.core.data.core.validation.I_40";
+  }
+
+  /**
+   * @see org.polarsys.capella.test.validation.rules.ju.testcases.ValidationRulePartialTestCase#getScopeDefinition()
+   * @generated
+   */
+  protected List<String> getScopeDefinition() {
+    return Arrays.asList(new String[] { MISSION_1, MISSION_2 });
+  }
+
+  /**
+   * @see org.polarsys.capella.test.validation.rules.ju.testcases.ValidationRuleTestCase#getOracleDefinitions()
+   * @generated
+   */
+  protected List<OracleDefinition> getOracleDefinitions() {
+    return Arrays
+        .asList(new OracleDefinition[] { new OracleDefinition(MISSION_1, 1), new OracleDefinition(MISSION_2, 0) });
+  }
+}
