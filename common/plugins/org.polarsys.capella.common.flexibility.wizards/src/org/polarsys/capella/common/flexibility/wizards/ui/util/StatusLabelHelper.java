91683264a915053309cc95f46c315e3caf28584b
Philippe DUL <philippe.dul@thalesgroup.com>
2018-12-05 11:59:15 +0100

    [2292] Disable suffix properties view on wrong context
    Bug: 2292
    Change-Id: Ib6678959897a403c1560efc46c219793b526a8ed
    Signed-off-by: Philippe DUL <philippe.dul@thalesgroup.com>

---
+++ common/plugins/org.polarsys.capella.common.flexibility.wizards/src/org/polarsys/capella/common/flexibility/wizards/ui/util/StatusLabelHelper.java
@@ -0,0 +1,53 @@
+/*******************************************************************************
+ * Copyright (c) 2018 THALES GLOBAL SERVICES.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *  
+ * Contributors:
+ *    Thales - initial API and implementation
+ *******************************************************************************/
+package org.polarsys.capella.common.flexibility.wizards.ui.util;
+
+import org.eclipse.core.runtime.IStatus;
+import org.eclipse.swt.widgets.Label;
+import org.polarsys.capella.common.flexibility.wizards.Activator;
+import org.polarsys.capella.common.mdsofa.common.constant.ICommonConstants;
+
+public class StatusLabelHelper {
+
+  /**
+   * This method allows to set an image to the given SWT label according to the status
+   */
+  public static void updateImage(IStatus status, Label label) {
+    if (!label.isDisposed() && status != null) {
+      if (status.isOK()) {
+        label.setImage(Activator.getDefault().getImage("full/etool16/empty.gif"));
+
+      } else if (status.matches(IStatus.INFO)) {
+        label.setImage(Activator.getDefault().getImage("full/etool16/info_tsk.gif"));
+
+      } else if (status.matches(IStatus.WARNING)) {
+        label.setImage(Activator.getDefault().getImage("full/etool16/warn_tsk.gif"));
+
+      } else if (status.matches(IStatus.ERROR)) {
+        label.setImage(Activator.getDefault().getImage("full/etool16/error_tsk.gif"));
+      }
+    }
+  }
+
+  /**
+   * This method allows to set the tooltip to the given SWT label according to the status
+   * @param okMessage : if the status is OK, set or not the message of the status
+   */
+  public static void updateTooltip(IStatus status, Label label, boolean okMessage) {
+    if (!label.isDisposed() && status != null) {
+      String message = status.getMessage();
+      if (status.isOK() && !okMessage) {
+        message = ICommonConstants.EMPTY_STRING;
+      }
+      label.setToolTipText(message);
+    }
+  }
+}
