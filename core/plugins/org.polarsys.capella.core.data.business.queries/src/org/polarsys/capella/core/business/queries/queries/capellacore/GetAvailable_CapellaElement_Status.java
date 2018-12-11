bd36a2a6ebe26422f596c195c2a06ffe5f87ea8b
Philippe DUL <philippe.dul@thalesgroup.com>
2018-12-11 15:45:58 +0100

    [2306] Progress Status doesn't work on Root Project element
    Change-Id: I03be21b1619afc2f32ca703e182b076c1c94a87a
    Signed-off-by: Philippe DUL <philippe.dul@thalesgroup.com>

--- core/plugins/org.polarsys.capella.core.data.business.queries/src/org/polarsys/capella/core/business/queries/queries/capellacore/GetAvailable_CapellaElement_Status.java
+++ core/plugins/org.polarsys.capella.core.data.business.queries/src/org/polarsys/capella/core/business/queries/queries/capellacore/GetAvailable_CapellaElement_Status.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2006, 2016 THALES GLOBAL SERVICES.
+ * Copyright (c) 2006, 2018 THALES GLOBAL SERVICES.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -14,45 +14,45 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.eclipse.emf.ecore.EObject;
-import org.polarsys.capella.common.helpers.EcoreUtil2;
 import org.polarsys.capella.common.queries.AbstractQuery;
 import org.polarsys.capella.common.queries.queryContext.IQueryContext;
 import org.polarsys.capella.core.data.capellacore.CapellaElement;
 import org.polarsys.capella.core.data.capellacore.EnumerationPropertyLiteral;
 import org.polarsys.capella.core.data.capellacore.EnumerationPropertyType;
-import org.polarsys.capella.core.data.capellamodeller.CapellamodellerPackage;
 import org.polarsys.capella.core.data.capellamodeller.Project;
 import org.polarsys.capella.core.model.handler.helpers.CapellaProjectHelper;
+import org.polarsys.capella.core.model.helpers.ProjectExt;
 
 public class GetAvailable_CapellaElement_Status extends AbstractQuery {
 
-	@SuppressWarnings({ "rawtypes", "unchecked" })
-	@Override
-	public List<Object> execute(Object input, IQueryContext context) {
-		CapellaElement capellaElement = (CapellaElement) input;
-		List<CapellaElement> availableElements = getAvailableElements(capellaElement);
-		return (List) availableElements;
-	}
+  @SuppressWarnings({ "rawtypes", "unchecked" })
+  @Override
+  public List<Object> execute(Object input, IQueryContext context) {
+    CapellaElement capellaElement = (CapellaElement) input;
+    List<CapellaElement> availableElements = getAvailableElements(capellaElement);
+    return (List) availableElements;
+  }
 
-	/** 
-	 * @see org.polarsys.capella.core.business.queries.IBusinessQuery#getAvailableElements(EObject)
-	 */
-	public List<CapellaElement> getAvailableElements(CapellaElement element) {
-		List<CapellaElement> availableElements = new ArrayList<CapellaElement>();
-		if (null == element) {
-			return availableElements;
-		}
-		Project project = (Project) EcoreUtil2.getFirstContainer(element, CapellamodellerPackage.Literals.PROJECT);
-		if (null != project) {
-			for (EnumerationPropertyType enumeration : project.getOwnedEnumerationPropertyTypes()) {
-				if (enumeration.getName().equals(CapellaProjectHelper.PROGRESS_STATUS_KEYWORD)) {
-					for (EnumerationPropertyLiteral literal : enumeration.getOwnedLiterals()) {
-						availableElements.add(literal);
-					}
-				}
-			}
-		}
-		return availableElements;
-	}
+  /**
+   * @see org.polarsys.capella.core.business.queries.IBusinessQuery#getAvailableElements(EObject)
+   */
+  public List<CapellaElement> getAvailableElements(CapellaElement element) {
+    List<CapellaElement> availableElements = new ArrayList<CapellaElement>();
+    if (null == element) {
+      return availableElements;
+    }
+
+    Project project = ProjectExt.getProject(element);
+    if (null != project) {
+      for (EnumerationPropertyType enumeration : project.getOwnedEnumerationPropertyTypes()) {
+        if (enumeration.getName().equals(CapellaProjectHelper.PROGRESS_STATUS_KEYWORD)) {
+          for (EnumerationPropertyLiteral literal : enumeration.getOwnedLiterals()) {
+            availableElements.add(literal);
+          }
+        }
+      }
+    }
+    return availableElements;
+  }
 
 }
