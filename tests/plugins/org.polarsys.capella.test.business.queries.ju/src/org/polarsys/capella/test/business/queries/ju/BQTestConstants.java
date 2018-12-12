182ec5b17184b67bae08ab7ad5da7ec867a805a6
Philippe DUL <philippe.dul@thalesgroup.com>
2018-12-12 09:25:03 +0100

    [2313] Add tools to generate business query tests
    Change-Id: Idec945d4929fdace43f213e2b77b82c802a1780d
    Signed-off-by: Philippe DUL <philippe.dul@thalesgroup.com>

--- tests/plugins/org.polarsys.capella.test.business.queries.ju/src/org/polarsys/capella/test/business/queries/ju/BQTestConstants.java
+++ tests/plugins/org.polarsys.capella.test.business.queries.ju/src/org/polarsys/capella/test/business/queries/ju/BQTestConstants.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2006, 2017 THALES GLOBAL SERVICES.
+ * Copyright (c) 2006, 2018 THALES GLOBAL SERVICES.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -21,31 +21,41 @@ import org.polarsys.capella.test.framework.helpers.IResourceHelpers;
  */
 public class BQTestConstants {
 
-	public static final String PROMPT_STRING = ">>"; //$NON-NLS-1$	
-	
-	public static final String TEST_SUITE_FILE_EXTENSION = "testSuite"; //$NON-NLS-1$
-	public static final String TEST_SUITES_RELATIVE_FOLDER = "testSuite"; //$NON-NLS-1$
-	
-  public static final String TEST_CASES_PACKAGE = "{0}/testcases/{1}/{2}"; //$NON-NLS-1$
-	public static final String TEST_CASES_RELATIVE_FOLDER = "src-gen/"; //$NON-NLS-1$
-	
-	public static final String GET_AVAILABLE_METHOD_NAME = "getAvailableElements"; //$NON-NLS-1$
-	public static final String GET_CURRENT_METHOD_NAME = "getCurrentElements"; //$NON-NLS-1$
+  public static final String PROMPT_STRING = ">>"; //$NON-NLS-1$
 
-	public static final String DISCARDED_BQ_CONFIGURATION_FILE_RELATIVE_PATH = "config/discardedBusinessQuery.conf"; //$NON-NLS-1$
-	
-	private static List<String> discardedBQs;
-	
-	public static boolean isDiscardedBQ(String bqFqn) {
-		if (discardedBQs == null) {
-			try {
-				File fileConf = IResourceHelpers.getFileInPlugin(TestBusinessQueriesPlugin.class, BQTestConstants.DISCARDED_BQ_CONFIGURATION_FILE_RELATIVE_PATH);
-				String data = IResourceHelpers.readFileAsString(fileConf);
-				discardedBQs = Arrays.asList(data.split("\\r?\\n")); //$NON-NLS-1$
-			} catch (Exception e) {
-				e.printStackTrace();
-			}
-		}
-		return discardedBQs.contains(bqFqn);
-	}
+  public static final String TEST_SUITE_FILE_EXTENSION = "testSuite"; //$NON-NLS-1$
+  public static final String TEST_SUITES_RELATIVE_FOLDER = "testSuite"; //$NON-NLS-1$
+
+  public static final String TEST_CASES_PACKAGE = "{0}/testcases/{1}/{2}"; //$NON-NLS-1$
+  public static final String TEST_CASES_RELATIVE_FOLDER = "src-gen/"; //$NON-NLS-1$
+
+  public static final String GET_AVAILABLE_METHOD_NAME = "getAvailableElements"; //$NON-NLS-1$
+  public static final String GET_CURRENT_METHOD_NAME = "getCurrentElements"; //$NON-NLS-1$
+
+  public static final String DISCARDED_BQ_CONFIGURATION_FILE_RELATIVE_PATH = "config/discardedBusinessQuery.conf"; //$NON-NLS-1$
+
+  private static List<String> discardedBQs;
+
+  public static final String JUNIT_TEMPLATE_FILE_RELATIVE_PATH = "template/JUnit.template.java"; //$NON-NLS-1$
+  public static final String TEST_SUITE_TEMPLATE_FILE_RELATIVE_PATH = "template/testSuite.template"; //$NON-NLS-1$
+
+  public static final String JUNIT_TEST_CASE_FILE_PACKAGE = "\\$JUNIT_TEST_CASE_FILE_PACKAGE\\$"; //$NON-NLS-1$
+  public static final String JUNIT_TEST_CASE_FILE_NAME__VARIABLE = "\\$JUNIT_TEST_CASE_FILE_NAME\\$"; //$NON-NLS-1$
+  public static final String PROJECT_NAME_FOR_TEST__VARIABLE = "\\$PROJECT_NAME_FOR_TEST\\$"; //$NON-NLS-1$
+  public static final String BUSINESS_QUERY_FQN__VARIABLE = "\\$BUSINESS_QUERY_FQN\\$"; //$NON-NLS-1$
+  public static final String TEST_CASES_DATA = "\\$TEST_CASES_DATA\\$"; //$NON-NLS-1$
+
+  public static boolean isDiscardedBQ(String bqFqn) {
+    if (discardedBQs == null) {
+      try {
+        File fileConf = IResourceHelpers.getFileInPlugin(TestBusinessQueriesPlugin.class,
+            BQTestConstants.DISCARDED_BQ_CONFIGURATION_FILE_RELATIVE_PATH);
+        String data = IResourceHelpers.readFileAsString(fileConf);
+        discardedBQs = Arrays.asList(data.split("\\r?\\n")); //$NON-NLS-1$
+      } catch (Exception e) {
+        e.printStackTrace();
+      }
+    }
+    return discardedBQs.contains(bqFqn);
+  }
 }
--- tests/plugins/org.polarsys.capella.test.business.queries.ju/src/org/polarsys/capella/test/business/queries/ju/BQTestHelpers.java
+++ tests/plugins/org.polarsys.capella.test.business.queries.ju/src/org/polarsys/capella/test/business/queries/ju/BQTestHelpers.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2006, 2017 THALES GLOBAL SERVICES.
+ * Copyright (c) 2006, 2018 THALES GLOBAL SERVICES.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -13,12 +13,16 @@ package org.polarsys.capella.test.business.queries.ju;
 import java.io.File;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.Hashtable;
 import java.util.List;
 
 import org.apache.commons.lang.StringUtils;
+import org.eclipse.core.internal.registry.ExtensionHandle;
 import org.eclipse.core.resources.IFile;
-import org.eclipse.core.resources.IProject;
+import org.eclipse.core.runtime.CoreException;
+import org.eclipse.core.runtime.IConfigurationElement;
+import org.eclipse.core.runtime.Platform;
 import org.eclipse.emf.common.util.TreeIterator;
 import org.eclipse.emf.ecore.EClass;
 import org.eclipse.emf.ecore.EObject;
@@ -27,20 +31,18 @@ import org.eclipse.osgi.util.NLS;
 import org.eclipse.sirius.business.api.session.Session;
 import org.osgi.framework.Bundle;
 import org.polarsys.capella.core.business.queries.IBusinessQuery;
-import org.polarsys.capella.test.framework.helpers.IResourceHelpers;
 
 /**
  * @author Erwan Brottier
  */
 public class BQTestHelpers {
 
-	public static String getQueryClassification(String fqn, char separator) {
-		List<String> segments = Arrays.asList(fqn.split("\\.")); //$NON-NLS-1$
-		String folder = segments.get(segments.size()-2);
+  public static String getQueryClassification(String fqn, char separator) {
+    List<String> segments = Arrays.asList(fqn.split("\\.")); //$NON-NLS-1$
+    String folder = segments.get(segments.size() - 2);
     return folder;
-	}
-	
-  
+  }
+
   public static Class<?> loadClass(Bundle bundle, String queryIdentifier) {
     try {
       Class<?> bqClass = null;
@@ -56,7 +58,6 @@ public class BQTestHelpers {
       return null;
     }
   }
-  
 
   public static <T> T newObject(Bundle bundle, String queryIdentifier) {
     try {
@@ -66,85 +67,118 @@ public class BQTestHelpers {
       return null;
     }
   }
-  
-	public static IBusinessQuery instanciateBQ(Bundle bundle, String queryIdentifier) {
-		return (IBusinessQuery) newObject(bundle, queryIdentifier);
-	}
-	
-	public static Hashtable<String, EObject> createId2ObjectTable(List<EObject> elements) {
-		Hashtable<String, EObject> table = new Hashtable<String, EObject>();
-		for (EObject capellaElement : elements) {
-			table.put(QueryResult.getObjectId(capellaElement), capellaElement);
-		}
-		return table;
-	}
 
-	public static String getFullQualifiedName(IFile javaFile) {
-		List<String> segs = new ArrayList<String>();
-		for (String string : javaFile.getFullPath().segments()) {
-			segs.add(string);
-		}
-		while (segs.size() > 0 && !segs.get(0).equals("src")) { //$NON-NLS-1$
-			segs.remove(0);
-		}
-		if (segs.size() > 0) {
-			segs.remove(0);
-		}
-		if (segs.size() > 0) {
-			String last = segs.get(segs.size()-1);
-			if (last.endsWith(".java")) { //$NON-NLS-1$
-				last = last.substring(0, last.length()-5);
-				segs.remove(segs.size()-1);
-				segs.add(last);
-			}
-		}
-		return StringUtils.join(segs, '.');
-	}
-	
-	public static Hashtable<String, EObject> getId2ObjectTableInSessionForTest(Session session, EClass clazz) {
-		Hashtable<String, EObject> id2ObjectTable = new Hashtable<String, EObject>();
-		for (EObject capellaElement : getAllObjectsInSession(session, clazz)) {
-			id2ObjectTable.put(QueryResult.getObjectId(capellaElement), capellaElement);
-		}			
-		return id2ObjectTable;
-	}
+  public static IBusinessQuery instanciateBQ(Bundle bundle, String queryIdentifier) {
+    return (IBusinessQuery) newObject(bundle, queryIdentifier);
+  }
 
-	public static List<EObject> getAllObjectsInSession(Session session, EClass clazz) {
-		List<EObject> objects = new ArrayList<EObject>();
-		for (Resource semanticResource : session.getSemanticResources()) {
-			TreeIterator<EObject> treeIterator = semanticResource.getAllContents();
-			while (treeIterator.hasNext()) {
-				EObject obj = treeIterator.next();				
-				if (clazz.isInstance(obj)) {
-					objects.add(obj);					
-				}
-			}
-		}
-		return objects;
-	}
-	
-	public static File getTestSuiteFile(File bqTestProjectFolder, String queryIdentifier, String testProjectName) {
-		String pck = getQueryClassification(queryIdentifier, '/');
-		String relativePath = BQTestConstants.TEST_SUITES_RELATIVE_FOLDER + "/" + testProjectName + "/" + pck + "/" + queryIdentifier + "." +BQTestConstants.TEST_SUITE_FILE_EXTENSION; //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$ //$NON-NLS-4$
-		return new File(bqTestProjectFolder.toString()+"/"+relativePath); //$NON-NLS-1$
-	}
-	
-	public static String getPackagePath(IProject bqTestProject, String queryIdentifier, String testProjectName) {
+  public static Hashtable<String, EObject> createId2ObjectTable(List<EObject> elements) {
+    Hashtable<String, EObject> table = new Hashtable<String, EObject>();
+    for (EObject capellaElement : elements) {
+      table.put(QueryResult.getObjectId(capellaElement), capellaElement);
+    }
+    return table;
+  }
+
+  public static String getFullQualifiedName(IFile javaFile) {
+    List<String> segs = new ArrayList<String>();
+    for (String string : javaFile.getFullPath().segments()) {
+      segs.add(string);
+    }
+    while (segs.size() > 0 && !segs.get(0).equals("src")) { //$NON-NLS-1$
+      segs.remove(0);
+    }
+    if (segs.size() > 0) {
+      segs.remove(0);
+    }
+    if (segs.size() > 0) {
+      String last = segs.get(segs.size() - 1);
+      if (last.endsWith(".java")) { //$NON-NLS-1$
+        last = last.substring(0, last.length() - 5);
+        segs.remove(segs.size() - 1);
+        segs.add(last);
+      }
+    }
+    return StringUtils.join(segs, '.');
+  }
+
+  public static Hashtable<String, EObject> getId2ObjectTableInSessionForTest(Session session, EClass clazz) {
+    Hashtable<String, EObject> id2ObjectTable = new Hashtable<String, EObject>();
+    for (EObject capellaElement : getAllObjectsInSession(session, clazz)) {
+      id2ObjectTable.put(QueryResult.getObjectId(capellaElement), capellaElement);
+    }
+    return id2ObjectTable;
+  }
+
+  public static List<EObject> getAllObjectsInSession(Session session, EClass clazz) {
+    List<EObject> objects = new ArrayList<EObject>();
+    for (Resource semanticResource : session.getSemanticResources()) {
+      TreeIterator<EObject> treeIterator = semanticResource.getAllContents();
+      while (treeIterator.hasNext()) {
+        EObject obj = treeIterator.next();
+        if (clazz.isInstance(obj)) {
+          objects.add(obj);
+        }
+      }
+    }
+    return objects;
+  }
+
+  /**
+   * Return the File location of this plugin. On a debug session, Generation will generate directly on it.
+   */
+  public static File getBqTestProject() {
+    Bundle bundle = TestBusinessQueriesPlugin.getDefault().getBundle();
+    File file = new File(bundle.getLocation().substring("reference:file:/".length()));
+    return file;
+  }
+
+  public static File getTestSuiteFile(File bqTestProjectFolder, String queryIdentifier, String testProjectName) {
     String pck = getQueryClassification(queryIdentifier, '/');
-    String path = bqTestProject.getName().replaceAll("\\.", "/");
-    return NLS.bind(BQTestConstants.TEST_CASES_PACKAGE, new String[]{ path, testProjectName, pck});
+    String relativePath = BQTestConstants.TEST_SUITES_RELATIVE_FOLDER + "/" + testProjectName + "/" + pck + "/" //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$
+        + queryIdentifier + "." + BQTestConstants.TEST_SUITE_FILE_EXTENSION; //$NON-NLS-1$
+    return new File(bqTestProjectFolder.toString() + "/" + relativePath); //$NON-NLS-1$
   }
 
-  public static String getPackageName(IProject bqTestProject, String queryIdentifier, String testProjectName) {
-    return getPackagePath(bqTestProject, queryIdentifier, testProjectName).replaceAll("/", ".");
+  public static String getPackagePath(String bqTestProjectName, String queryIdentifier, String testProjectName) {
+    String pck = getQueryClassification(queryIdentifier, '/');
+    String path = bqTestProjectName.replaceAll("\\.", "/");
+    return NLS.bind(BQTestConstants.TEST_CASES_PACKAGE, new String[] { path, testProjectName, pck });
   }
-	public static File getJUnitFile(IProject bqTestProject, String queryIdentifier, String testProjectName, String fileName) {
-    File bqTestProjectFolder = IResourceHelpers.convertToFile(bqTestProject);
-    
-    String packageName = getPackageName(bqTestProject, queryIdentifier, testProjectName).replaceAll("\\.", "/");
-    String junitFileName = bqTestProjectFolder.toString() + "/" + BQTestConstants.TEST_CASES_RELATIVE_FOLDER + "/" + packageName + "/" + fileName;
-		File junitFile = new File(junitFileName);
-		return junitFile;
-	}
-	
+
+  public static String getPackageName(String bqTestProjectName, String queryIdentifier, String testProjectName) {
+    return getPackagePath(bqTestProjectName, queryIdentifier, testProjectName).replaceAll("/", ".");
+  }
+
+  public static File getJUnitFile(File bqTestProject, String queryIdentifier, String testProjectName, String fileName) {
+    String packageName = getPackageName(bqTestProject.getName(), queryIdentifier, testProjectName).replaceAll("\\.",
+        "/");
+    String junitFileName = bqTestProject.toString() + "/" + BQTestConstants.TEST_CASES_RELATIVE_FOLDER + "/"
+        + packageName + "/" + fileName;
+    File junitFile = new File(junitFileName);
+    return junitFile;
+  }
+
+  /**
+   * Returns a map of <EPackageName, List<IBusinessQuery>> of all registered queries
+   */
+  public static HashMap<String, ArrayList<IBusinessQuery>> getQueryPerPackages() {
+
+    HashMap<String, ArrayList<IBusinessQuery>> pkgs = new HashMap<String, ArrayList<IBusinessQuery>>();
+    for (IConfigurationElement element : Platform.getExtensionRegistry()
+        .getConfigurationElementsFor("org.polarsys.capella.core.data.business.queries.MDEBusinessQueries")) {
+      String pkg = ((ExtensionHandle) element.getParent()).getLabel();
+      if (!pkgs.containsKey(pkg)) {
+        pkgs.put(pkg, new ArrayList<IBusinessQuery>());
+      }
+      try {
+        pkgs.get(pkg).add((IBusinessQuery) element.createExecutableExtension("class"));
+      } catch (CoreException e) {
+        e.printStackTrace();
+      }
+    }
+
+    return pkgs;
+  }
+
 }
