81312e4b9fe6dd703e1ec43dfc5f74bf91149332
Ali AKAR <ali.akar82@gmail.com>
2019-01-18 17:30:39 +0100

    [2301] Disable properties view for invalid representation
    - Due to some Exception raised if selecting Management or Description
    tabs in the view properties.
    
    Bug: 2301
    Change-Id: Idecdcc15d66ae06a850527ef6dd41b8fb890e7c9
    Signed-off-by: Ali AKAR <ali.akar82@gmail.com>

--- core/plugins/org.polarsys.capella.core.ui.properties/src/org/polarsys/capella/core/ui/properties/tabbed/CapellaTabDescriptorProvider.java
+++ core/plugins/org.polarsys.capella.core.ui.properties/src/org/polarsys/capella/core/ui/properties/tabbed/CapellaTabDescriptorProvider.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2006, 2015 THALES GLOBAL SERVICES.
+ * Copyright (c) 2006, 2019 THALES GLOBAL SERVICES.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -10,11 +10,17 @@
  *******************************************************************************/
 package org.polarsys.capella.core.ui.properties.tabbed;
 
+import org.eclipse.jface.viewers.ISelection;
+import org.eclipse.jface.viewers.IStructuredSelection;
+import org.eclipse.sirius.business.api.query.DRepresentationDescriptorQuery;
+import org.eclipse.sirius.viewpoint.DRepresentationDescriptor;
+import org.eclipse.ui.IWorkbenchPart;
+import org.eclipse.ui.views.properties.tabbed.ITabDescriptor;
 import org.polarsys.capella.common.flexibility.wizards.ui.tabbed.FixedTabDescriptorProvider;
 
 
 /**
- * A TabDescriptorProvider for semantic elements
+ * A TabDescriptorProvider for semantic elements.
  */
 public class CapellaTabDescriptorProvider extends FixedTabDescriptorProvider {
 
@@ -31,4 +37,22 @@ public class CapellaTabDescriptorProvider extends FixedTabDescriptorProvider {
     return false;
   }
 
+  @Override
+  public ITabDescriptor[] getTabDescriptors(IWorkbenchPart part, ISelection selection) {
+    if (selection instanceof IStructuredSelection && !selection.isEmpty()) {
+      Object firstElement = ((IStructuredSelection) selection).getFirstElement();
+      
+      if (firstElement instanceof DRepresentationDescriptor) {
+        DRepresentationDescriptor descriptor = (DRepresentationDescriptor) firstElement;
+        boolean isValidRepresentation = new DRepresentationDescriptorQuery(descriptor).isRepresentationValid();
+        
+        if (!isValidRepresentation) {
+          return new ITabDescriptor[] {};
+        }
+        
+      }
+    }
+    
+    return super.getTabDescriptors(part, selection);
+  }
 }
