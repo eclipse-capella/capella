101b5a53130bc305786facc040cd1efd1a439524
Felix Dorner <felix.dorner@gmail.com>
2019-01-21 13:10:58 +0100

    [2346] Remove unused NLS constants
    Bug: 2346
    Change-Id: Icadd325448dbb7634422ee226ddadd8790b523e8
    Signed-off-by: Felix Dorner <felix.dorner@gmail.com>

--- core/plugins/org.polarsys.capella.core.libraries.ui/src/org/polarsys/capella/core/libraries/ui/move/Messages.java
+++ core/plugins/org.polarsys.capella.core.libraries.ui/src/org/polarsys/capella/core/libraries/ui/move/Messages.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2017 THALES GLOBAL SERVICES.
+ * Copyright (c) 2019 THALES GLOBAL SERVICES.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -18,8 +18,6 @@ public class Messages extends NLS {
   public static String MoveStagingView_clearParentActionTooltip;
   public static String MoveStagingView_destinationSectionDescription;
   public static String MoveStagingView_destinationSectionTitle;
-  public static String MoveStagingView_formText;
-  public static String MoveStagingView_formTitle;
   public static String MoveStagingView_stageSectionDescription;
   public static String MoveStagingView_stageSectionTitle;
   public static String MoveStagingView_unstageActionTooltip;
