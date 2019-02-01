a9a0628fa4fcf807a03b8a93d5d3d9618daf7f39
Ali AKAR <ali.akar82@gmail.com>
2019-02-01 18:06:30 +0100

    [2332] EnumerationLiteral in Enumeration's ownedDataValues reference instead of ownedLiterals
    Bug 2332
    
    Change-Id: Ice4390e4861bf2f8940a5d45cf5760b179eade0b
    Signed-off-by: Ali AKAR <ali.akar82@gmail.com>

--- core/plugins/org.polarsys.capella.core.data.information.validation/src/org/polarsys/capella/core/data/information/validation/dataValue/DataValueContainingFeatureCheck.java
+++ core/plugins/org.polarsys.capella.core.data.information.validation/src/org/polarsys/capella/core/data/information/validation/dataValue/DataValueContainingFeatureCheck.java
@@ -13,20 +13,30 @@ package org.polarsys.capella.core.data.information.validation.dataValue;
 import org.eclipse.core.runtime.IStatus;
 import org.eclipse.emf.ecore.EObject;
 import org.eclipse.emf.validation.IValidationContext;
+import org.polarsys.capella.core.data.information.datatype.BooleanType;
 import org.polarsys.capella.core.data.information.datatype.Enumeration;
 import org.polarsys.capella.core.data.information.datavalue.EnumerationLiteral;
+import org.polarsys.capella.core.data.information.datavalue.LiteralBooleanValue;
 import org.polarsys.capella.core.validation.rule.AbstractValidationRule;
 
-public class EnumerationLiteraContainingFeatureCheck extends AbstractValidationRule {
+public class DataValueContainingFeatureCheck extends AbstractValidationRule {
 
   @Override
   public IStatus validate(IValidationContext ctx) {
     EObject target = ctx.getTarget();
     if (target instanceof EnumerationLiteral && target.eContainer() instanceof Enumeration) {
-      EnumerationLiteral enumLiteral = (EnumerationLiteral)target;
-      Enumeration enumeration = (Enumeration)target.eContainer();
-      if(enumeration.getOwnedDataValues().contains(enumLiteral)) {
-        return ctx.createFailureStatus(enumLiteral.getName(), enumeration.getName());
+      EnumerationLiteral enumLiteral = (EnumerationLiteral) target;
+      Enumeration enumeration = (Enumeration) target.eContainer();
+      if (enumeration.getOwnedDataValues().contains(enumLiteral)) {
+        return ctx.createFailureStatus(enumLiteral.getName(), "EnumerationLiteral", enumeration.getName(),
+            "Enumeration");
+      }
+    } else if (target instanceof LiteralBooleanValue && target.eContainer() instanceof BooleanType) {
+      LiteralBooleanValue literalBooleanValue = (LiteralBooleanValue) target;
+      BooleanType booleanType = (BooleanType) target.eContainer();
+      if (booleanType.getOwnedDataValues().contains(literalBooleanValue)) {
+        return ctx.createFailureStatus(literalBooleanValue.getName(), "LiteralBooleanValue", booleanType.getName(),
+            "BooleanType");
       }
     }
     // Return success
