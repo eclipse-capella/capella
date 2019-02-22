efd195261632a484868b9d14026e579d24bc6f55
Ali AKAR <ali.akar82@gmail.com>
2019-02-22 10:32:53 +0100

    [2387] Performance enhancement for Semantic Browser View
    Sync the view when it's visible with the explorer selection if no input
    set to the viewer
    
    Change-Id: I721eb8f3fa3e563ce49414982d66c0a81e9b2046
    Signed-off-by: Ali AKAR <ali.akar82@gmail.com>
    Signed-off-by: Philippe DUL <philippe.dul@thalesgroup.com>

--- core/plugins/org.polarsys.capella.core.ui.semantic.browser/src/org/polarsys/capella/core/ui/semantic/browser/actions/RefreshAction.java
+++ core/plugins/org.polarsys.capella.core.ui.semantic.browser/src/org/polarsys/capella/core/ui/semantic/browser/actions/RefreshAction.java
@@ -12,29 +12,84 @@ package org.polarsys.capella.core.ui.semantic.browser.actions;
 
 import org.eclipse.jface.action.Action;
 import org.eclipse.jface.action.IAction;
+import org.eclipse.jface.viewers.ISelection;
+import org.eclipse.jface.viewers.ISelectionProvider;
+import org.eclipse.ui.IEditorPart;
+import org.eclipse.ui.IViewPart;
+import org.eclipse.ui.IWorkbenchPage;
+import org.eclipse.ui.IWorkbenchPart;
 import org.polarsys.capella.common.ui.toolkit.browser.view.ISemanticBrowserViewPart;
+import org.polarsys.capella.core.platform.sirius.ui.navigator.view.CapellaCommonNavigator;
 import org.polarsys.capella.core.ui.semantic.browser.CapellaBrowserActivator;
 import org.polarsys.capella.core.ui.semantic.browser.IImageKeys;
 import org.polarsys.capella.core.ui.semantic.browser.view.SemanticBrowserView;
 
 public class RefreshAction extends Action {
 
-  private ISemanticBrowserViewPart semanticBrowserViewPart;
+  protected ISemanticBrowserViewPart semanticBrowserViewPart;
 
   public RefreshAction(ISemanticBrowserViewPart semanticBrowserViewPart) {
-    super(null, IAction.AS_PUSH_BUTTON);
+    this(semanticBrowserViewPart, IAction.AS_PUSH_BUTTON);
+  }
+  
+  public RefreshAction(ISemanticBrowserViewPart semanticBrowserViewPart, int type) {
+    super(null, type);
     this.semanticBrowserViewPart = semanticBrowserViewPart;
     setImageDescriptor(CapellaBrowserActivator.getDefault().getImageDescriptor(IImageKeys.IMG_REFRESH));
   }
 
   @Override
   public void run() {
-    boolean isListening = semanticBrowserViewPart.getModel().isListeningToPageSelectionEvents();
-    ListenToSelectionEventsAction action = new ListenToSelectionEventsAction(semanticBrowserViewPart);
-    action.doRun();
-    if(!isListening) {
-      ((SemanticBrowserView)semanticBrowserViewPart).deactivateListeningToPageSelectionEvents();
+    doRun();
+  }
+
+  protected void doRun() {
+    // Get the current selection.
+    IWorkbenchPage activePage = semanticBrowserViewPart.getSite().getPage();
+    // Get the active part to get something selectable.
+    IWorkbenchPart activePart = activePage.getActivePart();
+    IWorkbenchPart part = null;
+    ISelection selection = null;
+    if ((null != activePart) && (semanticBrowserViewPart != activePart)) {
+      // Handle selection at view creation time.
+      ISelectionProvider selectionProvider = activePart.getSite().getSelectionProvider();
+      if (null != selectionProvider) {
+        selection = selectionProvider.getSelection();
+        if (isSomethingSelectable(selection)) {
+          part = activePart;
+        }
+      }
+    } else {
+      IViewPart capellaExplorer = activePage.findView(CapellaCommonNavigator.ID);
+      if (null != capellaExplorer) {
+        // Capella explorer is displayed.
+        selection = getSelection(capellaExplorer);
+        if (isSomethingSelectable(selection)) {
+          part = capellaExplorer;
+        }
+      }
+      if (null == part) {
+        // Try to get a selection from active editor.
+        IEditorPart activeEditor = activePage.getActiveEditor();
+        if (null != activeEditor) {
+          selection = getSelection(activeEditor);
+          if (isSomethingSelectable(selection)) {
+            part = activeEditor;
+          }
+        }
+      }
     }
-    ((SemanticBrowserView)semanticBrowserViewPart).refreshTitleBar();
+    if (null != part) {
+      // Something to select.
+      ((SemanticBrowserView) semanticBrowserViewPart).getSelectionListener().selectionChanged(part, selection);
+    }
+  }
+
+  private ISelection getSelection(IWorkbenchPart part) {
+    return part.getSite().getSelectionProvider().getSelection();
+  }
+
+  private boolean isSomethingSelectable(ISelection selection) {
+    return (null != selection) && !selection.isEmpty();
   }
 }
