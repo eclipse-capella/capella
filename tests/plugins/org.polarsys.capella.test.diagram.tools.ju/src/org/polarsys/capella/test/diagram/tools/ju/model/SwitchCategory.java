efa3af7b7d6f619fae61ea3b83f4fc23188c5e68
Tu Ton <minhtutonthat@gmail.com>
2019-04-11 16:06:35 +0200

    [2468] The switch category/exchange tool does not work with hidden exchanges
    Bug: 2468
    Change-Id: I925ca378b0f4fa3fda6b6cba433de6e7c24ef692
    Signed-off-by: Tu Ton <minhtutonthat@gmail.com>

--- tests/plugins/org.polarsys.capella.test.diagram.tools.ju/src/org/polarsys/capella/test/diagram/tools/ju/model/SwitchCategory.java
+++ tests/plugins/org.polarsys.capella.test.diagram.tools.ju/src/org/polarsys/capella/test/diagram/tools/ju/model/SwitchCategory.java
@@ -17,8 +17,13 @@ public abstract class SwitchCategory extends AbstractDiagramTestCase {
   public static String SA__FUNCTIONPKG = "015492b3-98d6-4507-8b0a-6bccc844e653";
   public static String SA__FUNCTIONPKG__CATEGORY1 = "75a95691-eac6-460b-938c-fed7e5dd80ff";
   public static String SA__FUNCTIONPKG__SF__FUNTIONALEXCHANGE1 = "20b2edb7-49ad-4295-a020-89af7974897e";
+  public static final String FUNCTIONALEXCHANGE_3 = "973e6234-a468-4c82-9836-2e11d9174721"; //$NON-NLS-1$
+  public static final String FUNCTIONALEXCHANGE_4 = "c216d4cf-b52e-4801-a481-b71e48a77789"; //$NON-NLS-1$
+  public static final String FUNCTIONALEXCHANGE_5 = "d405695d-ae4c-45ec-a9d1-4a92ac5b0a71"; //$NON-NLS-1$
+  public static final String EXCHANGECATEGORY_2 = "5e94610f-0697-416b-aee0-efaf89ff01c0"; //$NON-NLS-1$
 
   public static String SDFB_ROOT_SYSTEM_FUNCTION = "[SDFB] Root System Function - System Data Flow Blank";
+  public static final String LAB_LOGICAL_SYSTEM = "[LAB] Logical System"; //$NON-NLS-1$
 
   @Override
   public String getRequiredTestModel() {
