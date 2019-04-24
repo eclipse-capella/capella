64ca70492cb8f7676213621250adefc5996397ff
Georgiana Ecobici <georgiana-elena.ecobici@thalesgroup.com>
2019-04-24 11:00:49 +0200

    [2481] New validation Rules on REC/RPL
    DCON_04: RPL/REC_RPL without REC
    DCON_05: REC without RPL
    DCON_06: REC without REC_RPL
    
    Bug: 2481
    Change-Id: Iba6a6c1da41abb7cadbfa630567502a03aee79dd
    Signed-off-by: Georgiana Ecobici <georgiana-elena.ecobici@thalesgroup.com>

---
+++ tests/plugins/org.polarsys.capella.test.validation.rules.ju/src/org/polarsys/capella/test/validation/rules/ju/testcases/dcon/Rule_DCON_05.java
@@ -0,0 +1,61 @@
+/*******************************************************************************
+ * Copyright (c) 2019 THALES GLOBAL SERVICES.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *  
+ * Contributors:
+ *    Thales - initial API and implementation
+ *******************************************************************************/
+package org.polarsys.capella.test.validation.rules.ju.testcases.dcon;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.eclipse.emf.ecore.EClass;
+import org.polarsys.capella.common.re.RePackage;
+import org.polarsys.capella.test.framework.api.OracleDefinition;
+import org.polarsys.capella.test.validation.rules.ju.testcases.AbstractRulesOnDesignTest;
+
+/**
+ * test on DCON_05 - REC without RPL.
+ */
+public class Rule_DCON_05 extends AbstractRulesOnDesignTest {
+  public static final String REC1 = "6db3e5ab-102f-4e6a-81f8-356a2cb05b60";
+  public static final String REC2 = "e0f20998-9fbe-42a3-a6c5-dc839cee57f7";
+  public static final String RPL2_REC2 = "d894fda0-85a5-44fe-aa00-6d639ca14d76";
+
+  /**
+   * @see org.polarsys.capella.test.validation.rules.ju.testcases.ValidationRuleTestCase#getTargetedEClass()
+   */
+  @Override
+  protected EClass getTargetedEClass() {
+    return RePackage.Literals.CATALOG_ELEMENT;
+  }
+
+  /**
+   * @see org.polarsys.capella.test.validation.rules.ju.testcases.ValidationRuleTestCase#getRuleID()
+   */
+  @Override
+  protected String getRuleID() {
+    return "org.polarsys.capella.core.re.validation.DCON_05";
+  }
+
+  /**
+   * @see org.polarsys.capella.test.validation.rules.ju.testcases.ValidationRulePartialTestCase#getScopeDefinition()
+   */
+  @Override
+  protected List<String> getScopeDefinition() {
+    return Arrays.asList(new String[] { REC1, REC2, RPL2_REC2 });
+  }
+
+  /**
+   * @see org.polarsys.capella.test.validation.rules.ju.testcases.ValidationRuleTestCase#getOracleDefinitions()
+   */
+  @Override
+  protected List<OracleDefinition> getOracleDefinitions() {
+    return Arrays.asList(new OracleDefinition[] { new OracleDefinition(REC1, 1), new OracleDefinition(REC2, 0),
+        new OracleDefinition(RPL2_REC2, 0) });
+  }
+}
