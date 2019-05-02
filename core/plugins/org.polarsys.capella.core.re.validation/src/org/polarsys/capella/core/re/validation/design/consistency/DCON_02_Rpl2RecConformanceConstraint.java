f1d3beb1ee3f469d8df464da47cdb8e4e90bb08a
Jeremy Aubry <jaubry@obeo.fr>
2019-05-02 10:51:11 +0200

    [2481] Also check if the REC is a proxy
    Bug: 2481
    Change-Id: I5ccf08a69e4a60c166da4dc8dcbd03b86af146fe
    Signed-off-by: Jeremy Aubry <jaubry@obeo.fr>

--- core/plugins/org.polarsys.capella.core.re.validation/src/org/polarsys/capella/core/re/validation/design/consistency/DCON_02_Rpl2RecConformanceConstraint.java
+++ core/plugins/org.polarsys.capella.core.re.validation/src/org/polarsys/capella/core/re/validation/design/consistency/DCON_02_Rpl2RecConformanceConstraint.java
@@ -16,6 +16,7 @@ import java.util.List;
 
 import org.eclipse.core.runtime.IStatus;
 import org.eclipse.core.runtime.NullProgressMonitor;
+import org.eclipse.core.runtime.Status;
 import org.eclipse.emf.ecore.EObject;
 import org.eclipse.emf.validation.AbstractModelConstraint;
 import org.eclipse.emf.validation.IValidationContext;
@@ -23,6 +24,7 @@ import org.eclipse.emf.validation.model.ConstraintStatus;
 import org.eclipse.emf.validation.service.ConstraintRegistry;
 import org.eclipse.emf.validation.service.IConstraintDescriptor;
 import org.polarsys.capella.common.helpers.EObjectExt;
+import org.polarsys.capella.common.re.Activator;
 import org.polarsys.capella.common.re.CatalogElement;
 import org.polarsys.capella.common.re.CatalogElementKind;
 import org.polarsys.capella.common.re.RePackage;
@@ -35,10 +37,16 @@ public class DCON_02_Rpl2RecConformanceConstraint extends AbstractModelConstrain
   @Override
   public IStatus validate(IValidationContext ctx) {
     CatalogElement catalogElement = (CatalogElement) ctx.getTarget();
-    if (catalogElement.getKind() == CatalogElementKind.RPL && catalogElement.getOrigin() != null) {
+
+    if (catalogElement.getKind() == CatalogElementKind.RPL) {
+      CatalogElement rec = catalogElement.getOrigin();
+      if (rec == null || rec.eIsProxy()) {
+        return new Status(IStatus.ERROR, Activator.PLUGIN_ID, "Your RPL is invalid (no REC), please validate your model.");
+      }
+      // if rec is valid
       return validateRPL(ctx, catalogElement);
     } else if (catalogElement.getKind() == CatalogElementKind.REC) {
-      Collection<IStatus> statuses = new ArrayList<IStatus>();
+      Collection<IStatus> statuses = new ArrayList<>();
       statuses.add(ctx.createSuccessStatus());
       // Collect all RPL from this REC and validate them
       List<EObject> allRPLs = EObjectExt.getReferencers(catalogElement, RePackage.eINSTANCE.getCatalogElement(),
@@ -55,7 +63,7 @@ public class DCON_02_Rpl2RecConformanceConstraint extends AbstractModelConstrain
   }
 
   private IStatus validateRPL(IValidationContext ctx, CatalogElement rpl) {
-    Collection<Object> selection = new ArrayList<Object>();
+    Collection<Object> selection = new ArrayList<>();
     selection.add(rpl);
     Rpl2RecConformanceCheckLauncher launcher = new Rpl2RecConformanceCheckLauncher();
     launcher.addSharedParameter(new GenericParameter<Boolean>(IOptionsConstants.IS_DRY_RUN, true, "This is a flag to indicate a dry run"));
@@ -63,7 +71,7 @@ public class DCON_02_Rpl2RecConformanceConstraint extends AbstractModelConstrain
     if (!launcher.isConform()) {
       // When the target is a REC, we need to set the RPL as target of the created failure status to be able to use it
       // in the quick-fix, so create always a status with the RPL as target.
-      return createFailureStatus(ctx, rpl, new Object[] { rpl, rpl.getOrigin() });
+      return createFailureStatus(ctx, rpl, rpl, rpl.getOrigin());
     }
     return ctx.createSuccessStatus();
   }
