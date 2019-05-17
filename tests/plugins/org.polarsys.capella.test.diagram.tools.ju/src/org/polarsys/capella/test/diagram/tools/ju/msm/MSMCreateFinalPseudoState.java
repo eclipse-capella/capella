ed4a5961d1dadea53dc0b9a5cffcb42db9aaf2bf
Matei-Alexandru Stamate <matei-alexandru.stamate@thalesgroup.com>
2019-05-17 17:41:50 +0200

    [Tests] Add missing tests for Mode State Machine Diagram
    Change-Id: Id86d3ebca308ce1affc07185f1afd2bc5a14bb44
    Signed-off-by: Matei-Alexandru Stamate <matei-alexandru.stamate@thalesgroup.com>

---
+++ tests/plugins/org.polarsys.capella.test.diagram.tools.ju/src/org/polarsys/capella/test/diagram/tools/ju/msm/MSMCreateFinalPseudoState.java
@@ -0,0 +1,38 @@
+/*******************************************************************************
+ * Copyright (c) 2019 THALES GLOBAL SERVICES.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *  
+ * Contributors:
+ *    Thales - initial API and implementation
+ *******************************************************************************/
+package org.polarsys.capella.test.diagram.tools.ju.msm;
+
+import org.eclipse.sirius.business.api.session.Session;
+import org.polarsys.capella.core.model.helpers.BlockArchitectureExt.Type;
+import org.polarsys.capella.test.diagram.common.ju.api.AbstractDiagramTestCase;
+import org.polarsys.capella.test.diagram.common.ju.context.MSMDiagram;
+import org.polarsys.capella.test.framework.context.SessionContext;
+
+public class MSMCreateFinalPseudoState extends AbstractDiagramTestCase {
+
+  private final String diagramName = "SA Mode State Machine Test Diagram";
+
+  @Override
+  protected String getRequiredTestModel() {
+    return "DiagramToolsModel";
+  }
+
+  @Override
+  public void test() throws Exception {
+
+    Session session = getSession(getRequiredTestModel());
+    SessionContext context = new SessionContext(session);
+
+    MSMDiagram diagram = MSMDiagram.openDiagram(context, this.diagramName, Type.SA);
+
+    diagram.createFinal(diagram.getDiagramId());
+  }
+}
