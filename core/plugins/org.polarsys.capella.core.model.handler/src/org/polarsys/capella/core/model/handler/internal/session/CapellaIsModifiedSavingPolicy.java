6215d318e6801f41e3b6071d92786bfbcf6c22d0
Maxime Porhel <maxime.porhel@obeo.fr>
2019-07-22 17:14:58 +0200

    [1667] Refactor saving policy and add CapellaIsModifiedSavingPolicy
    - Add CapellaIsModifiedSavingPolicy, subclass off the Sirius's
    IsModifiedSavingPolicy
    - Refactor CapellaSavingPolicy to extract Capella specific code in a
    helper and reuse it from CapellaIsModifiedSavingPolicy.
    - Activate "saveInExclusiveTransaction" and "deferSaveToPostCommit"
    CapellaIsModifiedSavingPolicy is used.
    
    The CapellaIsModifiedSavingPolicy can be enabled with the use of
    -Dorg.polarsys.capella.core.session.saving.policy.old=false
    
    This commit requires the following patch set in Sirius:
    https://git.eclipse.org/r/#/c/146456/
    
    
    Bug: 1667
    Change-Id: If5d34ddbeef1f83f6f713e11e89751ebd683f7d2
    Signed-off-by: Maxime Porhel <maxime.porhel@obeo.fr>

---
+++ core/plugins/org.polarsys.capella.core.model.handler/src/org/polarsys/capella/core/model/handler/internal/session/CapellaIsModifiedSavingPolicy.java
@@ -0,0 +1,48 @@
+/*******************************************************************************
+ * Copyright (c) 2019 THALES GLOBAL SERVICES.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *  
+ * Contributors:
+ *    Obeo - initial API and implementation
+ *******************************************************************************/
+package org.polarsys.capella.core.model.handler.internal.session;
+
+import java.util.Collection;
+import java.util.Map;
+
+import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.emf.ecore.resource.Resource;
+import org.eclipse.emf.transaction.TransactionalEditingDomain;
+import org.eclipse.sirius.business.internal.session.IsModifiedSavingPolicy;
+import org.eclipse.sirius.common.tools.api.resource.ResourceSetSync;
+import org.polarsys.capella.core.model.handler.internal.session.CapellaSavingPolicy.CapellaSavingPolicyHelper;
+
+/**
+ * Custom saving policy base on {@link ResourceSetSync} status.
+ */
+public class CapellaIsModifiedSavingPolicy extends IsModifiedSavingPolicy {
+
+    CapellaIsModifiedSavingPolicy(TransactionalEditingDomain domain) {
+        super(domain);
+    }
+
+    @Override
+    public Collection<Resource> save(final Iterable<Resource> allResources, final Map<?, ?> options, IProgressMonitor monitor) {
+        new CapellaSavingPolicyHelper().checkUnsaveableFiles(allResources);
+        return super.save(allResources, options, monitor);
+    }
+
+    /**
+     * Overridden to avoid changes detection based on temporary file creation.<br>
+     * {@inheritDoc}
+     */
+    @Override
+    protected boolean hasDifferentSerialization(Resource resource, Map<?, ?> options) {
+        // IsModifiedSavingPolicy has already check that ResourceSetSync.isReadOnly returns false and that the resource
+        // has been marked as modified.
+        return true;
+    }
+}
--- core/plugins/org.polarsys.capella.core.model.handler/src/org/polarsys/capella/core/model/handler/internal/session/CapellaSavingPolicy.java
+++ core/plugins/org.polarsys.capella.core.model.handler/src/org/polarsys/capella/core/model/handler/internal/session/CapellaSavingPolicy.java
@@ -1,10 +1,10 @@
 /*******************************************************************************
- * Copyright (c) 2006, 2017 THALES GLOBAL SERVICES.
+ * Copyright (c) 2006, 2019 THALES GLOBAL SERVICES.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v10.html
- *  
+ *
  * Contributors:
  *    Thales - initial API and implementation
  *******************************************************************************/
@@ -30,72 +30,86 @@ import org.polarsys.capella.common.mdsofa.common.constant.ICommonConstants;
  * Custom saving policy base on {@link ResourceSetSync} status.
  */
 public class CapellaSavingPolicy extends SavingPolicyImpl {
-  
-  CapellaSavingPolicy(TransactionalEditingDomain domain_p) {
-    super(domain_p);
-  }
 
-  public Collection<Resource> save(final Iterable<Resource> allResources, final Map<?, ?> options,
-      IProgressMonitor monitor) {
-
-    Collection<IFile> files = retrieveUnsaveableFiles(allResources);
-    if (!files.isEmpty()) {
-      String msg = Messages.CapellaSavingPolicy_unwriteableFiles; 
-      for (IFile file : files) {
-        msg += file.toString() + ICommonConstants.EOL_CHARACTER;
-      }
-      throw new RuntimeException(msg);
+    CapellaSavingPolicy(TransactionalEditingDomain domain_p) {
+        super(domain_p);
     }
 
-    return super.save(allResources, options, monitor);
-  }
+    @Override
+    public Collection<Resource> save(final Iterable<Resource> allResources, final Map<?, ?> options, IProgressMonitor monitor) {
 
-  /**
-   * Utility method in order to perform a pre checking on a Session about the saveable state of its embedded resource.
-   * 
-   * @param session
-   *          the target Session
-   * @param unsaveableFiles
-   *          return the list of unsaveable resources, otherwise an empty one.
-   * @return <code>true</code> if session can be saved, <code>false</code> otherwise.
-   */
-  public Collection<IFile> retrieveUnsaveableFiles(Iterable<Resource> allResources) {
-    Collection<IFile> files = new ArrayList<IFile>();
+        new CapellaSavingPolicyHelper().checkUnsaveableFiles(allResources);
 
-    // Let's check it
-    for (Resource resource : allResources) {
-      if (null != resource) {
-        // check whether we have the write permission to the folder containing the resource
-        IFile file = EcoreUtil2.getFile(resource);
-        if (null != file) {
-          IPath path = file.getLocation();
-          IPath parentFolderPath = path.removeLastSegments(1);
-          File dir = parentFolderPath.toFile();
+        return super.save(allResources, options, monitor);
+    }
 
-          // Read-only mode is already checked in org.polarsys.capella.core.model.handler.pre.commit.listener.FileModificationPreCommitListener
-          if ((dir.exists() && !dir.canWrite())) {
-            files.add(file);
-          }
+    /**
+     * Overridden to avoid changes detection based on temporary file creation.<br>
+     * {@inheritDoc}
+     */
+    @Override
+    protected boolean hasDifferentSerialization(Resource resource, Map<?, ?> options) {
+        boolean result = false;
+        if (!ResourceSetSync.isReadOnly(resource)) {
+            ResourceStatus resourceStatus = ResourceSetSync.getStatus(resource);
+            // If resource status different from SYNC, there are some changes to save.
+            if (ResourceStatus.SYNC != resourceStatus) {
+                result = true;
+            }
         }
-      }
+        return result;
     }
-    return files;
-  }
 
-  /**
-   * Overridden to avoid changes detection based on temporary file creation.<br>
-   * {@inheritDoc}
-   */
-  @Override
-  protected boolean hasChangesToSave(Resource resource_p) {
-    boolean result = false;
-    if (!ResourceSetSync.isReadOnly(resource_p)) {
-      ResourceStatus resourceStatus = ResourceSetSync.getStatus(resource_p);
-      // If resource status different from SYNC, there are some changes to save.
-      if (ResourceStatus.SYNC != resourceStatus) {
-        result = true;
-      }
+    protected static class CapellaSavingPolicyHelper {
+
+        /**
+         * Check that the given collection of resources does not con
+         *
+         * @param allResources
+         */
+        public void checkUnsaveableFiles(final Iterable<Resource> allResources) {
+            Collection<IFile> files = retrieveUnsaveableFiles(allResources);
+            if (!files.isEmpty()) {
+                String msg = Messages.CapellaSavingPolicy_unwriteableFiles;
+                for (IFile file : files) {
+                    msg += file.toString() + ICommonConstants.EOL_CHARACTER;
+                }
+                throw new RuntimeException(msg);
+            }
+        }
+
+        /**
+         * Utility method in order to perform a pre checking on a Session about the saveable state of its embedded
+         * resource.
+         *
+         * @param session
+         *            the target Session
+         * @param unsaveableFiles
+         *            return the list of unsaveable resources, otherwise an empty one.
+         * @return <code>true</code> if session can be saved, <code>false</code> otherwise.
+         */
+        public Collection<IFile> retrieveUnsaveableFiles(Iterable<Resource> allResources) {
+            Collection<IFile> files = new ArrayList<IFile>();
+
+            // Let's check it
+            for (Resource resource : allResources) {
+                if (null != resource) {
+                    // check whether we have the write permission to the folder containing the resource
+                    IFile file = EcoreUtil2.getFile(resource);
+                    if (null != file) {
+                        IPath path = file.getLocation();
+                        IPath parentFolderPath = path.removeLastSegments(1);
+                        File dir = parentFolderPath.toFile();
+
+                        // Read-only mode is already checked in
+                        // org.polarsys.capella.core.model.handler.pre.commit.listener.FileModificationPreCommitListener
+                        if ((dir.exists() && !dir.canWrite())) {
+                            files.add(file);
+                        }
+                    }
+                }
+            }
+            return files;
+        }
     }
-    return result;
-  }
 }
