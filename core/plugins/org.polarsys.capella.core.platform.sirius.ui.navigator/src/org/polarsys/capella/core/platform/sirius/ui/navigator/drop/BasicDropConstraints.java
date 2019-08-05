a34f3b40b5a2812aa7fa9d6a8f093941d2ab26f9
cong-bang.do <docongbang1993@gmail.com>
2019-08-05 16:55:48 +0200

    [2537] Add constraints for DnD of component in Capella Project Explorer
    - Use ComponentExt to check:
    -- if the component/part can be dropped in the target
    -- if the actor/part can be dropped in the target
    
    Bug: 2537
    Change-Id: Ia24eff02fe095e33275499a8fa19156084a62682
    Signed-off-by: cong-bang.do <docongbang1993@gmail.com>

--- core/plugins/org.polarsys.capella.core.platform.sirius.ui.navigator/src/org/polarsys/capella/core/platform/sirius/ui/navigator/drop/BasicDropConstraints.java
+++ core/plugins/org.polarsys.capella.core.platform.sirius.ui.navigator/src/org/polarsys/capella/core/platform/sirius/ui/navigator/drop/BasicDropConstraints.java
@@ -14,6 +14,9 @@ import java.util.List;
 
 import org.eclipse.emf.ecore.EObject;
 import org.eclipse.swt.dnd.DND;
+import org.polarsys.capella.core.data.cs.Component;
+import org.polarsys.capella.core.data.cs.Part;
+import org.polarsys.capella.core.model.helpers.ComponentExt;
 
 /**
  * Encapsulate basic constraints for a drag/drop operation.
@@ -23,65 +26,73 @@ public class BasicDropConstraints {
   /**
    * Returns whether the drop is a COPY or not
    */
-  protected boolean isCopy(int operation_p) {
-    return operation_p == DND.DROP_COPY;
+  protected boolean isCopy(int operation) {
+    return operation == DND.DROP_COPY;
   }
 
   /**
    * Returns whether the element can be drop inside one of dragged elements
    */
-  protected boolean allowInside(int operation_p) {
+  protected boolean allowInside(int operation) {
     return true;
   }
 
   /**
    * Returns whether the element can be drop inside one of parent of dragged elements
    */
-  protected boolean allowInsideParent(int operation_p) {
+  protected boolean allowInsideParent(int operation) {
     return true;
   }
 
   /**
    * Checks if basic drop constraints are satisfied:
    * 
-   * - If the drop is not a copy drop, one cannot drop an element onto itself or its direct parent
-   * - One can never drop an element onto one of its children
+   * - If the drop is not a copy drop, one cannot drop an element onto itself or its direct parent - One can never drop
+   * an element onto one of its children
    * 
-   * @param dragged_p elements being dragged
-   * @param target_p the current drop target
-   * @param operation_p the DND operation (copy, move, etc).
+   * @param draggedElements
+   *          elements being dragged
+   * @param dropTarget
+   *          the current drop target
+   * @param operation
+   *          the DND operation (copy, move, etc).
    * @return true if basic constraints are satisfied, false otherwise.
    */
-  public boolean canDrop(List<EObject> dragged_p, EObject target_p, int operation_p) {
-    boolean isCopy = isCopy(operation_p);
-    boolean allowInside = allowInside(operation_p);
-    boolean allowInsideParent = allowInsideParent(operation_p);
+  public boolean canDrop(List<EObject> draggedElements, EObject dropTarget, int operation) {
+    boolean isCopy = isCopy(operation);
+    boolean allowInside = allowInside(operation);
+    boolean allowInsideParent = allowInsideParent(operation);
 
-    for (EObject dragged : dragged_p) {
+    for (EObject dragged : draggedElements) {
 
       // drag an element on itself is ok if we have copy semantics
-      if (dragged == target_p) {
-        if (!isCopy || !allowInside) {
-          return false;
-        }
+      if (dragged == dropTarget && (!isCopy || !allowInside)) {
+        return false;
       }
 
       // drag an element on its parent is ok if we have copy semantics
-      if (dragged.eContainer() == target_p) {
-        if (!isCopy || !allowInsideParent) {
-          return false;
-        }
+      if (dragged.eContainer() == dropTarget && (!isCopy || !allowInsideParent)) {
+        return false;
       }
 
       // dragging an element onto one of its children is confusing,
       // we disallow this. even if it might theoretically be
-      // valid in a copy operation. 
-      EObject container = target_p.eContainer();
+      // valid in a copy operation.
+      EObject container = dropTarget.eContainer();
       while (container != null) {
         if (dragged == container) {
           return false;
         }
         container = container.eContainer();
+      }
+
+      if ((dragged instanceof Component || dragged instanceof Part) && !ComponentExt.isActor(dragged)
+          && !ComponentExt.canCreateABComponent(dropTarget)) {
+        return false;
+      }
+
+      if (ComponentExt.isActor(dragged) && !ComponentExt.canCreateABActor(dropTarget)) {
+        return false;
       }
     }
     return true;
