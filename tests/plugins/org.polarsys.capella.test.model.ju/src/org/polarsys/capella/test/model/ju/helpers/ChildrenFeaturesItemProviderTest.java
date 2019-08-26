f6507c854c043681af6de3f438a23043e1b50b6b
cong-bang.do <docongbang1993@gmail.com>
2019-08-26 17:40:16 +0200

    [2537] Fix DnD to not take ownedMigratedElements as default drop target
    Bug: 2537
    Change-Id: I38ab36ea6256ee5c3e5428e501a7a3c80bfabef9
    Signed-off-by: cong-bang.do <docongbang1993@gmail.com>

---
+++ tests/plugins/org.polarsys.capella.test.model.ju/src/org/polarsys/capella/test/model/ju/helpers/ChildrenFeaturesItemProviderTest.java
@@ -0,0 +1,33 @@
+/*******************************************************************************
+ * Copyright (c) 2019 THALES GLOBAL SERVICES.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *  
+ * Contributors:
+ *    Thales - initial API and implementation
+ *******************************************************************************/
+package org.polarsys.capella.test.model.ju.helpers;
+
+import org.polarsys.capella.common.data.modellingcore.ModellingcorePackage;
+import org.polarsys.capella.common.data.modellingcore.provider.ModelElementItemProvider;
+import org.polarsys.capella.core.data.la.LaFactory;
+import org.polarsys.capella.core.data.la.LogicalFunction;
+import org.polarsys.capella.core.model.handler.provider.CapellaAdapterFactoryProvider;
+import org.polarsys.capella.test.framework.api.BasicTestCase;
+
+public class ChildrenFeaturesItemProviderTest extends BasicTestCase {
+
+  @Override
+  public void test() throws Exception {
+    ModelElementItemProvider provider = new ModelElementItemProvider(
+        CapellaAdapterFactoryProvider.getInstance().getAdapterFactory());
+    LogicalFunction function = LaFactory.eINSTANCE.createLogicalFunction();
+    assertTrue(
+        "ownedMigratedElements shall be removed from ModelElementItemProvider.getChildrenFeatures to prevent drag-and-drop into this feature",
+        !provider.getChildrenFeatures(function)
+            .contains(ModellingcorePackage.Literals.MODEL_ELEMENT__OWNED_MIGRATED_ELEMENTS));
+  }
+
+}
