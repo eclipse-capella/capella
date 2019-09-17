5281bbab6215ad6791aa2334601afa294367c115
Georgiana Ecobici <georgiana-elena.ecobici@thalesgroup.com>
2019-09-17 08:34:29 +0200

    [2587] Add queries for DataValue, DataValueReference
    Change-Id: Ie56c616d1cd986df2fd5aa632bc0eaf13397e134
    Signed-off-by: Georgiana Ecobici <georgiana-elena.ecobici@thalesgroup.com>

---
+++ core/plugins/org.polarsys.capella.core.semantic.queries/src/org/polarsys/capella/core/semantic/queries/basic/queries/PropertyValue_applying_valued_element_DataValue.java
@@ -0,0 +1,47 @@
+/*******************************************************************************
+ * Copyright (c) 2019 THALES GLOBAL SERVICES.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ * Thales - initial API and implementation
+ *******************************************************************************/
+package org.polarsys.capella.core.semantic.queries.basic.queries;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.polarsys.capella.common.helpers.query.IQuery;
+import org.polarsys.capella.core.data.information.datavalue.LiteralBooleanValue;
+import org.polarsys.capella.core.data.information.datavalue.LiteralNumericValue;
+import org.polarsys.capella.core.data.information.datavalue.LiteralStringValue;
+
+public class PropertyValue_applying_valued_element_DataValue implements IQuery {
+
+  /**
+   * Constructor.
+   */
+  public PropertyValue_applying_valued_element_DataValue() {
+  }
+
+  @Override
+  public List<Object> compute(Object object) {
+    List<Object> result = new ArrayList<>();
+    if (object instanceof LiteralStringValue) {
+      LiteralStringValue pv = (LiteralStringValue) object;
+      result.add(pv.getValue());
+    }
+    else if (object instanceof LiteralNumericValue) {
+      LiteralNumericValue pv = (LiteralNumericValue) object;
+      result.add(pv.getValue());
+    }
+    else if (object instanceof LiteralBooleanValue) {
+      LiteralBooleanValue pv = (LiteralBooleanValue) object;
+      result.add(pv.isValue());
+    }
+
+    return result;
+  }
+}
