c64a619ed82a1aff5b68da4d5b427a2911681aa7
Jeremy Aubry <jaubry@obeo.fr>
2019-10-02 17:26:41 +0200

    [2626] Insert/Remove Component/Actor does not work in multipart mode
    Bug: 2626
    Change-Id: Ief221fd92afea912d3cb94c92b79f7ac4d810b54
    Signed-off-by: Jeremy Aubry <jaubry@obeo.fr>

---
+++ tests/plugins/org.polarsys.capella.test.diagram.tools.ju/src/org/polarsys/capella/test/diagram/tools/ju/xab/ShowHideComponentsMultipart.java
@@ -0,0 +1,33 @@
+/*******************************************************************************
+ * Copyright (c) 2019 THALES GLOBAL SERVICES.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *  
+ * Contributors:
+ *    Thales - initial API and implementation
+ *******************************************************************************/
+package org.polarsys.capella.test.diagram.tools.ju.xab;
+
+import org.eclipse.sirius.business.api.session.Session;
+import org.polarsys.capella.core.model.helpers.BlockArchitectureExt;
+import org.polarsys.capella.test.framework.context.SessionContext;
+
+public class ShowHideComponentsMultipart extends ShowHideComponents {
+
+  @Override
+  public void test() throws Exception {
+    Session session = getSession(getRequiredTestModel());
+    SessionContext context = new SessionContext(session);
+
+    context.setReusableComponents(PROJECT_APPROACH_ID);
+    
+    testOnXAB(context, OA__OAB_DIAGRAM, BlockArchitectureExt.Type.OA,
+        OA__OAB_ENTITY1, OA__OAB_DIAGRAM);
+    testOnXAB(context, LA__LAB_DIAGRAM, BlockArchitectureExt.Type.LA,
+        LA__LAB_LC1_PART, LA__LAB_LOGICAL_SYSTEM_PART);
+    
+    context.setSingletonComponents(PROJECT_APPROACH_ID);
+  }
+}
