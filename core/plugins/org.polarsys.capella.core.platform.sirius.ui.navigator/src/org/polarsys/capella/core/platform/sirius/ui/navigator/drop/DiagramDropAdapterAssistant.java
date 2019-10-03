aaa3042c13d4fcc7227c61be46c8e266a65239d6
Cosmin Visan <cosmin.visan@thalesgroup.com>
2019-10-03 12:54:30 +0200

    [2627] Fixed OAIB move from one OA to another.
    Bug: 2627
    Change-Id: Ia4fd34be1e51533ed52fe265af3375c037e83db9
    Signed-off-by: Cosmin Visan <cosmin.visan@thalesgroup.com>

--- core/plugins/org.polarsys.capella.core.platform.sirius.ui.navigator/src/org/polarsys/capella/core/platform/sirius/ui/navigator/drop/DiagramDropAdapterAssistant.java
+++ core/plugins/org.polarsys.capella.core.platform.sirius.ui.navigator/src/org/polarsys/capella/core/platform/sirius/ui/navigator/drop/DiagramDropAdapterAssistant.java
@@ -104,6 +104,7 @@ public class DiagramDropAdapterAssistant extends AbstractCapellaDropAdapterAssis
         || IDiagramNameConstants.MISSIONS_BLANK_DIAGRAM_NAME.equals(diagramId) || diagramId.contains("Data Flow Blank") //$NON-NLS-1$
         || IDiagramNameConstants.OPERATIONAL_ENTITY_BLANK_DIAGRAM_NAME.equals(diagramId)
         || IDiagramNameConstants.OPERATIONAL_ROLE_BLANK_DIAGRAM_NAME.equals(diagramId)
+        || IDiagramNameConstants.OPERATIONAL_ACTIVITY_INTERACTION_BLANK_DIAGRAM_NAME.equals(diagramId)
         || IDiagramNameConstants.LOGICAL_ARCHITECTURE_BLANK_DIAGRAM_NAME.equals(diagramId)
         || IDiagramNameConstants.PHYSICAL_ARCHITECTURE_BLANK_DIAGRAM_NAME.equals(diagramId)
         || IDiagramNameConstants.SYSTEM_ARCHITECTURE_BLANK_DIAGRAM_NAME.equals(diagramId)
