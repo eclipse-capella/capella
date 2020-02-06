d6122ddb8769ddef22cf1a2505a7e164b3eaac19
Philippe DUL <philippe.dul@thalesgroup.com>
2020-02-06 15:25:56 +0100

    [559825] Fix copyright
    Bug: 559825
    Change-Id: I0000000000000000000000000000000000000000
    Signed-off-by: Philippe DUL <philippe.dul@thalesgroup.com>

--- core/plugins/org.polarsys.capella.core.commandline.core.ui/src/org/polarsys/capella/core/commandline/core/ui/AbstractWorkbenchCommandLine.java
+++ core/plugins/org.polarsys.capella.core.commandline.core.ui/src/org/polarsys/capella/core/commandline/core/ui/AbstractWorkbenchCommandLine.java
@@ -5,6 +5,16 @@ import org.eclipse.ui.PlatformUI;
 import org.eclipse.ui.application.WorkbenchAdvisor;
 import org.polarsys.capella.core.commandline.core.AbstractCommandLine;
 
+/*******************************************************************************
+ * Copyright (c) 2020 THALES GLOBAL SERVICES.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *  
+ * Contributors:
+ *    Thales - initial API and implementation
+ *******************************************************************************/
 public class AbstractWorkbenchCommandLine extends AbstractCommandLine {
 
   /**
