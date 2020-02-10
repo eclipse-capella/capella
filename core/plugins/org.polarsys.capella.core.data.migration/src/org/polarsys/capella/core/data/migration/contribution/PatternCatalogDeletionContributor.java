e1342bb8f5fb38f777a55e3215ff6580d67b74eb
pdulth <philippe.dul@thalesgroup.com>
2020-02-10 13:57:15 +0100

    [559591] Remove diffmerge patterns and contribute to migration (#71)
    * [559591] Remove diffmerge patterns and contribute to migration
    
    Change-Id: I6ba2bd6eec8c714135fb0cefa5755c5ab259ab98
    Signed-off-by: Philippe DUL <philippe.dul@thalesgroup.com>

---
+++ core/plugins/org.polarsys.capella.core.data.migration/src/org/polarsys/capella/core/data/migration/contribution/PatternCatalogDeletionContributor.java
@@ -0,0 +1,60 @@
+/*******************************************************************************
+ * Copyright (c) 2020 THALES GLOBAL SERVICES.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *  
+ * Contributors:
+ *    Thales - initial API and implementation
+ *******************************************************************************/
+package org.polarsys.capella.core.data.migration.contribution;
+
+import org.eclipse.core.resources.IContainer;
+import org.eclipse.core.resources.IFile;
+import org.eclipse.core.resources.IFolder;
+import org.eclipse.core.resources.IResource;
+import org.eclipse.core.runtime.CoreException;
+import org.eclipse.core.runtime.IStatus;
+import org.eclipse.core.runtime.NullProgressMonitor;
+import org.eclipse.core.runtime.Status;
+import org.polarsys.capella.core.data.migration.AbstractMigrationRunnable;
+import org.polarsys.capella.core.data.migration.MigrationConstants;
+import org.polarsys.capella.core.data.migration.context.MigrationContext;
+import org.polarsys.capella.core.data.migration.contributor.AbstractMigrationContributor;
+
+/**
+ * This class delete catalog patterns
+ */
+public class PatternCatalogDeletionContributor extends AbstractMigrationContributor {
+
+  @Override
+  public String getKind() {
+    return MigrationConstants.MIGRATION_KIND__PATTERN;
+  }
+
+  @Override
+  public boolean isValidResource(IResource member) {
+    return "patterns".equals(member.getFileExtension());
+  }
+
+  @Override
+  public AbstractMigrationRunnable getRunnable(IFile file) {
+    return new AbstractMigrationRunnable(file) {
+      @Override
+      public IStatus run(MigrationContext context, boolean checkVersion) {
+        try {
+          IContainer parent = file.getParent();
+          file.delete(true, new NullProgressMonitor());
+          if (parent instanceof IFolder && parent.members().length == 0) {
+            parent.delete(true, new NullProgressMonitor());
+          }
+        } catch (CoreException e) {
+          return new Status(IStatus.ERROR, org.polarsys.capella.core.data.migration.Activator.PLUGIN_ID, e.getMessage(), e);
+        }
+        return Status.OK_STATUS;
+      }
+    };
+  }
+
+}
