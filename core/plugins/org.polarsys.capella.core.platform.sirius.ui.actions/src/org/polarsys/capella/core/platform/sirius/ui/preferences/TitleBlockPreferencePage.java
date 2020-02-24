95e647d734e8e318b99da6b3aa7fc49dd32a678f
Malina STOICANESCU <malina.stoicanescu@thalesgroup.com>
2020-02-24 13:55:17 +0100

    [Draft] resize columns, edit cell, add by default Diagram TitleBlock
    Change-Id: If9a15461ce4407c254dcc0280dd4a40de4529338
    Signed-off-by: Malina STOICANESCU <malina.stoicanescu@thalesgroup.com>

--- core/plugins/org.polarsys.capella.core.platform.sirius.ui.actions/src/org/polarsys/capella/core/platform/sirius/ui/preferences/TitleBlockPreferencePage.java
+++ core/plugins/org.polarsys.capella.core.platform.sirius.ui.actions/src/org/polarsys/capella/core/platform/sirius/ui/preferences/TitleBlockPreferencePage.java
@@ -13,6 +13,8 @@ package org.polarsys.capella.core.platform.sirius.ui.preferences;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.eclipse.jface.layout.TableColumnLayout;
+import org.eclipse.jface.preference.BooleanFieldEditor;
 import org.eclipse.jface.preference.IPreferenceStore;
 import org.eclipse.jface.preference.IntegerFieldEditor;
 import org.eclipse.jface.util.IPropertyChangeListener;
@@ -25,6 +27,7 @@ import org.eclipse.jface.viewers.ColumnViewerEditorActivationEvent;
 import org.eclipse.jface.viewers.ColumnViewerEditorActivationListener;
 import org.eclipse.jface.viewers.ColumnViewerEditorActivationStrategy;
 import org.eclipse.jface.viewers.ColumnViewerEditorDeactivationEvent;
+import org.eclipse.jface.viewers.ColumnWeightData;
 import org.eclipse.jface.viewers.FocusCellOwnerDrawHighlighter;
 import org.eclipse.jface.viewers.StyledCellLabelProvider;
 import org.eclipse.jface.viewers.TableViewer;
@@ -80,9 +83,11 @@ public class TitleBlockPreferencePage extends AbstractDefaultPreferencePage {
   TableViewer v;
   private Table table;
 
+  Button button1;
   Button add_button;
   Button edit_button;
   Button remove_button;
+  BooleanFieldEditor enableMonitoringFieldEditor;
   int columnsNumber;
   int rowsNumber;
 
@@ -107,6 +112,7 @@ public class TitleBlockPreferencePage extends AbstractDefaultPreferencePage {
 
   @Override
   protected void createFieldEditors() {
+    createCheckBox();
     createGroupForNumberOfColumns();
 
     Composite top = new Composite(getFieldEditorParent(), SWT.NONE);
@@ -118,8 +124,29 @@ public class TitleBlockPreferencePage extends AbstractDefaultPreferencePage {
     top.setLayoutData(new GridData(GridData.FILL_BOTH));
 
     createTable(top);
+
     // createButtons(top);
 
+  }
+
+  private void createCheckBox() {
+    button1 = new Button(getFieldEditorParent(), SWT.CHECK);
+    button1.setText("Add by default Diagram TitleBlock");
+    button1.setSelection(doGetPreferenceStore().getBoolean("defaultTitleBlock"));
+    /*
+     * enableMonitoringFieldEditor = new BooleanFieldEditor("defaultTitleBlock", "Add by default Diagram Title Block",
+     * getFieldEditorParent()) {
+     * 
+     * @SuppressWarnings("synthetic-access")
+     * 
+     * @Override protected void valueChanged(boolean oldValue_p, boolean newValue_p) { super.valueChanged(oldValue_p,
+     * newValue_p); } };
+     */
+    // enableMonitoringFieldEditor.setEnabled(doGetPreferenceStore().getBoolean("defaultTitleBlock"),getFieldEditorParent());
+    /*
+     * Composite compositeCheckBox = new Composite(top, SWT.NONE); Button buttonCheckBox = new Button(compositeCheckBox,
+     * SWT.CHECK); buttonCheckBox.setText("Add by default Diagram Title Block");
+     */
   }
 
   private Button createButton(Composite top, Composite container, String text, boolean enabled) {
@@ -200,12 +227,7 @@ public class TitleBlockPreferencePage extends AbstractDefaultPreferencePage {
         }
       }
     });
-    /*
-     * Menu menu = new Menu(getShell(), SWT.POP_UP); table.setMenu(menu); MenuItem menu_item = new MenuItem(menu,
-     * SWT.PUSH); menu_item.setText(DELETE_SELECTION); menu_item.addListener(SWT.Selection, new Listener() {
-     * 
-     * @Override public void handleEvent(Event event) { table.remove(table.getSelectionIndices()); } });
-     */
+
   }
 
   private void createTable(Composite top) {
@@ -222,11 +244,9 @@ public class TitleBlockPreferencePage extends AbstractDefaultPreferencePage {
     gridData.verticalAlignment = GridData.FILL;
     gridData.horizontalAlignment = GridData.FILL;
     gridData.widthHint = 200;
-    // gridData.heightHint = table.getItemHeight();
     gridData.horizontalSpan = 1;
     v.getTable().setLayoutData(gridData);
 
-    // v.getTable().setBounds();
     System.out.println("Lalal:" + v.getTable().getGridLineWidth());
     System.out.println("Border: " + v.getTable().getBorderWidth());
 
@@ -268,125 +288,6 @@ public class TitleBlockPreferencePage extends AbstractDefaultPreferencePage {
 
     v.getTable().addListener(SWT.MouseDoubleClick, treeListener);
   }
-  /*
-   * TableLayout tableLayout = new TableLayout();
-   * 
-   * table = new Table(top, SWT.MULTI | SWT.BORDER | SWT.H_SCROLL | SWT.V_SCROLL | SWT.FULL_SELECTION);
-   * table.setLayout(tableLayout); table.setHeaderVisible(true); // table.setLinesVisible(true);
-   * table.setLinesVisible(true);
-   * 
-   * table.setFont(top.getFont());
-   * 
-   * GridData gridData = new GridData(GridData.FILL_BOTH); gridData.grabExcessVerticalSpace = true;
-   * gridData.grabExcessHorizontalSpace = true; gridData.verticalAlignment = GridData.FILL; gridData.horizontalAlignment
-   * = GridData.FILL; gridData.widthHint = 200; gridData.heightHint = table.getItemHeight(); gridData.horizontalSpan =
-   * 1; table.setLayoutData(gridData);
-   */
-
-  /*
-   * int nrOfColumns = doGetPreferenceStore().getInt("columnField1");
-   * 
-   * column1.setWidth(150); column2.setWidth(150);
-   * 
-   * for (int i = 0; i < 2; i++) { TableItem item = new TableItem(table, SWT.NONE); item.setText(new String[] { "", ""
-   * }); Color color = Display.getDefault().getSystemColor(SWT.COLOR_GRAY);
-   * 
-   * item.setBackground(color);
-   * 
-   * /* TableEditor editorw = new TableEditor(table); editorw = new TableEditor(table); Text txtLang = new Text(table,
-   * SWT.BORDER); editorw.grabHorizontal = true; editorw.setEditor(txtLang, item, 0); editorw = new TableEditor(table);
-   * Text txtLang2 = new Text(table, SWT.BORDER); editorw.grabHorizontal = true; editorw.setEditor(txtLang2, item, 1);
-   */
-
-  // column1.pack();
-  // column2.pack();
-  /*
-   * final TableEditor editor = new TableEditor(table); // The editor must have the same size as the cell and must //
-   * not be any smaller than 50 pixels. editor.horizontalAlignment = SWT.LEFT; editor.grabHorizontal = true;
-   * editor.minimumWidth = 50; // editing the second column
-   */
-  /*
-   * table.addListener(SWT.MouseDown, new Listener() {
-   * 
-   * @Override public void handleEvent(Event event) { Point pt = new Point(event.x, event.y); TableItem item =
-   * table.getItem(pt); if (item != null) { for (int col = 0; col < table.getColumnCount(); col++) { Rectangle rect =
-   * item.getBounds(col); if (rect.contains(pt)) { System.out.println("item clicked."); System.out.println("column is "
-   * + col); System.out.println(item.getText(col)); String itemText = item.getText(col); String currentName = ""; String
-   * currentContent = ""; if (itemText != "") { itemText = itemText.replace(" ", ""); currentName =
-   * itemText.split("-")[0]; currentContent = itemText.split("-")[1]; } // int index = table.getSelectionIndex();
-   * 
-   * TitleBlockDialog dialog = new TitleBlockDialog(getShell());
-   * 
-   * dialog.setCurrentName(currentName); dialog.setCurrentContent(currentContent); dialog.create(); if (dialog.open() ==
-   * Window.OK) { item.setText(col, dialog.getName() + " - " + dialog.getContent()); table.setFocus();
-   * 
-   * } }
-   * 
-   * } } }
-   * 
-   * });
-   */
-
-  // 1.
-
-  /*
-   * for (int i = 0; i < 4; i++) { TableColumn column = new TableColumn(table, SWT.NONE); column.setWidth(100); }
-   * 
-   * TableItem x1 = new TableItem(table, SWT.NONE); TableEditor editorw = new TableEditor(table);
-   * 
-   * Label lblName = new Label(table, SWT.NONE); lblName.setText("Language"); editorw.grabHorizontal = true;
-   * editorw.setEditor(lblName, x1, 0);
-   * 
-   * editorw = new TableEditor(table); Text txtLang = new Text(table, SWT.BORDER); editorw.grabHorizontal = true;
-   * editorw.setEditor(txtLang, x1, 0);
-   * 
-   * editorw = new TableEditor(table); Label lblReference = new Label(table, SWT.NONE); lblReference.setText("Value");
-   * editorw.grabHorizontal = true; editorw.setEditor(lblReference, x1, 2);
-   * 
-   * editorw = new TableEditor(table); Text txtValue = new Text(table, SWT.BORDER);
-   * 
-   * editorw.grabHorizontal = true; editorw.setEditor(txtValue, x1, 1);
-   * 
-   * ///// table row 2 TableItem x2 = new TableItem(table, SWT.NONE); editorw = new TableEditor(table);
-   * 
-   * Label lblName2 = new Label(table, SWT.NONE);
-   * 
-   * lblName2.setText("Language"); editorw.grabHorizontal = true; editorw.setEditor(lblName2, x2, 0);
-   * 
-   * editorw = new TableEditor(table); Text txtLang2 = new Text(table, SWT.BORDER); editorw.grabHorizontal = true;
-   * editorw.setEditor(txtLang2, x2, 1);
-   * 
-   * editorw = new TableEditor(table); Label lblReference2 = new Label(table, SWT.NONE); lblReference2.setText("Value");
-   * editorw.grabHorizontal = true; editorw.setEditor(lblReference2, x2, 2);
-   * 
-   * editorw = new TableEditor(table); Text txtValue2 = new Text(table, SWT.BORDER); editorw.grabHorizontal = true;
-   * editorw.setEditor(txtValue2, x2, 3);
-   */
-  /*
-   * ColumnLayoutData[] fTableColumnLayouts = { new ColumnWeightData(165), new ColumnWeightData(165), new
-   * ColumnWeightData(165), new ColumnWeightData(165), new ColumnWeightData(165), new ColumnWeightData(165) };
-   * 
-   * TableColumn column;
-   * 
-   * tableLayout.addColumnData(fTableColumnLayouts[0]); column = new TableColumn(table, SWT.NONE, 0);
-   * column.setResizable(fTableColumnLayouts[0].resizable); column.setText(NAME);
-   * 
-   * tableLayout.addColumnData(fTableColumnLayouts[1]); column = new TableColumn(table, SWT.NONE, 1);
-   * column.setResizable(fTableColumnLayouts[1].resizable); column.setText(CONTENT);
-   * 
-   * tableLayout.addColumnData(fTableColumnLayouts[2]); column = new TableColumn(table, SWT.NONE, 2);
-   * column.setResizable(fTableColumnLayouts[1].resizable); column.setText("COloana3");
-   * tableLayout.addColumnData(fTableColumnLayouts[3]); column = new TableColumn(table, SWT.NONE, 2);
-   * column.setResizable(fTableColumnLayouts[1].resizable); column.setText("COloana4");
-   * tableLayout.addColumnData(fTableColumnLayouts[4]); column = new TableColumn(table, SWT.NONE, 2);
-   * column.setResizable(fTableColumnLayouts[1].resizable); // column.setText("COloana5");
-   * tableLayout.addColumnData(fTableColumnLayouts[5]); column = new TableColumn(table, SWT.NONE, 2);
-   * column.setResizable(fTableColumnLayouts[1].resizable); // column.setText("COloana6");
-   * 
-   * String[] currentTableItems = doGetPreferenceStore().getString("tableTitleBlock").split("#"); if
-   * (currentTableItems.length >= 2) { for (int i = 1; i < currentTableItems.length; i += 2) { TableItem item = new
-   * TableItem(table, SWT.NULL); item.setText(0, currentTableItems[i]); item.setText(1, currentTableItems[i + 1]); } }
-   */
 
   private void refreshTableColumns() {
     TableColumn[] columns = v.getTable().getColumns();
@@ -412,15 +313,6 @@ public class TitleBlockPreferencePage extends AbstractDefaultPreferencePage {
 
   private void createGroupForNumberOfColumns() {
     final Group group = createGroup(GROUP_LABEL, TOOLTIP_GROUP, getFieldEditorParent());
-    /*
-     * BooleanFieldEditor enableMonitoringFieldEditor = new BooleanFieldEditor("Enable Editor", "Custom value", group) {
-     * 
-     * @SuppressWarnings("synthetic-access")
-     * 
-     * @Override protected void valueChanged(boolean oldValue_p, boolean newValue_p) { super.valueChanged(oldValue_p,
-     * newValue_p); columnsFieldEditor.setEnabled(newValue_p, group); } }; addField(enableMonitoringFieldEditor,
-     * UserProfileModeEnum.Expert, group);
-     */
     columnsFieldEditor = new IntegerFieldEditor("columnField", "Columns", group, 2);
     columnsFieldEditor.setValidRange(1, 50);
 
@@ -471,30 +363,22 @@ public class TitleBlockPreferencePage extends AbstractDefaultPreferencePage {
 
     });
 
-    // labelControl = rowsFieldEditor.getLabelControl(group);
-    // labelControl.setLayoutData(layoutData);
-    // layoutData.horizontalIndent = 15;
-
-    // columnsFieldEditor.setEnabled(doGetPreferenceStore().getBoolean("EnableCustomValue"), group);
-
   }
 
   @Override
   public boolean performOk() {
-
+    boolean x = button1.getSelection();
+    doGetPreferenceStore().setValue("defaultTitleBlock", button1.getSelection());
     doGetPreferenceStore().setValue("columnField1", columnsFieldEditor.getIntValue());
     doGetPreferenceStore().setValue("rowField", rowsFieldEditor.getIntValue());
     return super.performOk();
   }
-
-  private TableViewerColumn createTableViewerColumn(final TableViewer viewer, final String title, final int bound) {
-    final TableViewerColumn viewerColumn = new TableViewerColumn(viewer, SWT.NONE);
-    final TableColumn column = viewerColumn.getColumn();
-    column.setText(title);
-    column.setWidth(bound);
-    column.setResizable(true);
-    return viewerColumn;
-  }
+  /*
+   * private TableViewerColumn createTableViewerColumn(final TableViewer viewer, final String title, final int bound) {
+   * final TableViewerColumn viewerColumn = new TableViewerColumn(viewer, SWT.NONE); final TableColumn column =
+   * viewerColumn.getColumn(); column.setText(title); column.setWidth(bound); column.setResizable(true); return
+   * viewerColumn; }
+   */
 
   private List<List<TitleBlockCell>> createModel(int nrCol, int nrRows) {
 
@@ -510,20 +394,14 @@ public class TitleBlockPreferencePage extends AbstractDefaultPreferencePage {
     // return persons;
     return tccMatrix;
   }
-
-  private void createColumns(TableViewer viewer, int nrColumns) {
-    Point size = viewer.getTable().getSize();
-    System.out.println("X este: " + size.x);
-    System.out.println("Y este: " + size.y);
-
-    final int[] bounds = { 100, 100, 100, 100 };
-    List<String> columnHeadings = new ArrayList<>();
-    for (int i = 0; i < nrColumns; i++) {
-      columnHeadings.add("");
-      createColumn(viewer, columnHeadings.get(i), bounds[0], i);
-    }
-    testSelectCell(viewer);
-  }
+  /*
+   * private void createColumns(TableViewer viewer, int nrColumns) { Point size = viewer.getTable().getSize();
+   * System.out.println("X este: " + size.x); System.out.println("Y este: " + size.y);
+   * 
+   * final int[] bounds = { 100, 100, 100, 100 }; List<String> columnHeadings = new ArrayList<>(); for (int i = 0; i <
+   * nrColumns; i++) { columnHeadings.add(""); createColumn(viewer, columnHeadings.get(i), bounds[0], i); }
+   * testSelectCell(viewer); }
+   */
 
   private void createColumn(TableViewer v, final String title, final int bound, int index) {
     // TableViewerColumn column = createTableViewerColumn(v, title, bound);
@@ -622,6 +500,44 @@ public class TitleBlockPreferencePage extends AbstractDefaultPreferencePage {
       }
 
     });
+  }
+
+  private void createColumns(TableViewer viewer, int nrColumns) {
+    Point size = viewer.getTable().getSize();
+    System.out.println("X este: " + size.x);
+    System.out.println("Y este: " + size.y);
+
+    final int[] bounds = { 100, 100, 100, 100 };
+    List<String> columnHeadings = new ArrayList<>();
+    for (int i = 0; i < nrColumns; i++) {
+      columnHeadings.add("");
+      createColumn(viewer, columnHeadings.get(i), bounds[0], i);
+    }
+    testSelectCell(viewer);
+    setColumnLayout(viewer, nrColumns);
+  }
+
+  protected void setColumnLayout(TableViewer viewer, int nrColumns) {
+    TableColumnLayout layout = new TableColumnLayout();
+    viewer.getControl().getParent().setLayout(layout);
+    for (int i = 0; i < nrColumns; i++) {
+      layout.setColumnData(viewer.getTable().getColumn(i), new ColumnWeightData(100));
+    }
+  }
+
+  private TableViewerColumn createTableViewerColumn(final TableViewer viewer, final String title, final int bound) {
+    final TableViewerColumn viewerColumn = new TableViewerColumn(viewer, SWT.NONE);
+    final TableColumn column = viewerColumn.getColumn();
+    column.setText(title);
+
+    int wd = viewer.getTable().getClientArea().width;
+    if (wd > 0) {
+      column.setWidth(wd / columnsNumber);
+    } else {
+      column.setWidth(bound);
+    }
+    column.setResizable(true);
+    return viewerColumn;
   }
 
 }
