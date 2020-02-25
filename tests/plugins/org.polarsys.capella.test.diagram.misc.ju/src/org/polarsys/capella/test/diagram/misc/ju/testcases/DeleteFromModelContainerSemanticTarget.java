258d918556bae38cce6eb77dec91dfffe4dd6324
Sandu Postaru <sandu.postaru@thalesgroup.com>
2020-02-25 16:14:19 +0100

    [2368] Add JUnits for the delete button precondition removal
    Change-Id: I2c5296674261504ff1cbb8cfa5c720bbe9e3f6a9
    Signed-off-by: Sandu Postaru <sandu.postaru@thalesgroup.com>

--- tests/plugins/org.polarsys.capella.test.diagram.misc.ju/src/org/polarsys/capella/test/diagram/misc/ju/testcases/DeleteFromModelContainerSemanticTarget.java
+++ tests/plugins/org.polarsys.capella.test.diagram.misc.ju/src/org/polarsys/capella/test/diagram/misc/ju/testcases/DeleteFromModelContainerSemanticTarget.java
@@ -10,25 +10,25 @@
  *******************************************************************************/
 package org.polarsys.capella.test.diagram.misc.ju.testcases;
 
-import java.util.Arrays;
-import java.util.List;
+import java.util.Collection;
 
 import org.eclipse.core.runtime.NullProgressMonitor;
-import org.eclipse.emf.ecore.resource.Resource;
 import org.eclipse.gmf.runtime.diagram.ui.editparts.IGraphicalEditPart;
+import org.eclipse.sirius.business.api.dialect.DialectManager;
 import org.eclipse.sirius.business.api.session.Session;
 import org.eclipse.sirius.common.ui.tools.api.util.EclipseUIUtil;
 import org.eclipse.sirius.diagram.DDiagram;
+import org.eclipse.sirius.diagram.DDiagramElement;
 import org.eclipse.sirius.ui.business.api.dialect.DialectUIManager;
-import org.eclipse.ui.IEditorPart;
+import org.eclipse.sirius.viewpoint.DRepresentation;
+import org.eclipse.ui.IEditorReference;
 import org.eclipse.ui.PlatformUI;
 import org.polarsys.capella.core.data.pa.PhysicalFunction;
-import org.polarsys.capella.core.model.preferences.DeletePreferences;
-import org.polarsys.capella.core.model.preferences.IDeletePreferences;
-import org.polarsys.capella.core.preferences.Activator;
-import org.polarsys.capella.test.framework.api.BasicTestCase;
+import org.polarsys.capella.core.platform.sirius.clipboard.util.LayerUtil;
+import org.polarsys.capella.core.sirius.analysis.DiagramServices;
+import org.polarsys.capella.test.diagram.misc.ju.testcases.delete.AbstractDeleteFromModelTestCase;
+import org.polarsys.capella.test.framework.context.SessionContext;
 import org.polarsys.capella.test.framework.helpers.GuiActions;
-import org.polarsys.capella.test.framework.helpers.TestHelper;
 
 /**
  * See description https://bugs.polarsys.org/show_bug.cgi?id=2669
@@ -36,53 +36,47 @@ import org.polarsys.capella.test.framework.helpers.TestHelper;
  * @author <a href="mailto:arthur.daussy@obeo.fr">Arthur Daussy</a>
  *
  */
-public class DeleteFromModelContainerSemanticTarget extends BasicTestCase {
+public class DeleteFromModelContainerSemanticTarget extends AbstractDeleteFromModelTestCase {
 
-    String projectTestName = "DeleteFromModelWithTarget";
+  @Override
+  public void test() throws Exception {
 
-    @Override
-    public List<String> getRequiredTestModels() {
-        return Arrays.asList(projectTestName);
-    }
+    Session session = getSession(TEST_MODEL_NAME);
+    SessionContext sessionContext = new SessionContext(session);
 
-    @Override
-    public void test() throws Exception {
+    // Get the B Function that is the owner of the diagram
+    PhysicalFunction bFunction = sessionContext.getSemanticElement(B_PHYSICAL_FUNCTION_ID);
 
-        Session session = getSession(projectTestName);
+    Collection<DRepresentation> representations = DialectManager.INSTANCE.getRepresentations(bFunction, session);
+    assertEquals(1, representations.size());
 
-        Resource resource = TestHelper.getSemanticResource(session);
+    DDiagram diagram = (DDiagram) representations.iterator().next();
+    DialectUIManager.INSTANCE.openEditor(session, diagram, new NullProgressMonitor());
 
-        // Get the B Function that is the owner of the diagram
-        PhysicalFunction bFunction = (PhysicalFunction) resource.getEObject("33bba6a8-e9da-4c5b-9b97-ec76e63e0dc3");
-        DDiagram diagram = DeleteFromModelSemanticTarget.getDiagram(session, bFunction);
+    disableConfirmDeletePreference();
 
-        IEditorPart editorPart = DialectUIManager.INSTANCE.openEditor(session, diagram, new NullProgressMonitor());
+    GuiActions.flushASyncGuiThread();
 
-        GuiActions.flushASyncGuiThread();
+    PhysicalFunction aFunction =  sessionContext.getSemanticElement(A_PHYSICAL_FUNCTION_ID);
+    DDiagramElement diagramElement = DiagramServices.getDiagramServices().getDiagramElement(diagram, aFunction);
+    
+    IGraphicalEditPart graphicalPart = LayerUtil.getGraphicalPart(diagramElement);
+    delete(graphicalPart);
 
-        // This line aims to avoid getting the confirmation dialog when deleting the element from model
-        Activator.getDefault().getPreferenceStore().putValue(IDeletePreferences.PREFERENCE_CONFIRM_DELETE, "false");
+    GuiActions.flushASyncGuiThread();
 
-        // Checks that the preference is set to false to avoid getting stuck later on the test
-        assertFalse(((DeletePreferences) IDeletePreferences.INSTANCE).isConfirmationRequired());
+    // The representation should be deleted
+    assertNull(diagram.eContainer());
 
-        // Gets the semantic container of the B
-        PhysicalFunction aFunction = (PhysicalFunction) resource.getEObject("2d787083-bce9-4858-b925-988fb9f4a418");
-        IGraphicalEditPart graphEdit = DeleteFromModelSemanticTarget.getFunctionEditPart(session, editorPart, aFunction);
+    // The 'A' function should be delete
+    assertNull(aFunction.eContainer());
 
-        DeleteFromModelSemanticTarget.delete(graphEdit);
+    // Check the editor is closed
+    assertNull(EclipseUIUtil.getActiveEditor());
 
-        GuiActions.flushASyncGuiThread();
-
-        // The representation should be deleted
-        assertNull(diagram.eContainer());
-
-        // The 'A' function should be delete
-        assertNull(aFunction.eContainer());
-
-        // Check the editor is closed
-        assertNull(EclipseUIUtil.getActiveEditor());
-        assertEquals(0, PlatformUI.getWorkbench().getActiveWorkbenchWindow().getActivePage().getEditorReferences().length);
-    }
+    IEditorReference[] editorReferences = PlatformUI.getWorkbench().getActiveWorkbenchWindow().getActivePage()
+        .getEditorReferences();
+    assertEquals(0, editorReferences.length);
+  }
 
 }
