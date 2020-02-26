f800c76367412c8ba99575a18551278219cf4b51
Malina STOICANESCU <malina.stoicanescu@thalesgroup.com>
2020-02-26 10:28:29 +0100

    [Draft] Move Title Block Preference Page under Diagrams, change Diagrams to Diagram and add Process/Chain/Path Display Page.
    Change-Id: If29f3418ed9b4ac5767beda6514961623d8e5691
    Signed-off-by: Malina STOICANESCU <malina.stoicanescu@thalesgroup.com>

--- core/plugins/org.polarsys.capella.core.platform.sirius.ui.actions/src/org/polarsys/capella/core/platform/sirius/ui/preferences/Messages.java
+++ core/plugins/org.polarsys.capella.core.platform.sirius.ui.actions/src/org/polarsys/capella/core/platform/sirius/ui/preferences/Messages.java
@@ -35,9 +35,6 @@ public class Messages extends NLS {
   public static String DeletePreferencePage_MultipartGroup_Title;
   public static String DeletePreferencePage_MultipartGroup_Description;
 
-  public static String TitleBlockPreferencePage_Title;
-  public static String TitleBlockPreferencePage_Description;
-
   static {
     // initialize resource bundle
     NLS.initializeMessages(BUNDLE_NAME, Messages.class);
