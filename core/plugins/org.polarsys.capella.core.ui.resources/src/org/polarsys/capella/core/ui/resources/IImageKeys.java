dc40e3775c64e70c289ef6ea1f31e841de12ef11
yugansan <36500196+yugansan@users.noreply.github.com>
2020-03-10 13:14:49 +0100

    [560780] Update views icons (#90)
    - Update mini icons for Capella views
    
    Bug: 560780
    Change-Id: I3c06a1dc71972a19b03233217ae4a665964b6276
    Signed-off-by: Yogaratnam YUGANSAN <yugansan.yogaratnam@gmail.com>
    Signed-off-by: Philippe DUL <philippe.dul@thalesgroup.com>

--- core/plugins/org.polarsys.capella.core.ui.resources/src/org/polarsys/capella/core/ui/resources/IImageKeys.java
+++ core/plugins/org.polarsys.capella.core.ui.resources/src/org/polarsys/capella/core/ui/resources/IImageKeys.java
@@ -14,7 +14,6 @@ package org.polarsys.capella.core.ui.resources;
  * Image constants definition.
  */
 public interface IImageKeys {
-  /** The Capella application icon (24x24 pixels). */
   String CAPELLA_APPLICATION_IMG_24 = "full/obj24/capella.png"; //$NON-NLS-1$
 
   /** The Capella application icon (16x16 pixels). */
@@ -28,13 +27,11 @@ public interface IImageKeys {
   String CAPELLA_CUT_IMG_16 = "full/obj16/capella_cut.gif"; //$NON-NLS-1$
   String CAPELLA_DELETE_IMG_16 = "full/obj16/capella_delete.gif"; //$NON-NLS-1$
   String CAPELLA_EDIT_IMG_16 = "full/obj16/capella_edit.gif"; //$NON-NLS-1$
-  String CAPELLA_EXPLORER_IMG_16 = "full/obj16/capella_explorer.png"; //$NON-NLS-1$
   String CAPELLA_EXPORT_IMG_16 = "full/obj16/capella_export.gif"; //$NON-NLS-1$
   String CAPELLA_IMPORT_IMG_16 = "full/obj16/capella_import.gif"; //$NON-NLS-1$
   String CAPELLA_NAVIGATE_IMG_16 = "full/obj16/capella_navigate.gif"; //$NON-NLS-1$
   String CAPELLA_PASTE_IMG_16 = "full/obj16/capella_paste.gif"; //$NON-NLS-1$
   String CAPELLA_PROCESS_IMG_16 = "full/obj16/capella_process.gif"; //$NON-NLS-1$
-  String CAPELLA_SEMANTIC_BROWSER_IMG_16 = "full/obj16/capella_semantic_browser.gif"; //$NON-NLS-1$
   String CAPELLA_SORT_IMG_16 = "full/obj16/capella_sort.gif"; //$NON-NLS-1$
   String CAPELLA_SORT_DISABLED_IMG_16 = "full/obj16/capella_sort_disabled.gif"; //$NON-NLS-1$
   String CAPELLA_TRACE_IMG_16 = "full/obj16/capella_trace.gif"; //$NON-NLS-1$
