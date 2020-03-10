89ab2f405724dd9542d3dad290453aafa480327f
Florian Barbin <florian.barbin@obeo.fr>
2020-03-10 11:27:19 +0100

    DRAFT [560962] Check is show icon and is custom icon.
    Allow style customization on icon path.
    
    Bug: 560962
    Change-Id: If36c4fbe879da157916b5f38c3c2c230ca9aeffb
    Signed-off-by: Florian Barbin <florian.barbin@obeo.fr>

--- core/plugins/org.polarsys.capella.core.sirius.analysis/src/org/polarsys/capella/core/sirius/analysis/extension/style/PartComponentStyleConfiguration.java
+++ core/plugins/org.polarsys.capella.core.sirius.analysis/src/org/polarsys/capella/core/sirius/analysis/extension/style/PartComponentStyleConfiguration.java
@@ -37,21 +37,26 @@ public class PartComponentStyleConfiguration extends SimpleStyleConfiguration im
     return this;
   }
 
-  @Override
-  public Image getLabelIcon(DDiagramElement representationElement, IGraphicalEditPart editPart) {
-    EObject target = representationElement.getTarget();
-    if (target instanceof Part) {
-      if (CapellaProjectHelper.isReusableComponentsDriven(target) != TriStateBoolean.True) {
-        Type type = ((Part) target).getType();
-        if (type != null) {
-          IItemLabelProvider provider = (IItemLabelProvider) CapellaAdapterFactoryProvider.getInstance()
-              .getAdapterFactory().adapt(type, IItemLabelProvider.class);
-          return ExtendedImageRegistry.getInstance().getImage(provider.getImage(type));
+    @Override
+    public Image getLabelIcon(DDiagramElement representationElement, IGraphicalEditPart editPart) {
+        if (representationElement != null) {
+            if (isShowIcon(representationElement, editPart)) {
+                EObject target = representationElement.getTarget();
+
+                if (target instanceof Part && !useCustomIcon(representationElement, editPart)) {
+                    if (CapellaProjectHelper.isReusableComponentsDriven(target) != TriStateBoolean.True) {
+                        Type type = ((Part) target).getType();
+                        if (type != null) {
+                            IItemLabelProvider provider = (IItemLabelProvider) CapellaAdapterFactoryProvider.getInstance().getAdapterFactory().adapt(type, IItemLabelProvider.class);
+                            return ExtendedImageRegistry.getInstance().getImage(provider.getImage(type));
+                        }
+                    }
+                }
+                return super.getLabelIcon(representationElement, editPart);
+            }
         }
-      }
+        return null;
     }
-    return super.getLabelIcon(representationElement, editPart);
-  }
 
   public boolean provides(DiagramElementMapping mapping, Style style) {
     if (mapping instanceof ContainerMapping) {
