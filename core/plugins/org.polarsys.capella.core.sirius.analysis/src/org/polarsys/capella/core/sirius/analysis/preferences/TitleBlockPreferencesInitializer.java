bbea59c40e2e09b20a331c1dbd06d8e16eb59b74
Cosmin <cosmin.visan@thalesgroup.com>
2020-03-19 16:47:01 +0200

    Changed rows to lines
    Change-Id: I64dfe0d77b72518df718a2a67c2a649ae4fce07c
    Signed-off-by: Cosmin <cosmin.visan@thalesgroup.com>

--- core/plugins/org.polarsys.capella.core.sirius.analysis/src/org/polarsys/capella/core/sirius/analysis/preferences/TitleBlockPreferencesInitializer.java
+++ core/plugins/org.polarsys.capella.core.sirius.analysis/src/org/polarsys/capella/core/sirius/analysis/preferences/TitleBlockPreferencesInitializer.java
@@ -1,13 +1,13 @@
 /*******************************************************************************
- * Copyright (c) 2020 THALES GLOBAL SERVICES.
- * All rights reserved. This program and the accompanying materials
- * are made available under the terms of the Eclipse Public License v1.0
- * which accompanies this distribution, and is available at
- * http://www.eclipse.org/legal/epl-v10.html
- *  
- * Contributors:
- *    Thales - initial API and implementation
- *******************************************************************************/
+* Copyright (c) 2020 THALES GLOBAL SERVICES.
+* All rights reserved. This program and the accompanying materials
+* are made available under the terms of the Eclipse Public License v1.0
+* which accompanies this distribution, and is available at
+* http://www.eclipse.org/legal/epl-v10.html
+* 
+* Contributors:
+*    Thales - initial API and implementation
+*******************************************************************************/
 package org.polarsys.capella.core.sirius.analysis.preferences;
 
 import org.eclipse.jface.preference.IPreferenceStore;
@@ -16,12 +16,11 @@ import org.polarsys.capella.core.platform.sirius.ui.actions.CapellaActionsActiva
 import org.polarsys.capella.core.preferences.Activator;
 
 public class TitleBlockPreferencesInitializer extends AbstractPreferencesInitializer {
-
   public static final String TABEL_CONTENT_PREFERENCE_STORE = "tableTitleBlock";
   public static final String COLUMNS_NUMBER_PREFERENCE_STORE = "columnsNumberTitleBlock";
-  public static final String ROWS_NUMBER_PREFERENCE_STORE = "rowsNumberTitleBlock";
+  public static final String LINES_NUMBER_PREFERENCE_STORE = "linesNumberTitleBlock";
   public static final String DEFAULT_TITLEBLOCK_PREFERENCE_STORE = "defaultTitleBlock";
-  public static final String DEFAULT_TABLE = "Name+aql:name+Synchronized+aql:synchronized+Summary+aql:summary+Description+aql:description";
+  public static final String DEFAULT_TABLE = "Name+feature:name+Synchronized+feature:synchronized+Summary+aql:self.target.summary+Description+feature:documentation";
 
   public TitleBlockPreferencesInitializer() {
     super(CapellaActionsActivator.PLUGIN_ID);
@@ -31,9 +30,8 @@ public class TitleBlockPreferencesInitializer extends AbstractPreferencesInitial
   public void initializeDefaultPreferences() {
     IPreferenceStore preferenceStore = Activator.getDefault().getPreferenceStore();
     preferenceStore.setDefault(COLUMNS_NUMBER_PREFERENCE_STORE, 2);
-    preferenceStore.setDefault(ROWS_NUMBER_PREFERENCE_STORE, 2);
+    preferenceStore.setDefault(LINES_NUMBER_PREFERENCE_STORE, 2);
     preferenceStore.setDefault(TABEL_CONTENT_PREFERENCE_STORE, DEFAULT_TABLE);
-
     preferenceStore.setDefault(DEFAULT_TITLEBLOCK_PREFERENCE_STORE, true);
   }
 
