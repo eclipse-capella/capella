3969dde3bc54646a6b09a1d642c112142f9cb33a
Yogaratnam YUGANSAN <yugansan.yogaratnam@gmail.com>
2020-03-26 11:48:15 +0100

    DRAFt
    Change-Id: Id588dac0c01522877de6614ce83b12e4af4e3347

--- core/plugins/org.polarsys.capella.core.sirius.ui/src/org/polarsys/capella/core/sirius/ui/commandline/RefreshAirdCommandLine.java
+++ core/plugins/org.polarsys.capella.core.sirius.ui/src/org/polarsys/capella/core/sirius/ui/commandline/RefreshAirdCommandLine.java
@@ -16,16 +16,13 @@ import org.eclipse.core.resources.IFile;
 import org.eclipse.core.runtime.IStatus;
 import org.eclipse.core.runtime.NullProgressMonitor;
 import org.eclipse.core.runtime.Status;
-import org.eclipse.core.runtime.jobs.IJobChangeEvent;
 import org.eclipse.core.runtime.jobs.Job;
-import org.eclipse.core.runtime.jobs.JobChangeAdapter;
 import org.eclipse.emf.common.util.URI;
 import org.eclipse.sirius.business.api.session.Session;
 import org.eclipse.sirius.business.api.session.SessionManager;
-import org.eclipse.ui.PlatformUI;
 import org.polarsys.capella.common.helpers.EcoreUtil2;
 import org.polarsys.capella.core.commandline.core.ui.AbstractWorkbenchCommandLine;
-import org.polarsys.capella.core.commandline.core.ui.CloseWorkbenchJob;
+import org.polarsys.capella.core.sirius.ui.SiriusUIPlugin;
 import org.polarsys.capella.core.sirius.ui.handlers.RefreshDiagramJob;
 
 public class RefreshAirdCommandLine extends AbstractWorkbenchCommandLine {
@@ -38,25 +35,46 @@ public class RefreshAirdCommandLine extends AbstractWorkbenchCommandLine {
   protected IStatus executeWithinWorkbench() {
     List<IFile> airdFiles = getAirdFilesFromInput();
     for (IFile file : airdFiles) {
+      URI selectedUri = EcoreUtil2.getURI(file);
+      Session session = SessionManager.INSTANCE.getSession(selectedUri, new NullProgressMonitor());
+      if (session == null) {
+        return new Status(IStatus.ERROR, SiriusUIPlugin.getDefault().getPluginId(), "No aird model found!");
+      }
       Job job = new RefreshDiagramJob(file);
-      job.addJobChangeListener(new JobChangeAdapter() {
-
-        @Override
-        public void done(IJobChangeEvent event) {
-          URI selectedUri = EcoreUtil2.getURI(file);
-          Session session = SessionManager.INSTANCE.getSession(selectedUri, new NullProgressMonitor());
-          session.save(new NullProgressMonitor());
-          try {
-            session.close(new NullProgressMonitor());
-          } catch (Exception e) {
-            e.printStackTrace();
-          }
-          if (PlatformUI.getTestableObject() != null || PlatformUI.getTestableObject().getTestHarness() != null) {
-            new CloseWorkbenchJob().schedule();
-          }
-        }
-      });
       job.schedule();
+      try {
+        job.join();
+      } catch (InterruptedException e) {
+        return Status.CANCEL_STATUS;
+      }
+      session.save(new NullProgressMonitor());
+      try {
+        session.close(new NullProgressMonitor());
+
+      } catch (Exception e) {
+        e.printStackTrace();
+      }
+
+      // job.addJobChangeListener(new JobChangeAdapter() {
+      //
+      // @Override
+      // public void done(IJobChangeEvent event) {
+      // URI selectedUri = EcoreUtil2.getURI(file);
+      // Session session = SessionManager.INSTANCE.getSession(selectedUri, new NullProgressMonitor());
+      // session.save(new NullProgressMonitor());
+      // try {
+      // session.close(new NullProgressMonitor());
+      // } catch (Exception e) {
+      // e.printStackTrace();
+      // }
+      // if (PlatformUI.getTestableObject() != null || PlatformUI.getTestableObject().getTestHarness() != null) {
+      // new CloseWorkbenchJob().schedule();
+      // }
+      // }
+      // });
+      // job.done(result);
+      // job.schedule();
+      //
     }
     return Status.OK_STATUS;
   }
