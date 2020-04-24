833713e6fb55dd12c946226856a1818b17af355c
Georgiana Ecobici <georgiana-elena.ecobici@thalesgroup.com>
2020-04-24 08:14:24 +0200

    Filters fixed bugs and added junits
    Change-Id: I2a8c616d3e4dc618c48a12cd7b02106add44ef79
    Signed-off-by: Georgiana Ecobici <georgiana-elena.ecobici@thalesgroup.com>

---
+++ tests/plugins/org.polarsys.capella.test.diagram.filters.ju/src/org/polarsys/capella/test/diagram/filters/ju/cdb/HideDiagramTitleBocksTest.java
@@ -0,0 +1,43 @@
+/*******************************************************************************
+ * Copyright (c) 2020 THALES GLOBAL SERVICES.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *  
+ * Contributors:
+ *    Thales - initial API and implementation
+ *******************************************************************************/
+package org.polarsys.capella.test.diagram.filters.ju.cdb;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.polarsys.capella.core.sirius.analysis.constants.IFilterNameConstants;
+import org.polarsys.capella.test.diagram.filters.ju.DiagramTitleBlockFilterTestCase;
+
+public class HideDiagramTitleBocksTest extends DiagramTitleBlockFilterTestCase {
+
+  private final String DIAGRAM_TITLE_BLOCK_ID = "_8t01EIXwEeqWaJq2mNMsRw";
+  
+
+  @Override
+  protected String getTestProjectName() {
+    return "TitleBlocksModel";
+  }
+
+  @Override
+  protected String getDiagramName() {
+    return "[CDB] Data Title Blocks";
+  }
+
+  @Override
+  protected String getFilterName() {
+    return IFilterNameConstants.FILTER_CDB_HIDE_DIAGRAM_TITLE_BLOCKS;
+  }
+
+  @Override
+  protected List<String> getFilteredObjetIDs() {
+    return Arrays.asList(new String[] { DIAGRAM_TITLE_BLOCK_ID });
+  }
+}
---
+++ tests/plugins/org.polarsys.capella.test.diagram.filters.ju/src/org/polarsys/capella/test/diagram/filters/ju/cdb/HideElementTitleBocks.java
@@ -0,0 +1,47 @@
+/*******************************************************************************
+ * Copyright (c) 2020 THALES GLOBAL SERVICES.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *  
+ * Contributors:
+ *    Thales - initial API and implementation
+ *******************************************************************************/
+package org.polarsys.capella.test.diagram.filters.ju.cdb;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.polarsys.capella.core.sirius.analysis.constants.IFilterNameConstants;
+import org.polarsys.capella.test.diagram.filters.ju.DiagramObjectFilterTestCase;
+import org.polarsys.capella.test.diagram.filters.ju.DiagramTitleBlockFilterTestCase;
+
+public class HideElementTitleBocks extends DiagramTitleBlockFilterTestCase {
+
+  private final String ELEMENT_TITLE_BLOCK_CLASS_ID = "_48t68IXwEeqWaJq2mNMsRw";
+  
+  private final String ELEMENT_TITLE_BLOCK_DATA_PKG_ID = "_6iC6AIXwEeqWaJq2mNMsRw";
+
+  @Override
+  protected String getTestProjectName() {
+    return "TitleBlocksModel";
+  }
+
+  @Override
+  protected String getDiagramName() {
+    return "[CDB] Data Title Blocks";
+  }
+
+  @Override
+  protected String getFilterName() {
+    return IFilterNameConstants.FILTER_CDB_HIDE_ELEMENT_TITLE_BLOCKS;
+  }
+
+  @Override
+  protected List<String> getFilteredObjetIDs() {
+    return Arrays.asList(new String[] { ELEMENT_TITLE_BLOCK_CLASS_ID,
+        ELEMENT_TITLE_BLOCK_DATA_PKG_ID});
+  }
+
+}
