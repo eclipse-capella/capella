2ba5f1088beb610e8c6c5f3dd5cb6cac8ff749bf
Philippe DUL <philippe.dul@thalesgroup.com>
2020-04-27 18:29:53 +0200

    Java Xml and Properties
    Change-Id: I6957ff6a0ee7bc3f081ffb070e248b17f42ceb5c
    Signed-off-by: Philippe DUL <philippe.dul@thalesgroup.com>

--- releng/plugins/org.polarsys.capella.core.egf/generated/org/polarsys/capella/core/editor/Editor.java
+++ releng/plugins/org.polarsys.capella.core.egf/generated/org/polarsys/capella/core/editor/Editor.java
@@ -1621,9 +1621,10 @@ public class Editor extends org.polarsys.kitalpha.emde.egf.editor.Editor {
 		/**
 		 * Copyright (c) 2002-2010 IBM Corporation and others.
 		 * All rights reserved.   This program and the accompanying materials
-		 * are made available under the terms of the Eclipse Public License v1.0
-		 * which accompanies this distribution, and is available at
-		 * http://www.eclipse.org/legal/epl-v10.html
+		 * terms of the Eclipse Public License 2.0 which is available at
+		 * http://www.eclipse.org/legal/epl-2.0
+		 * 
+		 * SPDX-License-Identifier: EPL-2.0
 		 *
 		 * Contributors:
 		 *   IBM - Initial API and implementation
