b345aa1ca1ec0449952f104859199af93eb89777
Malina STOICANESCU <malina.stoicanescu@thalesgroup.com>
2020-04-29 11:56:04 +0200

    Add evaluate expression in preferences
    Signed-off-by: Malina STOICANESCU <malina.stoicanescu@thalesgroup.com>

--- core/plugins/org.polarsys.capella.core.sirius.analysis/src/org/polarsys/capella/core/sirius/analysis/preferences/TitleBlockDialog.java
+++ core/plugins/org.polarsys.capella.core.sirius.analysis/src/org/polarsys/capella/core/sirius/analysis/preferences/TitleBlockDialog.java
@@ -26,8 +26,10 @@ import org.eclipse.sirius.common.tools.api.contentassist.ContentInstanceContext;
 import org.eclipse.sirius.common.tools.api.contentassist.ContentProposal;
 import org.eclipse.sirius.common.tools.api.contentassist.IProposalProvider;
 import org.eclipse.sirius.common.tools.api.interpreter.CompoundInterpreter;
+import org.eclipse.sirius.common.tools.api.interpreter.EvaluationException;
 import org.eclipse.sirius.common.tools.api.interpreter.IInterpreter;
 import org.eclipse.sirius.common.tools.internal.assist.ProposalProviderRegistry;
+import org.eclipse.sirius.diagram.DDiagram;
 import org.eclipse.sirius.diagram.business.internal.metamodel.spec.DSemanticDiagramSpec;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.layout.GridData;
@@ -39,6 +41,7 @@ import org.eclipse.swt.widgets.Shell;
 import org.eclipse.swt.widgets.Text;
 import org.polarsys.capella.common.ui.toolkit.browser.category.CategoryRegistry;
 import org.polarsys.capella.common.ui.toolkit.browser.category.ICategory;
+import org.polarsys.capella.core.diagram.helpers.TitleBlockHelper;
 import org.polarsys.capella.core.sirius.analysis.CapellaInterpreter;
 
 import com.google.common.base.CaseFormat;
@@ -47,9 +50,10 @@ public class TitleBlockDialog extends TitleAreaDialog {
   private final String TITLE_NAME = "Add name and content";
   private final String NAME_LABEL = "Name";
   private final String CONTENT_LABEL = "Content";
-  private final String ERROR_MESSAGE = "The content of data fields can be customized via aql, feature or capella:\r\n"
-      + "\r\n" + "      AQL query (aql:)\r\n" + "      Name of feature (feature:)\r\n"
-      + "      Predefined service (capella:)\r\n\r\n " + "      Example: feature:name";
+  private final String ERROR_MESSAGE = "Content Interpreter Error: Syntax not valid\r\n"
+      + "The content of data fields can be customized via aql, feature or capella:\r\n" + "      AQL query (aql:)\r\n"
+      + "      Name of feature (feature:)\r\n" + "      Predefined service (capella:)\r\n\r\n"
+      + "      Example: feature:name";
   private Text txtName;
   private Text txtContent;
 
@@ -159,6 +163,7 @@ public class TitleBlockDialog extends TitleAreaDialog {
           keyStroke, null);
 
       adapter.addContentProposalListener(new IContentProposalListener() {
+        @Override
         public void proposalAccepted(IContentProposal proposal) {
           int posOfDot = txtContent.getText().lastIndexOf(".");
           char charToAppend = '.';
@@ -188,12 +193,13 @@ public class TitleBlockDialog extends TitleAreaDialog {
   private boolean saveInput() {
     name = txtName.getText();
     content = txtContent.getText();
-    if (content.matches("feature:(.*)") || content.matches("aql:(.*)") || content.matches("capella:(.*)")) {
-
-      return true;
+    DDiagram diagram = new DSemanticDiagramSpec();
+    Object evaluateResult = TitleBlockHelper.getResultOfExpression(diagram, content, null);
+    if (evaluateResult instanceof EvaluationException) {
+      setErrorMessage(ERROR_MESSAGE);
+      return false;
     }
-    setErrorMessage(ERROR_MESSAGE);
-    return false;
+    return true;
   }
 
   @Override
