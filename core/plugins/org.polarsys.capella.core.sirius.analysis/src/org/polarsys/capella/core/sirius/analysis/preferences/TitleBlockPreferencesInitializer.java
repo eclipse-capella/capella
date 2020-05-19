d1fb6349cbc396b3d5786434ef876c06da5af008
Georgiana Ecobici <georgiana-elena.ecobici@thalesgroup.com>
2020-05-19 14:21:01 +0300

    rebase
    Change-Id: Ic0399ad8ea6d570c42049ed20784f353ff98485a
    Signed-off-by: Georgiana Ecobici <georgiana-elena.ecobici@thalesgroup.com>

---
+++ core/plugins/org.polarsys.capella.core.sirius.analysis/src/org/polarsys/capella/core/sirius/analysis/preferences/TitleBlockPreferencesInitializer.java
@@ -0,0 +1,40 @@
+/*******************************************************************************
+ * Copyright (c) 2020 THALES GLOBAL SERVICES.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *  
+ * Contributors:
+ *    Thales - initial API and implementation
+ *******************************************************************************/
+package org.polarsys.capella.core.sirius.analysis.preferences;
+
+import org.eclipse.jface.preference.IPreferenceStore;
+import org.polarsys.capella.core.commands.preferences.service.AbstractPreferencesInitializer;
+import org.polarsys.capella.core.platform.sirius.ui.actions.CapellaActionsActivator;
+import org.polarsys.capella.core.preferences.Activator;
+
+public class TitleBlockPreferencesInitializer extends AbstractPreferencesInitializer {
+
+  public static final String TABEL_CONTENT_PREFERENCE_STORE = "tableTitleBlock";
+  public static final String COLUMNS_NUMBER_PREFERENCE_STORE = "columnsNumberTitleBlock";
+  public static final String ROWS_NUMBER_PREFERENCE_STORE = "rowsNumberTitleBlock";
+  public static final String DEFAULT_TITLEBLOCK_PREFERENCE_STORE = "defaultTitleBlock";
+  public static final String DEFAULT_TABLE = "Name+aql:name+Synchronized+aql:synchronized+Summary+aql:summary+Description+aql:description";
+
+  public TitleBlockPreferencesInitializer() {
+    super(CapellaActionsActivator.PLUGIN_ID);
+  }
+
+  @Override
+  public void initializeDefaultPreferences() {
+    IPreferenceStore preferenceStore = Activator.getDefault().getPreferenceStore();
+    preferenceStore.setDefault(COLUMNS_NUMBER_PREFERENCE_STORE, 2);
+    preferenceStore.setDefault(ROWS_NUMBER_PREFERENCE_STORE, 2);
+    preferenceStore.setDefault(TABEL_CONTENT_PREFERENCE_STORE, DEFAULT_TABLE);
+
+    preferenceStore.setDefault(DEFAULT_TITLEBLOCK_PREFERENCE_STORE, true);
+  }
+
+}
