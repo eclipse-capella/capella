e4cfdc619202db91b455c0164bee33c0d48dbfb8
Philippe DUL <philippe.dul@thalesgroup.com>
2020-05-25 14:48:27 +0200

    Update Licence to EPL-2.0
    Change-Id: I4d75e7957fd11222ffdbdf440c532865d2be6129
    Signed-off-by: Philippe DUL <philippe.dul@thalesgroup.com>

--- tests/plugins/org.polarsys.capella.test.diagram.common.ju/src/org/polarsys/capella/test/diagram/common/ju/step/tools/titleblocks/InsertRemoveTitleBlockTool.java
+++ tests/plugins/org.polarsys.capella.test.diagram.common.ju/src/org/polarsys/capella/test/diagram/common/ju/step/tools/titleblocks/InsertRemoveTitleBlockTool.java
@@ -1,3 +1,15 @@
+/*******************************************************************************
+ * Copyright (c) 2020 THALES GLOBAL SERVICES.
+ * 
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0
+ * 
+ * SPDX-License-Identifier: EPL-2.0
+ * 
+ * Contributors:
+ *    Thales - initial API and implementation
+ *******************************************************************************/
 package org.polarsys.capella.test.diagram.common.ju.step.tools.titleblocks;
 
 import static org.junit.Assert.assertFalse;
