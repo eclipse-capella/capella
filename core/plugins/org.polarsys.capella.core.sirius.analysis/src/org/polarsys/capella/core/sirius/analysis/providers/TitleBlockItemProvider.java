49c3b4a8f8e61127326e8b77a42337adf80d75a1
Philippe DUL <philippe.dul@thalesgroup.com>
2020-05-26 18:25:56 +0200

    [558032] Display Title block as a tree and under its referenced element
    Change-Id: Ia72d0449583d5ca513ff83560b281ac54b5bbdce
    Signed-off-by: Philippe DUL <philippe.dul@thalesgroup.com>

--- core/plugins/org.polarsys.capella.core.sirius.analysis/src/org/polarsys/capella/core/sirius/analysis/providers/TitleBlockItemProvider.java
+++ core/plugins/org.polarsys.capella.core.sirius.analysis/src/org/polarsys/capella/core/sirius/analysis/providers/TitleBlockItemProvider.java
@@ -12,15 +12,44 @@
  *******************************************************************************/
 package org.polarsys.capella.core.sirius.analysis.providers;
 
+import java.util.Collection;
+
 import org.eclipse.emf.common.notify.AdapterFactory;
+import org.eclipse.emf.ecore.EStructuralFeature.Setting;
+import org.eclipse.sirius.business.api.session.Session;
+import org.eclipse.sirius.business.api.session.SessionManager;
+import org.eclipse.sirius.viewpoint.description.DAnnotation;
+import org.eclipse.sirius.viewpoint.description.DescriptionPackage;
 import org.eclipse.sirius.viewpoint.description.provider.DAnnotationItemProvider;
+import org.polarsys.capella.core.diagram.helpers.TitleBlockHelper;
 
 public class TitleBlockItemProvider extends DAnnotationItemProvider {
   public TitleBlockItemProvider(AdapterFactory adapterFactory) {
     super(adapterFactory);
   }
-  public String getString(String key)
-  {
+
+  public String getString(String key) {
     return "Title Block";
   }
+
+  @Override
+  public Object getParent(Object object) {
+    DAnnotation annotation = (DAnnotation) object;
+    if (TitleBlockHelper.isElementTitleBlock(annotation)) {
+      return TitleBlockHelper.getReferencedElement(annotation);
+
+    } else if (TitleBlockHelper.isTitleBlockCell(annotation) || TitleBlockHelper.isTitleBlockLine(annotation)
+        || TitleBlockHelper.isContentTitleBlock(annotation)) {
+      Session session = SessionManager.INSTANCE.getSession(annotation);
+      if (session != null) {
+        Collection<Setting> referencer = session.getSemanticCrossReferencer().getInverseReferences(annotation,
+            DescriptionPackage.Literals.DANNOTATION__REFERENCES, false);
+        if (!referencer.isEmpty()) {
+          return referencer.iterator().next().getEObject();
+        }
+      }
+    }
+    return super.getParent(object);
+  }
+
 }
