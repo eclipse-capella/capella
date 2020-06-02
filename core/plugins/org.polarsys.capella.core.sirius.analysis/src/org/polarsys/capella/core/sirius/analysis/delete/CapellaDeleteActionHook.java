cbc46c1bfef85b32fe4d9373f37bdeb15a2613b8
Sandu Postaru <59692543+sandupostaru@users.noreply.github.com>
2020-06-02 09:39:21 +0200

    [558032] Fix Deletion of Title Block Elements (#246)
    - Deletion of a Title Block cell clears the cell content
    - Deletion of a Title Block deletes elements as expected, except for any
    referenced Model Elements
    
    Bug: 558032
    Change-Id: If8a26cbcacab8562aa1d7752dfc5ec13d9480bfa
    Signed-off-by: Sandu Postaru <sandu.postaru@thalesgroup.com>

--- core/plugins/org.polarsys.capella.core.sirius.analysis/src/org/polarsys/capella/core/sirius/analysis/delete/CapellaDeleteActionHook.java
+++ core/plugins/org.polarsys.capella.core.sirius.analysis/src/org/polarsys/capella/core/sirius/analysis/delete/CapellaDeleteActionHook.java
@@ -20,27 +20,30 @@ import org.eclipse.core.runtime.IStatus;
 import org.eclipse.core.runtime.Status;
 import org.eclipse.emf.ecore.EObject;
 import org.eclipse.sirius.business.api.delete.IDeleteHook;
+import org.eclipse.sirius.diagram.DDiagramElement;
 import org.eclipse.sirius.viewpoint.DSemanticDecorator;
+import org.eclipse.sirius.viewpoint.description.DAnnotation;
+import org.eclipse.sirius.viewpoint.description.RepresentationElementMapping;
+import org.polarsys.capella.common.ef.ExecutionManager;
 import org.polarsys.capella.common.helpers.TransactionHelper;
+import org.polarsys.capella.core.diagram.helpers.TitleBlockHelper;
 import org.polarsys.capella.core.model.preferences.IDeletePreferences;
 import org.polarsys.capella.core.platform.sirius.ui.commands.CapellaDeleteCommand;
 
-/**
- * Delete hook allowing for impact analysis before delete execution.
- */
 public class CapellaDeleteActionHook implements IDeleteHook {
 
-  // Sirius triggers the hook for all selected elements but triggers the externalDeleteAction for each item of selection,
+  // Sirius triggers the hook for all selected elements but triggers the externalDeleteAction for each item of
+  // selection,
   // we need to store this selection to trigger only one time the externalDeleteAction with full initial selection.
 
-  private static Collection<EObject> _selection;
+  private static Collection<EObject> selection;
 
   public static Collection<EObject> getSelection() {
-    return _selection;
+    return selection;
   }
 
-  protected static void setSelection(Collection<EObject> semanticElements_p) {
-    _selection = semanticElements_p;
+  protected static void setSelection(Collection<EObject> semanticElements) {
+    selection = semanticElements;
   }
 
   public static void removeSelection() {
@@ -48,30 +51,76 @@ public class CapellaDeleteActionHook implements IDeleteHook {
   }
 
   /**
-   * @see org.eclipse.sirius.business.api.delete.IDeleteHook#beforeDeleteCommandExecution(java.util.Collection, java.util.Map)
+   * @see org.eclipse.sirius.business.api.delete.IDeleteHook#beforeDeleteCommandExecution(java.util.Collection,
+   *      java.util.Map)
    */
-  public IStatus beforeDeleteCommandExecution(Collection<DSemanticDecorator> selections_p, Map<String, Object> parameters_p) {
-    // Precondition.
-    if ((null == selections_p) || selections_p.isEmpty()) {
+  @Override
+  public IStatus beforeDeleteCommandExecution(Collection<DSemanticDecorator> selection,
+      Map<String, Object> parameters) {
+
+    if (null == selection || selection.isEmpty()) {
       return Status.CANCEL_STATUS;
     }
 
-    // Get semantic elements from selected ones.
-    Collection<EObject> semanticElements = new HashSet<EObject>(selections_p.size());
-    for (DSemanticDecorator semanticDecorator : selections_p) {
+    Collection<EObject> deletionCandidates = extractDeletionCandidates(selection);
+
+    setSelection(deletionCandidates);
+
+    if (confirmDeletion(deletionCandidates)) {
+      return Status.OK_STATUS;
+    }
+
+    removeSelection();
+    return Status.CANCEL_STATUS;
+  }
+
+  /**
+   * Extracts the deletion candidate elements from the user selection of DSemanticDecorators. All the
+   * {@link DSemanticDecorator#getTarget()} elements are extracted, except those that are referenced in TitleBlock elements.
+   * When deleting a TitleBlock, referenced Model Elements should not be deleted.
+   * 
+   * @param selection
+   *          the user selection
+   * @return the valid semantic elements that will be deleted.
+   */
+  private Collection<EObject> extractDeletionCandidates(Collection<DSemanticDecorator> selection) {
+    Collection<EObject> semanticElements = new HashSet<>(selection.size());
+
+    for (DSemanticDecorator semanticDecorator : selection) {
       EObject target = semanticDecorator.getTarget();
-      if (null != target) {
-        semanticElements.add(target);
+
+      if (target != null) {
+        
+        if (!(target instanceof DAnnotation) && semanticDecorator instanceof DDiagramElement) {
+          DDiagramElement diagramElement = (DDiagramElement) semanticDecorator;
+          RepresentationElementMapping mapping = diagramElement.getMapping();
+
+          if (!TitleBlockHelper.isTitleBlockMapping(mapping)) {
+            semanticElements.add(target);
+          }
+        }
+
+        else {
+          semanticElements.add(target);
+        }
       }
     }
 
-    setSelection(semanticElements);
-    // Ask user confirmation if it is required in preferences.
-    boolean deletionConfirmed = !IDeletePreferences.INSTANCE.isConfirmationRequired() || CapellaDeleteCommand.confirmDeletion(TransactionHelper.getExecutionManager(semanticElements), getSelection());
-    if (!deletionConfirmed) {
-      removeSelection();
-    }
-    return deletionConfirmed ? Status.OK_STATUS : Status.CANCEL_STATUS;
+    return semanticElements;
   }
 
+  /**
+   * Confirms the deletion with the user (by displaying the appropriate dialog), if the required preference is activated. 
+   * @param deletionCandidates the deletion candidates
+   * @return false if the deletion is not confirmed, true otherwise.
+   */
+  private boolean confirmDeletion(Collection<EObject> deletionCandidates) {
+
+    if (IDeletePreferences.INSTANCE.isConfirmationRequired()) {
+      ExecutionManager executionManager = TransactionHelper.getExecutionManager(deletionCandidates);
+      return executionManager != null && CapellaDeleteCommand.confirmDeletion(executionManager, deletionCandidates);
+    }
+
+    return true;
+  }
 }
