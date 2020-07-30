ce1528aa85718d4367c07ac2182b3d283fb18800
Sandu Postaru <sandu.postaru@thalesgroup.com>
2020-07-30 17:05:51 +0200

    [tests] Fix failing tests due to Eclipse 2020-06 platform update
    Change-Id: Ic38b68de88622f3177e86204eaa0ad15e269e96a
    Signed-off-by: Sandu Postaru <sandu.postaru@thalesgroup.com>

--- tests/plugins/org.polarsys.capella.test.platform.ju/src/org/polarsys/capella/test/platform/ju/testcases/CapellaDefaultEditorEnabled.java
+++ tests/plugins/org.polarsys.capella.test.platform.ju/src/org/polarsys/capella/test/platform/ju/testcases/CapellaDefaultEditorEnabled.java
@@ -13,7 +13,10 @@
 package org.polarsys.capella.test.platform.ju.testcases;
 
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.Collection;
 import java.util.HashMap;
+import java.util.stream.Collectors;
 
 import org.eclipse.core.runtime.IConfigurationElement;
 import org.eclipse.core.runtime.Platform;
@@ -32,16 +35,15 @@ public class CapellaDefaultEditorEnabled extends BasicTestCase {
         .getConfigurationElementsFor("org.eclipse.ui.editors");
 
     HashMap<String, String> expectedEditors = new HashMap<String, String>();
-    
+
     expectedEditors.put(CapellaResourceHelper.AIRD_FILE_EXTENSION, "org.eclipse.sirius.ui.editor.session");
-    
+
     expectedEditors.put(CapellaResourceHelper.CAPELLA_MODEL_FILE_EXTENSION,
         "org.polarsys.capella.core.data.capellamodeller.presentation.CapellamodellerEditorID");
-    
-    
+
     HashMap<String, String> currentEditors = new HashMap<String, String>();
-    
-    //Retrieve default editors for checked extensions
+
+    // Retrieve default editors for checked extensions
     for (IConfigurationElement element : configurationElements) {
       String extensions = element.getAttribute("extensions");
       if (extensions != null) {
@@ -57,13 +59,13 @@ public class CapellaDefaultEditorEnabled extends BasicTestCase {
       }
     }
 
-    //Check default editors are the expected ones
-    for (String extension: expectedEditors.keySet()) {
+    // Check default editors are the expected ones
+    for (String extension : expectedEditors.keySet()) {
       String expectedEditor = expectedEditors.get(extension);
       String currentEditor = currentEditors.get(extension);
 
       if (!expectedEditor.equals(currentEditor)) {
-        // If the test does not pass because of the default editor for "melodymodeller" is null, 
+        // If the test does not pass because of the default editor for "melodymodeller" is null,
         // please think of checking the generated "org.polarsys.capella.core.data.gen.editor/plugin.xml"
         // All tag <editor> must have the property default="true"
         // The property is not set after re-generation of meta-model.
@@ -71,27 +73,36 @@ public class CapellaDefaultEditorEnabled extends BasicTestCase {
             new String[] { extension, currentEditor, expectedEditor }), true);
       }
     }
-    
-    //For all editors, check that icon exist
+
+    // For all editors, check that icon exist
+    Collection<String> errorMessages = new ArrayList<>();
+
     for (IConfigurationElement element : configurationElements) {
-      String url = MenuHelper.getIconURI(element, "icon");
-      String icon = element.getAttribute("icon");
-      String editorId = element.getAttribute("id");
-      if (url != null) {
-        ImageDescriptor s = ImageDescriptor.createFromURL(new URL(url));
-        Image image = s.createImage(false);
-        if (image == null) {
-          assertFalse(NLS.bind("Image ''{0}'' for editor ''{1}'' is invalid",
-              new String[] { icon, editorId }), true);
+      String iconURI = MenuHelper.getIconURI(element, "icon");
+
+      if (shouldCheckIconURI(iconURI)) {
+        String icon = element.getAttribute("icon");
+        String editorId = element.getAttribute("id");
+
+        if (iconURI != null) {
+          ImageDescriptor descriptor = ImageDescriptor.createFromURL(new URL(iconURI));
+          Image image = descriptor.createImage(false);
+          if (image == null) {
+            errorMessages.add(NLS.bind("Image ''{0}'' for editor ''{1}'' is invalid", icon, editorId));
+          } else {
+            image.dispose();
+          }
         } else {
-          image.dispose();
-        } 
-      } else {
-        assertFalse(NLS.bind("Image ''{0}'' for editor ''{1}'' is not set",
-            new String[] { icon, editorId }), true);
+          errorMessages.add(NLS.bind("Image ''{0}'' for editor ''{1}'' is not set", icon, editorId));
+        }
       }
     }
-    
+
+    assertTrue(errorMessages.stream().collect(Collectors.joining("\n")), errorMessages.isEmpty());
+  }
+
+  private boolean shouldCheckIconURI(String iconURI) {
+    return iconURI != null && iconURI.startsWith("platform:/plugin/org.polarsys.capella");
   }
 
 }
