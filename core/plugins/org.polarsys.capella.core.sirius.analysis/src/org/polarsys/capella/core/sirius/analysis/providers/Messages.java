d25e8275d0499dc808c2a60cfce04ba691ed86c2
Philippe DUL <philippe.dul@thalesgroup.com>
2020-08-25 11:37:04 +0200

    [566374] Deletion of Title Block prevent deletion of other elements
    Bug: 566374
    Change-Id: I7e27df4db6ba9645ab85bd870acb5df5fe5f1dbd
    Signed-off-by: Philippe DUL <philippe.dul@thalesgroup.com>

---
+++ core/plugins/org.polarsys.capella.core.sirius.analysis/src/org/polarsys/capella/core/sirius/analysis/providers/Messages.java
@@ -0,0 +1,27 @@
+/*******************************************************************************
+ * Copyright (c) 2020 THALES GLOBAL SERVICES.
+ * 
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0
+ * 
+ * SPDX-License-Identifier: EPL-2.0
+ * 
+ * Contributors:
+ *    Thales - initial API and implementation
+ *******************************************************************************/
+package org.polarsys.capella.core.sirius.analysis.providers;
+
+import org.eclipse.osgi.util.NLS;
+
+public class Messages extends NLS {
+  private static final String BUNDLE_NAME = "org.polarsys.capella.core.sirius.analysis.providers.messages"; //$NON-NLS-1$
+  public static String TitleBlock_type;
+  static {
+    // initialize resource bundle
+    NLS.initializeMessages(BUNDLE_NAME, Messages.class);
+  }
+
+  private Messages() {
+  }
+}
--- tests/plugins/org.polarsys.capella.test.diagram.common.ju/src/org/polarsys/capella/test/diagram/common/ju/step/tools/DeleteElementTool.java
+++ tests/plugins/org.polarsys.capella.test.diagram.common.ju/src/org/polarsys/capella/test/diagram/common/ju/step/tools/DeleteElementTool.java
@@ -12,42 +12,100 @@
  *******************************************************************************/
 package org.polarsys.capella.test.diagram.common.ju.step.tools;
 
-import java.util.List;
+import static org.junit.Assert.assertTrue;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.stream.Collectors;
+
+import org.eclipse.emf.common.command.Command;
+import org.eclipse.emf.common.command.UnexecutableCommand;
 import org.eclipse.emf.ecore.EObject;
 import org.eclipse.osgi.util.NLS;
-import org.eclipse.sirius.business.api.session.Session;
-import org.eclipse.sirius.diagram.DDiagram;
 import org.eclipse.sirius.diagram.DDiagramElement;
-import org.eclipse.sirius.diagram.business.api.componentization.DiagramComponentizationManager;
+import org.eclipse.sirius.diagram.business.api.helper.delete.DeleteHookHelper;
 import org.eclipse.sirius.viewpoint.description.tool.AbstractToolDescription;
 import org.junit.Assert;
+import org.polarsys.capella.common.ef.command.AbstractReadWriteCommand;
+import org.polarsys.capella.core.commands.preferences.service.ScopedCapellaPreferencesStore;
+import org.polarsys.capella.core.model.preferences.CapellaModelPreferencesPlugin;
+import org.polarsys.capella.core.model.preferences.IDeletePreferences;
 import org.polarsys.capella.test.diagram.common.ju.context.DiagramContext;
 import org.polarsys.capella.test.diagram.common.ju.wrapper.Messages;
 import org.polarsys.capella.test.diagram.common.ju.wrapper.factory.ToolWrapperFactory;
 import org.polarsys.capella.test.diagram.common.ju.wrapper.utils.ArgumentType;
+import org.polarsys.capella.test.framework.helpers.TestHelper;
 
 public class DeleteElementTool extends AbstractToolStep<EObject> {
 
-  DDiagramElement _elementToDelete;
-  DDiagram _containingToolDiagram;
+  protected DDiagramElement _elementToDelete;
 
   /**
-   * 
    * @param context
    *          The diagram context in which the tool is used
    * @param containingToolDiagramContext
    *          The diagram context in which the tool is defined
    * @param toolName
    */
+  public DeleteElementTool(DiagramContext context) {
+    super(context, null);
+  }
+
+  /**
+   * @param toolName
+   *          is deprecated, please use without name of tool (for a mapping, there is at most one delete tool)
+   */
+  @Deprecated
   public DeleteElementTool(DiagramContext context, DiagramContext containingToolDiagramContext, String toolName) {
     super(context, toolName);
-    _containingToolDiagram = containingToolDiagramContext.getDiagram();
   }
 
   public void delete(DDiagramElement element) {
+    disableConfirmation();
+    
     _elementToDelete = element;
+    final DeleteHookHelper deleteHookHelper = new DeleteHookHelper(Arrays.asList(element));
+    if (!deleteHookHelper.checkDeleteHook()) {
+      assertTrue(false);
+    }
     run();
+  }
+
+  private void disableConfirmation() {
+    // ScopedCapellaPreferencesStore.setValue(boolean) doesn't work if setDefault has same value
+    ScopedCapellaPreferencesStore.getInstance(CapellaModelPreferencesPlugin.PLUGIN_ID).setDefault(IDeletePreferences.PREFERENCE_CONFIRM_DELETE, true);
+    ScopedCapellaPreferencesStore.getInstance(CapellaModelPreferencesPlugin.PLUGIN_ID).setValue(IDeletePreferences.PREFERENCE_CONFIRM_DELETE, false);
+  }
+
+  public void deleteAll() {
+    disableConfirmation();
+    
+    TestHelper.getExecutionManager(getExecutionContext().getSession()).execute(new AbstractReadWriteCommand() {
+      public void run() {
+        Collection<DDiagramElement> deletableElements = getDiagramContext().getDiagram().getDiagramElements().stream()
+            .filter(x -> x.getDiagramElementMapping().getDeletionDescription() != null).collect(Collectors.toList());
+        Collection<Command> commands = new ArrayList<Command>();
+
+        final DeleteHookHelper deleteHookHelper = new DeleteHookHelper((Collection) deletableElements);
+        if (!deleteHookHelper.checkDeleteHook()) {
+          assertTrue(false);
+        }
+
+        for (DDiagramElement element : deletableElements) {
+          _elementToDelete = element;
+          preRunTest();
+          Command cmd = _toolWrapper.createCommand();
+          if (!UnexecutableCommand.INSTANCE.equals(cmd)) {
+            commands.add(cmd);
+          }
+        }
+
+        for (Command command : commands) {
+          command.execute();
+        }
+      }
+    });
   }
 
   /**
@@ -69,11 +127,14 @@ public class DeleteElementTool extends AbstractToolStep<EObject> {
    */
   @Override
   protected void preRunTest() {
-    AbstractToolDescription tool = getTool(getExecutionContext().getSession(), _containingToolDiagram, toolName);
-    Assert.assertNotNull(NLS.bind(Messages.toolDoesNotExist, toolName), tool);
+    toolName = _elementToDelete.getDiagramElementMapping().getDeletionDescription().getName();
+    AbstractToolDescription tool = _elementToDelete.getDiagramElementMapping().getDeletionDescription();
+    Assert.assertNotNull(NLS.bind(Messages.toolDoesNotExist, toolName, getDiagramContext().getDiagram().getName()),
+        tool);
+    Assert.assertTrue(toolName != null && toolName == tool.getName());
 
     _toolWrapper = ToolWrapperFactory.INSTANCE.createToolCommandWrapper(tool);
-    Assert.assertNotNull(NLS.bind(Messages.toolWrapperNotAvailable, toolName), _toolWrapper);
+    Assert.assertNotNull(NLS.bind(Messages.toolWrapperNotAvailable, tool.getName()), _toolWrapper);
 
     initToolArguments();
 
@@ -81,28 +142,13 @@ public class DeleteElementTool extends AbstractToolStep<EObject> {
     Assert.assertTrue(Messages.toolWrapperArgumentErr, isArgumentOk);
 
     boolean isContextOk = _toolWrapper.isContextOk();
-    Assert.assertTrue(NLS.bind(Messages.toolWrapperArgumentValueErr, toolName), isContextOk);
+    Assert.assertTrue(NLS.bind(Messages.toolWrapperArgumentValueErr, tool.getName()), isContextOk);
   }
 
-  /**
-   * Find the tool defined in a diagram
-   * 
-   * @param session
-   * @param diagram
-   * @param toolName
-   * @return
-   */
-  private AbstractToolDescription getTool(Session session, DDiagram diagram, final String toolName) {
-    final List<AbstractToolDescription> tools = new DiagramComponentizationManager().getAllTools(
-        session.getSelectedViewpoints(true), diagram.getDescription());
-    AbstractToolDescription theAbstractToolDescription = null;
-
-    for (AbstractToolDescription current : tools) {
-      if (current.getName().equals(toolName)) {
-        theAbstractToolDescription = current;
-        break;
-      }
-    }
-    return theAbstractToolDescription;
+  @Override
+  protected void dispose() {
+    super.dispose();
+    _elementToDelete = null;
   }
+
 }
