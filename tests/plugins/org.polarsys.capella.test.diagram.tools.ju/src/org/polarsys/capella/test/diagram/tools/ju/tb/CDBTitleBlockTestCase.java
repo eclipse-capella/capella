184088a22725de16c058d96ad5c72a3d5b3d43e4
Philippe DUL <philippe.dul@thalesgroup.com>
2020-08-25 11:37:04 +0200

    [566374] Deletion of Title Block prevent deletion of other elements
    Bug: 566374
    Change-Id: I7e27df4db6ba9645ab85bd870acb5df5fe5f1dbd
    Signed-off-by: Philippe DUL <philippe.dul@thalesgroup.com>

--- tests/plugins/org.polarsys.capella.test.diagram.tools.ju/src/org/polarsys/capella/test/diagram/tools/ju/tb/CDBTitleBlockTestCase.java
+++ tests/plugins/org.polarsys.capella.test.diagram.tools.ju/src/org/polarsys/capella/test/diagram/tools/ju/tb/CDBTitleBlockTestCase.java
@@ -12,10 +12,31 @@
  *******************************************************************************/
 package org.polarsys.capella.test.diagram.tools.ju.tb;
 
+import java.util.ArrayList;
+
+import org.eclipse.core.runtime.NullProgressMonitor;
+import org.eclipse.sirius.business.api.session.Session;
+import org.eclipse.sirius.diagram.DDiagramElement;
+import org.eclipse.sirius.viewpoint.description.DAnnotation;
+import org.eclipse.ui.preferences.ScopedPreferenceStore;
+import org.polarsys.capella.common.ui.MdeCommonUiActivator;
+import org.polarsys.capella.core.diagram.helpers.TitleBlockHelper;
+import org.polarsys.capella.core.sirius.analysis.DiagramServices;
+import org.polarsys.capella.core.sirius.analysis.preferences.TitleBlockPreferencePage;
+import org.polarsys.capella.test.diagram.common.ju.api.AbstractDiagramTestCase;
 import org.polarsys.capella.test.diagram.common.ju.context.CDBDiagram;
 import org.polarsys.capella.test.diagram.common.ju.context.CommonDiagram;
+import org.polarsys.capella.test.diagram.common.ju.wrapper.utils.DiagramHelper;
+import org.polarsys.capella.test.framework.context.SessionContext;
 
-public class CDBTitleBlockTestCase extends AbstractTitleBlockTestCase {
+public class CDBTitleBlockTestCase extends AbstractDiagramTestCase {
+  private ScopedPreferenceStore preferenceStore;
+  protected Session session;
+  protected SessionContext context;
+  protected CommonDiagram diagram;
+  protected CommonDiagram newDiagram;
+  protected ArrayList<String> elementsId = new ArrayList<String>();
+
   protected String diagramName;
   protected String dataPKG;
 
@@ -34,12 +55,88 @@ public class CDBTitleBlockTestCase extends AbstractTitleBlockTestCase {
   }
 
   @Override
-  protected CommonDiagram initDiagram() {
-    return CDBDiagram.openDiagram(context, diagramName);
+  protected String getRequiredTestModel() {
+    return "TitleBlocksModel";
   }
 
   @Override
-  protected CommonDiagram createDiagram() {
-    return CDBDiagram.createDiagram(context, dataPKG);
+  public void test() throws Exception {
+    initTest();
+    titleBlocks();
+    // unsynchronized
+    DiagramHelper.setSynchronized(diagram.getDiagram(), false);
+    titleBlocks();
+    deleteAll();
   }
+
+  public void titleBlocks() {
+    diagramTitleBlock();
+    for (String elementId : elementsId) {
+      elementTitleBlock(elementId);
+    }
+  }
+
+  public void diagramTitleBlock() {
+
+    // Check if diagram was created by default
+    // TODO Reactivate or remove the test when the preference option is decided
+    // newDiagram.checkAutocreateDiagramTitleBlock();
+
+    // create TB tools
+    DDiagramElement viewTB = diagram.createDiagramTitleBlock();
+    DAnnotation diagramTB = (DAnnotation) viewTB.getTarget();
+
+    // show/hide tool + create TB after hide it
+    diagram.removeDiagramTitleBlock(diagramTB.getUid());
+    diagram.checkCreateDiagramTitleBlock();
+    diagram.insertDiagramTitleBlock(diagramTB.getUid());
+
+    viewTB = DiagramServices.getDiagramServices().getDiagramElement(diagram.getDiagram(), diagramTB);
+    diagram.deleteSemantic(viewTB);
+    assertTrue(TitleBlockHelper.getAllAnnotationsForTitleBlock(diagramTB).isEmpty());
+  }
+
+  public void elementTitleBlock(String elementId) {
+    // create TB tools
+    DDiagramElement viewTB = diagram.createDiagramTitleBlock();
+    DAnnotation elementTB = (DAnnotation) viewTB.getTarget();
+
+    // show/hide tool + create TB after hide it
+    diagram.removeElementTitleBlock(elementTB.getUid());
+    diagram.checkCreateElementTitleBlock(elementId);
+    diagram.insertElementTitleBlock(elementTB.getUid());
+
+    // insert line/column in TB element
+    diagram.insertLineInTitleBlock(elementTB, 0);
+    diagram.insertColumnInTitleBlock(elementTB, 0);
+
+    // remove line/column from TB element
+    diagram.removeLineFromTitleBlock(elementTB, 0);
+    diagram.removeColumnFromTitleBlock(elementTB, 0);
+
+    viewTB = DiagramServices.getDiagramServices().getDiagramElement(diagram.getDiagram(), elementTB);
+    diagram.deleteSemantic(viewTB);
+    // no inner annotations
+    assertTrue(TitleBlockHelper.getAllAnnotationsForTitleBlock(elementTB).isEmpty());
+    // ensure element is not deleted
+    assertTrue(diagram.getSessionContext().getSemanticElement(elementId).eContainer() != null);
+  }
+
+  public void deleteAll() {
+    newDiagram.createDiagramTitleBlock();
+    newDiagram.createConstraint(newDiagram.getDiagramId());
+    newDiagram.deleteAllSemantic();
+    assertTrue(newDiagram.getDiagram().getOwnedDiagramElements().size() == 0);
+  }
+
+  protected void initTest() {
+    session = getSession(getRequiredTestModel());
+    context = new SessionContext(session);
+    diagram = CDBDiagram.openDiagram(context, diagramName);
+    preferenceStore = (ScopedPreferenceStore) MdeCommonUiActivator.getDefault().getPreferenceStore();
+    preferenceStore.setValue(TitleBlockPreferencePage.DEFAULT_TITLEBLOCK_PREFERENCE_STORE, true);
+    newDiagram = CDBDiagram.createDiagram(context, dataPKG);
+    session.save(new NullProgressMonitor());
+  }
+
 }
