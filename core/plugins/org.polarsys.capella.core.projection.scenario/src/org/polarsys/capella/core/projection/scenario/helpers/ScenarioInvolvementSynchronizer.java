f47c028b4c22b7ea69109d5c5f16beefe6b233f5
Georgiana Ecobici <georgiana-elena.ecobici@thalesgroup.com>
2020-10-07 10:45:51 +0300

    [567469] Fix circular references after transition
    Change-Id: I5a584f7edf37e05290880e7e22fdd3145a64764c
    Signed-off-by: Georgiana Ecobici <georgiana-elena.ecobici@thalesgroup.com>

--- core/plugins/org.polarsys.capella.core.projection.scenario/src/org/polarsys/capella/core/projection/scenario/helpers/ScenarioInvolvementSynchronizer.java
+++ core/plugins/org.polarsys.capella.core.projection.scenario/src/org/polarsys/capella/core/projection/scenario/helpers/ScenarioInvolvementSynchronizer.java
@@ -13,29 +13,22 @@
 package org.polarsys.capella.core.projection.scenario.helpers;
 
 import java.util.Collection;
-import java.util.List;
-import java.util.Optional;
 
-import org.eclipse.core.runtime.NullProgressMonitor;
 import org.eclipse.emf.ecore.EObject;
-import org.eclipse.jface.action.MenuManager;
 import org.eclipse.sirius.business.api.dialect.DialectManager;
+import org.eclipse.sirius.business.api.query.EObjectQuery;
 import org.eclipse.sirius.business.api.session.Session;
 import org.eclipse.sirius.business.api.session.SessionManager;
+import org.eclipse.sirius.viewpoint.ViewpointPackage;
 import org.eclipse.sirius.viewpoint.description.RepresentationDescription;
 import org.eclipse.sirius.viewpoint.description.Viewpoint;
-import org.polarsys.capella.common.helpers.TransactionHelper;
-import org.polarsys.capella.core.data.cs.BlockArchitecture;
 import org.polarsys.capella.core.data.cs.Component;
 import org.polarsys.capella.core.data.cs.Part;
 import org.polarsys.capella.core.data.fa.AbstractFunction;
 import org.polarsys.capella.core.data.interaction.AbstractCapability;
 import org.polarsys.capella.core.data.interaction.InstanceRole;
-import org.polarsys.capella.core.data.interaction.InteractionUse;
 import org.polarsys.capella.core.data.interaction.Scenario;
-import org.polarsys.capella.core.data.interaction.TimeLapse;
 import org.polarsys.capella.core.model.helpers.AbstractCapabilityExt;
-import org.polarsys.capella.core.model.helpers.BlockArchitectureExt;
 import org.polarsys.capella.core.sirius.ui.actions.NewRepresentationAction;
 import org.polarsys.capella.core.tiger.IFinalizer;
 import org.polarsys.capella.core.tiger.ITransfo;
@@ -64,39 +57,26 @@ public class ScenarioInvolvementSynchronizer implements IFinalizer {
         }
       }
 
-      // set the referenced scenarios
-      List<TimeLapse> timelapses = scenario.getOwnedTimeLapses();
-      for (TimeLapse timelapse : timelapses) {
-        if (timelapse instanceof InteractionUse) {
-          InteractionUse interaction = (InteractionUse) timelapse;
-          Scenario refScenario = interaction.getReferencedScenario();
-          if (refScenario != null) {
-            BlockArchitecture sourceBlock = BlockArchitectureExt.getRootBlockArchitecture(refScenario);
-            BlockArchitecture targetBlock = BlockArchitectureExt.getRootBlockArchitecture(scenario);
-            if (!sourceBlock.equals(targetBlock)) {
-              Optional<Scenario> match = refScenario.getRealizingScenarios().stream().filter(sc -> sc.getName().equals(refScenario.getName())).findFirst();
-              if(match.isPresent()) {
-                 interaction.setReferencedScenario(match.get());
-              }
-            }
-          }
-        }
-      }
-      
       // check if the option to Initialize the creation of related scenario is checked
       if (PreferenceHelper.getInstance().transitionInitializeTransitionedScenario()) {
-        Session currentSession = SessionManager.INSTANCE.getSession(scenario);
-        Collection<Viewpoint> selectedViewpoints = currentSession.getSelectedViewpoints(false);
+        // create new Diagram Scenario only if it doesn't already exist one
+        Collection<EObject> result = new EObjectQuery(scenario)
+            .getInverseReferences(ViewpointPackage.Literals.DSEMANTIC_DECORATOR__TARGET);
+        if (result.isEmpty()) {
 
-        Collection<RepresentationDescription> descriptions = DialectManager.INSTANCE
-            .getAvailableRepresentationDescriptions(selectedViewpoints, scenario);
-        
-        if (!descriptions.isEmpty()) {
-          // Computes the "New Diagram..." menu content according to the current selection.
-          for (RepresentationDescription description : descriptions) {
-            NewRepresentationAction representationAction = new NewRepresentationAction(description, scenario,
-                currentSession, true, false);
-            representationAction.run();
+          Session currentSession = SessionManager.INSTANCE.getSession(scenario);
+          Collection<Viewpoint> selectedViewpoints = currentSession.getSelectedViewpoints(false);
+
+          Collection<RepresentationDescription> descriptions = DialectManager.INSTANCE
+              .getAvailableRepresentationDescriptions(selectedViewpoints, scenario);
+
+          if (!descriptions.isEmpty()) {
+            // Computes the "New Diagram..." menu content according to the current selection.
+            for (RepresentationDescription description : descriptions) {
+              NewRepresentationAction representationAction = new NewRepresentationAction(description, scenario,
+                  currentSession, true, false);
+              representationAction.run();
+            }
           }
         }
       }
