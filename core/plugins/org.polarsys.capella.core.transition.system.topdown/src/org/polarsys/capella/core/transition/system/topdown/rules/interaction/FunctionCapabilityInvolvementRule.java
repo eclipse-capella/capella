1527a3a891c34bd440df5326c8c6bcfecf7858af
georgiana-ecobici <georgiana-elena.ecobici@thalesgroup.com>
2020-10-14 11:21:27 +0300

    [567857] Cannot save model after IS transition from PA to EPBS (#400)
    - disabled the transition of
    AbstractFunctionAbstractCapabilityInvolvement from PA to EPBS
    
    Conflicts: Migrated 1.4.x models to 5.0 models
    
    Change-Id: I80435457b3bb2cad8fc3a83d9578eb9bea12116e
    Signed-off-by: Georgiana Ecobici <georgiana-elena.ecobici@thalesgroup.com>

--- core/plugins/org.polarsys.capella.core.transition.system.topdown/src/org/polarsys/capella/core/transition/system/topdown/rules/interaction/FunctionCapabilityInvolvementRule.java
+++ core/plugins/org.polarsys.capella.core.transition.system.topdown/src/org/polarsys/capella/core/transition/system/topdown/rules/interaction/FunctionCapabilityInvolvementRule.java
@@ -12,10 +12,16 @@
  *******************************************************************************/
 package org.polarsys.capella.core.transition.system.topdown.rules.interaction;
 
+import org.eclipse.core.runtime.IStatus;
+import org.eclipse.core.runtime.Status;
 import org.eclipse.emf.ecore.EClass;
-
+import org.eclipse.emf.ecore.EObject;
+import org.polarsys.capella.core.data.interaction.AbstractFunctionAbstractCapabilityInvolvement;
 import org.polarsys.capella.core.data.interaction.InteractionPackage;
+import org.polarsys.capella.core.data.pa.PhysicalFunction;
+import org.polarsys.capella.core.transition.common.constants.Messages;
 import org.polarsys.capella.core.transition.system.topdown.rules.common.InvolvementRule;
+import org.polarsys.kitalpha.transposer.rules.handler.rules.api.IContext;
 
 /**
  *
@@ -25,6 +31,18 @@ public class FunctionCapabilityInvolvementRule extends InvolvementRule {
   @Override
   protected EClass getSourceType() {
     return InteractionPackage.Literals.ABSTRACT_FUNCTION_ABSTRACT_CAPABILITY_INVOLVEMENT;
+  }
+  
+  /**
+   * {@inheritDoc}
+   */
+  @Override
+  public IStatus transformRequired(EObject element_p, IContext context_p) {
+    AbstractFunctionAbstractCapabilityInvolvement involvement = (AbstractFunctionAbstractCapabilityInvolvement) element_p;
+    if (involvement.getInvolved() instanceof PhysicalFunction) {
+      return new Status(IStatus.WARNING, Messages.Activity_Transformation, "involvement not transitioned");
+    }
+    return super.transformRequired(element_p, context_p);
   }
 
 }
