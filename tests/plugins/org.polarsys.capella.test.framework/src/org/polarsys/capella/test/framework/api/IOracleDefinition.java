108143e1cbb78404920cfcffaf570b79fbd6fa71
Sandu Postaru <sandu.postaru@thalesgroup.com>
2020-11-05 15:30:39 +0100

    [test] Add Composed Oracle Definition concept for validation rule tests
    - Add Composed Oracle Definition for validation rule tests that raise
    child diagnostics
    - Refactor the ValidationRuleTestCase class
    
    Change-Id: I44467ff64b25fa90f5c22d56453865e67c85c4d4
    Signed-off-by: Sandu Postaru <sandu.postaru@thalesgroup.com>

---
+++ tests/plugins/org.polarsys.capella.test.framework/src/org/polarsys/capella/test/framework/api/IOracleDefinition.java
@@ -0,0 +1,25 @@
+/*******************************************************************************
+ * Copyright (c) 2020 THALES GLOBAL SERVICES.
+ * 
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0
+ * 
+ * SPDX-License-Identifier: EPL-2.0
+ * 
+ * Contributors:
+ *    Thales - initial API and implementation
+ *******************************************************************************/
+package org.polarsys.capella.test.framework.api;
+
+public interface IOracleDefinition {
+
+  String getObjectID();
+
+  int getNbExpectedErrors();
+
+  void countOneError();
+
+  int getNbFoundErrors();
+
+}
--- tests/plugins/org.polarsys.capella.test.validation.rules.ju/src/org/polarsys/capella/test/validation/rules/ju/testcases/ValidationRuleTestCase.java
+++ tests/plugins/org.polarsys.capella.test.validation.rules.ju/src/org/polarsys/capella/test/validation/rules/ju/testcases/ValidationRuleTestCase.java
@@ -12,12 +12,16 @@
  *******************************************************************************/
 package org.polarsys.capella.test.validation.rules.ju.testcases;
 
+import java.text.MessageFormat;
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.Collections;
-import java.util.Hashtable;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
 
+import org.eclipse.core.resources.IFile;
 import org.eclipse.core.resources.IMarker;
 import org.eclipse.core.runtime.IStatus;
 import org.eclipse.core.runtime.Status;
@@ -30,7 +34,6 @@ import org.eclipse.emf.validation.service.IBatchValidator;
 import org.eclipse.emf.validation.service.IConstraintDescriptor;
 import org.eclipse.emf.validation.service.IConstraintFilter;
 import org.eclipse.emf.validation.service.ModelValidationService;
-import org.eclipse.osgi.util.NLS;
 import org.eclipse.sirius.business.api.session.SessionManager;
 import org.polarsys.capella.common.helpers.EObjectExt;
 import org.polarsys.capella.common.helpers.EcoreUtil2;
@@ -43,6 +46,8 @@ import org.polarsys.capella.core.libraries.model.ICapellaModel;
 import org.polarsys.capella.core.model.handler.markers.ICapellaValidationConstants;
 import org.polarsys.capella.core.validation.CapellaValidationActivator;
 import org.polarsys.capella.test.framework.api.BasicTestCase;
+import org.polarsys.capella.test.framework.api.ComposedOracleDefinition;
+import org.polarsys.capella.test.framework.api.IOracleDefinition;
 import org.polarsys.capella.test.framework.api.OracleDefinition;
 
 /**
@@ -57,8 +62,17 @@ import org.polarsys.capella.test.framework.api.OracleDefinition;
  * 
  * @author Erwan Brottier
  * @contributor Joao Barata
+ * @contributor Sandu Postaru
  */
 public abstract class ValidationRuleTestCase extends BasicTestCase {
+
+  private static final String RULE_DOES_NOT_EXIST_PATTERN = "Rule {0} does not exist. Test can not be performed"; // $NON-NLS-N$
+  private static final String MISSING_ERROR_ON_OBJECT_PATTERN = "Validation rule {0} has not detected an error on object {1} while it must be the case"; // $NON-NLS-N$
+  private static final String UNEXPECTED_ERROR_ON_OBJECT_PATTERN = "Validation rule {0} has detected an error on object(s): \n{1}\nwhile it must not be the case."; // $NON-NLS-N$
+  private static final String LESS_ERRORS_THAN_EXPECTED_PATTERN = "Validation rule {0} has only detected {1} of {2} expected error(s) on object {3}"; // $NON-NLS-N$
+  private static final String MORE_ERRORS_THAT_EXPECTED_PATTER = "Validation rule {0} has detected {1} error(s) instead of {2} error(s) on object {3}"; // $NON-NLS-N$
+  private static final String UNEXPECTED_EMF_ERROR_KIND_PATTERN = "Validation rule {0} has detected another kind of error, please check EMF integrity of the model: {1}"; // $NON-NLS-N$
+  private static final String UNEXPECTED_ERROR_KIND = "Validation rule {0} has detected an error but {1} expected"; // $NON-NLS-N$
 
   // these values are obtained by using methods defined in concrete test cases
   protected String ruleID = getRuleID();
@@ -69,10 +83,9 @@ public abstract class ValidationRuleTestCase extends BasicTestCase {
   protected IConstraintDescriptor ruleDescriptor;
   protected IConstraintFilter filter;
   protected IBatchValidator validator;
-  protected Hashtable<String, OracleDefinition> objectID2OracleDefinition = new Hashtable<String, OracleDefinition>();
 
   // stores the status of the rule before to launch the test
-  private boolean _ruleWasDisabled;
+  private boolean ruleWasDisabled;
 
   // these methods must be overridden by concrete test cases
   /** returns the name of the test project folder (by default in the folder "model") */
@@ -86,11 +99,11 @@ public abstract class ValidationRuleTestCase extends BasicTestCase {
 
   /** returns the oracle definition to be checked */
   protected abstract List<OracleDefinition> getOracleDefinitions();
-  
+
   protected boolean getCheckQuickFix() {
     return false;
   }
-  
+
   protected IStatus testCheckQuickFix(List<IMarker> markers) {
     return Status.OK_STATUS;
   }
@@ -99,25 +112,24 @@ public abstract class ValidationRuleTestCase extends BasicTestCase {
   protected void setUp() throws Exception {
     super.setUp();
 
-    // init validator
     validator = CapellaValidationActivator.getDefault().getCapellaValidatorAdapter().getValidator();
     ModelValidationService.getInstance().loadXmlConstraintDeclarations();// load the xml definition of constraints
-    // get the descriptor of the rule to test
+
     ConstraintRegistry registry = ConstraintRegistry.getInstance();
 
     if (ruleID != null) {
       ruleDescriptor = registry.getDescriptor(ruleID);
       if (ruleDescriptor == null) {
-        throw new InternalError("Rule " + ruleID + " does not exist. Test can not be performed"); //$NON-NLS-1$//$NON-NLS-2$
+        throw new InternalError(MessageFormat.format(RULE_DOES_NOT_EXIST_PATTERN, ruleID)); // $NON-NLS-1$ //$NON-NLS-2$
       }
 
       if (!ruleDescriptor.isEnabled()) {
         ruleDescriptor.setEnabled(true);
-        _ruleWasDisabled = true;
+        ruleWasDisabled = true;
       }
 
-      // create the filter and add it to the validator
       filter = new IConstraintFilter() {
+        @Override
         @SuppressWarnings("synthetic-access")
         public boolean accept(IConstraintDescriptor constraint_p, EObject target_p) {
           return ruleDescriptor == constraint_p;
@@ -128,12 +140,105 @@ public abstract class ValidationRuleTestCase extends BasicTestCase {
   }
 
   @Override
-  public List<String> getRequiredTestModels() {
-    if (getRequiredTestModel() != null) {
-      return Collections.singletonList(getRequiredTestModel());
-    } else {
-      return Collections.emptyList();
+  public void test() throws Exception {
+    List<EObject> elementsToValidate = getElementsToValidate();
+    List<OracleDefinition> oracleDefinitions = getOracleDefinitions();
+    Map<String, IOracleDefinition> elementIdToOracleMap = computeElementIdToOracleMap(oracleDefinitions);
+
+    List<IMarker> markers = new ArrayList<IMarker>();
+    List<EObject> unexpectedFailingElements = new ArrayList<>();
+
+    Diagnostician diagnostician = new Diagnostician();
+
+    for (EObject element : elementsToValidate) {
+      String elementId = getId(element);
+      IOracleDefinition rootOracle = elementIdToOracleMap.get(elementId);
+      Diagnostic diagnostic = diagnostician.validate(element);
+
+      if (missingValidationError(rootOracle, diagnostic)) {
+        fail(MessageFormat.format(MISSING_ERROR_ON_OBJECT_PATTERN, ruleID, elementId));
+      } else {
+
+        if (diagnostic.getSeverity() != Diagnostic.OK) {
+          for (Diagnostic childDiagnostic : diagnostic.getChildren()) {
+
+            if (rootOracle instanceof ComposedOracleDefinition) {
+              List<CapellaElement> childFailedElements = getElements(childDiagnostic);
+
+              for (CapellaElement childFailedElement : childFailedElements) {
+                String childId = getId(childFailedElement);
+                IOracleDefinition childOracle = elementIdToOracleMap.get(childId);
+
+                if (childOracle == null || childOracle.getNbExpectedErrors() == 0) {
+                  unexpectedFailingElements.add(childFailedElement);
+                } else {
+                  childOracle.countOneError();
+                }
+              }
+            }
+
+            IFile resourceFile = EcoreUtil2.getFile(element.eResource());
+            IMarker marker = LightMarkerRegistry.getInstance().createMarker(resourceFile, childDiagnostic,
+                ICapellaValidationConstants.CAPELLA_MARKER_ID);
+
+            if (marker != null) {
+              markers.add(marker);
+            }
+          }
+
+          assertExpectedRuleHasBeenThrown(diagnostic, element);
+
+          if (rootOracle != null) {
+            rootOracle.countOneError();
+          } else {
+            unexpectedFailingElements.add(element);
+          }
+        }
+      }
     }
+
+    if (!unexpectedFailingElements.isEmpty()) {
+      String objects = unexpectedFailingElements.stream().map(elt -> " - " + getId(elt))
+          .collect(Collectors.joining("\n"));
+      fail(MessageFormat.format(UNEXPECTED_ERROR_ON_OBJECT_PATTERN, ruleID, objects));
+    }
+
+    for (IOracleDefinition oracleDef : elementIdToOracleMap.values()) {
+      int nbExpectedErrors = oracleDef.getNbExpectedErrors();
+      int nbFoundErrors = oracleDef.getNbFoundErrors();
+
+      if (nbFoundErrors < nbExpectedErrors) {
+        fail(MessageFormat.format(LESS_ERRORS_THAN_EXPECTED_PATTERN, ruleID, nbFoundErrors, nbExpectedErrors,
+            oracleDef.getObjectID()));
+      } else if (nbFoundErrors > nbExpectedErrors) {
+        fail(MessageFormat.format(MORE_ERRORS_THAT_EXPECTED_PATTER, ruleID, nbFoundErrors, nbExpectedErrors,
+            oracleDef.getObjectID()));
+      }
+    }
+
+    if (getCheckQuickFix()) {
+      IStatus status = testCheckQuickFix(markers);
+      if (status != null && !status.isOK()) {
+        fail(status.getMessage());
+      }
+    }
+  }
+
+  private List<CapellaElement> getElements(Diagnostic diagnostic) {
+    return diagnostic.getData().stream() //
+        .filter(CapellaElement.class::isInstance)//
+        .map(CapellaElement.class::cast) //
+        .collect(Collectors.toList());
+  }
+
+  private boolean missingValidationError(IOracleDefinition rootOracle, Diagnostic diagnostic) {
+    return diagnostic.getSeverity() == Diagnostic.OK && rootOracle != null && rootOracle.getNbExpectedErrors() > 0;
+  }
+
+  @Override
+  public List<String> getRequiredTestModels() {
+    String requiredTestModel = getRequiredTestModel();
+    return requiredTestModel != null ? Collections.singletonList(requiredTestModel) : Collections.emptyList();
   }
 
   protected List<EObject> getTestScope(ICapellaModel model) {
@@ -149,82 +254,41 @@ public abstract class ValidationRuleTestCase extends BasicTestCase {
     return scope;
   }
 
-  public void test() throws Exception {
-    // get the objects to validate (only CapellaElement because the oracle is based on object ID)
+  private List<EObject> getElementsToValidate() {
     ICapellaModel model = getTestModel(getRequiredTestModel());
-    List<EObject> objectsToValidate = getTestScope(model);
-    List<OracleDefinition> oracleDefinitions = getOracleDefinitions();
-    List<IMarker> markers = new ArrayList<IMarker>();
+    return getTestScope(model);
+  }
+
+  protected Map<String, IOracleDefinition> computeElementIdToOracleMap(List<OracleDefinition> oracleDefinitions) {
+    Map<String, IOracleDefinition> elementIdToOracleMap = new HashMap<>();
+
     if (oracleDefinitions != null) {
-      // prepare oracle table
-      for (OracleDefinition definition : oracleDefinitions) {
-        objectID2OracleDefinition.put(definition.getObjectID(), definition);
-      }
-      // check the validation result for each objects
-      List<EObject> failedObjects = new ArrayList<EObject>();
-      Diagnostician diagnostician = new Diagnostician();
-      for (EObject object : objectsToValidate) {
-        String objectID = getId(object);
-        OracleDefinition oracleDef = objectID2OracleDefinition.get(objectID);
-        Diagnostic diagnostic = diagnostician.validate(object);
-        
-        if ((diagnostic.getSeverity() == Diagnostic.OK) && (oracleDef != null) && oracleDef.getNbExpectedErrors() > 0) {
-          fail("Validation rule " + ruleID + " has not detected an error on object " + objectID + " while it must be the case"); //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$
-        } else {
-          if (diagnostic.getSeverity() != Diagnostic.OK) {
-            for(Diagnostic diag : diagnostic.getChildren()) {
-              IMarker marker = LightMarkerRegistry.getInstance().createMarker(EcoreUtil2.getFile(object.eResource()), diag, ICapellaValidationConstants.CAPELLA_MARKER_ID);
-              if(marker != null) {
-                markers.add(marker);
-              }
-            }
-            assertExpectedRuleHasBeenThrown(diagnostic, object);
-            if (oracleDef != null) {
-              oracleDef.countOneError();
-            } else {
-              failedObjects.add(object);
-            }
+      for (OracleDefinition oracle : oracleDefinitions) {
+        elementIdToOracleMap.put(oracle.getObjectID(), oracle);
+
+        if (oracle instanceof ComposedOracleDefinition) {
+          ComposedOracleDefinition composedOracle = (ComposedOracleDefinition) oracle;
+          Collection<IOracleDefinition> childOracleDefinitions = composedOracle.getChildren();
+          for (IOracleDefinition childOracle : childOracleDefinitions) {
+            elementIdToOracleMap.put(childOracle.getObjectID(), childOracle);
           }
         }
-      }
-      // check all objects to be validated before throwing a message
-      if (!failedObjects.isEmpty()) {
-        String objects = failedObjects.stream().map(elt -> " - " + getId(elt)).collect(Collectors.joining("\n"));
-        fail(NLS.bind("Validation rule {0} has detected an error on object(s): \n{1}\nwhile it must not be the case.", ruleID, objects));
-      }
-      // check that some errors has not been detected according to the number of error occurences for each object in
-      // error
-      for (OracleDefinition oracleDef : oracleDefinitions) {
-        int nbExpectedErrors = oracleDef.getNbExpectedErrors();
-        int nbFoundErrors = oracleDef.getNbFoundErrors();
-        if (nbFoundErrors < nbExpectedErrors) {
-          fail("Validation rule " + ruleID + " has only detected " + nbFoundErrors + " of " + nbExpectedErrors + " expected error(s) on object " + oracleDef.getObjectID()); //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$ //$NON-NLS-4$
-        } else if (nbFoundErrors > nbExpectedErrors) {
-          fail("Validation rule " + ruleID + " has detected " + nbFoundErrors + " error(s) instead of " + nbExpectedErrors + " error(s) on object " + oracleDef.getObjectID()); //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$ //$NON-NLS-4$
-        }
-      }
-      
-      if(getCheckQuickFix()) {
-        IStatus status = testCheckQuickFix(markers);
-        if(status != null && !status.isOK()) {
-          fail(status.getMessage());
-        }
+
       }
     }
+    return elementIdToOracleMap;
   }
 
   protected void assertExpectedRuleHasBeenThrown(Diagnostic diagnostic, EObject object) {
     assertFalse(diagnostic.getChildren().isEmpty());
     Diagnostic nestedDiag = diagnostic.getChildren().get(0);
-    
-    assertTrue("Validation rule " + ruleID
-        + " has detected another kind of error, please check EMF integrity of the model: "+ getAirdURI(object),
+
+    assertTrue(MessageFormat.format(UNEXPECTED_EMF_ERROR_KIND_PATTERN, ruleID, getAirdURI(object)),
         nestedDiag instanceof ConstraintStatusDiagnostic);
 
     String ruleIdThrown = ((ConstraintStatusDiagnostic) nestedDiag).getConstraintStatus().getConstraint()
         .getDescriptor().getId();
-    assertEquals("Validation rule " + ruleIdThrown + " has detected an error but " + ruleID + " expected",
-        ruleIdThrown, ruleID);
+    assertEquals(MessageFormat.format(UNEXPECTED_ERROR_KIND, ruleIdThrown, ruleID), ruleIdThrown, ruleID);
   }
 
   protected String getAirdURI(EObject object) {
@@ -243,15 +307,14 @@ public abstract class ValidationRuleTestCase extends BasicTestCase {
 
   @Override
   protected void tearDown() throws Exception {
-    if (_ruleWasDisabled && ruleDescriptor != null) {
+    if (ruleWasDisabled && ruleDescriptor != null) {
       ruleDescriptor.setEnabled(false);
     }
 
     if (validator != null) {
-      // remove the filter from the validator
       validator.removeConstraintFilter(filter);
     }
-    
+
     super.tearDown();
   }
 
