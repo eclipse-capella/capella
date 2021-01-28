92fd99b016b91fe34cf170bec87dcf4b410eb599
MalinaStefaniaStoicanescu <malina.stoicanescu@thalesgroup.com>
2021-01-28 10:54:26 +0200

    #2057 Re-add siblings resolver + refactor
    Change-Id: Ib105ef9cbe8a15e92ed3ce5278f8f8abd848414d
    Signed-off-by: MalinaStefaniaStoicanescu <malina.stoicanescu@thalesgroup.com>

--- core/plugins/org.polarsys.capella.core.data.common.ui.quickfix/src/org/polarsys/capella/core/data/common/ui/quickfix/resolver/DWF_SM_06Resolver.java
+++ core/plugins/org.polarsys.capella.core.data.common.ui.quickfix/src/org/polarsys/capella/core/data/common/ui/quickfix/resolver/DWF_SM_06Resolver.java
@@ -12,23 +12,17 @@
  *******************************************************************************/
 package org.polarsys.capella.core.data.common.ui.quickfix.resolver;
 
-import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
 
 import org.eclipse.core.resources.IMarker;
-import org.eclipse.emf.common.util.EList;
 import org.eclipse.emf.ecore.EObject;
 import org.polarsys.capella.core.data.capellacommon.AbstractState;
 import org.polarsys.capella.core.data.capellacommon.FinalState;
 import org.polarsys.capella.core.data.capellacommon.Pseudostate;
-import org.polarsys.capella.core.data.capellacommon.Region;
-import org.polarsys.capella.core.data.capellacommon.State;
-import org.polarsys.capella.core.data.capellacommon.impl.ModeImpl;
-import org.polarsys.capella.core.data.capellacommon.impl.StateImpl;
 import org.polarsys.capella.core.validation.ui.ide.quickfix.AbstractDeleteCommandResolver;
 
-public class DWF_SM_06Resolver extends AbstractDeleteCommandResolver {
+public abstract class DWF_SM_06Resolver extends AbstractDeleteCommandResolver {
 
   /**
    * {@inheritDoc}
@@ -36,7 +30,7 @@ public class DWF_SM_06Resolver extends AbstractDeleteCommandResolver {
   @Override
   public Object getElementToDelete(Object obj) {
     if (obj != null) {
-      return getChildrenMixedStates(obj);
+      return getMixedStates(obj);
     }
     return null;
   }
@@ -46,33 +40,20 @@ public class DWF_SM_06Resolver extends AbstractDeleteCommandResolver {
     for (IMarker marker : markers) {
       EObject modelElement = getModelElements(marker).get(0);
       if ((null != modelElement) && (modelElement instanceof AbstractState)) {
-        return getChildrenMixedStates(modelElement).size() > 0;
+        return getMixedStates(modelElement).size() > 0;
       }
     }
     return false;
   }
 
-  private List<AbstractState> getChildrenMixedStates(Object obj) {
-    List<AbstractState> lstStates = new ArrayList<>();
-    if (obj instanceof State) {
-      EList<Region> regions = ((State) obj).getOwnedRegions();
-      for (Region region : regions) {
-        for (AbstractState state : region.getOwnedStates()) {
-          if (areMixedStateMode(obj, state)) {
-            lstStates.add(state);
-          }
-        }
-      }
+  protected abstract List<AbstractState> getMixedStates(Object obj);
+
+  protected boolean areMixedStateMode(Object object, AbstractState state) {
+    if ((state instanceof Pseudostate) || (state instanceof FinalState)) {
+      return false;
     }
-    return lstStates;
+
+    return !object.getClass().equals(state.getClass());
   }
-  
-  private boolean areMixedStateMode(Object object, AbstractState state) {
-      if ((state instanceof Pseudostate) || (state instanceof FinalState)) {
-        return false;
-      }
-       
-      return !object.getClass().equals(state.getClass());
-    }
-  
+
 }
