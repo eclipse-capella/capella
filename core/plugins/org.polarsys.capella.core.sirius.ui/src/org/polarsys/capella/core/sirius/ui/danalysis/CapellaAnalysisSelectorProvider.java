5f05c942cb0c291d3d0e92c16ccaa947c5bb823f
Laurent Fasani <laurent.fasani@obeo.fr>
2021-04-29 09:45:32 +0200

    #2189 Associate the right DAnalysisSelection to the Capella Sirius Session
    Now, when creating the Sirius Session for a Capella project, the right
    DAnalysisSelector is injected to the Sirius Session. It allows to cancel
    properly the representation creation when the project is fragmented.
    
    Change-Id: Ic6f1415a0e890c9deca88971c40ad032eeca551a
    Signed-off-by: Laurent Fasani <laurent.fasani@obeo.fr>
    Signed-off-by: Philippe DUL <philippe.dul@thalesgroup.com>

--- core/plugins/org.polarsys.capella.core.sirius.ui/src/org/polarsys/capella/core/sirius/ui/danalysis/CapellaAnalysisSelectorProvider.java
+++ core/plugins/org.polarsys.capella.core.sirius.ui/src/org/polarsys/capella/core/sirius/ui/danalysis/CapellaAnalysisSelectorProvider.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2006, 2020 THALES GLOBAL SERVICES.
+ * Copyright (c) 2006, 2021 THALES GLOBAL SERVICES.
  * 
  * This program and the accompanying materials are made available under the
  * terms of the Eclipse Public License 2.0 which is available at
@@ -16,7 +16,7 @@ import org.eclipse.sirius.business.api.session.danalysis.DAnalysisSelector;
 import org.eclipse.sirius.business.api.session.danalysis.DAnalysisSelectorProvider;
 import org.eclipse.sirius.business.api.session.danalysis.DAnalysisSession;
 import org.eclipse.sirius.ui.business.internal.session.analysis.SmartDialogAnalysisSelectorProvider;
-import org.polarsys.capella.core.sirius.ui.helper.SessionHelper;
+import org.polarsys.capella.core.model.handler.command.CapellaResourceHelper;
 
 /**
  * Provides a customized {@link DAnalysisSelector}.
@@ -36,10 +36,9 @@ public class CapellaAnalysisSelectorProvider implements DAnalysisSelectorProvide
 
   @Override
   public boolean provides(DAnalysisSession session) {
-    if (SessionHelper.getCapellaProject(session) != null) {
-      return true;
-    }
-    return false;
+    // do not use SessionHelper.getCapellaProject(session) because when creating a new Capella project it returns
+    // false until a capella resource is added
+    return CapellaResourceHelper.isCapellaProject(session.getSessionResource());
   }
 
   @Override
