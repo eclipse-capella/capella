ccf109d07f22f740a7b06eaa9af7eac8bbd2ad3c
Philippe DUL <philippe.dul@thalesgroup.com>
2021-05-05 16:54:59 +0200

    #2187 Documentation contains broken links (#2188)
    * #2187 Documentation contains broken links
    
    Change-Id: I05acc0485929b88276fcdd992e947d68afaf037f
    Signed-off-by: Philippe DUL <philippe.dul@thalesgroup.com>
    
    * #2194 External links doesn't open if not open in new tab
    
    Change-Id: I0a400994cbcd00e8a3732bd940a0939f4bd686cd
    Signed-off-by: Philippe DUL <philippe.dul@thalesgroup.com>

--- tests/plugins/org.polarsys.capella.test.doc.ju/src/org/polarsys/capella/test/doc/ju/BrokenLinksCheckTestCase.java
+++ tests/plugins/org.polarsys.capella.test.doc.ju/src/org/polarsys/capella/test/doc/ju/BrokenLinksCheckTestCase.java
@@ -19,8 +19,10 @@ import java.net.URL;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Enumeration;
+import java.util.HashMap;
 import java.util.List;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
@@ -31,6 +33,8 @@ import javax.xml.xpath.XPathExpression;
 import javax.xml.xpath.XPathExpressionException;
 import javax.xml.xpath.XPathFactory;
 
+import org.eclipse.emf.common.util.URI;
+import org.eclipse.help.internal.base.BaseHelpSystem;
 import org.eclipse.help.internal.server.WebappManager;
 import org.eclipse.osgi.util.NLS;
 import org.osgi.framework.Bundle;
@@ -50,7 +54,7 @@ public class BrokenLinksCheckTestCase extends BasicTestCase {
 
   @Override
   public void test() throws Exception {
-    WebappManager.start("help");
+    BaseHelpSystem.ensureWebappRunning();
 
     List<Bundle> docPlugins = new ArrayList<>();
 
@@ -69,19 +73,15 @@ public class BrokenLinksCheckTestCase extends BasicTestCase {
       while (entries.hasMoreElements()) {
         URL htmlURL = entries.nextElement();
         String stringURL = getHelpServer() + docPlugin.getSymbolicName() + htmlURL.getFile().replace(" ", "%20");
-        System.out.println("Analyzing..." + stringURL);
+        System.out.println("Analyzing... " + stringURL);
 
         URL rootURL = new URL(stringURL);
-
         if (!isExluded(htmlURL)) {
           Collection<String> messages = extractBrokenLinkMessages(rootURL);
           errorMessages.addAll(messages);
         }
       }
-
     }
-
-    WebappManager.stop("help");
 
     assertTrue(errorMessages.stream().collect(Collectors.joining("\n")), errorMessages.isEmpty());
   }
@@ -92,22 +92,115 @@ public class BrokenLinksCheckTestCase extends BasicTestCase {
         + WebappManager.getPort() + "/help/topic/";
   }
 
+  public Collection<String> extractBrokenLinkMessages(URL rootUrl) throws IOException {
+
+    Collection<String> errorMessages = new ArrayList<>();
+
+    Collection<Node> linkNodes = getAllNodes(rootUrl);
+
+    // Exclude links that are only for online visualization
+    linkNodes = linkNodes.stream().filter(BrokenLinksCheckTestCase::isOfflineLink).collect(Collectors.toList());
+
+    for (Node linkNode : linkNodes) {
+      String linkText = adaptToURL(linkNode);
+      if (linkText == null) {
+        String errorMessage = NLS.bind("Null link in page {0}", rootUrl.toString());
+        errorMessages.add(errorMessage);
+      }
+    }
+
+    Collection<String> urls = linkNodes.stream().map(x -> adaptToURL(x)).distinct().collect(Collectors.toList());
+    for (String linkText : urls) {
+      linkText = toValidURL(linkText);
+      URL url = new URL(rootUrl, linkText);
+      if (isFakeUrl(url)) {
+        continue;
+      }
+
+      HttpURLConnection linkConnection = (HttpURLConnection) url.openConnection();
+      try {
+        int responseCode = linkConnection.getResponseCode();
+        if (responseCode == 407) {
+          String errorMessage = NLS.bind("Proxy issue for {0} ( {1} ) in page {2} : {3}",
+              new String[] { linkText, url.toString(), rootUrl.toString(), "" + responseCode });
+          errorMessages.add(errorMessage);
+
+        } else if (responseCode != 200) {
+          String errorMessage = NLS.bind("Invalid url {0} ( {1} ) in page {2} : {3}",
+              new String[] { linkText, url.toString(), rootUrl.toString(), "" + responseCode });
+          errorMessages.add(errorMessage);
+        }
+
+      } catch (Exception e) {
+        String errorMessage = NLS.bind("Invalid url {0} ( {1} ) in page {2} : {3}",
+            new String[] { linkText, url.toString(), rootUrl.toString(), e.getMessage() });
+        errorMessages.add(errorMessage);
+
+      } finally {
+        linkConnection.disconnect();
+      }
+    }
+
+    return errorMessages;
+  }
+
+  /**
+   * From a node, retrieve its href
+   */
+  protected String adaptToURL(Node linkNode) {
+    Node href = linkNode.getAttributes().getNamedItem("href");
+    if (href != null) {
+      return href.getTextContent();
+    }
+    Node src = linkNode.getAttributes().getNamedItem("src");
+    if (src != null) {
+      return src.getTextContent();
+    }
+    return null;
+  }
+
+  /**
+   * From a href, return a valid url with correct encoding
+   */
+  protected String toValidURL(String href) {
+    if (href.startsWith("/wiki/../")) {
+      href = href.substring("/wiki/../".length() - 1);
+    }
+    href = URI.createURI(href, true).toString();
+    return href;
+  }
+
+  /**
+   * Returns whether we look links into this bundle
+   */
   protected boolean isDocPlugin(Bundle bundle) {
     return bundle.getSymbolicName().startsWith("org.polarsys.capella") && bundle.getSymbolicName().endsWith("doc");
   }
 
-  public Collection<String> extractBrokenLinkMessages(URL rootUrl) throws IOException {
+  /**
+   * Returns whether the URL is a real http. Some url are known to be fake.
+   */
+  protected boolean isFakeUrl(URL url) {
+    String urlValue = url.toString();
+    if (urlValue.startsWith("http://www.polarsys.org/capella/core/")
+        || urlValue.startsWith("http://www.polarsys.org/capella/common/")
+        || urlValue.startsWith("http://www.polarsys.org/capella/derived")
+        || urlValue.startsWith("https://update-site.url")) {
+      return true;
+    }
+    return false;
+  }
 
-    List<String> errorMessages = new ArrayList<>();
+  /**
+   * Returns all <A> and <IMG>
+   */
+  private Collection<Node> getAllNodes(URL rootUrl) throws IOException {
 
     HttpURLConnection connection = (HttpURLConnection) rootUrl.openConnection();
     connection.setRequestMethod("POST");
     connection.setRequestProperty("ACCEPT", "application/xml");
-    InputStream xmlContent = connection.getInputStream();
 
-    NodeList linkNodes = null;
-
-    try {
+    try (InputStream xmlContent = connection.getInputStream()) {
       // The parser might try to download the DTD in order to validate the parsed XML, we do not need to.
       DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
       factory.setValidating(false);
@@ -118,35 +211,29 @@ public class BrokenLinksCheckTestCase extends BasicTestCase {
 
       XPathFactory pathFactory = XPathFactory.newInstance();
       XPath path = pathFactory.newXPath();
-      XPathExpression linkExpression = path.compile("//a/@href");
-      linkNodes = (NodeList) linkExpression.evaluate(document, XPathConstants.NODESET);
+      XPathExpression links = path.compile("//a | //img");
+      return toList((NodeList) links.evaluate(document, XPathConstants.NODESET));
 
     } catch (ParserConfigurationException | SAXException | IOException | XPathExpressionException e) {
       String errorMessage = NLS.bind("Could not process resource {0}. {1}", rootUrl.toString(), e.getMessage());
-      errorMessages.add(errorMessage);
+      throw new IOException(errorMessage);
     }
+  }
 
-    if (linkNodes != null) {
-
-      for (int i = 0; i < linkNodes.getLength(); i++) {
-        Node linkNode = linkNodes.item(i);
-        String linkText = linkNode.getTextContent();
-
-        URL absoluteUrl = new URL(rootUrl, linkText);
-        System.out.println("Checking link: " + linkText);
-
-        HttpURLConnection linkConnection = (HttpURLConnection) rootUrl.openConnection();
-
-        int responseCode = linkConnection.getResponseCode();
-        if (responseCode != 200) {
-          String errorMessage = NLS.bind("Invalid url {0} in page {1} ", absoluteUrl, rootUrl);
-          errorMessages.add(errorMessage);
-        }
-
+  protected Collection<Node> toList(NodeList list) {
+    Collection<Node> result = new ArrayList<Node>();
+    if (list != null) {
+      for (int i = 0; i < list.getLength(); i++) {
+        Node node = list.item(i);
+        result.add(node);
       }
     }
+    return result;
+  }
 
-    return errorMessages;
+  protected static boolean isOfflineLink(Node linkNode) {
+    return !(linkNode.getParentNode() != null && linkNode.getParentNode().getNodeName() != null
+        && linkNode.getParentNode().getNodeName().equals("span"));
   }
 
   protected boolean isExluded(URL htmlURL) {
