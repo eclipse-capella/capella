551dbb6a6fef32057e8c721d4c1b9ca3565461f4
Arnaud Dieumegard <arnaud.dieumegard@obeo.fr>
2021-09-13 16:46:00 +0200

    [releng] Bump to 5.2
    - Bump to 5.2
    - Update splash screen
    - Enable migration from 5.1.x towards 5.2.0
    - Migrate test models to 5.2.0
    
    Change-Id: Ia1764c185e4caedeb518871199f7a9153cbdd22b

--- core/plugins/org.polarsys.capella.core.data.migration/src/org/polarsys/capella/core/data/migration/af/ViewpointMigrationContribution.java
+++ core/plugins/org.polarsys.capella.core.data.migration/src/org/polarsys/capella/core/data/migration/af/ViewpointMigrationContribution.java
@@ -205,8 +205,6 @@ public class ViewpointMigrationContribution extends AbstractMigrationContributio
       }
     }
 
-    // Allow migration from 1.4.x to 5.1.x
-    return fileVersion.getMajor() == 1 && fileVersion.getMinor() == 4 && currentVersion.getMajor() == 5
-        && currentVersion.getMinor() == 1;
+    return false;
   }
 }
