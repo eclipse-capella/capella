0a25c7802c3c8cba44ef134d0f8f12225f95ac36
Laurent Fasani <laurent.fasani@obeo.fr>
2021-11-15 15:37:18 +0100

    #2269 Enhance the Capella rename participant for images path change
    It changes the path to image in the cases the image is used for a node
    in a diagram or in an attribute containing rich text.
    
    Change-Id: I43ee77467c629c2bd61652e62ddeb036d6233c60
    Signed-off-by: Laurent Fasani <laurent.fasani@obeo.fr>

--- core/plugins/org.polarsys.capella.core.sirius.ui/src/org/polarsys/capella/core/sirius/ui/refactoring/WorkspaceImagePathChange.java
+++ core/plugins/org.polarsys.capella.core.sirius.ui/src/org/polarsys/capella/core/sirius/ui/refactoring/WorkspaceImagePathChange.java
@@ -14,6 +14,10 @@ package org.polarsys.capella.core.sirius.ui.refactoring;
 
 import java.util.Collection;
 import java.util.Iterator;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import java.util.stream.Collectors;
 
 import org.eclipse.core.resources.IContainer;
 import org.eclipse.core.resources.IFile;
@@ -22,12 +26,19 @@ import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.IPath;
 import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.core.runtime.Path;
+import org.eclipse.emf.common.util.TreeIterator;
+import org.eclipse.emf.ecore.EAttribute;
 import org.eclipse.emf.ecore.EObject;
+import org.eclipse.emf.ecore.EStructuralFeature;
+import org.eclipse.emf.ecore.resource.Resource;
 import org.eclipse.ltk.core.refactoring.Change;
 import org.eclipse.ltk.core.refactoring.RefactoringStatus;
 import org.eclipse.ltk.core.refactoring.participants.RenameArguments;
 import org.eclipse.osgi.util.NLS;
 import org.eclipse.sirius.business.api.dialect.DialectManager;
+import org.eclipse.sirius.business.api.image.ImageManager;
+import org.eclipse.sirius.business.api.image.RichTextAttributeRegistry;
+import org.eclipse.sirius.business.api.query.ResourceQuery;
 import org.eclipse.sirius.business.api.session.Session;
 import org.eclipse.sirius.business.api.session.SessionManager;
 import org.eclipse.sirius.diagram.WorkspaceImage;
@@ -42,11 +53,13 @@ import org.polarsys.capella.common.helpers.EcoreUtil2;
 import org.polarsys.capella.common.helpers.TransactionHelper;
 import org.polarsys.capella.core.data.capellamodeller.Project;
 import org.polarsys.capella.core.data.capellamodeller.SystemEngineering;
+import org.polarsys.capella.core.model.handler.command.CapellaResourceHelper;
 import org.polarsys.capella.core.model.helpers.SystemEngineeringExt;
 import org.polarsys.capella.core.sirius.ui.helper.ResourceHelper;
 import org.polarsys.capella.core.sirius.ui.helper.SessionHelper;
 
 public class WorkspaceImagePathChange extends Change {
+  private static final String QUOTE = "\"";
 
   /**
    * The hierarchical position of the renamed container relative to the workspace root. <br>
@@ -121,15 +134,8 @@ public class WorkspaceImagePathChange extends Change {
         public void run() {
           if (renameArguments.getUpdateReferences()
               && (position > 0 || position == 0 && !ResourceHelper.collectImageFiles(container).isEmpty())) {
-            // Get all diagrams from the session
-            pm.subTask(org.polarsys.capella.core.sirius.ui.internal.Messages.collecting_diagrams);
-            Collection<DRepresentationDescriptor> descriptors = DialectManager.INSTANCE.getAllRepresentationDescriptors(session);
-            // Update the workspace images for each diagram
-            for (DRepresentationDescriptor descriptor : descriptors) {
-              pm.subTask(
-                  NLS.bind(org.polarsys.capella.core.sirius.ui.internal.Messages.updating_diagram, descriptor.getName()));
-              updateWorkspaceImagePath(descriptor.getRepresentation());
-            }
+            updateImagePath(session, pm);
+
           }
           // If it's an IProject rename => rename Capella model objects: Project & SystemEngineering
           if (position == 0) {
@@ -141,7 +147,73 @@ public class WorkspaceImagePathChange extends Change {
       saveSession(session, pm);
     }
   }
-  
+
+  private void updateImagePath(Session session, IProgressMonitor pm) {
+
+    updateWorkspaceImage(session, pm);
+
+    updateRichTextString(session, pm);
+
+  }
+
+  private void updateRichTextString(Session session, IProgressMonitor pm) {
+    pm.subTask(org.polarsys.capella.core.sirius.ui.internal.Messages.updating_richtext_description);
+    Collection<Resource> semanticResources = session.getSemanticResources();
+    for (Resource resource : semanticResources) {
+      if (CapellaResourceHelper.isCapellaResource(resource) || new ResourceQuery(resource).isAirdOrSrmResource()) {
+        TreeIterator<EObject> allContents = resource.getAllContents();
+        while (allContents.hasNext()) {
+          EObject eObject = allContents.next();
+
+          List<EAttribute> featuresToMigrate = eObject.eClass().getEAllAttributes().stream().filter(feature -> {
+            return RichTextAttributeRegistry.INSTANCE.getEAttributes().contains(feature);
+          }).collect(Collectors.toList());
+
+          for (EStructuralFeature attr : featuresToMigrate) {
+            Object value = eObject.eGet(attr);
+            if (value instanceof String) {
+              String textWithOriginalImagePaths = (String) value;
+              String textWithUpdatedImagePaths = textWithOriginalImagePaths;
+              Pattern pattern = Pattern.compile(ImageManager.HTML_IMAGE_PATH_PATTERN);
+              Matcher matcher = pattern.matcher(textWithOriginalImagePaths);
+
+              boolean stringUpdate = false;
+              while (matcher.find()) {
+                String originalPath = matcher.group(1);
+                if (!originalPath.startsWith("/")) {
+                  String newPath = getNewPath(originalPath);
+                  if (!newPath.equals(originalPath)) {
+                    stringUpdate = true;
+                    // Use quote as start and end character to be sure to replace the path as a whole and not only a
+                    // part of the path
+                    textWithUpdatedImagePaths = textWithUpdatedImagePaths.replace(QUOTE + originalPath + QUOTE,
+                        QUOTE + newPath + QUOTE);
+                  }
+                }
+              }
+              if (stringUpdate) {
+                eObject.eSet(attr, textWithUpdatedImagePaths);
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+
+  private void updateWorkspaceImage(Session session, IProgressMonitor pm) {
+    // Get all diagrams from the session
+    pm.subTask(org.polarsys.capella.core.sirius.ui.internal.Messages.collecting_diagrams);
+    Collection<DRepresentationDescriptor> descriptors = DialectManager.INSTANCE
+        .getAllRepresentationDescriptors(session);
+    // Update the workspace images for each diagram
+    for (DRepresentationDescriptor descriptor : descriptors) {
+      pm.subTask(
+          NLS.bind(org.polarsys.capella.core.sirius.ui.internal.Messages.updating_diagram, descriptor.getName()));
+      updateWorkspaceImagePath(descriptor.getRepresentation());
+    }
+  }
+
   private void renameRelevantCapellaObjects(Session session) {
     Project capellaProject = SessionHelper.getCapellaProject(session);
     if (capellaProject != null) {
@@ -166,22 +238,26 @@ public class WorkspaceImagePathChange extends Change {
         while (iterator.hasNext()) {
           EObject eObject = iterator.next();
           if (ResourceHelper.isCustomizedWorkspaceImageWorkspacePath(eObject)) {
-            updatePath((WorkspaceImage) eObject);
+            String workspacePath = ((WorkspaceImage) eObject).getWorkspacePath();
+            String newPath = getNewPath(workspacePath);
+            if (!newPath.equals(workspacePath)) {
+              ((WorkspaceImage) eObject).setWorkspacePath(newPath);
+            }
           }
         }
       }
     }
   }
 
-  private void updatePath(WorkspaceImage workspaceImage) {
-    String workspacePath = workspaceImage.getWorkspacePath();
-    Path path = new Path(workspacePath);
+  private String getNewPath(String oldPath) {
+    String newPath = oldPath;
+    Path path = new Path(oldPath);
     String segment = path.segment(position);
     String oldName = container.getName();
     if (oldName.equals(segment)) {
-      workspacePath = workspacePath.replace(oldName, renameArguments.getNewName());
-      workspaceImage.setWorkspacePath(workspacePath);
+      newPath = oldPath.replace(oldName, renameArguments.getNewName());
     }
+    return newPath;
   }
 
   private void openSession(IProject project, IProgressMonitor monitor) {
