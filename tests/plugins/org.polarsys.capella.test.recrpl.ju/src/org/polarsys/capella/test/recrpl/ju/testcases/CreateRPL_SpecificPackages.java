df4525afd6de1aeb691dab6791f8316dff1d2541
Arnaud Dieumegard <arnaud.dieumegard@obeo.fr>
2022-01-26 14:50:50 +0100

    #2310 Fix RECRPL tests
    Wrong InstanceNNode property node was used in refactoring

--- tests/plugins/org.polarsys.capella.test.recrpl.ju/src/org/polarsys/capella/test/recrpl/ju/testcases/CreateRPL_SpecificPackages.java
+++ tests/plugins/org.polarsys.capella.test.recrpl.ju/src/org/polarsys/capella/test/recrpl/ju/testcases/CreateRPL_SpecificPackages.java
@@ -26,6 +26,7 @@ import org.osgi.framework.FrameworkUtil;
 import org.polarsys.capella.common.ef.ExecutionManager;
 import org.polarsys.capella.common.ef.ExecutionManagerRegistry;
 import org.polarsys.capella.common.helpers.EObjectExt;
+import org.polarsys.capella.common.re.Activator;
 import org.polarsys.capella.common.re.CatalogElement;
 import org.polarsys.capella.common.re.CatalogElementLink;
 import org.polarsys.capella.common.re.RePackage;
@@ -116,7 +117,7 @@ public abstract class CreateRPL_SpecificPackages extends RecRplTestCase {
 
     super.setUp();
     
-    InstanceScope.INSTANCE.getNode(FrameworkUtil.getBundle(CatalogElement.class).getSymbolicName()).put(IReConstants.PROPERTY__PARENT_LOCATOR, IReConstants.LOCATOR_OPTION_SPECIFIC_PACKAGES);
+    InstanceScope.INSTANCE.getNode(FrameworkUtil.getBundle(Activator.class).getSymbolicName()).put(IReConstants.PROPERTY__PARENT_LOCATOR, IReConstants.LOCATOR_OPTION_SPECIFIC_PACKAGES);
 
     manager = ExecutionManagerRegistry.getInstance().addNewManager();
     project = new CapellaModelSkeleton.Builder(manager).build();
@@ -134,7 +135,7 @@ public abstract class CreateRPL_SpecificPackages extends RecRplTestCase {
   public void tearDown() throws Exception {
     ExecutionManagerRegistry.getInstance().removeManager(manager);
 
-    InstanceScope.INSTANCE.getNode(FrameworkUtil.getBundle(CatalogElement.class).getSymbolicName()).put(IReConstants.PROPERTY__PARENT_LOCATOR, IReConstants.LOCATOR_OPTION_DEFAULT);
+    InstanceScope.INSTANCE.getNode(FrameworkUtil.getBundle(Activator.class).getSymbolicName()).put(IReConstants.PROPERTY__PARENT_LOCATOR, IReConstants.LOCATOR_OPTION_DEFAULT);
 
     super.tearDown();
   }
