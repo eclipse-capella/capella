9a2c3ce8fed141778e1ecaedb23f1f8aa4e7aa26
Arnaud Dieumegard <arnaud.dieumegard@obeo.fr>
2022-03-10 12:00:53 +0100

    #2351 Add dedicated OperationalProcess queries
    - Add OperationalProcess queries for
      - Involved OperationaActivities and Interactions
      - Starting/Ending OperationalActivites
    - Correct FunctionalChains related queries names
    - Udpate copyrights
    
    Change-Id: Ie0354d337b16c38f6a9a088248ee218fbf8f5477
    Signed-off-by: Arnaud Dieumegard <arnaud.dieumegard@obeo.fr>

--- core/plugins/org.polarsys.capella.core.semantic.queries/src/org/polarsys/capella/core/semantic/queries/basic/queries/FunctionalChainFlatFunctions.java
+++ core/plugins/org.polarsys.capella.core.semantic.queries/src/org/polarsys/capella/core/semantic/queries/basic/queries/FunctionalChainFlatFunctions.java
@@ -1,24 +1,38 @@
 /*******************************************************************************
- * This program is the Confidential and Proprietary product of Thales Global   *
- * Services. Any unauthorized use, reproduction or transfer of this program is *
- * strictly prohibited.                                                        *
- *                                                                             *
- * Copyright (c) 2022 Thales Global Services. All Rights Reserved.             *
- * *****************************************************************************/
+ * Copyright (c) 2022 THALES GLOBAL SERVICES.
+ * 
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0
+ * 
+ * SPDX-License-Identifier: EPL-2.0
+ * 
+ * Contributors:
+ *    Thales - initial API and implementation
+ *******************************************************************************/
 package org.polarsys.capella.core.semantic.queries.basic.queries;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 import org.polarsys.capella.common.helpers.query.IQuery;
 import org.polarsys.capella.core.data.fa.FunctionalChain;
+import org.polarsys.capella.core.data.oa.OperationalProcess;
 import org.polarsys.capella.core.model.helpers.FunctionalChainExt;
 
 public class FunctionalChainFlatFunctions implements IQuery {
 
-  @Override
-  public List<Object> compute(Object object) {
-    return new ArrayList(FunctionalChainExt.getFlatFunctions((FunctionalChain) object));
-  }
+    @Override
+    public List<Object> compute(Object object) {
+        if (isValidInstanceOf(object)) {
+            return new ArrayList<>(FunctionalChainExt.getFlatFunctions((FunctionalChain) object));
+        }
+        return Collections.emptyList();
+    }
+
+    protected boolean isValidInstanceOf(Object object) {
+        return !(object instanceof OperationalProcess);
+    }
 
 }
