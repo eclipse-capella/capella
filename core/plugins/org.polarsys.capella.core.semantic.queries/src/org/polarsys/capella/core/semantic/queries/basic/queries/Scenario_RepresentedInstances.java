de76dfe4c85abc511032e85bb8918dddfbc39023
Arnaud Dieumegard <arnaud.dieumegard@obeo.fr>
2022-03-14 11:27:57 +0100

    #2354 Add Scenario-related queries for semantic browser
    - Related Functions / OperationalActivities
    - Invoked Messages
    - Related States
    - RepresentedInstances
    
    Add declaration in plugin.xml
    
    Change-Id: I0000000000000000000000000000000000000000
    Signed-off-by: Arnaud Dieumegard <arnaud.dieumegard@obeo.fr>

---
+++ core/plugins/org.polarsys.capella.core.semantic.queries/src/org/polarsys/capella/core/semantic/queries/basic/queries/Scenario_RepresentedInstances.java
@@ -0,0 +1,66 @@
+/*******************************************************************************
+ * Copyright (c) 2006, 2020 THALES GLOBAL SERVICES.
+ * 
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0
+ * 
+ * SPDX-License-Identifier: EPL-2.0
+ * 
+ * Contributors:
+ *    Thales - initial API and implementation
+ *******************************************************************************/
+
+package org.polarsys.capella.core.semantic.queries.basic.queries;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.polarsys.capella.common.data.modellingcore.AbstractType;
+import org.polarsys.capella.common.helpers.query.IQuery;
+import org.polarsys.capella.core.data.cs.Part;
+import org.polarsys.capella.core.data.information.AbstractInstance;
+import org.polarsys.capella.core.data.interaction.InstanceRole;
+import org.polarsys.capella.core.data.interaction.Scenario;
+import org.polarsys.capella.core.model.handler.helpers.CapellaProjectHelper;
+import org.polarsys.capella.core.model.handler.helpers.CapellaProjectHelper.TriStateBoolean;
+
+/**
+ *
+ */
+public class Scenario_RepresentedInstances implements IQuery {
+
+    public Scenario_RepresentedInstances() {
+        // Do nothing
+    }
+
+    /**
+     * @see org.polarsys.capella.common.helpers.query.IQuery#compute(java.lang.Object)
+     */
+    public List<Object> compute(Object object) {
+        List<Object> result = new ArrayList<>();
+        if (object instanceof Scenario) {
+            Scenario af = (Scenario) object;
+            boolean reusableComponentsDriven = false;
+            boolean reusableComponentsDrivenChecked = false;
+            for (InstanceRole role : af.getOwnedInstanceRoles()) {
+                AbstractInstance representedInstance = role.getRepresentedInstance();
+
+                if (representedInstance != null) {
+                    AbstractType abstractType = representedInstance.getAbstractType();
+                    // First time we get an abstract type, check for ReusableComponents configuration
+                    if (abstractType != null && !reusableComponentsDrivenChecked && representedInstance instanceof Part) {
+                        reusableComponentsDriven = CapellaProjectHelper.isReusableComponentsDriven(af).equals(TriStateBoolean.True);
+                        reusableComponentsDrivenChecked = true;
+                    }
+                    if (abstractType != null && !reusableComponentsDriven) {
+                        result.add(abstractType);
+                    } else {
+                        result.add(representedInstance);
+                    }
+                }
+            }
+        }
+        return result;
+    }
+}
