930fa0841eb626ed6451ebae6e1a16bbab49b9ca
Arnaud Dieumegard <arnaud.dieumegard@obeo.fr>
2022-03-15 17:03:08 +0100

    #2357 Add sub queries for sources and targets of exchanges
    Added sub queries (reused existing query)
    - CommunicationMeans "Allocated Interactions" source and target (Connected Operational Activities)
    - ComponentExchanges "Allocated Functional Exchanges" sources and target (Connected Functions)
    - ComponentExchanges "Allocating Physical Link" source and target (Physical Link Source and
      Target)
    - FunctionalExchange "Allocating Communication Mean" source and target
      (Connected Entities)
    - FunctionalExchange "Allocating Component Exchange" source and target
      (Source And Target Components)
    - PhysicalLink "Allocated Component Exchanges" source and target (Source
      and Target Components)
    
    New queries:
    - CommunicationMeans to "Connected Entities"
    - FunctionalExchanges to "Connected Functions" and "Connected Operational Activities"
    
    Rename query "Physical Link Ends" to "Connected Node Components".
    
    Change-Id: I0000000000000000000000000000000000000000
    Signed-off-by: Arnaud Dieumegard <arnaud.dieumegard@obeo.fr>

---
+++ core/plugins/org.polarsys.capella.core.semantic.queries/src/org/polarsys/capella/core/semantic/queries/basic/queries/Connection_connectedEntities.java
@@ -0,0 +1,55 @@
+/*******************************************************************************
+ * Copyright (c) 2022 THALES GLOBAL SERVICES.
+ * 
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0
+ * 
+ * SPDX-License-Identifier: EPL-2.0
+ * 
+ * Contributors:
+ *    Thales - initial API and implementation
+ *******************************************************************************/
+
+package org.polarsys.capella.core.semantic.queries.basic.queries;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.eclipse.emf.ecore.EObject;
+import org.polarsys.capella.common.helpers.query.IQuery;
+import org.polarsys.capella.core.data.oa.CommunicationMean;
+
+/**
+ *
+ */
+public class Connection_connectedEntities implements IQuery {
+
+	/** 
+	 * 
+	 */
+	public Connection_connectedEntities() {
+	  // do nothing
+	}
+
+	/**
+	 * 
+	 * ownedCommunicationEnds.connectedPart(select "Standard Port").ownerElement (select "Physical Component" AND "not current Component") 
+	 * 
+	 * @see org.polarsys.capella.common.helpers.query.IQuery#compute(java.lang.Object)
+	 */
+    public List<Object> compute(Object object) {
+        List<Object> result = new ArrayList<Object>();
+        if (object instanceof CommunicationMean) {
+            CommunicationMean comm = (CommunicationMean) object;
+            EObject source = comm.getSourceEntity();
+            EObject target = comm.getTargetEntity();
+
+            if (source != null)
+                result.add(source);
+            if (target != null)
+                result.add(target);
+        }
+        return result;
+    }
+}
---
+++ core/plugins/org.polarsys.capella.core.semantic.queries/src/org/polarsys/capella/core/semantic/queries/basic/queries/Connection_connectedFunctions.java
@@ -0,0 +1,62 @@
+/*******************************************************************************
+ * Copyright (c) 2022 THALES GLOBAL SERVICES.
+ * 
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0
+ * 
+ * SPDX-License-Identifier: EPL-2.0
+ * 
+ * Contributors:
+ *    Thales - initial API and implementation
+ *******************************************************************************/
+
+package org.polarsys.capella.core.semantic.queries.basic.queries;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.eclipse.emf.ecore.EObject;
+import org.polarsys.capella.common.helpers.query.IQuery;
+import org.polarsys.capella.core.data.fa.FunctionalExchange;
+import org.polarsys.capella.core.data.oa.OperationalAnalysis;
+import org.polarsys.capella.core.model.helpers.BlockArchitectureExt;
+import org.polarsys.capella.core.model.helpers.FunctionalExchangeExt;
+
+/**
+ *
+ */
+public class Connection_connectedFunctions implements IQuery {
+
+	/** 
+	 * 
+	 */
+	public Connection_connectedFunctions() {
+	  // do nothing
+	}
+
+	/**
+	 * 
+	 * ownedCommunicationEnds.connectedPart(select "Standard Port").ownerElement (select "Physical Component" AND "not current Component") 
+	 * 
+	 * @see org.polarsys.capella.common.helpers.query.IQuery#compute(java.lang.Object)
+	 */
+	public List<Object> compute(Object object) {
+		List<Object> result = new ArrayList<Object>();
+		if (isValidInstanceOf(object)) {
+            FunctionalExchange fun = (FunctionalExchange) object;
+            EObject source = FunctionalExchangeExt.getSourceFunction(fun);
+            EObject target = FunctionalExchangeExt.getTargetFunction(fun);
+
+            if (source != null)
+                result.add(source);
+            if (target != null)
+                result.add(target);
+        }
+        return result;
+    }
+
+	protected boolean isValidInstanceOf(Object exchange) {
+        return (exchange instanceof FunctionalExchange) && !(BlockArchitectureExt.getRootBlockArchitecture((FunctionalExchange) exchange) instanceof OperationalAnalysis);
+    }
+}
