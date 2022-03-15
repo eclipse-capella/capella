1d01c0ab9a320ecc3c6e8ad93aa6e96bcf391b2b
Arnaud Dieumegard <arnaud.dieumegard@obeo.fr>
2022-03-15 17:03:08 +0100

    #2357 Add sub queries for sources and targets of exchanges
    Added sub queries (reused existing query)
    - CommunicationMeans "Allocated Interactions" source and target (Connected Operational Activities)
    - ComponentExchanges "Allocated Functional Exchanges" sources and target (Connected Functions)
    - ComponentExchanges "Allocating Physical Link" source and target (Physical Link Source and
      Target)
    - FunctionalExchange "Allocating Communication Mean" source and target
      (Connected Entities)
    - FunctionalExchange "Allocating Component Exchange" source and target
      (Source And Target Components)
    - PhysicalLink "Allocated Component Exchanges" source and target (Source
      and Target Components)
    
    New queries:
    - CommunicationMeans to "Connected Entities"
    - FunctionalExchanges to "Connected Functions" and "Connected Operational Activities"
    
    Rename query "Physical Link Ends" to "Connected Node Components".
    
    Change-Id: I0000000000000000000000000000000000000000
    Signed-off-by: Arnaud Dieumegard <arnaud.dieumegard@obeo.fr>

---
+++ core/plugins/org.polarsys.capella.core.semantic.queries/src/org/polarsys/capella/core/semantic/queries/basic/queries/Connection_connectedOperationalActivities.java
@@ -0,0 +1,36 @@
+/*******************************************************************************
+ * Copyright (c) 2022 THALES GLOBAL SERVICES.
+ * 
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0
+ * 
+ * SPDX-License-Identifier: EPL-2.0
+ * 
+ * Contributors:
+ *    Thales - initial API and implementation
+ *******************************************************************************/
+
+package org.polarsys.capella.core.semantic.queries.basic.queries;
+
+import org.polarsys.capella.core.data.fa.FunctionalExchange;
+import org.polarsys.capella.core.data.oa.OperationalAnalysis;
+import org.polarsys.capella.core.model.helpers.BlockArchitectureExt;
+
+/**
+ *
+ */
+public class Connection_connectedOperationalActivities extends Connection_connectedFunctions {
+
+	/** 
+	 * 
+	 */
+	public Connection_connectedOperationalActivities() {
+	  // do nothing
+	}
+
+    @Override
+    protected boolean isValidInstanceOf(Object exchange) {
+        return (exchange instanceof FunctionalExchange) && (BlockArchitectureExt.getRootBlockArchitecture((FunctionalExchange) exchange) instanceof OperationalAnalysis);
+    }
+}
