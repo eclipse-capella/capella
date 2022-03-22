cf510a4a1428119595e171e32adfff84505b44df
Arnaud Dieumegard <arnaud.dieumegard@obeo.fr>
2022-03-22 17:07:13 +0100

    #2363 Migrate test models
    Migrate test models to 6.0.0 and ensure png to svg migration has been
    applied.
    Duplicate migration tests to include both 1.4.x and 5.2.0 to 6.0.0.
    Delete not-used org.polarsys.capella.test.odesign/model/EmptyProject
    project.
    Updated migration.sysmodel.aird.linux.layout
    
    Change-Id: I0000000000000000000000000000000000000000
    Signed-off-by: Arnaud Dieumegard <arnaud.dieumegard@obeo.fr>

---
+++ tests/plugins/org.polarsys.capella.test.migration.ju/model/representationDescription-5_2_0/README.txt
@@ -0,0 +1,2 @@
+This model is used during a migration test.
+If the current major version of the product is X, then this model should be at version X-1.
---
+++ tests/plugins/org.polarsys.capella.test.migration.ju/src/org/polarsys/capella/test/migration/ju/testcases/basic/InvalidAirdReferenceMigrationV5Test.java
@@ -0,0 +1,19 @@
+/*******************************************************************************
+ * Copyright (c) 2022 THALES GLOBAL SERVICES.
+ * 
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0
+ * 
+ * SPDX-License-Identifier: EPL-2.0
+ * 
+ * Contributors:
+ *    Thales - initial API and implementation
+ *******************************************************************************/
+package org.polarsys.capella.test.migration.ju.testcases.basic;
+
+public class InvalidAirdReferenceMigrationV5Test extends InvalidAirdReferenceMigrationTest {
+    
+    protected static String SOURCE_MODEL = "InvalidAirdElement-5_2_0";
+    
+}
