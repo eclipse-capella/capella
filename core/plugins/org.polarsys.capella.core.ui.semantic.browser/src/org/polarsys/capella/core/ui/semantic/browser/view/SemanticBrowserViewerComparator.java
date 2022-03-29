26904124ebc05dfe970169aeb69ba563ca4ab1b6
Arnaud Dieumegard <arnaud.dieumegard@obeo.fr>
2022-03-29 09:01:50 +0200

    #2371 Add copyrights and comments
    Add missed copyrights and licenses.
    Add some comments on API methods.
    
    Change-Id: I0000000000000000000000000000000000000000
    Signed-off-by: Arnaud Dieumegard <arnaud.dieumegard@obeo.fr>

--- core/plugins/org.polarsys.capella.core.ui.semantic.browser/src/org/polarsys/capella/core/ui/semantic/browser/view/SemanticBrowserViewerComparator.java
+++ core/plugins/org.polarsys.capella.core.ui.semantic.browser/src/org/polarsys/capella/core/ui/semantic/browser/view/SemanticBrowserViewerComparator.java
@@ -1,3 +1,15 @@
+/*******************************************************************************
+ * Copyright (c) 2022 THALES GLOBAL SERVICES.
+ * 
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0
+ * 
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ * Contributors:
+ *    Thales - initial API and implementation
+ *******************************************************************************/
 package org.polarsys.capella.core.ui.semantic.browser.view;
 
 import java.util.Arrays;
@@ -8,10 +20,14 @@ import org.eclipse.jface.viewers.ViewerComparator;
 import org.polarsys.capella.common.ui.toolkit.browser.content.provider.wrapper.EObjectWrapper;
 import org.polarsys.capella.common.ui.toolkit.browser.model.ISemanticBrowserModel;
 
+/**
+ * When SemanticBrowser LexicographicSortTree option is not active and the element to sort are {@code EObjectWrapper}s
+ * then this comparator does nothing to prevent the default {@code ViewerComparator} comparison (lexicographic).
+ */
 public class SemanticBrowserViewerComparator extends ViewerComparator {
 
     ISemanticBrowserModel model;
-    
+
     public SemanticBrowserViewerComparator(ISemanticBrowserModel model) {
         this.model = model;
     }
@@ -26,5 +42,5 @@ public class SemanticBrowserViewerComparator extends ViewerComparator {
         }
         super.sort(viewer, elements);
     }
-    
+
 }
