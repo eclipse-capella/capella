0b131c02a1a0cb23b0d87b66ca7f0c23b683e279
Erwann Traisnel <erwann.traisnel@obeo.fr>
2022-09-07 12:11:19 +0200

    #2452 Fixed Double Click behavior in Semantic Browser
    Double click wasn't properly managed in "Referenced" and "Referencing"
    viewers
    
    Fixed opening representations on double click in "Referenced" and
    "Referencing" viewers
    
    Change-Id: Iaa95ff4f7b783a90fb32e83c77e84ae6e2490c58
    Signed-off-by: Erwann Traisnel <erwann.traisnel@obeo.fr>

---
+++ tests/plugins/org.polarsys.capella.test.semantic.ui.ju/src/org/polarsys/capella/test/semantic/ui/ju/TestSemanticUIPlugin.java
@@ -0,0 +1,58 @@
+/*******************************************************************************
+ * Copyright (c) 2016, 2022 THALES GLOBAL SERVICES.
+ * 
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0
+ * 
+ * SPDX-License-Identifier: EPL-2.0
+ * 
+ * Contributors:
+ *    Thales Global Services - initial API and implementation
+ *******************************************************************************/
+package org.polarsys.capella.test.semantic.ui.ju;
+
+import org.eclipse.core.runtime.Plugin;
+import org.osgi.framework.BundleContext;
+
+/**
+ * 
+ * Activator
+ *
+ */
+public class TestSemanticUIPlugin extends Plugin {
+  /**
+   * The plug-in ID 
+   */
+  public static final String PLUGIN_ID = "org.polarsys.capella.test.semantic.ui.ju"; //$NON-NLS-1$
+  /**
+   * The shared instance 
+   */
+  private static TestSemanticUIPlugin plugin;
+
+  /**
+   * {@inheritDoc}
+   */
+  @Override
+  public void start(BundleContext context) throws Exception {
+    super.start(context);
+    plugin = this;
+  }
+
+  /**
+   * {@inheritDoc}
+   */
+  @Override
+  public void stop(BundleContext context) throws Exception {
+    plugin = null;
+    super.stop(context);
+  }
+
+  /**
+   * Returns the shared instance
+   * @return the shared instance
+   */
+  public static TestSemanticUIPlugin getDefault() {
+    return plugin;
+  }
+}
