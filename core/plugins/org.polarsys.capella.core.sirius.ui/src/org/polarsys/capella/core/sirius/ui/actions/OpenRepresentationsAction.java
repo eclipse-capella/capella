3529fe8162d8cf59b6f77ca16f3979f557e49688
Erwann Traisnel <erwann.traisnel@obeo.fr>
2022-09-21 16:00:29 +0200

    #2483 Fixed missing icons in Diagram creation menu
    Change-Id: I32d06d89a9ed4a904d369e89ca9cc8219914596f
    Signed-off-by: Erwann Traisnel <erwann.traisnel@obeo.fr>

--- core/plugins/org.polarsys.capella.core.sirius.ui/src/org/polarsys/capella/core/sirius/ui/actions/OpenRepresentationsAction.java
+++ core/plugins/org.polarsys.capella.core.sirius.ui/src/org/polarsys/capella/core/sirius/ui/actions/OpenRepresentationsAction.java
@@ -24,16 +24,10 @@ import org.eclipse.jface.operation.IRunnableWithProgress;
 import org.eclipse.jface.resource.ImageDescriptor;
 import org.eclipse.jface.viewers.IStructuredSelection;
 import org.eclipse.sirius.common.tools.api.query.IllegalStateExceptionQuery;
-import org.eclipse.sirius.diagram.sequence.description.SequenceDiagramDescription;
-import org.eclipse.sirius.diagram.ui.provider.DiagramUIPlugin;
-import org.eclipse.sirius.table.metamodel.table.description.CrossTableDescription;
-import org.eclipse.sirius.table.metamodel.table.description.EditionTableDescription;
-import org.eclipse.sirius.table.metamodel.table.provider.TableUIPlugin;
 import org.eclipse.sirius.viewpoint.DRepresentationDescriptor;
 import org.eclipse.sirius.viewpoint.description.RepresentationDescription;
 import org.eclipse.swt.widgets.Display;
 import org.eclipse.ui.actions.BaseSelectionListenerAction;
-import org.eclipse.ui.plugin.AbstractUIPlugin;
 import org.polarsys.capella.common.mdsofa.common.constant.ICommonConstants;
 import org.polarsys.capella.common.tools.report.EmbeddedMessage;
 import org.polarsys.capella.common.tools.report.appenders.usage.UsageMonitoringLogger;
@@ -41,6 +35,7 @@ import org.polarsys.capella.common.tools.report.appenders.usage.util.UsageMonito
 import org.polarsys.capella.common.tools.report.config.registry.ReportManagerRegistry;
 import org.polarsys.capella.common.tools.report.util.IReportManagerDefaultComponents;
 import org.polarsys.capella.core.model.handler.helpers.RepresentationHelper;
+import org.polarsys.capella.core.sirius.ui.helper.DescriptionImageDescriptorHelper;
 import org.polarsys.capella.core.sirius.ui.runnable.OpenRepresentationsRunnable;
 
 /**
@@ -69,31 +64,13 @@ public class OpenRepresentationsAction extends BaseSelectionListenerAction {
     this.descriptor = descriptor;
 
     RepresentationDescription description = descriptor.getDescription();
-    ImageDescriptor imageDescriptor = null;
-    // Handle specific representations : Table ones.
-    if (description instanceof CrossTableDescription) {
-      imageDescriptor = AbstractUIPlugin.imageDescriptorFromPlugin(TableUIPlugin.ID,
-          "/icons/full/obj16/CrossTableDescription.gif"); //$NON-NLS-1$
-    } else if (description instanceof EditionTableDescription) {
-      imageDescriptor = AbstractUIPlugin.imageDescriptorFromPlugin(TableUIPlugin.ID, "/icons/full/obj16/DTable.gif"); //$NON-NLS-1$
-    } else if (description instanceof SequenceDiagramDescription) {
-      imageDescriptor = AbstractUIPlugin.imageDescriptorFromPlugin("org.eclipse.sirius.diagram.sequence.edit", //$NON-NLS-1$
-          "/icons/full/obj16/TSequenceDiagram.gif"); //$NON-NLS-1$
-    } else {
-      // Standard diagram.
-      imageDescriptor = AbstractUIPlugin.imageDescriptorFromPlugin(DiagramUIPlugin.ID,
-          "/icons/full/obj16/DDiagram.gif"); //$NON-NLS-1$
-    }
-
-    if (null == imageDescriptor) {
-      imageDescriptor = ImageDescriptor.getMissingImageDescriptor();
-    }
+    ImageDescriptor imageDescriptor = DescriptionImageDescriptorHelper.getDescriptionImageDescriptor(description);
     setImageDescriptor(imageDescriptor);
   }
 
   @Override
   protected boolean updateSelection(IStructuredSelection selection) {
-    try {  
+    try {
       return !getOpenableRepresentationDescriptors(selection).isEmpty();
     } catch (IllegalStateException e) {
       if (new IllegalStateExceptionQuery(e).isAConnectionLostException()) {
