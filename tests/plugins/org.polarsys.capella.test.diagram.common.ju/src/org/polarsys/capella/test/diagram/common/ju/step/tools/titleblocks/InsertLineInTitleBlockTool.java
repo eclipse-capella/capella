0df38e51e35da94eaa754dd6bd86e1e804c60a5a
Erwann Traisnel <erwann.traisnel@obeo.fr>
2022-10-19 10:30:46 +0200

    #2504 Fixed Insertion of TitleBlock lines and columns
    In unsynchronized mode lines and columns were always inserted
    graphically at the end
    Fixed it and updated junit tests
    
    Change-Id: Ic3c26ae29a1fc593446b5a05e5b7600c3115c1e0
    Signed-off-by: Erwann Traisnel <erwann.traisnel@obeo.fr>

--- tests/plugins/org.polarsys.capella.test.diagram.common.ju/src/org/polarsys/capella/test/diagram/common/ju/step/tools/titleblocks/InsertLineInTitleBlockTool.java
+++ tests/plugins/org.polarsys.capella.test.diagram.common.ju/src/org/polarsys/capella/test/diagram/common/ju/step/tools/titleblocks/InsertLineInTitleBlockTool.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2020 THALES GLOBAL SERVICES.
+ * Copyright (c) 2020, 2022 THALES GLOBAL SERVICES.
  * 
  * This program and the accompanying materials are made available under the
  * terms of the Eclipse Public License 2.0 which is available at
@@ -14,8 +14,12 @@ package org.polarsys.capella.test.diagram.common.ju.step.tools.titleblocks;
 
 import static org.junit.Assert.assertTrue;
 
+import org.eclipse.emf.common.util.EList;
+import org.eclipse.sirius.diagram.DDiagramElement;
+import org.eclipse.sirius.diagram.DNodeContainer;
 import org.eclipse.sirius.viewpoint.description.DAnnotation;
 import org.polarsys.capella.core.diagram.helpers.TitleBlockHelper;
+import org.polarsys.capella.core.sirius.analysis.DiagramServices;
 import org.polarsys.capella.test.diagram.common.ju.context.DiagramContext;
 
 public class InsertLineInTitleBlockTool extends InsertRemoveLineInTitleBlockTool {
@@ -30,9 +34,23 @@ public class InsertLineInTitleBlockTool extends InsertRemoveLineInTitleBlockTool
     int newLinesNo = TitleBlockHelper.getTitleBlockLines(titleBlock).size();
     assertTrue(newLinesNo == currentLinesNo + 1);
 
+    // Check that newly created line is at the same index in both titleblock.references, and in the diagram elements
+
     DAnnotation currentLine = TitleBlockHelper.getTitleBlockLines(titleBlock).get(currentLineNo);
     DAnnotation newLine = TitleBlockHelper.getTitleBlockLines(titleBlock).get(currentLineNo + 1);
     assertTrue(
         TitleBlockHelper.getTitleBlockCells(newLine).size() == TitleBlockHelper.getTitleBlockCells(currentLine).size());
+
+    DNodeContainer titleBlockSpec = (DNodeContainer) DiagramServices.getDiagramServices()
+        .getDiagramElement(getDiagramContext().getDiagram(), titleBlock);
+
+    EList<DDiagramElement> diagramElements = titleBlockSpec.getOwnedDiagramElements();
+    DDiagramElement newLineDiagramElement = diagramElements.stream().filter(element -> element.getTarget() == newLine)
+        .findFirst().get();
+    assertTrue(newLineDiagramElement != null);
+    int newLineDiagramElementLineNo = diagramElements.indexOf(newLineDiagramElement);
+    int newLineLineNo = titleBlock.getReferences().indexOf(newLine);
+
+    assertTrue(newLineDiagramElementLineNo == newLineLineNo);
   }
 }
