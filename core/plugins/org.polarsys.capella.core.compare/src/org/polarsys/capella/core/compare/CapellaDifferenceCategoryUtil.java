9a346052151607a7ef9795c8afd36c573bdf537d
Philippe DUL <philippe.dul@thalesgroup.com>
2022-11-30 11:02:16 +0100

    #2511 Fix Actor, Component, Trace Filters and improve some labels
    Change-Id: I356d9e4ffd3a07ea9a065c058f26711ce3651385
    Signed-off-by: Philippe DUL <philippe.dul@thalesgroup.com>

--- core/plugins/org.polarsys.capella.core.compare/src/org/polarsys/capella/core/compare/CapellaDifferenceCategoryUtil.java
+++ core/plugins/org.polarsys.capella.core.compare/src/org/polarsys/capella/core/compare/CapellaDifferenceCategoryUtil.java
@@ -13,8 +13,10 @@
 package org.polarsys.capella.core.compare;
 
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.eclipse.emf.diffmerge.ui.viewers.EMFDiffNode;
 import org.eclipse.emf.diffmerge.ui.viewers.IDifferenceCategorySet;
@@ -25,6 +27,8 @@ import org.eclipse.ui.navigator.ICommonFilterDescriptor;
 import org.eclipse.ui.navigator.INavigatorFilterService;
 import org.eclipse.ui.navigator.NavigatorContentServiceFactory;
 import org.polarsys.capella.core.compare.categories.EObjectCategoryFilter;
+import org.polarsys.capella.core.platform.sirius.ui.navigator.filters.SemanticFiltersHelper;
+import org.polarsys.capella.core.platform.sirius.ui.navigator.view.CapellaCommonNavigator;
 
 /**
  * CapellaDifferenceCategoryutil populates the given diffnode categories with all filters from the Capella project
@@ -34,28 +38,10 @@ import org.polarsys.capella.core.compare.categories.EObjectCategoryFilter;
  *
  */
 public class CapellaDifferenceCategoryUtil {
-  /** Filter for .afm file */
-  private final String AFM_FILTER = "capella.project.explorer.filters.metadata"; //$NON-NLS-1$
-  /** Filter for .capella file */
-  private final String CAPELLA_FILTER = "capella.project.explorer.filters.capellaModeller"; //$NON-NLS-1$
-  /** Filter for .aird file */
-  private final String AIRD_FILTER = "capella.project.explorer.filters.capellaAird"; //$NON-NLS-1$
-  /** Filter for emf resources */
-  private final String EMF_FILTER = "capella.project.explorer.filters.EMFResource"; //$NON-NLS-1$
-  /** Filter for vp per category */
-  private final String VP_CAT_FILTER = "capella.project.explorer.filters.ViewpointsFolderItem"; //$NON-NLS-1$
-  /** Filter for vp per resource */
-  private final String VP_RES_FILTER = "capella.project.explorer.filters.ResourcesFolderItem"; //$NON-NLS-1$
-  /** Filter for invalid representation */
-  private final String INVALID_REP_FILTER = "capella.project.explorer.filters.invalidRepresentation"; //$NON-NLS-1$
-
-  /** Capella Project Explorer view id */
-  private final String CAPELLA_PROJECT_EXPLORER = "capella.project.explorer"; //$NON-NLS-1$
 
   public static CapellaDifferenceCategoryUtil eINSTANCE = new CapellaDifferenceCategoryUtil();
 
   private CapellaDifferenceCategoryUtil() {
-
   }
 
   /**
@@ -67,49 +53,30 @@ public class CapellaDifferenceCategoryUtil {
         org.polarsys.capella.core.compare.Messages.CapellaDifferenceCategoryProvider_CapellaSet_Description);
 
     INavigatorFilterService filterService = NavigatorContentServiceFactory.INSTANCE
-        .createContentService(CAPELLA_PROJECT_EXPLORER).getFilterService();
+        .createContentService(CapellaCommonNavigator.ID).getFilterService();
 
-    List<ICommonFilterDescriptor> itemList = Arrays.asList(filterService.getVisibleFilterDescriptors());
-
-    /**
-     * Sorts the array in alphanumerical order
-     */
-    Object[] sortedArray = itemList.stream().sorted(new Comparator<ICommonFilterDescriptor>() {
+    Comparator<ICommonFilterDescriptor> sortByName = new Comparator<ICommonFilterDescriptor>() {
       public int compare(ICommonFilterDescriptor o1_p, ICommonFilterDescriptor o2_p) {
         return o1_p.getName().compareTo(o2_p.getName());
       }
-    }).toArray();
+    };
 
-    for (Object item : sortedArray) {
-      CommonFilterDescriptor descriptor = (CommonFilterDescriptor) item;
-      if (descriptor.isVisibleInUi()) {
-        if (descriptor.getId().contains("capella")) { // if interesting //$NON-NLS-1$
-          if (!isFiltered(descriptor)) {
-            ViewerFilter f = descriptor.createFilter();
-            diagramCategorySet.getChildren()
-                .add(new EObjectCategoryFilter(descriptor.getId(), descriptor.getName(), false) {
-                  @Override
-                  public boolean keepElement(Object element) {
-                    return !f.select(null, null, element);
-                  }
-                });
-          }
+    List<CommonFilterDescriptor> itemList = Arrays.asList(filterService.getVisibleFilterDescriptors()).stream()
+        .filter(d -> d instanceof CommonFilterDescriptor).map(CommonFilterDescriptor.class::cast)
+        .filter(d -> d.isVisibleInUi() && SemanticFiltersHelper.isSemanticFilter(d)).collect(Collectors.toList());
+    Collections.sort(itemList, sortByName);
+
+    for (CommonFilterDescriptor descriptor : itemList) {
+      ViewerFilter f = descriptor.createFilter();
+      diagramCategorySet.getChildren().add(new EObjectCategoryFilter(descriptor.getId(), descriptor.getName(), false) {
+        @Override
+        public boolean keepElement(Object element) {
+          return !f.select(null, null, element);
         }
-      }
+      });
     }
     node_p.getCategoryManager().addCategories(diagramCategorySet);
   }
 
-  /**
-   * 
-   * @param descriptor
-   * @return whether the filter descriptor should be filtered out
-   */
-  private boolean isFiltered(ICommonFilterDescriptor descriptor) {
-    String id = descriptor.getId();
-    return id == null || id.equals(AFM_FILTER) || id.equals(CAPELLA_FILTER) || id.equals(AIRD_FILTER)
-        || id.equals(EMF_FILTER) || id.equals(VP_CAT_FILTER) || id.equals(VP_RES_FILTER)
-        || id.equals(INVALID_REP_FILTER);
-  }
 
 }
