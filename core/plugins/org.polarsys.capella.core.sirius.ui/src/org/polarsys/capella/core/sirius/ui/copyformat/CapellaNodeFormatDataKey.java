4e3ffebd5939be070ea2712ecb7b6c725aa7658f
Erwann Traisnel <erwann.traisnel@obeo.fr>
2023-01-18 09:54:18 +0100

    #2534 Fixed copy paste layout for FC/PP and FCI on selection
    It is now possible to copy paste layout :
    - 1 on/from Functions from FunctionalChainInvolvements
    - 2 on/from Functions involved in FC, displayed in FunctionalChainRef
    - 3 on/from Functional Chain displayed in FunctionalChainRef
    - 4 on/from PhysicalPaths and their involvements
    
    However for 1/2/3 the global copy pastelayout will not completely work
    if any of these items are displayed several times
    For example a Function displayed in different involvements, or
    functional chains displayed in two functional chain reference
    
    In fact, there is no way to properly identify a layout from a given FCI, on from a FCRef
    As a consequence, there is no way to copy the layout of an element displayed twice
    This could be achieved if we had a proper way to add/reduce the number of discriminants while searching for a FormatData in the layoutmap
    
    Yet, the layout can be copied and pasted on "selection"
    
    Added JUnit
    Reactivated FC Junit in CopyPasteLayout existing test
    
    Change-Id: I4b61549b8e1373eb306b07cec1dc7aee237ea448
    Signed-off-by: Erwann Traisnel <erwann.traisnel@obeo.fr>

--- core/plugins/org.polarsys.capella.core.sirius.ui/src/org/polarsys/capella/core/sirius/ui/copyformat/CapellaNodeFormatDataKey.java
+++ core/plugins/org.polarsys.capella.core.sirius.ui/src/org/polarsys/capella/core/sirius/ui/copyformat/CapellaNodeFormatDataKey.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2006, 2020 THALES GLOBAL SERVICES.
+ * Copyright (c) 2006, 2023 THALES GLOBAL SERVICES.
  * 
  * This program and the accompanying materials are made available under the
  * terms of the Eclipse Public License 2.0 which is available at
@@ -16,12 +16,18 @@ import org.eclipse.emf.ecore.EObject;
 import org.eclipse.sirius.diagram.AbstractDNode;
 import org.eclipse.sirius.diagram.DDiagramElement;
 import org.eclipse.sirius.diagram.DNode;
+import org.eclipse.sirius.diagram.DNodeContainer;
 import org.eclipse.sirius.diagram.DiagramPackage;
 import org.eclipse.sirius.diagram.description.style.SquareDescription;
 import org.eclipse.sirius.viewpoint.DSemanticDecorator;
 import org.eclipse.sirius.viewpoint.description.ColorDescription;
 import org.eclipse.sirius.viewpoint.description.style.StyleDescription;
+import org.polarsys.capella.core.data.capellacore.InvolvedElement;
+import org.polarsys.capella.core.data.cs.PhysicalPathInvolvement;
+import org.polarsys.capella.core.data.fa.AbstractFunction;
 import org.polarsys.capella.core.data.fa.ExchangeCategory;
+import org.polarsys.capella.core.data.fa.FunctionalChain;
+import org.polarsys.capella.core.data.fa.FunctionalChainInvolvementFunction;
 
 /**
  * Specific key for {@link DNode}.
@@ -47,30 +53,75 @@ public class CapellaNodeFormatDataKey extends CapellaDecoratorFormatDataKey {
     super(key);
     decorator = inputDecorator;
 
-    //Add decoration on parent
-    EObject parent = getRelatedParent(inputDecorator);
-    if (parent != null) {
-      addDecoration(parent);
-    }
+    EObject parent = getRelatedParent(decorator);
+    EObject parentSemanticElement = null;
 
-    if ((inputDecorator == null) || (inputDecorator instanceof AbstractDNode)) {
+    if ((decorator == null) || (decorator instanceof AbstractDNode)) {
       addDecoration(DiagramPackage.Literals.ABSTRACT_DNODE);
     }
-    if ((inputDecorator != null) && (inputDecorator.getTarget() != null)) {
-      if (inputDecorator.getTarget() instanceof ExchangeCategory) {
+    if ((decorator != null) && (decorator.getTarget() != null)) {
+      EObject semanticElement = decorator.getTarget();
+      if (semanticElement instanceof ExchangeCategory) {
+        if (parent != null)
+          addDecoration(parent);
         //This should not work with copyFormat between phases
-        addDecoration(((DDiagramElement) inputDecorator).getTarget());
-        addDecoration(((DDiagramElement) inputDecorator.eContainer()).getTarget());
-        StyleDescription desc = ((DDiagramElement) inputDecorator).getStyle().getDescription();
+        addDecoration(((DDiagramElement) decorator).getTarget());
+        addDecoration(((DDiagramElement) decorator.eContainer()).getTarget());
+        StyleDescription desc = ((DDiagramElement) decorator).getStyle().getDescription();
         if (desc instanceof SquareDescription) {
           SquareDescription sd = (SquareDescription) desc;
           ColorDescription color = sd.getColor();
           super.addDecoration(color);
         }
+      } else if (semanticElement instanceof FunctionalChainInvolvementFunction) {
+        FunctionalChainInvolvementFunction fcif = (FunctionalChainInvolvementFunction) semanticElement;
+        AbstractFunction involved = (AbstractFunction) fcif.getInvolved();
+
+        // Add the involved function as a decorator
+        addDecoration(involved);
+
+        for (FunctionalChain involvingFC : involved.getInvolvingFunctionalChains()) {
+          if (!decorations.contains(involvingFC))
+            addDecoration(involvingFC);
+
+        }
+
+      }
+
+      else if (semanticElement instanceof PhysicalPathInvolvement) {
+        PhysicalPathInvolvement ppi = (PhysicalPathInvolvement) semanticElement;
+        InvolvedElement involved = (InvolvedElement) ppi.getInvolved();
+
+        // Add the involved element as a decorator
+        addDecoration(involved);
+
+      }
+
+      else if (semanticElement instanceof AbstractFunction) {
+        AbstractFunction function = (AbstractFunction) semanticElement;
+        addDecoration(function);
+        for (FunctionalChain involvingFC : function.getInvolvingFunctionalChains()) {
+          if (!decorations.contains(involvingFC))
+            addDecoration(involvingFC);
+
+        }
+
+      } else if (semanticElement instanceof FunctionalChain) {
+        if (!(decorator instanceof DNodeContainer)) {
+          // If not a DNodeContainer, the decorator represents the square FC
+          addDecoration(DiagramPackage.Literals.DNODE);
+        }
+      }
+
+      else {
+        if (parent != null)
+          // Add decoration on parent
+          addDecoration(parent);
       }
 
     }
   }
+
 
   /**
    * @param decorator
