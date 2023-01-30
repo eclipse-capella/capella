cd999e989da7f8cf69128a48e958fdeef8333e32
Erwann Traisnel <erwann.traisnel@obeo.fr>
2023-01-30 17:07:45 +0100

    #2563 New actions "Select Related REC" and "Select Related RPL"
    Contributes 2 new actions to "Capella Select" menu
    on a selection that has common REC -> Select Related REC Elements
    on a selection that has common RPL -> Select Related RPL Elements
    
    Updated Documentation
    
    Added tests
    
    Change-Id: I71af679a1d558be87983b0807fc311535bb3d0b8
    Signed-off-by: Erwann Traisnel <erwann.traisnel@obeo.fr>

---
+++ common/plugins/org.polarsys.capella.common.re.ui/src/org/polarsys/capella/common/re/ui/testers/ValidRplRelatedElementMenuTargetTester.java
@@ -0,0 +1,39 @@
+/*******************************************************************************
+ * Copyright (c) 2023 THALES GLOBAL SERVICES.
+ * 
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0
+ * 
+ * SPDX-License-Identifier: EPL-2.0
+ * 
+ * Contributors:
+ *    Thales - initial API and implementation
+ *******************************************************************************/
+package org.polarsys.capella.common.re.ui.testers;
+
+import org.eclipse.core.expressions.PropertyTester;
+import org.eclipse.jface.viewers.IStructuredSelection;
+import org.polarsys.capella.common.re.ui.menu.RplDynamicMenu;
+
+public class ValidRplRelatedElementMenuTargetTester extends PropertyTester {
+
+  public static final String PROPERTY_IS_VALID_MENU_TARGET = "hasRelatedRplElements"; //$NON-NLS-1$
+
+  @Override
+  public boolean test(Object receiver, String property, Object[] args, Object expectedValue) {
+
+    if (receiver instanceof IStructuredSelection) {
+      IStructuredSelection selection = (IStructuredSelection) receiver;
+      if (PROPERTY_IS_VALID_MENU_TARGET.equals(property)) {
+        return RplDynamicMenu.hasCommonRpls(selection);
+      }
+    } else {
+      throw new IllegalArgumentException("Receiver must be a structured selection");//$NON-NLS-1$
+    }
+
+    throw new IllegalArgumentException("Unknown property: " + property); //$NON-NLS-1$
+  }
+
+
+}
--- tests/plugins/org.polarsys.capella.test.diagram.tools.ju/src/org/polarsys/capella/test/diagram/tools/ju/common/SelectToolsTest.java
+++ tests/plugins/org.polarsys.capella.test.diagram.tools.ju/src/org/polarsys/capella/test/diagram/tools/ju/common/SelectToolsTest.java
@@ -14,6 +14,7 @@ package org.polarsys.capella.test.diagram.tools.ju.common;
 
 import java.util.ArrayList;
 import java.util.HashSet;
+import java.util.Optional;
 
 import org.eclipse.sirius.business.api.dialect.DialectManager;
 import org.eclipse.sirius.business.api.session.Session;
@@ -24,6 +25,7 @@ import org.eclipse.sirius.diagram.DNodeContainer;
 import org.eclipse.sirius.diagram.description.DiagramDescription;
 import org.eclipse.sirius.diagram.description.DiagramElementMapping;
 import org.eclipse.sirius.viewpoint.DRepresentation;
+import org.eclipse.sirius.viewpoint.DRepresentationDescriptor;
 import org.polarsys.capella.test.diagram.common.ju.api.AbstractDiagramTestCase;
 import org.polarsys.capella.test.diagram.common.ju.context.CommonDiagram;
 import org.polarsys.capella.test.diagram.common.ju.context.DiagramContext;
@@ -36,6 +38,20 @@ import org.polarsys.capella.test.framework.helpers.GuiActions;
  */
 public class SelectToolsTest extends AbstractDiagramTestCase {
 
+  public final String REC_DIAGRAM = "_yYiSnnjqEea__MYrXGSERA";//$NON-NLS-1$
+  public final String REC_PHYSICAL_COMPONENT = "1c483402-df88-446f-a39b-1e0e9720faa9";//$NON-NLS-1$
+  public final String REC_PHYSICAL_FUNCTION_1 = "e24662c4-47b2-445f-9d18-7214fca7013e";//$NON-NLS-1$
+  public final String REC_FIP1_PF1 = "f0c1cbdc-5fe6-44e9-a691-1d5b540a8e51";//$NON-NLS-1$
+  public final String REC_PHYSICAL_FUNCTION_2 = "ccfb7dc0-c861-44db-95ea-0a216b9ec643";//$NON-NLS-1$
+  public final String REC_FIP1_PF2 = "0ced1a83-7e58-4db2-87b9-223756fcfed6";//$NON-NLS-1$
+
+  public final String RPL_DIAGRAM = "_yYiSn3jqEea__MYrXGSERA";//$NON-NLS-1$
+  public final String RPL_PHYSICAL_COMPONENT = "c9e311a0-b855-48f6-8ee6-5830f2873e0b";//$NON-NLS-1$
+  public final String RPL_PHYSICAL_FUNCTION_1 = "c5503a00-fae8-439d-90a7-6b8644d2f609";//$NON-NLS-1$
+  public final String RPL_FIP1_PF1 = "cf123dcb-5d86-47fa-9dbb-e6670495bfdb";//$NON-NLS-1$
+  public final String RPL_PHYSICAL_FUNCTION_2 = "238797f1-c012-446a-9b79-e94cf88bd180";//$NON-NLS-1$
+  public final String RPL_FIP1_PF2 = "78736f13-8b41-4119-b0b4-48e733598c85";//$NON-NLS-1$
+
   @Override
   protected String getRequiredTestModel() {
     return "In-Flight Entertainment System"; //$NON-NLS-1$
@@ -46,6 +62,13 @@ public class SelectToolsTest extends AbstractDiagramTestCase {
 
     Session s = getSession(getRequiredTestModel());
     SessionContext sc = new SessionContext(s);
+
+
+    testSelectRelatedRecs(sc, REC_DIAGRAM, REC_PHYSICAL_COMPONENT, REC_FIP1_PF2, REC_PHYSICAL_COMPONENT,
+        REC_PHYSICAL_FUNCTION_1, REC_PHYSICAL_FUNCTION_2, REC_FIP1_PF1);
+
+    testSelectRelatedRpls(sc, RPL_DIAGRAM, RPL_PHYSICAL_COMPONENT, RPL_PHYSICAL_FUNCTION_2, RPL_PHYSICAL_FUNCTION_1,
+        RPL_FIP1_PF2, RPL_PHYSICAL_COMPONENT, RPL_FIP1_PF1);
 
     // For each kind of diagrams from sample project, check if select tools are working.
     HashSet<DiagramDescription> descriptions = new HashSet<DiagramDescription>();
@@ -80,6 +103,8 @@ public class SelectToolsTest extends AbstractDiagramTestCase {
           }
           if (de instanceof AbstractDNode) {
             cd.selectOwnedPorts(de.getUid());
+            cd.selectRelatedRecs(de.getUid());
+            cd.selectRelatedRpls(de.getUid());
           }
           cd.selectSameType(de.getUid());
           cd.selectSameMapping(de.getUid());
@@ -92,4 +117,30 @@ public class SelectToolsTest extends AbstractDiagramTestCase {
     GuiActions.flushASyncGuiJobs();
   }
 
+  protected void testSelectRelatedRecs(SessionContext sc, String diagramId, String sourceElementId,
+      String... expectedSelectedElements) {
+    // Get diagram and ensure it is opened
+    DDiagram diagram = getDiagram(sc.getSession(), diagramId);
+    CommonDiagram cd = new CommonDiagram(sc, diagram);
+    cd.open();
+    cd.selectRelatedRecs(sourceElementId, expectedSelectedElements);
+  }
+
+  protected void testSelectRelatedRpls(SessionContext sc, String diagramId, String sourceElementId,
+      String... expectedSelectedElements) {
+    // Get diagram and ensure it is opened
+    DDiagram diagram = getDiagram(sc.getSession(), diagramId);
+    CommonDiagram cd = new CommonDiagram(sc, diagram);
+    cd.open();
+    cd.selectRelatedRpls(sourceElementId, expectedSelectedElements);
+  }
+
+  protected DDiagram getDiagram(Session s, String repId) {
+    Optional<DRepresentationDescriptor> repDescriptor = DialectManager.INSTANCE.getAllRepresentationDescriptors(s).stream()
+        .filter(d -> d.getUid().equals(repId)).findFirst();
+    if (repDescriptor.isEmpty())
+      fail("Could not find descriptor with id " + repId);
+    DDiagram diagram = (DDiagram) repDescriptor.get().getRepresentation();
+    return diagram;
+  }
 }
