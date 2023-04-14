f4d58c6800fd8b6109b103b714cdae1db526560c
Séraphin Costa <seraphin.costa@obeosoft.com>
2023-04-14 16:33:32 +0200

    #2644 Add quick fix to remove image not found for diagram elements
    Signed-off-by: Séraphin Costa <seraphin.costa@obeosoft.com>

--- core/plugins/org.polarsys.capella.core.platform.sirius.sirius.validation/src/org/polarsys/capella/core/platform/sirius/sirius/validation/ddiagram/ImagePathRemoveResolver.java
+++ core/plugins/org.polarsys.capella.core.platform.sirius.sirius.validation/src/org/polarsys/capella/core/platform/sirius/sirius/validation/ddiagram/ImagePathRemoveResolver.java
@@ -1,12 +1,12 @@
 /*******************************************************************************
  * Copyright (c) 2022, 2023 THALES GLOBAL SERVICES.
- * 
+ *
  * This program and the accompanying materials are made available under the
  * terms of the Eclipse Public License 2.0 which is available at
  * http://www.eclipse.org/legal/epl-2.0
- * 
+ *
  * SPDX-License-Identifier: EPL-2.0
- * 
+ *
  * Contributors:
  *    Thales - initial API and implementation
  *******************************************************************************/
@@ -18,15 +18,24 @@ import java.util.Optional;
 import org.apache.log4j.Logger;
 import org.eclipse.core.resources.IMarker;
 import org.eclipse.core.runtime.CoreException;
+import org.eclipse.core.runtime.NullProgressMonitor;
 import org.eclipse.core.runtime.Path;
 import org.eclipse.emf.common.util.Diagnostic;
 import org.eclipse.emf.ecore.EAttribute;
 import org.eclipse.emf.ecore.EObject;
-import org.eclipse.emf.transaction.RecordingCommand;
 import org.eclipse.emf.transaction.TransactionalEditingDomain;
+import org.eclipse.sirius.business.api.query.EObjectQuery;
 import org.eclipse.sirius.business.api.session.Session;
 import org.eclipse.sirius.common.tools.api.resource.FileProvider;
+import org.eclipse.sirius.diagram.DDiagram;
+import org.eclipse.sirius.diagram.DDiagramElement;
 import org.eclipse.sirius.diagram.tools.internal.validation.constraints.ImagePathWrappingStatus;
+import org.eclipse.sirius.diagram.tools.internal.validation.constraints.ImagePathWrappingStatus.ImagePathTarget;
+import org.eclipse.sirius.diagram.ui.internal.quickfix.ImageMarkerRemoveResolution;
+import org.eclipse.sirius.ext.base.Option;
+import org.eclipse.sirius.ui.business.api.dialect.DialectUIManager;
+import org.eclipse.sirius.viewpoint.DRepresentation;
+import org.eclipse.sirius.viewpoint.DSemanticDecorator;
 import org.polarsys.capella.common.helpers.validation.ConstraintStatusDiagnostic;
 import org.polarsys.capella.common.tools.report.appenders.reportlogview.MarkerViewHelper;
 import org.polarsys.capella.common.tools.report.config.registry.ReportManagerRegistry;
@@ -36,65 +45,50 @@ import org.polarsys.capella.core.validation.ui.ide.quickfix.AbstractCapellaMarke
 @SuppressWarnings("restriction")
 public class ImagePathRemoveResolver extends AbstractCapellaMarkerResolution {
 
-  protected Logger _logger = ReportManagerRegistry.getInstance().subscribe(IReportManagerDefaultComponents.VALIDATION);
+    protected Logger _logger = ReportManagerRegistry.getInstance().subscribe(IReportManagerDefaultComponents.VALIDATION);
 
-  public boolean removeImagePathInRichText(EObject eObject, String featurename, String imagePath,
-      TransactionalEditingDomain ted) {
-    boolean fixSucceeded = false;
-    Optional<EAttribute> eAttributeOpt = eObject.eClass().getEAllAttributes().stream()
-        .filter(attr -> featurename.equals(attr.getName())).findFirst();
-    if (eAttributeOpt.isPresent()) {
-      EAttribute eAttribute = eAttributeOpt.get();
-      Object stringObj = eObject.eGet(eAttribute);
-      if (stringObj instanceof String) {
-        String htmlText = (String) stringObj;
-        String imgRegex = "<img\\s+[^>]*?src\\s*=\\s*(\"|')" + imagePath + "(\"|')[^>]*?>"; //$NON-NLS-1$
-        ted.getCommandStack().execute(new RecordingCommand(ted) {
-          @Override
-          protected void doExecute() {
-            String newHtmlText = htmlText.replaceAll(imgRegex, "");
-            eObject.eSet(eAttribute, newHtmlText);
-          }
-        });
-        fixSucceeded = true;
-      }
-    }
-    ;
-    return fixSucceeded;
-  }
+    @Override
+    public void run(IMarker marker_p) {
+        final List<EObject> modelElements = getModelElements(marker_p);
+        if (modelElements.size() < 1)
+            return;
+        EObject target = modelElements.get(0);
+        Optional<Session> sessionOpt = Session.of(target);
 
-  @Override
-  public void run(IMarker marker_p) {
-    final List<EObject> modelElements = getModelElements(marker_p);
-    if (modelElements.size() < 1)
-      return;
-    EObject target = modelElements.get(0);
-    Optional<Session> sessionOpt = Session.of(target);
-
-    Diagnostic diagnostic = MarkerViewHelper.getDiagnostic(marker_p);
-    ImagePathWrappingStatus imagePathStatus = null;
-    if (diagnostic instanceof ConstraintStatusDiagnostic
-        && ((ConstraintStatusDiagnostic) diagnostic).getConstraintStatus() instanceof ImagePathWrappingStatus) {
-      imagePathStatus = (ImagePathWrappingStatus) ((ConstraintStatusDiagnostic) diagnostic).getConstraintStatus();
-    }
-
-    if (imagePathStatus != null && sessionOpt.isPresent()) {
-      String notReachableImagePath = imagePathStatus.getNotReachableImagePath();
-      TransactionalEditingDomain ted = sessionOpt.get().getTransactionalEditingDomain();
-      boolean fixSucceeded = FileProvider.getDefault().exists(new Path(notReachableImagePath), sessionOpt.get());
-
-      if (!fixSucceeded) {
-        EAttribute eAttribute = imagePathStatus.getEAttribute();
-        fixSucceeded = removeImagePathInRichText(target, eAttribute.getName(), notReachableImagePath, ted);
-      }
-
-      if (fixSucceeded) {
-        try {
-          marker_p.delete();
-        } catch (CoreException exception_p) {
-          _logger.error("Exception while deleting marker : " + exception_p.toString()); //$NON-NLS-1$
+        Diagnostic diagnostic = MarkerViewHelper.getDiagnostic(marker_p);
+        ImagePathWrappingStatus imagePathStatus = null;
+        if (diagnostic instanceof ConstraintStatusDiagnostic && ((ConstraintStatusDiagnostic) diagnostic).getConstraintStatus() instanceof ImagePathWrappingStatus) {
+            imagePathStatus = (ImagePathWrappingStatus) ((ConstraintStatusDiagnostic) diagnostic).getConstraintStatus();
         }
-      }
+
+        if (imagePathStatus != null && sessionOpt.isPresent()) {
+            String notReachableImagePath = imagePathStatus.getNotReachableImagePath();
+            TransactionalEditingDomain ted = sessionOpt.get().getTransactionalEditingDomain();
+            boolean fixSucceeded = FileProvider.getDefault().exists(new Path(notReachableImagePath), sessionOpt.get());
+
+            if (!fixSucceeded) {
+                ImagePathTarget imagePathTarget = imagePathStatus.getImagePathTarget();
+                EAttribute eAttribute = imagePathStatus.getEAttribute();
+
+                if (imagePathTarget.equals(ImagePathTarget.WORKSPACE_IMAGE) && target instanceof DSemanticDecorator) {
+                    Option<DRepresentation> dRepresention = new EObjectQuery(target).getRepresentation();
+                    if (dRepresention.some() && dRepresention.get() instanceof DDiagram) {
+                        DDiagram dDiagram = (DDiagram) dRepresention.get();
+                        DialectUIManager.INSTANCE.openEditor(sessionOpt.get(), dDiagram, new NullProgressMonitor());
+                        fixSucceeded = ImageMarkerRemoveResolution.removeImageInDiagram(dDiagram, (DDiagramElement) target, ted);
+                    }
+                } else {
+                    fixSucceeded = ImageMarkerRemoveResolution.removeImagePathInRichText(target, eAttribute.getName(), notReachableImagePath, ted);
+                }
+            }
+
+            if (fixSucceeded) {
+                try {
+                    marker_p.delete();
+                } catch (CoreException exception_p) {
+                    _logger.error("Exception while deleting marker : " + exception_p.toString()); //$NON-NLS-1$
+                }
+            }
+        }
     }
-  }
 }
