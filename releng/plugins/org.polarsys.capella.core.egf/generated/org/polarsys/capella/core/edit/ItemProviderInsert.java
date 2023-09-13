532a29e21045a92c8c8ac54ba2645b5016dae57a
Erwann Traisnel <erwann.traisnel@obeo.fr>
2023-09-13 10:47:29 +0200

    #2720 Update egf patterns to 1.6.4
    Change-Id: Ife2fcb90d24ce5dac5a21a4302d6965dc0f25b21
    Signed-off-by: Erwann Traisnel <erwann.traisnel@obeo.fr>

--- releng/plugins/org.polarsys.capella.core.egf/generated/org/polarsys/capella/core/edit/ItemProviderInsert.java
+++ releng/plugins/org.polarsys.capella.core.egf/generated/org/polarsys/capella/core/edit/ItemProviderInsert.java
@@ -1,4 +1,4 @@
-//Generated with EGF 1.6.3.202110291409
+//Generated with EGF 1.6.4.202309111303
 package org.polarsys.capella.core.edit;
 
 import org.eclipse.egf.common.helper.*;
@@ -9,155 +9,155 @@ import org.eclipse.egf.pattern.execution.*;
 import org.eclipse.egf.pattern.query.*;
 
 public class ItemProviderInsert extends org.eclipse.egf.emf.pattern.edit.call.ItemProvider.ItemProviderinsert {
-	protected static String nl;
+  protected static String nl;
 
-	public static synchronized ItemProviderInsert create(String lineSeparator) {
-		nl = lineSeparator;
-		ItemProviderInsert result = new ItemProviderInsert();
-		nl = null;
-		return result;
-	}
+  public static synchronized ItemProviderInsert create(String lineSeparator) {
+    nl = lineSeparator;
+    ItemProviderInsert result = new ItemProviderInsert();
+    nl = null;
+    return result;
+  }
 
-	public final String NL = nl == null ? (System.getProperties().getProperty("line.separator")) : nl;
-	protected final String TEXT_1 = "\t// begin-capella-code" + NL + "\t/**" + NL + "\t * <!-- begin-user-doc -->" + NL
-			+ "\t * <!-- end-user-doc -->" + NL + "\t * @generated" + NL + "\t */";
-	protected final String TEXT_2 = NL + "\t@Override";
-	protected final String TEXT_3 = NL + "\tprotected ";
-	protected final String TEXT_4 = " createInitializeCopyCommand(";
-	protected final String TEXT_5 = " domain, ";
-	protected final String TEXT_6 = " owner, ";
-	protected final String TEXT_7 = " helper) {" + NL + "\t\treturn new ";
-	protected final String TEXT_8 = "(domain, owner, helper);" + NL + "\t}" + NL + "\t// end-capella-code";
-	protected final String TEXT_9 = NL;
+  public final String NL = nl == null ? (System.getProperties().getProperty("line.separator")) : nl;
+  protected final String TEXT_1 = "\t// begin-capella-code" + NL + "\t/**" + NL + "\t * <!-- begin-user-doc -->" + NL
+      + "\t * <!-- end-user-doc -->" + NL + "\t * @generated" + NL + "\t */";
+  protected final String TEXT_2 = NL + "\t@Override";
+  protected final String TEXT_3 = NL + "\tprotected ";
+  protected final String TEXT_4 = " createInitializeCopyCommand(";
+  protected final String TEXT_5 = " domain, ";
+  protected final String TEXT_6 = " owner, ";
+  protected final String TEXT_7 = " helper) {" + NL + "\t\treturn new ";
+  protected final String TEXT_8 = "(domain, owner, helper);" + NL + "\t}" + NL + "\t// end-capella-code";
+  protected final String TEXT_9 = NL;
 
-	public ItemProviderInsert() {
-		//Here is the constructor
-		StringBuffer stringBuffer = new StringBuffer();
+  public ItemProviderInsert() {
+    //Here is the constructor
+    StringBuffer stringBuffer = new StringBuffer();
 
-		// add initialisation of the pattern variables (declaration has been already done).
+    // add initialisation of the pattern variables (declaration has been already done).
 
-	}
+  }
 
-	public String generate(Object argument) throws Exception {
-		final StringBuffer stringBuffer = new StringBuffer();
+  public String generate(Object argument) throws Exception {
+    final StringBuffer stringBuffer = new StringBuffer();
 
-		InternalPatternContext ctx = (InternalPatternContext) argument;
-		Map<String, String> queryCtx = null;
-		IQuery.ParameterDescription paramDesc = null;
-		Node.Container currentNode = ctx.getNode();
+    InternalPatternContext ctx = (InternalPatternContext) argument;
+    Map<String, String> queryCtx = null;
+    IQuery.ParameterDescription paramDesc = null;
+    Node.Container currentNode = ctx.getNode();
 
-		List<Object> genClassList = null;
-		//this pattern can only be called by another (i.e. it's not an entry point in execution)
-		List<Object> genPackageList = null;
-		//this pattern can only be called by another (i.e. it's not an entry point in execution)
-		List<Object> genModelList = null;
-		//this pattern can only be called by another (i.e. it's not an entry point in execution)
-		List<Object> isJDK50List = null;
-		//this pattern can only be called by another (i.e. it's not an entry point in execution)
-		List<Object> forceDefaultCaseList = null;
-		//this pattern can only be called by another (i.e. it's not an entry point in execution)
-		List<Object> indentDefaultCaseList = null;
-		//this pattern can only be called by another (i.e. it's not an entry point in execution)
-		List<Object> _ListList = null;
-		//this pattern can only be called by another (i.e. it's not an entry point in execution)
-		List<Object> hasSwitchList = null;
-		//this pattern can only be called by another (i.e. it's not an entry point in execution)
+    List<Object> genClassList = null;
+    //this pattern can only be called by another (i.e. it's not an entry point in execution)
+    List<Object> genPackageList = null;
+    //this pattern can only be called by another (i.e. it's not an entry point in execution)
+    List<Object> genModelList = null;
+    //this pattern can only be called by another (i.e. it's not an entry point in execution)
+    List<Object> isJDK50List = null;
+    //this pattern can only be called by another (i.e. it's not an entry point in execution)
+    List<Object> forceDefaultCaseList = null;
+    //this pattern can only be called by another (i.e. it's not an entry point in execution)
+    List<Object> indentDefaultCaseList = null;
+    //this pattern can only be called by another (i.e. it's not an entry point in execution)
+    List<Object> _ListList = null;
+    //this pattern can only be called by another (i.e. it's not an entry point in execution)
+    List<Object> hasSwitchList = null;
+    //this pattern can only be called by another (i.e. it's not an entry point in execution)
 
-		for (Object genClassParameter : genClassList) {
-			for (Object genPackageParameter : genPackageList) {
-				for (Object genModelParameter : genModelList) {
-					for (Object isJDK50Parameter : isJDK50List) {
-						for (Object forceDefaultCaseParameter : forceDefaultCaseList) {
-							for (Object indentDefaultCaseParameter : indentDefaultCaseList) {
-								for (Object _ListParameter : _ListList) {
-									for (Object hasSwitchParameter : hasSwitchList) {
+    for (Object genClassParameter : genClassList) {
+      for (Object genPackageParameter : genPackageList) {
+        for (Object genModelParameter : genModelList) {
+          for (Object isJDK50Parameter : isJDK50List) {
+            for (Object forceDefaultCaseParameter : forceDefaultCaseList) {
+              for (Object indentDefaultCaseParameter : indentDefaultCaseList) {
+                for (Object _ListParameter : _ListList) {
+                  for (Object hasSwitchParameter : hasSwitchList) {
 
-										this.genClass = (org.eclipse.emf.codegen.ecore.genmodel.GenClass) genClassParameter;
-										this.genPackage = (org.eclipse.emf.codegen.ecore.genmodel.GenPackage) genPackageParameter;
-										this.genModel = (org.eclipse.emf.codegen.ecore.genmodel.GenModel) genModelParameter;
-										this.isJDK50 = (java.lang.Boolean) isJDK50Parameter;
-										this.forceDefaultCase = (java.lang.Boolean) forceDefaultCaseParameter;
-										this.indentDefaultCase = (java.lang.String) indentDefaultCaseParameter;
-										this._List = (java.lang.String) _ListParameter;
-										this.hasSwitch = (java.lang.Boolean) hasSwitchParameter;
+                    this.genClass = (org.eclipse.emf.codegen.ecore.genmodel.GenClass) genClassParameter;
+                    this.genPackage = (org.eclipse.emf.codegen.ecore.genmodel.GenPackage) genPackageParameter;
+                    this.genModel = (org.eclipse.emf.codegen.ecore.genmodel.GenModel) genModelParameter;
+                    this.isJDK50 = (java.lang.Boolean) isJDK50Parameter;
+                    this.forceDefaultCase = (java.lang.Boolean) forceDefaultCaseParameter;
+                    this.indentDefaultCase = (java.lang.String) indentDefaultCaseParameter;
+                    this._List = (java.lang.String) _ListParameter;
+                    this.hasSwitch = (java.lang.Boolean) hasSwitchParameter;
 
-										if (preCondition(ctx)) {
-											ctx.setNode(new Node.Container(currentNode, getClass()));
-											orchestration(ctx);
-										}
+                    if (preCondition(ctx)) {
+                      ctx.setNode(new Node.Container(currentNode, getClass()));
+                      orchestration(ctx);
+                    }
 
-									}
-								}
-							}
-						}
-					}
-				}
-			}
-		}
-		ctx.setNode(currentNode);
-		if (ctx.useReporter()) {
-			ctx.getReporter().executionFinished(OutputManager.computeExecutionOutput(ctx), ctx);
-		}
+                  }
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+    ctx.setNode(currentNode);
+    if (ctx.useReporter()) {
+      ctx.getReporter().executionFinished(OutputManager.computeExecutionOutput(ctx), ctx);
+    }
 
-		stringBuffer.append(TEXT_9);
-		stringBuffer.append(TEXT_9);
-		return stringBuffer.toString();
-	}
+    stringBuffer.append(TEXT_9);
+    stringBuffer.append(TEXT_9);
+    return stringBuffer.toString();
+  }
 
-	public String orchestration(PatternContext ctx) throws Exception {
-		InternalPatternContext ictx = (InternalPatternContext) ctx;
+  public String orchestration(PatternContext ctx) throws Exception {
+    InternalPatternContext ictx = (InternalPatternContext) ctx;
 
-		super.orchestration(new SuperOrchestrationContext(ictx));
+    super.orchestration(new SuperOrchestrationContext(ictx));
 
-		if (ictx.useReporter()) {
-			Map<String, Object> parameterValues = new HashMap<String, Object>();
-			parameterValues.put("genClass", this.genClass);
-			parameterValues.put("genPackage", this.genPackage);
-			parameterValues.put("genModel", this.genModel);
-			parameterValues.put("isJDK50", this.isJDK50);
-			parameterValues.put("forceDefaultCase", this.forceDefaultCase);
-			parameterValues.put("indentDefaultCase", this.indentDefaultCase);
-			parameterValues.put("_List", this._List);
-			parameterValues.put("hasSwitch", this.hasSwitch);
-			String outputWithCallBack = OutputManager.computeLoopOutput(ictx);
-			String loop = OutputManager.computeLoopOutputWithoutCallback(ictx);
-			ictx.getReporter().loopFinished(loop, outputWithCallBack, ictx, parameterValues);
-		}
-		return null;
-	}
+    if (ictx.useReporter()) {
+      Map<String, Object> parameterValues = new HashMap<String, Object>();
+      parameterValues.put("genClass", this.genClass);
+      parameterValues.put("genPackage", this.genPackage);
+      parameterValues.put("genModel", this.genModel);
+      parameterValues.put("isJDK50", this.isJDK50);
+      parameterValues.put("forceDefaultCase", this.forceDefaultCase);
+      parameterValues.put("indentDefaultCase", this.indentDefaultCase);
+      parameterValues.put("_List", this._List);
+      parameterValues.put("hasSwitch", this.hasSwitch);
+      String outputWithCallBack = OutputManager.computeLoopOutput(ictx);
+      String loop = OutputManager.computeLoopOutputWithoutCallback(ictx);
+      ictx.getReporter().loopFinished(loop, outputWithCallBack, ictx, parameterValues);
+    }
+    return null;
+  }
 
-	public Map<String, Object> getParameters() {
-		final Map<String, Object> parameters = new HashMap<String, Object>();
-		parameters.put("genClass", this.genClass);
-		parameters.put("genPackage", this.genPackage);
-		parameters.put("genModel", this.genModel);
-		parameters.put("isJDK50", this.isJDK50);
-		parameters.put("forceDefaultCase", this.forceDefaultCase);
-		parameters.put("indentDefaultCase", this.indentDefaultCase);
-		parameters.put("_List", this._List);
-		parameters.put("hasSwitch", this.hasSwitch);
-		return parameters;
-	}
+  public Map<String, Object> getParameters() {
+    final Map<String, Object> parameters = new HashMap<String, Object>();
+    parameters.put("genClass", this.genClass);
+    parameters.put("genPackage", this.genPackage);
+    parameters.put("genModel", this.genModel);
+    parameters.put("isJDK50", this.isJDK50);
+    parameters.put("forceDefaultCase", this.forceDefaultCase);
+    parameters.put("indentDefaultCase", this.indentDefaultCase);
+    parameters.put("_List", this._List);
+    parameters.put("hasSwitch", this.hasSwitch);
+    return parameters;
+  }
 
-	protected void method_doGenerate(final StringBuffer stringBuffer, final PatternContext ctx) throws Exception {
+  protected void method_doGenerate(final StringBuffer stringBuffer, final PatternContext ctx) throws Exception {
 
-		stringBuffer.append(TEXT_1);
-		if (genModel.useClassOverrideAnnotation()) {
-			stringBuffer.append(TEXT_2);
-		}
-		stringBuffer.append(TEXT_3);
-		stringBuffer.append(genModel.getImportedName("org.eclipse.emf.common.command.Command"));
-		stringBuffer.append(TEXT_4);
-		stringBuffer.append(genModel.getImportedName("org.eclipse.emf.edit.domain.EditingDomain"));
-		stringBuffer.append(TEXT_5);
-		stringBuffer.append(genModel.getImportedName("org.eclipse.emf.ecore.EObject"));
-		stringBuffer.append(TEXT_6);
-		stringBuffer.append(genModel.getImportedName("org.eclipse.emf.edit.command.CopyCommand.Helper"));
-		stringBuffer.append(TEXT_7);
-		stringBuffer.append(
-				genModel.getImportedName("org.polarsys.capella.common.model.copypaste.SharedInitializeCopyCommand"));
-		stringBuffer.append(TEXT_8);
-		InternalPatternContext ictx = (InternalPatternContext) ctx;
-		new Node.DataLeaf(ictx.getNode(), getClass(), "doGenerate", stringBuffer.toString());
-	}
+    stringBuffer.append(TEXT_1);
+    if (genModel.useClassOverrideAnnotation()) {
+      stringBuffer.append(TEXT_2);
+    }
+    stringBuffer.append(TEXT_3);
+    stringBuffer.append(genModel.getImportedName("org.eclipse.emf.common.command.Command"));
+    stringBuffer.append(TEXT_4);
+    stringBuffer.append(genModel.getImportedName("org.eclipse.emf.edit.domain.EditingDomain"));
+    stringBuffer.append(TEXT_5);
+    stringBuffer.append(genModel.getImportedName("org.eclipse.emf.ecore.EObject"));
+    stringBuffer.append(TEXT_6);
+    stringBuffer.append(genModel.getImportedName("org.eclipse.emf.edit.command.CopyCommand.Helper"));
+    stringBuffer.append(TEXT_7);
+    stringBuffer
+        .append(genModel.getImportedName("org.polarsys.capella.common.model.copypaste.SharedInitializeCopyCommand"));
+    stringBuffer.append(TEXT_8);
+    InternalPatternContext ictx = (InternalPatternContext) ctx;
+    new Node.DataLeaf(ictx.getNode(), getClass(), "doGenerate", stringBuffer.toString());
+  }
 }
--- releng/plugins/org.polarsys.capella.core.egf/generated/org/polarsys/capella/core/model/FactoryImplCreateAndSetName.java
+++ releng/plugins/org.polarsys.capella.core.egf/generated/org/polarsys/capella/core/model/FactoryImplCreateAndSetName.java
@@ -1,4 +1,4 @@
-//Generated with EGF 1.6.3.202110291409
+//Generated with EGF 1.6.4.202309111303
 package org.polarsys.capella.core.model;
 
 import org.eclipse.egf.common.helper.*;
@@ -9,141 +9,141 @@ import org.eclipse.egf.pattern.execution.*;
 import org.eclipse.egf.pattern.query.*;
 
 public class FactoryImplCreateAndSetName {
-	protected static String nl;
+  protected static String nl;
 
-	public static synchronized FactoryImplCreateAndSetName create(String lineSeparator) {
-		nl = lineSeparator;
-		FactoryImplCreateAndSetName result = new FactoryImplCreateAndSetName();
-		nl = null;
-		return result;
-	}
+  public static synchronized FactoryImplCreateAndSetName create(String lineSeparator) {
+    nl = lineSeparator;
+    FactoryImplCreateAndSetName result = new FactoryImplCreateAndSetName();
+    nl = null;
+    return result;
+  }
 
-	public final String NL = nl == null ? (System.getProperties().getProperty("line.separator")) : nl;
-	protected final String TEXT_1 = "\t";
-	protected final String TEXT_2 = "\t" + NL + "\t/**" + NL + "\t * Creates class and sets its name" + NL
-			+ "\t * (This method comes from a customization of the standard EMF generator)" + NL + "\t *" + NL
-			+ "\t * @param name_p : default name of created element" + NL + "\t * @generated" + NL + "\t */" + NL
-			+ "\tpublic ";
-	protected final String TEXT_3 = " create";
-	protected final String TEXT_4 = "(String name_p)" + NL + "\t{" + NL + "\t\t";
-	protected final String TEXT_5 = " ";
-	protected final String TEXT_6 = " = create";
-	protected final String TEXT_7 = "();" + NL + "\t\t";
-	protected final String TEXT_8 = ".set";
-	protected final String TEXT_9 = "(name_p);\t  " + NL + "\t\treturn ";
-	protected final String TEXT_10 = ";" + NL + "\t}" + NL;
-	protected final String TEXT_11 = NL;
+  public final String NL = nl == null ? (System.getProperties().getProperty("line.separator")) : nl;
+  protected final String TEXT_1 = "\t";
+  protected final String TEXT_2 = "\t" + NL + "\t/**" + NL + "\t * Creates class and sets its name" + NL
+      + "\t * (This method comes from a customization of the standard EMF generator)" + NL + "\t *" + NL
+      + "\t * @param name_p : default name of created element" + NL + "\t * @generated" + NL + "\t */" + NL
+      + "\tpublic ";
+  protected final String TEXT_3 = " create";
+  protected final String TEXT_4 = "(String name_p)" + NL + "\t{" + NL + "\t\t";
+  protected final String TEXT_5 = " ";
+  protected final String TEXT_6 = " = create";
+  protected final String TEXT_7 = "();" + NL + "\t\t";
+  protected final String TEXT_8 = ".set";
+  protected final String TEXT_9 = "(name_p);\t  " + NL + "\t\treturn ";
+  protected final String TEXT_10 = ";" + NL + "\t}" + NL;
+  protected final String TEXT_11 = NL;
 
-	public FactoryImplCreateAndSetName() {
-		//Here is the constructor
-		StringBuffer stringBuffer = new StringBuffer();
+  public FactoryImplCreateAndSetName() {
+    //Here is the constructor
+    StringBuffer stringBuffer = new StringBuffer();
 
-		// add initialisation of the pattern variables (declaration has been already done).
+    // add initialisation of the pattern variables (declaration has been already done).
 
-	}
+  }
 
-	public String generate(Object argument) throws Exception {
-		final StringBuffer stringBuffer = new StringBuffer();
+  public String generate(Object argument) throws Exception {
+    final StringBuffer stringBuffer = new StringBuffer();
 
-		InternalPatternContext ctx = (InternalPatternContext) argument;
-		Map<String, String> queryCtx = null;
-		IQuery.ParameterDescription paramDesc = null;
-		Node.Container currentNode = ctx.getNode();
+    InternalPatternContext ctx = (InternalPatternContext) argument;
+    Map<String, String> queryCtx = null;
+    IQuery.ParameterDescription paramDesc = null;
+    Node.Container currentNode = ctx.getNode();
 
-		List<Object> genClassList = null;
-		//this pattern can only be called by another (i.e. it's not an entry point in execution)
-		List<Object> namingAttributeList = null;
-		//this pattern can only be called by another (i.e. it's not an entry point in execution)
+    List<Object> genClassList = null;
+    //this pattern can only be called by another (i.e. it's not an entry point in execution)
+    List<Object> namingAttributeList = null;
+    //this pattern can only be called by another (i.e. it's not an entry point in execution)
 
-		for (Object genClassParameter : genClassList) {
-			for (Object namingAttributeParameter : namingAttributeList) {
+    for (Object genClassParameter : genClassList) {
+      for (Object namingAttributeParameter : namingAttributeList) {
 
-				this.genClass = (org.eclipse.emf.codegen.ecore.genmodel.GenClass) genClassParameter;
-				this.namingAttribute = (org.eclipse.emf.codegen.ecore.genmodel.GenFeature) namingAttributeParameter;
+        this.genClass = (org.eclipse.emf.codegen.ecore.genmodel.GenClass) genClassParameter;
+        this.namingAttribute = (org.eclipse.emf.codegen.ecore.genmodel.GenFeature) namingAttributeParameter;
 
-				if (preCondition(ctx)) {
-					ctx.setNode(new Node.Container(currentNode, getClass()));
-					orchestration(ctx);
-				}
+        if (preCondition(ctx)) {
+          ctx.setNode(new Node.Container(currentNode, getClass()));
+          orchestration(ctx);
+        }
 
-			}
-		}
-		ctx.setNode(currentNode);
-		if (ctx.useReporter()) {
-			ctx.getReporter().executionFinished(OutputManager.computeExecutionOutput(ctx), ctx);
-		}
+      }
+    }
+    ctx.setNode(currentNode);
+    if (ctx.useReporter()) {
+      ctx.getReporter().executionFinished(OutputManager.computeExecutionOutput(ctx), ctx);
+    }
 
-		stringBuffer.append(TEXT_11);
-		stringBuffer.append(TEXT_11);
-		return stringBuffer.toString();
-	}
+    stringBuffer.append(TEXT_11);
+    stringBuffer.append(TEXT_11);
+    return stringBuffer.toString();
+  }
 
-	public String orchestration(PatternContext ctx) throws Exception {
-		InternalPatternContext ictx = (InternalPatternContext) ctx;
+  public String orchestration(PatternContext ctx) throws Exception {
+    InternalPatternContext ictx = (InternalPatternContext) ctx;
 
-		method_body(new StringBuffer(), ictx);
+    method_body(new StringBuffer(), ictx);
 
-		if (ictx.useReporter()) {
-			Map<String, Object> parameterValues = new HashMap<String, Object>();
-			parameterValues.put("genClass", this.genClass);
-			parameterValues.put("namingAttribute", this.namingAttribute);
-			String outputWithCallBack = OutputManager.computeLoopOutput(ictx);
-			String loop = OutputManager.computeLoopOutputWithoutCallback(ictx);
-			ictx.getReporter().loopFinished(loop, outputWithCallBack, ictx, parameterValues);
-		}
-		return null;
-	}
+    if (ictx.useReporter()) {
+      Map<String, Object> parameterValues = new HashMap<String, Object>();
+      parameterValues.put("genClass", this.genClass);
+      parameterValues.put("namingAttribute", this.namingAttribute);
+      String outputWithCallBack = OutputManager.computeLoopOutput(ictx);
+      String loop = OutputManager.computeLoopOutputWithoutCallback(ictx);
+      ictx.getReporter().loopFinished(loop, outputWithCallBack, ictx, parameterValues);
+    }
+    return null;
+  }
 
-	protected org.eclipse.emf.codegen.ecore.genmodel.GenClass genClass = null;
+  protected org.eclipse.emf.codegen.ecore.genmodel.GenClass genClass = null;
 
-	public void set_genClass(org.eclipse.emf.codegen.ecore.genmodel.GenClass object) {
-		this.genClass = object;
-	}
+  public void set_genClass(org.eclipse.emf.codegen.ecore.genmodel.GenClass object) {
+    this.genClass = object;
+  }
 
-	protected org.eclipse.emf.codegen.ecore.genmodel.GenFeature namingAttribute = null;
+  protected org.eclipse.emf.codegen.ecore.genmodel.GenFeature namingAttribute = null;
 
-	public void set_namingAttribute(org.eclipse.emf.codegen.ecore.genmodel.GenFeature object) {
-		this.namingAttribute = object;
-	}
+  public void set_namingAttribute(org.eclipse.emf.codegen.ecore.genmodel.GenFeature object) {
+    this.namingAttribute = object;
+  }
 
-	public Map<String, Object> getParameters() {
-		final Map<String, Object> parameters = new HashMap<String, Object>();
-		parameters.put("genClass", this.genClass);
-		parameters.put("namingAttribute", this.namingAttribute);
-		return parameters;
-	}
+  public Map<String, Object> getParameters() {
+    final Map<String, Object> parameters = new HashMap<String, Object>();
+    parameters.put("genClass", this.genClass);
+    parameters.put("namingAttribute", this.namingAttribute);
+    return parameters;
+  }
 
-	protected void method_body(final StringBuffer stringBuffer, final PatternContext ctx) throws Exception {
+  protected void method_body(final StringBuffer stringBuffer, final PatternContext ctx) throws Exception {
 
-		//IN : GenClass genClass
-		stringBuffer.append(TEXT_1);
-		//IN : GenFeature namingAttribute
-		stringBuffer.append(TEXT_2);
-		stringBuffer.append(genClass.getTypeParameters());
-		stringBuffer.append(genClass.getImportedInterfaceName());
-		stringBuffer.append(genClass.getInterfaceTypeArguments());
-		stringBuffer.append(TEXT_3);
-		stringBuffer.append(genClass.getName());
-		stringBuffer.append(TEXT_4);
-		stringBuffer.append(genClass.getTypeParameters());
-		stringBuffer.append(genClass.getImportedInterfaceName());
-		stringBuffer.append(genClass.getInterfaceTypeArguments());
-		stringBuffer.append(TEXT_5);
-		stringBuffer.append(genClass.getSafeUncapName());
-		stringBuffer.append(TEXT_6);
-		stringBuffer.append(genClass.getName());
-		stringBuffer.append(TEXT_7);
-		stringBuffer.append(genClass.getSafeUncapName());
-		stringBuffer.append(TEXT_8);
-		stringBuffer.append(namingAttribute.getCapName());
-		stringBuffer.append(TEXT_9);
-		stringBuffer.append(genClass.getSafeUncapName());
-		stringBuffer.append(TEXT_10);
-		InternalPatternContext ictx = (InternalPatternContext) ctx;
-		new Node.DataLeaf(ictx.getNode(), getClass(), "body", stringBuffer.toString());
-	}
+    //IN : GenClass genClass
+    stringBuffer.append(TEXT_1);
+    //IN : GenFeature namingAttribute
+    stringBuffer.append(TEXT_2);
+    stringBuffer.append(genClass.getTypeParameters());
+    stringBuffer.append(genClass.getImportedInterfaceName());
+    stringBuffer.append(genClass.getInterfaceTypeArguments());
+    stringBuffer.append(TEXT_3);
+    stringBuffer.append(genClass.getName());
+    stringBuffer.append(TEXT_4);
+    stringBuffer.append(genClass.getTypeParameters());
+    stringBuffer.append(genClass.getImportedInterfaceName());
+    stringBuffer.append(genClass.getInterfaceTypeArguments());
+    stringBuffer.append(TEXT_5);
+    stringBuffer.append(genClass.getSafeUncapName());
+    stringBuffer.append(TEXT_6);
+    stringBuffer.append(genClass.getName());
+    stringBuffer.append(TEXT_7);
+    stringBuffer.append(genClass.getSafeUncapName());
+    stringBuffer.append(TEXT_8);
+    stringBuffer.append(namingAttribute.getCapName());
+    stringBuffer.append(TEXT_9);
+    stringBuffer.append(genClass.getSafeUncapName());
+    stringBuffer.append(TEXT_10);
+    InternalPatternContext ictx = (InternalPatternContext) ctx;
+    new Node.DataLeaf(ictx.getNode(), getClass(), "body", stringBuffer.toString());
+  }
 
-	public boolean preCondition(PatternContext ctx) throws Exception {
-		return true;
-	}
+  public boolean preCondition(PatternContext ctx) throws Exception {
+    return true;
+  }
 }
