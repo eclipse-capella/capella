924c6ce625c52e37fd422611aced63375558dff3
Erwann Traisnel <erwann.traisnel@obeo.fr>
2023-10-10 12:07:50 +0200

    #2737 Library wizard is now blocked when session is dirty, or a library is in a dirty state
    If unreferencing a library, warning + popup to tell the user to follow
    the procedure in the documentation
    
    Change-Id: I99550809e74e484e634d3c670ced761e86f59a4f
    Signed-off-by: Erwann Traisnel <erwann.traisnel@obeo.fr>

--- core/plugins/org.polarsys.capella.core.libraries/src/org/polarsys/capella/core/libraries/properties/ReferencesProperty.java
+++ core/plugins/org.polarsys.capella.core.libraries/src/org/polarsys/capella/core/libraries/properties/ReferencesProperty.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2006, 2020 THALES GLOBAL SERVICES.
+ * Copyright (c) 2006, 2023 THALES GLOBAL SERVICES.
  * 
  * This program and the accompanying materials are made available under the
  * terms of the Eclipse Public License 2.0 which is available at
@@ -12,17 +12,22 @@
  *******************************************************************************/
 package org.polarsys.capella.core.libraries.properties;
 
+import java.util.ArrayList;
 import java.util.Collection;
 import java.util.stream.Collectors;
 
 import org.eclipse.core.runtime.IStatus;
 import org.eclipse.core.runtime.Status;
+import org.eclipse.emf.ecore.resource.Resource;
+import org.eclipse.emf.ecore.resource.ResourceSet;
 import org.eclipse.osgi.util.NLS;
 import org.polarsys.capella.common.flexibility.properties.property.AbstractProperty;
 import org.polarsys.capella.common.flexibility.properties.schema.IEditableProperty;
 import org.polarsys.capella.common.flexibility.properties.schema.IPropertyContext;
 import org.polarsys.capella.common.libraries.IModel;
 import org.polarsys.capella.core.libraries.Activator;
+import org.polarsys.capella.core.libraries.Messages;
+import org.polarsys.capella.core.model.handler.command.CapellaResourceHelper;
 
 public class ReferencesProperty extends AbstractProperty implements IEditableProperty {
 
@@ -49,30 +54,47 @@ public class ReferencesProperty extends AbstractProperty implements IEditablePro
   @Override
   public IStatus validate(Object newValue_p, IPropertyContext context_p) {
     if (!model.getUnsavedModels().isEmpty()) {
-      String unsavedTxt = model.getUnsavedModels().stream().map(m -> m.getIdentifier().getName()).collect(Collectors.joining(", "));
-      return new Status(IStatus.ERROR, Activator.PLUGIN_ID, NLS.bind(
-          "There is other unsaved session(s) ({0}), this may lead to inconsistencies,\n You should save others sessions before manage references.", unsavedTxt));
+      String unsavedTxt = model.getUnsavedModels().stream().map(m -> m.getIdentifier().getName())
+          .collect(Collectors.joining(", "));
+      return new Status(IStatus.ERROR, Activator.PLUGIN_ID, NLS.bind(Messages.otherSessionDirtyMsg, unsavedTxt));
     }
 
     boolean unsavedModel = model.isUnsavedRootModel();
     if (unsavedModel) {
-      return new Status(IStatus.WARNING, Activator.PLUGIN_ID,
-          "The session is unsaved. Manage references will save the session.");
+      return new Status(IStatus.ERROR, Activator.PLUGIN_ID, Messages.dirtySessionMsg);
     }
 
     if (!model.getUnresolvableReferencedLibraries().isEmpty()) {
       String unresolvableLibs = model.getUnresolvableReferencedLibraries().stream()
           .map(m -> m.getIdentifier().getName()).collect(Collectors.joining(", "));
-      return new Status(IStatus.ERROR, Activator.PLUGIN_ID, NLS.bind(
-          "The following libraries are not properly referenced: ({0}). This may lead to inconsistencies.\n You should uncheck these dependencies, click OK and reference them again using the same wizard.",
-          unresolvableLibs));
+      return new Status(IStatus.ERROR, Activator.PLUGIN_ID,
+          NLS.bind(Messages.unresolvedLibrariesMsg, unresolvableLibs));
     }
-    
+
     if (!model.getCycles().isEmpty()) {
       Collection<IModel> cycle = model.getCycles().iterator().next();
-      String cycleLibTxt = cycle.stream()
-          .map(m -> m.getIdentifier().getName()).collect(Collectors.joining(" "));
-      return new Status(IStatus.ERROR, Activator.PLUGIN_ID, model.getCycles().size() + " cycles found. First one is [" + cycleLibTxt + "]");
+      String cycleLibTxt = cycle.stream().map(m -> m.getIdentifier().getName()).collect(Collectors.joining(" "));
+      return new Status(IStatus.ERROR, Activator.PLUGIN_ID,
+          NLS.bind(Messages.cyclesMsg, model.getCycles().size(), cycleLibTxt));
+    }
+
+    ResourceSet rSet = model.domain.getResourceSet();
+    boolean resourceSetDirty = false;
+    for (Resource res : rSet.getResources()) {
+      if (res.isModified() && CapellaResourceHelper.isCapellaResource(res)) {
+        resourceSetDirty = true;
+        break;
+      }
+    }
+    if (resourceSetDirty) {
+      return new Status(IStatus.ERROR, Activator.PLUGIN_ID, Messages.resourceSetDirtyMsg);
+    }
+
+    Collection<IModel> removedLibraries = new ArrayList<IModel>(model.getInitialAllReferencedLibrariesByRootModel());
+    removedLibraries.removeAll(model.getAllReferencedLibrariesByRootModel());
+
+    if (!removedLibraries.isEmpty()) {
+      return new Status(IStatus.WARNING, Activator.PLUGIN_ID, Messages.warningMsg);
     }
 
     return Status.OK_STATUS;
