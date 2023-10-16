205164d1d4afd9b5f36e7412f06ebe946bbdc96b
Erwann Traisnel <erwann.traisnel@obeo.fr>
2023-10-16 07:39:01 +0200

    #2737 JUnit on Library Wizard erros & warning
    Initialized library.ui tests
    Normalized library tests
    
    Change-Id: I1458b7c6c9cc17cfbf0360a46f3d6a56336c9b0f
    Signed-off-by: Erwann Traisnel <erwann.traisnel@obeo.fr>

--- tests/plugins/org.polarsys.capella.test.library.ui.ju/src/org/polarsys/capella/test/libraries/ui/ju/testsuites/main/LibrariesUITestSuite.java
+++ tests/plugins/org.polarsys.capella.test.library.ui.ju/src/org/polarsys/capella/test/libraries/ui/ju/testsuites/main/LibrariesUITestSuite.java
@@ -1,48 +1,39 @@
 /*******************************************************************************
- * Copyright (c) 2017, 2020 THALES GLOBAL SERVICES.
+ * Copyright (c) 2023 THALES GLOBAL SERVICES.
  * 
  * This program and the accompanying materials are made available under the
  * terms of the Eclipse Public License 2.0 which is available at
  * http://www.eclipse.org/legal/epl-2.0
  * 
  * SPDX-License-Identifier: EPL-2.0
- *
+ * 
  * Contributors:
  *    Thales - initial API and implementation
  *******************************************************************************/
-package org.polarsys.capella.test.suite.inui.ju;
+package org.polarsys.capella.test.libraries.ui.ju.testsuites.main;
 
 import java.util.ArrayList;
 import java.util.List;
 
 import org.polarsys.capella.test.framework.api.BasicTestArtefact;
 import org.polarsys.capella.test.framework.api.BasicTestSuite;
-import org.polarsys.capella.test.libraries.ui.ju.testsuites.main.LibrariesUITestSuite;
-import org.polarsys.capella.test.libraries.ju.testsuites.main.LibrariesTestSuite;
-import org.polarsys.capella.test.re.updateconnections.ju.UpdateConnectionsTestSuite;
-import org.polarsys.capella.test.recrpl.ju.testsuites.main.RecRplTestSuite;
+import org.polarsys.capella.test.libraries.ui.ju.testcases.basic.LibraryManagerWizardTestCase;
 
 import junit.framework.Test;
 
-/**
- *
- */
-public class RunInUIStep3TestSuite extends BasicTestSuite {
+public class LibrariesUITestSuite extends BasicTestSuite {
 
   /**
    * Returns the suite. This is required to unary launch this test.
    */
   public static Test suite() {
-    return new RunInUIStep3TestSuite();
+    return new LibrariesUITestSuite();
   }
 
   @Override
   protected List<BasicTestArtefact> getTests() {
     List<BasicTestArtefact> tests = new ArrayList<BasicTestArtefact>();
-    tests.add(new LibrariesTestSuite());
-    tests.add(new LibrariesUITestSuite());
-    tests.add(new RecRplTestSuite());
-    tests.add(new UpdateConnectionsTestSuite());
+    tests.add(new LibraryManagerWizardTestCase());
     return tests;
   }
 
