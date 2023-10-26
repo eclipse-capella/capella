2eab8f4aa1ad75dc7136c2148e36328c147225c4
mohammed choubby <mohammed.choubby@gmail.com>
2023-10-26 17:31:13 +0200

    #2709 PL can allocate some CE but does not propose the right similar suitable CE ports (#2735)

--- core/plugins/org.polarsys.capella.core.data.business.queries/src/org/polarsys/capella/core/business/queries/queries/cs/GetAvailable_PhysicalPort_AllocatedComponentPorts.java
+++ core/plugins/org.polarsys.capella.core.data.business.queries/src/org/polarsys/capella/core/business/queries/queries/cs/GetAvailable_PhysicalPort_AllocatedComponentPorts.java
@@ -12,13 +12,13 @@
  *******************************************************************************/
 package org.polarsys.capella.core.business.queries.queries.cs;
 
+import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Collections;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Objects;
 import java.util.Set;
-import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 import org.eclipse.emf.ecore.EObject;
@@ -27,18 +27,23 @@ import org.polarsys.capella.common.data.modellingcore.AbstractTypedElement;
 import org.polarsys.capella.common.queries.AbstractQuery;
 import org.polarsys.capella.common.queries.queryContext.IQueryContext;
 import org.polarsys.capella.core.data.capellacore.CapellaElement;
+import org.polarsys.capella.core.data.cs.AbstractPhysicalLinkEnd;
 import org.polarsys.capella.core.data.cs.Component;
 import org.polarsys.capella.core.data.cs.Part;
+import org.polarsys.capella.core.data.cs.PhysicalLink;
+import org.polarsys.capella.core.data.cs.PhysicalLinkEnd;
 import org.polarsys.capella.core.data.cs.PhysicalPort;
 import org.polarsys.capella.core.data.ctx.SystemComponent;
-import org.polarsys.capella.core.data.fa.ComponentPort;
+import org.polarsys.capella.core.data.fa.ComponentExchange;
 import org.polarsys.capella.core.data.fa.ComponentPortAllocation;
 import org.polarsys.capella.core.data.information.Port;
 import org.polarsys.capella.core.data.la.LogicalComponent;
 import org.polarsys.capella.core.data.pa.PhysicalComponent;
+import org.polarsys.capella.core.model.helpers.ComponentExchangeExt;
 import org.polarsys.capella.core.model.helpers.ComponentExt;
 import org.polarsys.capella.core.model.helpers.LogicalComponentExt;
 import org.polarsys.capella.core.model.helpers.PartExt;
+import org.polarsys.capella.core.model.helpers.PhysicalComponentExt;
 import org.polarsys.capella.core.model.helpers.PortExt;
 
 public class GetAvailable_PhysicalPort_AllocatedComponentPorts extends AbstractQuery {
@@ -68,8 +73,7 @@ public class GetAvailable_PhysicalPort_AllocatedComponentPorts extends AbstractQ
     Set<Port> currentElements = new HashSet<>();
     PhysicalPort elt = (PhysicalPort) element;
     for (AbstractTrace trace : elt.getOutgoingTraces()) {
-      if (trace instanceof ComponentPortAllocation) {
-        ComponentPortAllocation portAllocation = (ComponentPortAllocation) trace;
+      if (trace instanceof ComponentPortAllocation portAllocation) {
         if (portAllocation.getAllocatedPort() != null) {
           currentElements.add(portAllocation.getAllocatedPort());
         }
@@ -81,15 +85,53 @@ public class GetAvailable_PhysicalPort_AllocatedComponentPorts extends AbstractQ
 
   /** 
    */
-  private Set<ComponentPort> getAvailablePorts(PhysicalPort port) {
+  private Set<Port> getAvailablePorts(PhysicalPort sourcePort) {
+
+    List<PhysicalPort> targetPorts = getTheOthersPhysicalPort(sourcePort);
+    List<Component> sourceDeployedComponents = getDeployedElements(sourcePort);
+    List<Component> targetDeployedComponents = targetPorts.stream().flatMap(pp -> getDeployedElements(pp).stream())
+        .toList();
+    Set<ComponentExchange> availableElements = new HashSet<>();
+    availableElements.addAll(PhysicalComponentExt.findConnectionsBetweenPhysicalComponentes(sourceDeployedComponents,
+        targetDeployedComponents));
+    availableElements.addAll(PhysicalComponentExt.findConnectionsBetweenPhysicalComponentes(targetDeployedComponents,
+        sourceDeployedComponents));
+    Set<Port> list = new HashSet<>();
+    for (ComponentExchange ce : availableElements) {
+      Component source = ComponentExchangeExt.getSourceComponent(ce);
+      if (sourceDeployedComponents.contains(source))
+        list.add(ce.getSourcePort());
+      else
+        list.add(ce.getTargetPort());
+    }
+    return list;
+  }
+
+  private List<PhysicalPort> getTheOthersPhysicalPort(PhysicalPort port) {
+    PhysicalPort pp = port;
+    List<PhysicalPort> result = new ArrayList<>();
+    for (PhysicalLink pLink : port.getInvolvedLinks()) {
+      for (AbstractPhysicalLinkEnd linkEnd : pLink.getLinkEnds()) {
+        if (linkEnd instanceof PhysicalPort) {
+          pp = (PhysicalPort) linkEnd;
+        } else if (linkEnd instanceof PhysicalLinkEnd) {
+          pp = ((PhysicalLinkEnd) linkEnd).getPort();
+        }
+        if (!pp.equals(port))
+          result.add(pp);
+      }
+    }
+    return result;
+  }
+
+  private List<Component> getDeployedElements(PhysicalPort port) {
     Component container = PortExt.getRelatedComponent(port);
     if (container == null)
-      return Collections.emptySet();
-    Collection<Component> components = ComponentExt.getAllSubUsedComponents(container);
-    components.add(container);
-
-    Set<Component> deployedComponents = new HashSet<>();
-    for (Component component : components) {
+      return Collections.emptyList();
+    Collection<Component> sourceComponents = ComponentExt.getAllSubUsedComponents(container);
+    sourceComponents.add(container);
+    List<Component> deployedComponents = new ArrayList<>();
+    for (Component component : sourceComponents) {
       if (component instanceof SystemComponent) {
         deployedComponents.add(component);
       } else if (component instanceof LogicalComponent) {
@@ -97,8 +139,7 @@ public class GetAvailable_PhysicalPort_AllocatedComponentPorts extends AbstractQ
       } else if (component instanceof PhysicalComponent) {
         deployedComponents.add(component);
         for (AbstractTypedElement abstractTypedElement : component.getAbstractTypedElements()) {
-          if (abstractTypedElement instanceof Part) {
-            Part typedPart = (Part) abstractTypedElement;
+          if (abstractTypedElement instanceof Part typedPart) {
             Stream
                 .concat(PartExt.getSubUsedParts(typedPart).stream(),
                     PartExt.getAllDeployableElements(typedPart).stream())
@@ -109,9 +150,6 @@ public class GetAvailable_PhysicalPort_AllocatedComponentPorts extends AbstractQ
         }
       }
     }
-
-    return deployedComponents.stream().flatMap(c -> c.getContainedComponentPorts().stream())
-        .collect(Collectors.toSet());
+    return deployedComponents;
   }
-
 }
