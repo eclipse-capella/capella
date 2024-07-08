b3844fbd7e6cc34690e89c292324019b54b168b5
Glenn Plouhinec <glenn.plouhinec@obeo.fr>
2024-07-08 16:05:55 +0200

    #2884 Optimize import and update crlf
    Signed-off-by: Glenn Plouhinec <glenn.plouhinec@obeo.fr>

--- core/plugins/org.polarsys.capella.core.sirius.analysis/src/org/polarsys/capella/core/sirius/analysis/FaServices.java
+++ core/plugins/org.polarsys.capella.core.sirius.analysis/src/org/polarsys/capella/core/sirius/analysis/FaServices.java
@@ -16,7 +16,6 @@ import static org.polarsys.capella.common.helpers.cache.ModelCache.getCache;
 
 import java.util.AbstractMap;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Collection;
 import java.util.Collections;
 import java.util.HashMap;
