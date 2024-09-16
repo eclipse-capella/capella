58aa68415052e83ecbd4b265cd13456aed78aca3
Philippe DUL <philippe.dul@thalesgroup.com>
2024-09-16 13:07:49 +0000

    [releng] Bump to 7.0.1

--- core/plugins/org.polarsys.capella.core.data.migration/src/org/polarsys/capella/core/data/migration/contribution/ActivateDiagramFiltersContribution.java
+++ core/plugins/org.polarsys.capella.core.data.migration/src/org/polarsys/capella/core/data/migration/contribution/ActivateDiagramFiltersContribution.java
@@ -26,7 +26,7 @@ import org.polarsys.capella.core.sirius.analysis.helpers.DDiagramHelper;
 
 /**
  * This class activate filters for computed transitions on MSM diagrams. This contribution is only active for model
- * version 7.0.0.
+ * version 7.0.1.
  */
 public class ActivateDiagramFiltersContribution extends AbstractMigrationContribution {
 
