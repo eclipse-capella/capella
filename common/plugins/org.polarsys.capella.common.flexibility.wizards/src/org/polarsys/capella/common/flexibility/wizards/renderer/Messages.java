dbaa35adb92e36696f94b52e3b7e058e929ea98e
lgoubet <laurent.goubet@obeo.fr>
2026-03-13 15:46:23 +0100

    Add missing externalisation for flexibility wizard

--- common/plugins/org.polarsys.capella.common.flexibility.wizards/src/org/polarsys/capella/common/flexibility/wizards/renderer/Messages.java
+++ common/plugins/org.polarsys.capella.common.flexibility.wizards/src/org/polarsys/capella/common/flexibility/wizards/renderer/Messages.java
@@ -4,6 +4,8 @@ import org.eclipse.osgi.util.NLS;
 
 public class Messages extends NLS {
 	private static final String BUNDLE_NAME = Messages.class.getPackageName() + ".messages"; //$NON-NLS-1$
+	public static String BrowseRenderer_SelectElementWizard_title;
+	public static String BrowseRenderer_SelectElementWizard_message;
 	public static String BrowseRenderer_0;
 	public static String BrowseRenderer_1;
 	public static String BrowseRenderer_3;
